4da8ae9c60f88656b008f683fda3052ee8aab9c8
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 15:24:35 +0200

    Implement dummy host

--- rust/template/host/src/main.rs
+++ rust/template/host/src/main.rs
@@ -1,3 +1,18 @@
+use risc0_zkvm::{default_prover, ExecutorEnv};
+
 fn main() {
-    println!("Hello, World!");
+    // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .init();
+
+    let input: u32 = 42;
+    let _env = ExecutorEnv::builder()
+        .write(&input)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    // Obtain the default prover.
+    let _prover = default_prover();
 }
