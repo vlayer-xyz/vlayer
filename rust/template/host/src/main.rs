e084cb7dd9012e27539dfedef20007379e59ef22
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 15:44:36 +0200

    Add sol Simple interface to guest

--- rust/template/host/src/main.rs
+++ rust/template/host/src/main.rs
@@ -1,5 +1,14 @@
 use risc0_zkvm::{default_prover, ExecutorEnv};
 
+use alloy_sol_types::sol;
+
+sol! {
+    interface Simple {
+        function sum(uint256, uint256) public pure returns (uint256);
+    }
+
+}
+
 fn main() {
     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
