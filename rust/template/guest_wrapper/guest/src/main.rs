080e63aa5b275a736c902dba5b5d6b4cc72612f3
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 15:44:22 +0200

    Add sol interface for Simple to guest

--- rust/template/guest_wrapper/guest/src/main.rs
+++ rust/template/guest_wrapper/guest/src/main.rs
@@ -2,6 +2,15 @@
 
 risc0_zkvm::guest::entry!(main);
 
+use alloy_sol_types::sol;
+
+sol! {
+    interface Simple {
+        function sum(uint256, uint256) public pure returns (uint256);
+    }
+
+}
+
 fn main() {
     println!("Hello, world!")
 }
