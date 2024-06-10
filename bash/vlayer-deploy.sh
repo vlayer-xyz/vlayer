966dd1e31d4a9a24248cb7eeab157cf2aaa1264d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 13:09:55 +0200

    Add simple deployment script

---
+++ bash/vlayer-deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -uexo pipefail
+
+DEPLOYABLE_CONTRACT="Simple"
+export STABLE_DEPLOYER_PRIV="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+ROOT_PATH=$(pwd)
+
+DEPLOY_CONTRACTS_BASE=${ROOT_PATH}/script
+DEPLOYMENT_SCRIPT=${DEPLOY_CONTRACTS_BASE}/DeployVlayer.s.sol
+
+
+echo ${DEPLOYMENT_SCRIPT}
+
+function deploy_contract() {
+  results_file=$(
+    forge script ${DEPLOYMENT_SCRIPT} \
+      --rpc-url 127.0.0.1:8545 \
+      --broadcast 2>/dev/null \
+    | grep "Transactions saved to" \
+    | sed "s/^Transactions saved to: //"
+  )
+
+  jq -r ".transactions[] | select(.transactionType == \"CREATE\" and .contractName == \"${DEPLOYABLE_CONTRACT}\").contractAddress" \
+    <${results_file}
+
+}
+
+result_address=$(deploy_contract)
+
+echo Finished deploying contracts.
+echo 
+echo VLAYER_CONTRACT_ADDRESS=${result_address}
--- examples/simple/script/DeployVlayer.s.sol
+++ examples/simple/script/DeployVlayer.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Simple} from "../src/vlayer/Simple.v.sol";
 
 contract SimpleScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+
+        uint256 deployerPrivateKey = vm.envUint("STABLE_DEPLOYER_PRIV");
+        vm.startBroadcast(deployerPrivateKey);
+        Simple simple = new Simple();
+
+
     }
 }
