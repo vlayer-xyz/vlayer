de2fd624ce9998be87ae5dcc647fd0525e5260d9
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 13:37:36 +0200

    Update clone script

--- bash/vlayer-build.sh
+++ bash/vlayer-build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -uexo pipefail
 
+TMP=$(mktemp -d)
 OUTPATH=$(pwd)/out/_vlayer
 
 mkdir -p ${OUTPATH}
 
-git clone --no-checkout https://github.com/vlayer-xyz/vlayer.git ${OUTPATH}
-cd ${OUTPATH}
-git sparse-checkout init --cone
-git sparse-checkout set rust/template
-git checkout main
+git clone https://github.com/vlayer-xyz/vlayer.git ${TMP}
+cp -r ${TMP}/rust/template ${OUTPATH}/rust
