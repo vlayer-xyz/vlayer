edaae507db40652242aac63396d3d2e19403b1ad
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-10 17:03:43 +0200

    Working version with steel

--- rust/template/guest_wrapper/guest/src/main.rs
+++ rust/template/guest_wrapper/guest/src/main.rs
@@ -2,15 +2,28 @@
 
 risc0_zkvm::guest::entry!(main);
 
+use alloy_primitives::{address, Address, U256};
 use alloy_sol_types::sol;
+use risc0_steel::{config::ETH_SEPOLIA_CHAIN_SPEC, ethereum::EthEvmInput, Contract};
+use risc0_zkvm::guest::env;
 
 sol! {
     interface Simple {
-        function sum(uint256, uint256) public pure returns (uint256);
+        function sum(uint256 lhs, uint256 rhs) public pure returns (uint256);
     }
-
 }
 
+const CONTRACT: Address = address!("e7f1725e7734ce288f8367e1bb143e90bb3f0512");
+
 fn main() {
-    println!("Hello, world!")
+    let input: EthEvmInput = env::read();
+    let env = input.into_env().with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC);
+
+    let lhs = U256::from(2);
+    let rhs = U256::from(2);
+    let call = Simple::sumCall { lhs, rhs };
+
+    let returns = Contract::new(CONTRACT, &env).call_builder(&call).call();
+
+    assert!(returns._0 == U256::from(4));
 }
--- rust/template/host/src/main.rs
+++ rust/template/host/src/main.rs
@@ -1,22 +1,41 @@
+use alloy_primitives::{address, Address, U256};
 use alloy_sol_types::sol;
 use anyhow::Context;
 use guest_wrapper::GUEST_ELF;
+use risc0_steel::{config::ETH_SEPOLIA_CHAIN_SPEC, ethereum::EthEvmEnv, Contract};
 use risc0_zkvm::{default_prover, ExecutorEnv};
 
 sol! {
     interface Simple {
-        function sum(uint256, uint256) public pure returns (uint256);
+        function sum(uint256 lhs, uint256 rhs) public pure returns (uint256);
     }
-
 }
 
+const CALLER: Address = address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+const CONTRACT: Address = address!("e7f1725e7734ce288f8367e1bb143e90bb3f0512");
+
 fn main() -> anyhow::Result<()> {
+    let call: Simple::sumCall = Simple::sumCall {
+        lhs: U256::from(2),
+        rhs: U256::from(2),
+    };
     // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
 
-    let env = ExecutorEnv::builder().build().unwrap();
+    let mut env = EthEvmEnv::from_rpc("http://localhost:8545", None)?;
+    env = env.with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC);
+    let mut contract = Contract::preflight(CONTRACT, &mut env);
+    let returns = contract.call_builder(&call).from(CALLER).call()?;
+
+    let input = env.into_input()?;
+
+    let env = ExecutorEnv::builder()
+        .write(&input)
+        .unwrap()
+        .build()
+        .unwrap();
 
     let prover = default_prover();
     prover
