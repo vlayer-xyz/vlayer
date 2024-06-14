fce8fbdb03809501552e3d2ee47c1849ad41c4b2
Arkadiusz Konior <akonior@gmail.com>
2024-06-14 13:30:50 +0200

    Remove CallBuilder struct

--- rust/host/src/main.rs
+++ rust/host/src/main.rs
@@ -1,8 +1,9 @@
 use alloy_primitives::{address, Address};
+use alloy_sol_types::SolCall;
 use anyhow::Context;
 use guest_wrapper::GUEST_ELF;
 use risc0_zkvm::{default_prover, ExecutorEnv};
-use vlayer_common::CallBuilder;
+use vlayer_common::Simple::sumCall;
 use vlayer_steel::{
     config::ETH_SEPOLIA_CHAIN_SPEC,
     contract::call_builder::{evm_call, CallBuilder as SteelCallBuilder},
@@ -20,7 +21,14 @@ fn main() -> anyhow::Result<()> {
 
     let mut env = EthEvmEnv::from_rpc("http://localhost:8545", None)?;
     env = env.with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC);
-    let call = CallBuilder::build();
+
+    let call_data: Vec<u8> = vec![
+        202, 208, 137, 155, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+    ];
+    let call = <sumCall as SolCall>::abi_decode(&call_data, true).unwrap();
+
     let call_builder = SteelCallBuilder::new(CONTRACT, &call).from(CALLER);
     let _returns = evm_call(call_builder, &mut env)?;
 
