abc2eb96c9d0bf409af85a7349da2fbfd78eedf4
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-06-17 16:43:42 +0200

    Extract GuestInput

--- rust/guest_wrapper/guest/src/main.rs
+++ rust/guest_wrapper/guest/src/main.rs
@@ -7,21 +7,22 @@ use risc0_zkvm::guest::env;
 use vlayer_steel::{
     config::ETH_SEPOLIA_CHAIN_SPEC,
     contract::{call::guest_evm_call, CallTxData},
-    ethereum::EthEvmInput,
+    guest_input::GuestInput,
 };
 
 const CONTRACT: Address = address!("5fbdb2315678afecb367f032d93f642f64180aa3");
 
 fn main() {
-    let input: EthEvmInput = env::read();
-    let env = input
+    let GuestInput {
+        evm_input,
+        call_data,
+    } = env::read();
+
+    let env = evm_input
         .into_env()
         .with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC)
         .unwrap();
-
-    let raw_call_data: Vec<u8> = env::read();
-
-    let call_data = CallTxData::<()>::new_from_bytes(CONTRACT, raw_call_data);
+    let call_data = CallTxData::<()>::new_from_bytes(CONTRACT, call_data);
     let returns = guest_evm_call(call_data, &env);
 
     env::commit(&returns);
