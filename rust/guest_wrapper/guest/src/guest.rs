370a03eb26b420449f9963c071531736e776c1c8
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-06-18 10:52:33 +0200

    Extract guest file

---
+++ rust/guest_wrapper/guest/src/guest.rs
@@ -0,0 +1,25 @@
+use vlayer_steel::{
+    config::ETH_SEPOLIA_CHAIN_SPEC,
+    contract::{call::guest_evm_call, CallTxData},
+    ethereum::EthBlockHeader,
+    EvmEnv, EvmInput, StateDb,
+};
+
+pub struct Guest {
+    env: EvmEnv<StateDb, EthBlockHeader>,
+}
+
+impl Guest {
+    pub fn new(evm_input: EvmInput<EthBlockHeader>) -> Self {
+        let env = evm_input
+            .into_env()
+            .with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC)
+            .unwrap();
+
+        Guest { env }
+    }
+
+    pub fn run(self, call_data: CallTxData<()>) {
+        guest_evm_call(call_data, &self.env);
+    }
+}
--- rust/guest_wrapper/guest/src/main.rs
+++ rust/guest_wrapper/guest/src/main.rs
@@ -3,38 +3,20 @@
 risc0_zkvm::guest::entry!(main);
 
 use alloy_primitives::{address, Address};
+use guest::Guest;
 use risc0_zkvm::guest::env;
-use vlayer_steel::{
-    config::ETH_SEPOLIA_CHAIN_SPEC, contract::{call::guest_evm_call, CallTxData}, ethereum::EthBlockHeader, guest_input::GuestInput, EvmEnv, EvmInput, StateDb
-};
+use vlayer_steel::{contract::CallTxData, guest_input::GuestInput};
+
+pub mod guest;
 
 const CONTRACT: Address = address!("5fbdb2315678afecb367f032d93f642f64180aa3");
-
-struct Guest {
-    env: EvmEnv<StateDb, EthBlockHeader>
-}
-
-impl Guest {
-    pub fn new(evm_input: EvmInput<EthBlockHeader>) -> Self {
-        let env = evm_input
-        .into_env()
-        .with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC)
-        .unwrap();
-
-        Guest { env }
-    }
-
-    pub fn run(self, call_data: CallTxData<()>) {
-        guest_evm_call(call_data, &self.env);
-    }
-}
 
 fn main() {
     let GuestInput {
         evm_input,
         call_data,
     } = env::read();
-    
+
     let call_data = CallTxData::<()>::new_from_bytes(CONTRACT, call_data);
 
     let returns = Guest::new(evm_input).run(call_data);
