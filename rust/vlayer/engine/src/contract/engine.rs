df3e552eb26e398affe87183ad057069edb76cbb
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-06-19 12:20:15 +0200

    Add universal call method

--- rust/vlayer/engine/src/contract/engine.rs
+++ rust/vlayer/engine/src/contract/engine.rs
@@ -1,8 +1,15 @@
 use super::{db::WrapStateDb, new_evm, transact};
+use anyhow::anyhow;
+use revm::{
+    primitives::{
+        CfgEnvWithHandlerCfg, ExecutionResult, ResultAndState, SuccessReason, TransactTo,
+    },
+    Database, Evm,
+};
 
 #[cfg(feature = "host")]
 use crate::host::{provider::Provider, HostEvmEnv};
-use crate::{guest::Call, EvmBlockHeader, GuestEvmEnv};
+use crate::{guest::Call, EvmBlockHeader, EvmEnv, GuestEvmEnv};
 
 pub struct Engine {}
 
@@ -27,5 +34,44 @@ impl Engine {
         let evm = new_evm(WrapStateDb::new(&env.db), env.cfg_env.clone(), &env.header);
         #[allow(clippy::unwrap_used)]
         transact(evm, tx).unwrap()
+    }
+
+    pub fn call<DB, H>(tx: &Call, env: &mut EvmEnv<DB, H>) -> anyhow::Result<Vec<u8>>
+    where
+        DB: Database,
+        H: EvmBlockHeader,
+    {
+        let cfg: CfgEnvWithHandlerCfg = env.cfg_env.clone();
+
+        let evm = Evm::builder()
+            .with_db(&mut env.db)
+            .with_cfg_env_with_handler_cfg(cfg)
+            .modify_block_env(|blk_env| env.header.fill_block_env(blk_env))
+            .build();
+
+        Self::transact(evm, tx)
+    }
+
+    fn transact<DB>(mut evm: Evm<'_, (), DB>, tx: &Call) -> anyhow::Result<Vec<u8>>
+    where
+        DB: Database,
+    {
+        let tx_env = evm.tx_mut();
+        tx_env.caller = tx.caller;
+        tx_env.transact_to = TransactTo::call(tx.to);
+        tx_env.data = tx.data.clone().into();
+
+        let ResultAndState { result, .. } = evm
+            .transact_preverified()
+            .map_err(|_err| anyhow!("Transact error"))?;
+        let ExecutionResult::Success { reason, output, .. } = result else {
+            return Err(anyhow!("Call did not return: "));
+        };
+
+        if reason != SuccessReason::Return {
+            return Err(anyhow!("Call did not return: "));
+        }
+
+        Ok(output.into_data().into())
     }
 }
