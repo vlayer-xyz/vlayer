5e5063ffaac60b98fa76462cf2c384eb7080150c
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-19 21:45:10 +0200

    Fix tests to pass JSON  data

--- rust/server/src/handlers/hello.rs
+++ rust/server/src/handlers/hello.rs
@@ -1,10 +1,18 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::json::AppJson;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct UserParams {
     name: String,
+}
+
+impl From<&str> for UserParams {
+    fn from(name: &str) -> Self {
+        UserParams {
+            name: name.to_string(),
+        }
+    }
 }
 
 pub(crate) async fn hello(AppJson(params): AppJson<UserParams>) -> String {
