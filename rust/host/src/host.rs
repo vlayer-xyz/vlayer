8de9cd2f1645c6fe826706c0100a3a62a2534047
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-06-20 10:18:34 +0200

    Export consts

--- rust/host/src/host.rs
+++ rust/host/src/host.rs
@@ -1,5 +1,5 @@
 use alloy_primitives::Sealable;
-use anyhow::{anyhow, Error};
+use anyhow::anyhow;
 use ethers_providers::Provider as OGEthersProvider;
 use ethers_providers::{Http, RetryClient};
 use guest_wrapper::GUEST_ELF;
@@ -9,9 +9,15 @@ use vlayer_engine::{
     config::ETH_SEPOLIA_CHAIN_SPEC,
     contract::engine::Engine,
     ethereum::EthEvmEnv,
-    host::{db::ProofDb, provider::{EthersProvider, Provider}},
+    host::{
+        db::ProofDb,
+        provider::{EthersProvider, Provider},
+    },
     EvmEnv,
 };
+
+const MAX_RETRY: u32 = 3;
+const INITIAL_BACKOFF: u64 = 500;
 
 pub type EthersClient = OGEthersProvider<RetryClient<Http>>;
 
@@ -46,10 +52,11 @@ impl From<anyhow::Error> for HostError {
 }
 
 impl Host {
-    pub fn try_new(url: &str) -> anyhow::Result<Self> {
-        let client = EthersClient::new_client(url, MAX_RETRY, INITIAL_BACKOFF)?;
+    pub fn try_new(url: &str) -> Result<Self, HostError> {
+        let client = EthersClient::new_client(url, MAX_RETRY, INITIAL_BACKOFF)
+            .map_err(|err| anyhow!(err))?;
         let provider = EthersProvider::new(client);
-        let block_number = provider.get_block_number()?;
+        let block_number = provider.get_block_number().map_err(|err| anyhow!(err))?;
         let header = provider.get_block_header(block_number).unwrap().unwrap();
         let db = ProofDb::new(provider, block_number);
         let env = EvmEnv::new(db, header.seal_slow()).with_chain_spec(&ETH_SEPOLIA_CHAIN_SPEC)?;
