50a530380def6b75fef0ab5e11666a47c37f132d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-20 16:23:56 +0200

    Implement proving endpoint with tests

---
+++ rust/server/src/handlers/prove.rs
@@ -0,0 +1,74 @@
+use alloy_primitives::Address;
+use serde::{Deserialize, Serialize};
+
+use crate::{error::AppError, json::AppJson};
+
+#[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct ProveArgs {
+    from: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ProveResult {
+    pub result: String,
+}
+
+pub(crate) async fn prove(
+    AppJson(params): AppJson<ProveArgs>,
+) -> Result<AppJson<ProveResult>, AppError> {
+    let from: Address = params
+        .from
+        .parse()
+        .map_err(|err| AppError::InvalidAddress {
+            field: "from".to_string(),
+            error: err,
+        })?;
+    Ok(AppJson(ProveResult {
+        result: format!("I am {from}!"),
+    }))
+}
+
+#[cfg(test)]
+mod test {
+    use alloy_primitives::hex::FromHexError;
+
+    use crate::{error::AppError, json::AppJson};
+
+    use super::{prove, ProveArgs};
+
+    #[tokio::test]
+    async fn success() -> anyhow::Result<()> {
+        let actual = prove(AppJson(ProveArgs {
+            from: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f".to_string(),
+        }))
+        .await?
+        .0;
+
+        assert_eq!(
+            actual.result,
+            "I am 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f!".to_string()
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn invalid_from_address() -> anyhow::Result<()> {
+        let actual_err = prove(AppJson(ProveArgs {
+            from: "0x".to_string(),
+        }))
+        .await
+        .unwrap_err();
+
+        assert_eq!(
+            actual_err,
+            AppError::InvalidAddress {
+                field: "from".to_string(),
+                error: FromHexError::InvalidStringLength,
+            }
+        );
+
+        Ok(())
+    }
+}
