a43a7e1f97195ca7e6f59b3a827e8fad2eba936b
Artur Chmaro <artur@vlayer.xyz>
2024-06-24 12:59:38 +0200

    forge install: openzeppelin-contracts
    v5.0.2

--- examples/airdrop/src/Airdrop.sol
+++ examples/airdrop/src/Airdrop.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 interface IERC721 {
     function ownerOf(uint256 tokenId) external view returns (address owner);
 }
 
-contract Airdrop {
+contract Airdrop is ERC721("GameItem", "ITM") {
     address public targetNftAddr;
+    uint256 public lastClaimedId = 0;
 
     mapping(uint256 => bool) public claimed;
 
@@ -15,12 +18,15 @@ contract Airdrop {
     } 
 
     function claim(uint tokenId) public {
-        require(!claimed[tokenId], "NFT already claimed");
         require(
             IERC721(targetNftAddr).ownerOf(tokenId) == msg.sender, 
             "You are not the owner of the given NFT"
-        );
+        ); // check if caller is the owner of the given NFT
+        require(!claimed[tokenId], "NFT already claimed"); // check if given NFT is already claimed
+
 
         claimed[tokenId] = true;
+        lastClaimedId += 1;
+        _mint(msg.sender, lastClaimedId); // deliver Airdrop NFT as reward
     }
 }
--- examples/airdrop/test/Airdrop.t.sol
+++ examples/airdrop/test/Airdrop.t.sol
@@ -2,23 +2,46 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Airdrop } from "../src/Airdrop.sol";
 
+contract TargetNft is ERC721 {
+    constructor() ERC721("GameItem", "ITM") {}
+
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+}
+
 contract AirdropTest is Test {
+    TargetNft public exampleNft;
     Airdrop public aidrop;
+    
+    address public alice = address(0x1);
+    address public joe = address(0x2);
 
     function setUp() public {
-        aidrop = new Airdrop();
-        aidrop.setNumber(0);
+        exampleNft = new TargetNft();
+        aidrop = new Airdrop(address(exampleNft));
     }
 
-    function test_Increment() public {
-        aidrop.increment();
-        assertEq(aidrop.number(), 1);
+    function test_targetNftAddr() public view {
+        assertEq(aidrop.targetNftAddr(), address(exampleNft));
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        aidrop.setNumber(x);
-        assertEq(aidrop.number(), x);
+    function test_claim() public {
+        vm.startPrank(alice);
+        exampleNft.mint(alice, 1);
+        aidrop.claim(1);
+        assertEq(aidrop.ownerOf(1), alice);
+        vm.stopPrank();
+    }
+
+    function test_claim_reverts() public {
+        vm.startPrank(alice);
+        exampleNft.mint(joe, 1);
+        vm.expectRevert(bytes("You are not the owner of the given NFT"));
+        aidrop.claim(1);
+        vm.stopPrank();
     }
 }
