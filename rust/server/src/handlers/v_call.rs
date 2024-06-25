03cc1d6422071be5e90505341b3f6dbbefab78f9
Anna Jabłonowska <ania@vlayer.xyz>
2024-06-25 16:40:57 +0200

    Add data field to Call structure

--- rust/server/src/handlers/v_call.rs
+++ rust/server/src/handlers/v_call.rs
@@ -1,4 +1,7 @@
-use crate::{error::AppError, utils::parse_address_field};
+use crate::{
+    error::AppError,
+    utils::{parse_address_field, parse_hex_field},
+};
 use alloy_chains::Chain;
 use alloy_primitives::{BlockNumber, ChainId};
 use host::Call as HostCall;
@@ -9,6 +12,7 @@ use serde::{Deserialize, Serialize};
 pub struct Call {
     caller: String,
     to: String,
+    data: String,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -30,7 +34,7 @@ impl TryFrom<Call> for HostCall {
         Ok(Self {
             caller: parse_address_field("caller", value.caller)?,
             to: parse_address_field("to", value.to)?,
-            data: Vec::new(),
+            data: parse_hex_field("data", value.data)?,
         })
     }
 }
@@ -46,8 +50,8 @@ pub(crate) async fn call(params: (Call, CallContext)) -> Result<CallResult, AppE
 
     Ok(CallResult {
         result: format!(
-            "Call: caller {} to {}. Context: block {} chain {}.",
-            call.caller, call.to, context.block_no, context.chain_id
+            "Call: caller {} to {} with data {:?}. Context: block {} chain {}.",
+            call.caller, call.to, call.data, context.block_no, context.chain_id
         ),
     })
 }
@@ -60,6 +64,7 @@ mod test {
 
     const FROM: &str = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
     const TO: &str = "0x7Ad53bbA1004e46dd456316912D55dBc5D311a03";
+    const DATA: &str = "0x0000";
     const INVALID_ADDRESS: &str = "0x";
 
     #[tokio::test]
@@ -68,6 +73,7 @@ mod test {
             Call {
                 caller: FROM.to_string(),
                 to: TO.to_string(),
+                data: DATA.to_string(),
             },
             CallContext {
                 block_no: 0,
@@ -78,7 +84,8 @@ mod test {
 
         assert_eq!(
             actual.result,
-            format!("Call: caller {FROM} to {TO}. Context: block 0 chain 1.").to_string()
+            format!("Call: caller {FROM} to {TO} with data [0, 0]. Context: block 0 chain 1.")
+                .to_string()
         );
 
         Ok(())
@@ -90,6 +97,7 @@ mod test {
             Call {
                 caller: INVALID_ADDRESS.to_string(),
                 to: TO.to_string(),
+                data: DATA.to_string(),
             },
             CallContext {
                 block_no: 0,
@@ -113,6 +121,7 @@ mod test {
             Call {
                 caller: FROM.to_string(),
                 to: INVALID_ADDRESS.to_string(),
+                data: DATA.to_string(),
             },
             CallContext {
                 block_no: 0,
@@ -125,6 +134,31 @@ mod test {
         assert_eq!(
             actual_err.to_string(),
             "Invalid field `to`: Invalid string length `0x`"
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn invalid_data() -> anyhow::Result<()> {
+        const INVALID_DATA: &str = "xx";
+        let actual_err = call((
+            Call {
+                caller: FROM.to_string(),
+                to: TO.to_string(),
+                data: INVALID_DATA.to_string(),
+            },
+            CallContext {
+                block_no: 0,
+                chain_id: mainnet_chain_id(),
+            },
+        ))
+        .await
+        .unwrap_err();
+
+        assert_eq!(
+            actual_err.to_string(),
+            "Invalid field `data`: Invalid prefix `xx`"
         );
 
         Ok(())
