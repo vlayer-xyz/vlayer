5f2d94ac37fe9c397c3cbae2586f8692cfc3fb66
Artur Chmaro <artur@vlayer.xyz>
2024-06-25 11:49:12 +0200

    NaiveAirdrop example (no privacy & no vlayer magic)

--- examples/airdrop/test/NaiveAirdrop.t.sol
+++ examples/airdrop/test/NaiveAirdrop.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { Airdrop } from "../src/Airdrop.sol";
+import { NaiveAirdrop } from "../src/NaiveAirdrop.sol";
 
 contract TargetNft is ERC721 {
     constructor() ERC721("GameItem", "ITM") {}
@@ -13,16 +13,16 @@ contract TargetNft is ERC721 {
     }
 }
 
-contract AirdropTest is Test {
+contract NaiveAirdropTest is Test {
     TargetNft public exampleNft;
-    Airdrop public aidrop;
+    NaiveAirdrop public aidrop;
     
     address public alice = address(0x1);
     address public joe = address(0x2);
 
     function setUp() public {
         exampleNft = new TargetNft();
-        aidrop = new Airdrop(address(exampleNft));
+        aidrop = new NaiveAirdrop(address(exampleNft));
     }
 
     function test_targetNftAddr() public view {
