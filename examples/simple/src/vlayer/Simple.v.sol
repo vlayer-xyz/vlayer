383e4713305516a0bc465744941ff11411accab4
Piotr Żelazko <piotr@vlayer.xyz>
2024-06-26 14:49:26 +0200

    test: add simple proof verification test

--- examples/simple/src/vlayer/Simple.v.sol
+++ examples/simple/src/vlayer/Simple.v.sol
@@ -2,7 +2,19 @@
 pragma solidity ^0.8.13;
 
 contract Simple {
+
+    uint256 public latestSum;
+
     function sum(uint256 lhs, uint256 rhs) public pure returns (uint256) {
         return lhs + rhs;
     }
+
+
+    function updateSum(bytes calldata _proof, uint256 _sum, bytes32 _journalHash) public {
+
+        assert(keccak256(_proof) == _journalHash);
+        
+        latestSum = _sum;
+    }
+
 }
