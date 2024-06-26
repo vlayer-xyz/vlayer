ac3c9170d60ffc0bdd4e0ae40e095893e83dd74f
Stanisław Durka <stas.durka@gmail.com>
2024-06-26 14:29:48 +0200

    Implement basic CLI that calls server (#73)
    * CLI that calls server

---
+++ rust/cli/src/main.rs
@@ -0,0 +1,31 @@
+use clap::{Parser, Subcommand};
+use server::server::serve;
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Serve,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Serve => {
+            println!("running 'vlayer serve'");
+            serve().await
+        }
+        Commands::Init => {
+            println!("running 'vlayer init'");
+            Ok(())
+        }
+    }
+}
---
+++ rust/server/src/lib.rs
@@ -0,0 +1,9 @@
+mod error;
+mod handlers;
+mod json_rpc;
+mod layers;
+pub mod server;
+#[cfg(test)]
+mod test_helpers;
+mod trace;
+mod utils;
