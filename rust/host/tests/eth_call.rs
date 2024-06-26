2c93933eb066356a75a165a71eb241a1399f9f39
Leonid Logvinov <logvinov.leon@gmail.com>
2024-06-26 16:01:07 +0200

    Move e2e tests from engine to host

--- rust/host/tests/eth_call.rs
+++ rust/host/tests/eth_call.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![cfg(feature = "host")]
-
 use alloy_primitives::{address, b256, uint, Address, Sealable, U256};
 use alloy_sol_types::{sol, SolCall};
 use std::fmt::Debug;
