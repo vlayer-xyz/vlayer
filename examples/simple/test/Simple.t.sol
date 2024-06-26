8f080103344b2ee6603bbd6fe88cacea08531173
Piotr Żelazko <piotr@vlayer.xyz>
2024-06-26 14:49:26 +0200

    test: add simple proof verification test

--- examples/simple/test/Simple.t.sol
+++ examples/simple/test/Simple.t.sol
@@ -5,13 +5,21 @@ import {Test, console} from "forge-std/Test.sol";
 import {Simple} from "../src/vlayer/Simple.v.sol";
 
 contract SimpleTest is Test {
+
     Simple public simple;
+
+    bytes proof; 
+    bytes32 journalHash;
 
     function setUp() public {
         simple = new Simple();
+        proof = "hello world";
+        journalHash = keccak256(proof);
     }
 
-    function test_Sum() public view {
-        assertEq(simple.sum(2, 2), 4);
+
+    function test_updateSum() public {
+        simple.updateSum(proof, 4, journalHash);
+        assertEq(simple.latestSum(), 4);
     }
 }
