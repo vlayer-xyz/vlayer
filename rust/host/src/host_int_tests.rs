bc7af334be1e5b55bfc7f5dbc5b56f8b956a40d3
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-06-27 16:44:21 +0200

    Extract NULL_RPC_URL variable, return Result from tests

--- rust/host/src/host_int_tests.rs
+++ rust/host/src/host_int_tests.rs
@@ -3,12 +3,16 @@ mod test {
     use alloy_primitives::{address, uint, Address};
     use alloy_sol_types::{sol, SolCall};
     use host::Call;
-    use vlayer_engine::{config::SEPOLIA_ID, host::provider::EthFileProvider};
+    use vlayer_engine::config::SEPOLIA_ID;
     use IERC20::balanceOfCall;
 
-    use crate::host::{Host, HostConfig};
+    use crate::{
+        host::{Host, HostConfig},
+        provider::EthFileProvider,
+    };
 
     const RPC_CACHE_FILE: &str = "testdata/rpc_cache.json";
+    const NULL_RPC_URL: &str = "a null url value because url is not needed in tests";
 
     const ERC20_TEST_CONTRACT: Address = address!("dAC17F958D2ee523a2206206994597C13D831ec7"); // USDT
     const ERC20_TEST_BLOCK_NO: u64 = 19493153;
@@ -27,7 +31,7 @@ mod test {
     }
 
     #[test]
-    fn erc20_balance_of() {
+    fn erc20_balance_of() -> anyhow::Result<()> {
         let call = IERC20::balanceOfCall {
             account: address!("F977814e90dA44bFA03b6295A0616a897441aceC"), // Binance 8
         };
@@ -37,15 +41,14 @@ mod test {
             data: call.abi_encode(),
         };
 
-        let test_provider = EthFileProvider::from_file(&RPC_CACHE_FILE.into()).unwrap();
+        let test_provider = EthFileProvider::from_file(&RPC_CACHE_FILE.into())?;
         let host = Host::try_new_with_provider(
             test_provider,
-            HostConfig::new("", SEPOLIA_ID, ERC20_TEST_BLOCK_NO),
-        )
-        .unwrap();
+            HostConfig::new(NULL_RPC_URL, SEPOLIA_ID, ERC20_TEST_BLOCK_NO),
+        )?;
         let host_result =
-            balanceOfCall::abi_decode_returns(&host.run(call).unwrap().evm_call_result, false)
-                .unwrap();
-        assert_eq!(host_result._0, uint!(3000000000000000_U256));
+            balanceOfCall::abi_decode_returns(&host.run(call)?.evm_call_result, false)?;
+        assert_eq!(host_result._0, uint!(3_000_000_000_000_000_U256));
+        Ok(())
     }
 }
