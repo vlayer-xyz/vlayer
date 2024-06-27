9cb15a785fd0d93b33258ced7a937c033690040e
Piotr Żelazko <piotr@vlayer.xyz>
2024-06-27 14:28:37 +0200

    test: add first journal encoding test

--- examples/simple/src/Simple.sol
+++ examples/simple/src/Simple.sol
@@ -7,17 +7,33 @@ contract Simple {
 
     IRiscZeroVerifier verifier;
     uint256 public latestSum;
+    
+    struct Commitment {
+        uint16 offset;
+        uint32 length;
+        uint16 version;
+        uint64 chainId;
+        uint256 blockNumber; // Block number at which the commitment was made.
+        bytes32 blockHash; // Hash of the block at the specified block number.
+        bytes seal;
+    }
 
     constructor(IRiscZeroVerifier _verifier) {
         verifier = _verifier;
     }
 
-    function updateSum(bytes32 proof, bytes calldata journal, uint256 sum) public {
-
-        assert(proof == keccak256(journal));
-        assert(uint(uint8(journal[0])) == sum);
+    function updateSum(Commitment memory commitment, uint256 sum, bytes32 journalHash) public {
+        _verify(commitment, sum, journalHash);
 
         latestSum = sum;
+    }
+
+    function _verify(Commitment memory commitment, uint256 sum, bytes32 journalHash) private pure {
+
+        bytes32 computedJournalHash = keccak256(abi.encode(commitment, sum));
+
+        // assert(journalHash == computedJournalHash);
+       
     }
 
 }
--- examples/simple/test/Simple.t.sol
+++ examples/simple/test/Simple.t.sol
@@ -10,24 +10,30 @@ contract SimpleTest is Test {
     Simple public simple;
     RiscZeroMockVerifier public verifier;
 
-    bytes proof;
-    bytes journal;
-    bytes32 journalHash;
-
     function setUp() public {
         verifier = new RiscZeroMockVerifier(bytes4(0));
         simple = new Simple(verifier);
-
-        journal = new bytes(1);
-        journal[0] = 0x04;
-
-        journalHash = keccak256(journal);
-
     }
 
     function test_updateSum() public {
-        simple.updateSum(journalHash, journal, 4);
-        assertEq(simple.latestSum(), 4);
+
+        Simple.Commitment memory commitment = Simple.Commitment({
+            offset: 0,
+            length: 0,
+            version: 1,
+            chainId: 11155111,
+            blockNumber: 2,
+            blockHash: bytes32(0xcbbeae20657c38f6ae82403a1c5d4e7b27142af11b02bde8bf1e3e93878e451f),
+            seal: new bytes(0)
+        });
+        uint256 sum = 3;
+      
+        // journal value taken from host execution
+        uint8[316] memory journal = [78, 0, 0, 0, 248, 0, 0, 0, 76, 0, 0, 0, 234, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 1, 0, 0, 0, 131, 0, 0, 0, 170, 0, 0, 0, 54, 0, 0, 0, 167, 0, 0, 0, 2, 0, 0, 0, 160, 0, 0, 0, 203, 0, 0, 0, 190, 0, 0, 0, 174, 0, 0, 0, 32, 0, 0, 0, 101, 0, 0, 0, 124, 0, 0, 0, 56, 0, 0, 0, 246, 0, 0, 0, 174, 0, 0, 0, 130, 0, 0, 0, 64, 0, 0, 0, 58, 0, 0, 0, 28, 0, 0, 0, 93, 0, 0, 0, 78, 0, 0, 0, 123, 0, 0, 0, 39, 0, 0, 0, 20, 0, 0, 0, 42, 0, 0, 0, 241, 0, 0, 0, 27, 0, 0, 0, 2, 0, 0, 0, 189, 0, 0, 0, 232, 0, 0, 0, 191, 0, 0, 0, 30, 0, 0, 0, 62, 0, 0, 0, 147, 0, 0, 0, 135, 0, 0, 0, 142, 0, 0, 0, 69, 0, 0, 0, 31, 0, 0, 0, 128, 0, 0, 0, 224, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 128, 0, 0, 0, 3, 0, 0, 0];
+        bytes32 journalHash = keccak256(abi.encode(journal));
+
+        simple.updateSum(commitment, sum, journalHash);
+        assertEq(simple.latestSum(), 3);
     }
 
 }
