a1b6428ff81646627711446b5223a28331624e6b
Piotr Żelazko <piotr@vlayer.xyz>
2024-06-27 17:06:44 +0200

    feat: build ImageId.sol contract from guest

--- rust/guest_wrapper/build.rs
+++ rust/guest_wrapper/build.rs
@@ -1,4 +1,16 @@
 fn main() {
     #[cfg(not(clippy))]
-    risc0_build::embed_methods();
+    {
+        use risc0_build_ethereum::generate_solidity_files;
+        use std::env;
+
+        // Generate Solidity source files for use with Forge.
+        let out_dir = env::var_os("OUT_DIR").unwrap().into_string().unwrap();
+        let solidity_opts = risc0_build_ethereum::Options::default()
+            .with_image_id_sol_path(format!("{out_dir}/ImageId.sol"))
+            .with_elf_sol_path(format!("{out_dir}/Elf.sol"));
+
+        let guests = risc0_build::embed_methods();
+        generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
+    }
 }
