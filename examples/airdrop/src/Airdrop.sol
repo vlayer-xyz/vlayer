75a6409eefdcad8841d2af609a124223ef608d11
Artur Chmaro <artur@vlayer.xyz>
2024-06-27 09:08:47 +0200

    cleanup

--- examples/airdrop/src/Airdrop.sol
+++ examples/airdrop/src/Airdrop.sol
@@ -1,17 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
 import { VlayerVerifier } from "@vlayer/VlayerVerifier.sol";
 
-address constant NFT_OWNERSHIP_VLAYER_CONTRACT = 0x1744ac92e0ff310ff836bb68d56d4159e37d0bdf;
+address constant NFT_OWNERSHIP_VLAYER_CONTRACT = 0x1744aC92e0Ff310Ff836bB68d56D4159E37D0BdF;
 
 // This contract is executed on-chain (Ethereum Mainnet, Arbitrum, Base, etc.)
 
+
+interface IAwesomeToken {
+  function transfer(address to, uint256 amount) external;
+}
+
 contract Airdrop is VlayerVerifier {
-  address awesomeTokenAddr = 0x510848be71eac101a4eb871c6436178e52210646;
+  address awesomeTokenAddr = 0x510848bE71Eac101a4Eb871C6436178e52210646;
   mapping (address => bool) public withdrawn;
 
-  function drop(Proof proof, address sender) onlyVerified(NFT_OWNERSHIP_CONTRACT) {
+  function claim(Proof calldata proof, address sender) public onlyVerified(NFT_OWNERSHIP_VLAYER_CONTRACT) {
     require(withdrawn[sender] == false, "Already withdrawn");
-    
-    AwesomeToken.transfer(sender, 1000);
+
+    IAwesomeToken(awesomeTokenAddr).transfer(sender, 1000);
     withdrawn[sender] = true;
   }
 }
--- examples/airdrop/src/vlayer/NftOwnership.v.sol
+++ examples/airdrop/src/vlayer/NftOwnership.v.sol
@@ -8,25 +8,29 @@ pragma solidity ^0.8.13;
 import { VlayerProver } from "@vlayer/VlayerProver.v.sol";
 
 interface IERC721 {
-    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function balanceOf(address owner) external view returns (uint256 balance);
 }
 
 
-address public constant BYAC_NFT_ADDR = 0xb123123123;
+address constant BYAC_NFT_ADDR = 0x1744aC92e0Ff310Ff836bB68d56D4159E37D0BdF;
 
 contract NftOwnership is VlayerProver  {
 
-    function main() public returns (address) {  
-      // 🔥 Teleport to chain on which the verification is happening
-      setChainId(1); 
-      // some block no in future 
-      setBlockNumber(21_000_000); 
-      
+    function require_byac_nft() public view {
       // Terminate proving if NFT is not owned by the prover
       require(
         IERC721(BYAC_NFT_ADDR).balanceOf(msg.sender) > 0, 
         "You are not owning any specified NFT"
       );
+    } 
+
+    function main() public returns (address) {  
+      // 🔥 Teleport to chain on which the verification is happening
+      setChainId(1); 
+      // set block no in future 
+      setBlockNumber(21_000_000); 
+      
+      require_byac_nft();
 
       // anything returned here would be visible to the public
       return (msg.sender); 
