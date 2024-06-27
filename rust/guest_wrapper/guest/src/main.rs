b749770b8914475508d849ecaa3af625c293d198
Arkadiusz Konior <akonior@gmail.com>
2024-06-27 14:27:57 +0200

    remove rlp encoding

--- rust/guest_wrapper/guest/src/main.rs
+++ rust/guest_wrapper/guest/src/main.rs
@@ -12,7 +12,6 @@ fn main() {
     let Input { evm_input, call } = env::read();
 
     let output = Guest::new(evm_input).run(call);
-    let rlp_output = alloy_rlp::encode(output);
 
-    env::commit(&rlp_output);
+    env::commit(&output);
 }
