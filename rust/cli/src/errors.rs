d1e7c25375ba2353b2cab1c9441a2a7d930b053c
Stanisław Durka <stanislaw@vlayer.xyz>
2024-06-28 09:50:42 +0200

    extract errors from main

---
+++ rust/cli/src/errors.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum CLIError {
+    #[error("Command execution failed: {0}")]
+    CommandExecutionError(#[from] std::io::Error),
+    #[error("Invalid UTF-8 sequence: {0}")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("Git command failed: {0}")]
+    GitError(String),
+}
