08640d4ead63c4ad8a7932b1bff9bbb16df2f787
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-01 15:35:43 +0200

    Improve guest output decoding error handling

--- rust/vlayer/engine/src/io.rs
+++ rust/vlayer/engine/src/io.rs
@@ -1,6 +1,7 @@
 use alloy_primitives::Address;
 use alloy_sol_types::SolValue;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use crate::ethereum::EthEvmInput;
 use crate::ExecutionCommitment;
@@ -28,27 +29,33 @@ impl Default for Call {
     }
 }
 
+#[derive(Error, Debug)]
+pub enum GuestOutputError {
+    #[error("Cannot decode execution commitment: {0}")]
+    CannonDecodeExecutionCommitment(String),
+}
+
 pub struct GuestOutput {
     pub execution_commitment: ExecutionCommitment,
     pub evm_call_result: Vec<u8>,
 }
 
 impl GuestOutput {
-    pub fn from_outputs(host_output: &[u8], guest_output: &[u8]) -> Self {
+    pub fn from_outputs(host_output: &[u8], guest_output: &[u8]) -> Result<Self, GuestOutputError> {
         let execution_commitment_len = guest_output.len() - host_output.len();
 
         let (execution_commitment_abi_encoded, evm_call_result_abi_encoded) =
             guest_output.split_at(execution_commitment_len);
 
-        assert_eq!(host_output, evm_call_result_abi_encoded);
-
         let execution_commitment =
-            ExecutionCommitment::abi_decode(execution_commitment_abi_encoded, true)
-                .expect("Cannot decode execution commitment");
-        Self {
+            ExecutionCommitment::abi_decode(execution_commitment_abi_encoded, true).map_err(
+                |err| GuestOutputError::CannonDecodeExecutionCommitment(err.to_string()),
+            )?;
+
+        Ok(Self {
             execution_commitment,
             evm_call_result: evm_call_result_abi_encoded.to_vec(),
-        }
+        })
     }
 }
 
