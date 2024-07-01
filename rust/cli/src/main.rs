57a651405f4a609138f92823ee6b95a9d0a62c2d
Stanisław Durka <stanislaw@vlayer.xyz>
2024-07-01 12:06:44 +0200

    fixed tests & moved find_foundry to path.rs

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,14 +1,10 @@
 use crate::errors::CLIError;
-use crate::misc::path::find_git_root;
+use crate::misc::path::find_foundry_root;
 use clap::{Parser, Subcommand};
 use server::server::serve;
-use std::path::PathBuf;
-
-#[cfg(test)]
-use std::path::Path;
 
 pub mod errors;
-pub mod misc;
+mod misc;
 
 #[cfg(test)]
 mod test_utils;
@@ -56,101 +52,4 @@ async fn run() -> Result<(), CLIError> {
         }
     }
     Ok(())
-}
-
-fn find_foundry_root() -> Result<PathBuf, CLIError> {
-    let current_dir = std::env::current_dir()?;
-    find_foundry_root_from(&current_dir)
-}
-
-fn find_foundry_root_from(start: &PathBuf) -> Result<PathBuf, CLIError> {
-    let git_root = find_git_root(start)?;
-
-    let mut current = start.as_path().canonicalize()?;
-
-    // traverse as long as we're in the current git repository
-    while current.starts_with(&git_root) {
-        let file_path = current.join("foundry.toml");
-        if file_path.is_file() {
-            return Ok(current.to_path_buf());
-        }
-        if let Some(parent) = current.parent() {
-            current = parent.to_path_buf();
-        } else {
-            break;
-        }
-    }
-    Err(CLIError::NoFoundryError)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::test_utils::create_temp_git_repo;
-
-    fn change_directory(new_dir: &Path) -> std::io::Result<()> {
-        std::env::set_current_dir(new_dir)?;
-        Ok(())
-    }
-
-    #[test]
-    fn test_find_foundry_root_from_nonexistent_path() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let nonexistent_path = temp_dir.path().join("not_a_real_dir");
-        let result = find_foundry_root_from(&nonexistent_path);
-        assert!(matches!(
-            result.unwrap_err(),
-            CLIError::CommandExecutionError(err) if err.kind() == std::io::ErrorKind::NotFound
-        ));
-    }
-
-    #[test]
-    fn test_find_foundry_root() {
-        let temp_dir = create_temp_git_repo();
-        let file_path = temp_dir.path().join("foundry.toml");
-        std::fs::File::create(file_path).unwrap();
-
-        change_directory(temp_dir.path()).unwrap();
-        let result = find_foundry_root();
-
-        assert_eq!(result.unwrap(), temp_dir.path().canonicalize().unwrap());
-    }
-
-    #[test]
-    fn test_find_foundry_root_no_git() {
-        let temp_dir = tempfile::tempdir().unwrap();
-
-        change_directory(temp_dir.path()).unwrap();
-        let result = find_foundry_root();
-
-        let expected_error_msg =
-            "fatal: not a git repository (or any of the parent directories): .git\n".to_string();
-        let error = result.unwrap_err();
-        assert!(matches!(error, CLIError::GitError(msg) if msg == expected_error_msg));
-    }
-
-    #[test]
-    fn test_find_foundry_root_no_foundry() {
-        let temp_dir = create_temp_git_repo();
-
-        change_directory(temp_dir.path()).unwrap();
-        let result = find_foundry_root();
-
-        assert!(matches!(result.unwrap_err(), CLIError::NoFoundryError));
-    }
-
-    #[test]
-    fn test_find_foundry_root_subdir() {
-        let temp_dir = create_temp_git_repo();
-        let sub_dir1 = temp_dir.path().join("dir1");
-        let sub_dir2 = sub_dir1.join("dir2");
-        std::fs::create_dir_all(&sub_dir2).unwrap();
-        let file_path = temp_dir.path().join("foundry.toml");
-        std::fs::File::create(file_path).unwrap();
-
-        change_directory(&sub_dir2).unwrap();
-        let result = find_foundry_root();
-
-        assert_eq!(result.unwrap(), temp_dir.path().canonicalize().unwrap());
-    }
 }
--- rust/cli/src/misc/mod.rs
+++ rust/cli/src/misc/mod.rs
@@ -1,1 +1,1 @@
-pub mod path;
+pub(crate) mod path;
