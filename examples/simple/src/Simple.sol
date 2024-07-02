9d7c644bd7b25561cefa4988fa45924a38ff8bf6
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-02 17:47:19 +0200

    refactor: extract Verifiable base contract

--- examples/simple/src/Simple.sol
+++ examples/simple/src/Simple.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
-import {Steel} from "vlayer/Steel.sol";
+import {VlayerVerifiable} from "vlayer/VlayerVerifiable.sol";
+import {Steel} from "vlayer-engine/Steel.sol";
 
-contract Simple {
-    bytes32 public constant GUEST_ID = bytes32(0xb7079f57c71b4e1d95b8b1254303e13f78914599a8c119534c4c947c996b4d7d);
+contract Simple is VlayerVerifiable {
 
-    IRiscZeroVerifier verifier;
     uint256 public latestSum;
 
-    constructor(IRiscZeroVerifier _verifier) {
-        verifier = _verifier;
+    constructor(IRiscZeroVerifier verifier) VlayerVerifiable(verifier) {
+
     }
 
     function updateSum(bytes calldata seal, Steel.ExecutionCommitment memory commitment, uint256 sum) public {
@@ -20,9 +19,4 @@ contract Simple {
         latestSum = sum;
     }
 
-    function _verify(bytes calldata seal, Steel.ExecutionCommitment memory commitment, uint256 sum) private view {
-        bytes32 computedJournalHash = keccak256(abi.encode(commitment, sum));
-
-        verifier.verify(seal, GUEST_ID, computedJournalHash);
-    }
 }
