6738dd9e2d685c0368879a1fc07f44c7d0325048
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-03 14:37:09 +0200

    chore: rename base contract into Verifier

--- contracts/src/Verifier.sol
+++ contracts/src/Verifier.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
 import {Steel} from "vlayer-engine/Steel.sol";
 
-contract VlayerVerifiable {
+contract Verifier {
 
     bytes32 public constant GUEST_ID = bytes32(0xb7079f57c71b4e1d95b8b1254303e13f78914599a8c119534c4c947c996b4d7d);
 
--- examples/simple/src/Simple.sol
+++ examples/simple/src/Simple.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
-import {VlayerVerifiable} from "vlayer/VlayerVerifiable.sol";
+import {Verifier} from "vlayer/Verifier.sol";
 import {Steel} from "vlayer-engine/Steel.sol";
 
-contract Simple is VlayerVerifiable {
+contract Simple is Verifier {
 
     uint256 public latestSum;
 
-    constructor(IRiscZeroVerifier verifier) VlayerVerifiable(verifier) {
-
+    constructor(IRiscZeroVerifier verifier) Verifier(verifier) {
     }
 
     function updateSum(bytes calldata seal, Steel.ExecutionCommitment calldata commitment, uint256 sum) public  onlyVerified(seal, commitment, sum) {
