8dc0ed6b43a9ac080f578f5733b6b46218d4e98d
Anna Jabłonowska <ania@vlayer.xyz>
2024-07-04 15:52:35 +0200

    Change println to info

--- rust/vlayer/engine/src/set_block.rs
+++ rust/vlayer/engine/src/set_block.rs
@@ -3,6 +3,7 @@ use revm::{
     interpreter::{CallInputs, CallOutcome},
     Database, EvmContext, Inspector,
 };
+use tracing::info;
 
 const SET_BLOCK_CONTRACT_ADDR: Address = address!("1234567890AbcdEF1234567890aBcdef12345678");
 
@@ -17,13 +18,13 @@ impl<DB: Database> Inspector<DB> for SetBlockInspector {
         _context: &mut EvmContext<DB>,
         inputs: &mut CallInputs,
     ) -> Option<CallOutcome> {
-        println!(
+        info!(
             "Address: {:?}, caller:{:?}, input:{:?}",
             inputs.bytecode_address, inputs.caller, inputs.input,
         );
 
         if self.set_block {
-            println!("Need to change block!");
+            info!("Need to change block!");
         }
         self.set_block = inputs.bytecode_address == SET_BLOCK_CONTRACT_ADDR;
 
