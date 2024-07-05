d0aecbed3d227c9dc1b30407ea2eff50af38b5bd
Stanisław Durka <stanislaw@vlayer.xyz>
2024-07-05 16:02:15 +0200

    move tracing-subscriber dependency to workspace level

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -27,7 +27,7 @@ enum Commands {
 
 #[tokio::main]
 async fn main() {
-    // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
+    // In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
         .init();
