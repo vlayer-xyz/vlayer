99006e758b4b6994fe5a2698f8216596694c904e
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-05 15:17:32 +0200

    Simplify tests

--- rust/vlayer/engine/src/set_block.rs
+++ rust/vlayer/engine/src/set_block.rs
@@ -34,34 +34,17 @@ impl<DB: Database> Inspector<DB> for SetBlockInspector {
 
 #[cfg(test)]
 mod test {
-    use std::collections::HashSet;
-
     use alloy_primitives::{address, Address, Bytes, U256};
     use revm::{
         db::{CacheDB, EmptyDB},
         interpreter::{CallInputs, CallScheme, CallValue},
-        primitives::{AccountInfo, Env, SpecId},
-        ContextPrecompiles, EvmContext, InnerEvmContext, Inspector, JournaledState,
+        primitives::AccountInfo,
+        EvmContext, Inspector,
     };
 
-    use crate::set_block::SetBlockInspector;
+    use super::{SetBlockInspector, SET_BLOCK_CONTRACT_ADDR};
 
     const MOCK_CALLER: Address = address!("0000000000000000000000000000000000000000");
-
-    fn create_cache_db_evm_context(
-        env: Box<Env>,
-        db: CacheDB<EmptyDB>,
-    ) -> EvmContext<CacheDB<EmptyDB>> {
-        EvmContext {
-            inner: InnerEvmContext {
-                env,
-                journaled_state: JournaledState::new(SpecId::CANCUN, HashSet::new()),
-                db,
-                error: Ok(()),
-            },
-            precompiles: ContextPrecompiles::default(),
-        }
-    }
 
     fn create_mock_call_inputs(to: Address) -> CallInputs {
         CallInputs {
@@ -78,31 +61,29 @@ mod test {
         }
     }
 
-    #[test]
-    fn dont_set_block() {
+    fn inspector_call(addr: Address) -> SetBlockInspector {
         let mut mock_db = CacheDB::new(EmptyDB::default());
-        let not_set_block_contract = address!("dead10000000000000000000000000000001dead");
-        mock_db.insert_account_info(not_set_block_contract, AccountInfo::default());
-        let mut evm_context = create_cache_db_evm_context(Box::new(Env::default()), mock_db);
-        let mut call_inputs = create_mock_call_inputs(not_set_block_contract);
+        mock_db.insert_account_info(addr, AccountInfo::default());
+
+        let mut evm_context = EvmContext::new(mock_db);
+        let mut call_inputs = create_mock_call_inputs(addr);
 
         let mut set_block_inspector = SetBlockInspector::default();
         set_block_inspector.call(&mut evm_context, &mut call_inputs);
 
-        assert!(!set_block_inspector.set_block);
+        set_block_inspector
     }
 
     #[test]
-    fn set_block() {
-        let mut mock_db = CacheDB::new(EmptyDB::default());
-        let set_block_contract = address!("1234567890AbcdEF1234567890aBcdef12345678");
-        mock_db.insert_account_info(set_block_contract, AccountInfo::default());
-        let mut evm_context = create_cache_db_evm_context(Box::new(Env::default()), mock_db);
-        let mut call_inputs = create_mock_call_inputs(set_block_contract);
+    fn call_to_set_block_contract() {
+        let inspector = inspector_call(SET_BLOCK_CONTRACT_ADDR);
+        assert!(inspector.set_block);
+    }
 
-        let mut set_block_inspector = SetBlockInspector::default();
-        set_block_inspector.call(&mut evm_context, &mut call_inputs);
-
-        assert!(set_block_inspector.set_block);
+    #[test]
+    fn call_to_other_contract() {
+        let other_contract = address!("0000000000000000000000000000000000000000");
+        let inspector = inspector_call(other_contract);
+        assert!(!inspector.set_block);
     }
 }
