0316d7f0db66221d389e086eedb1fa1967f83606
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-08 16:37:36 +0200

    feat: decode Proof struct from calldata

--- contracts/src/Verifier.sol
+++ contracts/src/Verifier.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {console2} from "forge-std/console2.sol";
-
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
 import {ControlID, RiscZeroGroth16Verifier} from "risc0-ethereum/groth16/RiscZeroGroth16Verifier.sol";
 import {Steel} from "vlayer-engine/Steel.sol";
+import {Proof} from "./Proof.sol";
 
 contract VerifierBase {
     bytes32 public GUEST_ID = bytes32(0xb7079f57c71b4e1d95b8b1254303e13f78914599a8c119534c4c947c996b4d7d);
@@ -18,8 +17,9 @@ contract VerifierBase {
     }
 
     function _verify(address prover, bytes4) internal virtual {
-        address _prover = address(bytes20(msg.data[112:132]));
-        require(prover == _prover, "Invalid prover");
+        Proof memory proof = abi.decode(msg.data[4:], (Proof));
+
+        require(prover == proof.commitment.startContractAddress, "Invalid prover");
     }
 }
 
