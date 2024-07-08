fc09be007c5439a6f558dfe4f8874f3bffc85806
Marek Kirejczyk <marek@vlayer.xyz>
2024-07-08 16:06:44 +0200

    test: add first onlyVerified modifier test

--- contracts/src/Verifier.sol
+++ contracts/src/Verifier.sol
@@ -1,24 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {console2} from "forge-std/console2.sol";
+
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
+import {ControlID, RiscZeroGroth16Verifier} from "risc0-ethereum/groth16/RiscZeroGroth16Verifier.sol";
 import {Steel} from "vlayer-engine/Steel.sol";
 
-contract Verifier {
-    bytes32 public constant GUEST_ID = bytes32(0xb7079f57c71b4e1d95b8b1254303e13f78914599a8c119534c4c947c996b4d7d);
+contract VerifierBase {
+    bytes32 public GUEST_ID = bytes32(0xb7079f57c71b4e1d95b8b1254303e13f78914599a8c119534c4c947c996b4d7d);
 
-    IRiscZeroVerifier public _verifier;
+    IRiscZeroVerifier public verifier;
 
-    constructor(IRiscZeroVerifier verifier) {
-        _verifier = verifier;
-    }
-
-    modifier onlyVerified() {
-        _verify();
+    modifier onlyVerified(address prover, bytes4 selector) {
+        _verify(prover, selector);
         _;
     }
 
-    function _verify() internal virtual {
-        require(false);
+    function _verify(address prover, bytes4) internal virtual {
+        address _prover = address(bytes20(msg.data[112:132]));
+        require(prover == _prover, "Invalid prover");
+    }
+}
+
+contract Verifier is VerifierBase {
+
+    constructor() {
+        verifier = new RiscZeroGroth16Verifier(ControlID.CONTROL_ROOT, ControlID.BN254_CONTROL_ID);
+    }
+}
+
+contract VerifierUnderTest is VerifierBase {
+    function setVerifier(IRiscZeroVerifier _verifier) public {
+        verifier = _verifier;
     }
 }
--- contracts/test/Verifier.t.sol
+++ contracts/test/Verifier.t.sol
@@ -2,75 +2,60 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
+import {IRiscZeroVerifier, Receipt} from "risc0-ethereum/IRiscZeroVerifier.sol";
+import {RiscZeroMockVerifier} from "risc0-ethereum/test/RiscZeroMockVerifier.sol";
 
 import {Steel} from "vlayer-engine/Steel.sol";
-import {Seal} from "../src/Seal.sol";
+import {Seal, SealLib} from "../src/Seal.sol";
 import {Proof} from "../src/Proof.sol";
 
-import {Verifier} from "../src/Verifier.sol";
+import {VerifierUnderTest} from "../src/Verifier.sol";
 
-contract ExampleVerifier is Verifier {
-    event Increment(uint256 count);
+contract Prover {}
 
-    struct Counter {
-        uint64 count;
-        uint64 multiplier;
+contract ExampleProver is Prover {
+    function doSomething() public pure returns (uint256) {
+        return 42;
+    }
+}
+
+contract ExampleVerifier is VerifierUnderTest {
+    address public immutable PROVER;
+
+    bytes4 constant SIMPLE_PROVER_SELECTOR = ExampleProver.doSomething.selector;
+
+    constructor() {
+        PROVER = address(new ExampleProver());
     }
 
-    uint256 public counter;
-
-    constructor(IRiscZeroVerifier verifier) Verifier(verifier) {
-        counter = 0;
-    }
-
-    function withoutExtraParams(Proof calldata proof) external onlyVerified returns (bool) {
-        emit Increment(++counter);
-        return true;
-    }
-
-    function withOneExtraParam(Proof calldata proof, address) external onlyVerified returns (bool) {
-        emit Increment(++counter);
-        return true;
-    }
-
-    function withManyExtraParams(Proof calldata proof, address, Counter calldata, uint256)
-        external
-        onlyVerified
-        returns (bool)
-    {
-        emit Increment(++counter);
+    function verifySomething(
+        Proof calldata
+    ) external onlyVerified(PROVER, SIMPLE_PROVER_SELECTOR) returns (bool) {
         return true;
     }
 }
 
 contract Verifier_OnlyVerified_Modifier_Tests is Test {
-    ExampleVerifier public vlayer;
-    address public prover;
+    function test_verify_something() public {
+        ExampleVerifier exampleVerifier = new ExampleVerifier();
+        RiscZeroMockVerifier mockVerifier = new RiscZeroMockVerifier(bytes4(0));
 
-    function setUp() public {
-        prover = address(1);
-        vlayer = new ExampleVerifier(IRiscZeroVerifier(address(0)));
-    }
+        Steel.ExecutionCommitment memory commitment = Steel.ExecutionCommitment(
+            exampleVerifier.PROVER(),
+            ExampleVerifier.verifySomething.selector,
+            block.number - 1,
+            blockhash(block.number - 1)
+        );
 
-    // Fixtures
+        bytes memory seal = mockVerifier
+            .mockProve(
+                exampleVerifier.GUEST_ID(),
+                sha256(abi.encode(commitment))
+            )
+            .seal;
 
-    function seal_fixture() public view returns (Seal memory) {
-        return Seal(bytes32(0), bytes32(0));
-    }
-
-    function commitment_fixture() public view returns (Steel.ExecutionCommitment memory) {
-        return Steel.ExecutionCommitment(prover, bytes4(0x00), 1, bytes32(0x00));
-    }
-
-    function proof_fixture() public view returns (Proof memory) {
-        return Proof(0, seal_fixture(), commitment_fixture());
-    }
-
-    // Happy paths
-
-    function test_verifiesWithoutAdditonalParams() public {
-        bool result = vlayer.withoutExtraParams(proof_fixture());
-        require(result);
+        exampleVerifier.setVerifier(mockVerifier);
+        Proof memory proof = Proof(128, SealLib.encodeSeal(seal), commitment);
+        exampleVerifier.verifySomething(proof);
     }
 }
