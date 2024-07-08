052423a3c8c13cea5d54ffb78993f56f3cf8a48e
Marek Kirejczyk <marek@vlayer.xyz>
2024-07-08 16:06:44 +0200

    test: add first onlyVerified modifier test

--- contracts/src/Seal.sol
+++ contracts/src/Seal.sol
@@ -2,8 +2,21 @@
 pragma solidity ^0.8.13;
 
 struct Seal {
-    bytes32 lhv;
-    bytes32 rhv;
+    bytes18 lhv;
+    bytes18 rhv;
 }
 
-library SealLib {}
+library SealLib {
+    function encodeSeal(bytes calldata seal) public pure returns (Seal memory) {
+        require(seal.length == 36, "Invalid seal length");
+        uint256 lhv = 0;
+        uint256 rhv = 0;
+        for (uint256 i = 0; i < 18; i++) {
+            lhv <<= 8;
+            lhv += uint8(seal[i]);
+            rhv <<= 8;
+            rhv += uint8(seal[i + 18]);
+        }
+        return Seal(bytes18(bytes32(lhv)), bytes18(bytes32(rhv)));
+    }
+}
