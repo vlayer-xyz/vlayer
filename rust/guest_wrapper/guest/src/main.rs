49d348be9167e479bbbd4a5c0a01188193b9ae6d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-08 10:07:57 +0200

    Pass MutliEvmInput to Guest::new

--- rust/guest_wrapper/guest/src/main.rs
+++ rust/guest_wrapper/guest/src/main.rs
@@ -16,11 +16,8 @@ fn main() {
         call,
         start_execution_location,
     } = env::read();
-    let start_evm_input = multi_evm_input
-        .get(&start_execution_location)
-        .expect("cannot get start evm input")
-        .to_owned(); // TODO: Remove clone and convert this object into MultiEnv
-    let output = Guest::new(start_evm_input, start_execution_location).run(call);
+    
+    let output = Guest::new(multi_evm_input, start_execution_location).run(call);
 
     env::commit_slice(&output.execution_commitment.abi_encode());
     env::commit_slice(&output.evm_call_result);
