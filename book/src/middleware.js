2286f503ed376669cc1b0a53c6e49f5494600b44
Artur Chmaro <artur@vlayer.xyz>
2024-07-08 08:22:26 +0200

    Docs: Better auth logs

--- book/src/middleware.js
+++ book/src/middleware.js
@@ -9,15 +9,16 @@ export default function middleware(request) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    console.log("basic auth: ", [user, pwd]);
+    console.log("Login attempt: ", [url.toString(), user]);
 
     if (user === 'admin' && pwd === 'admin') {
-      console.log("going next...")
+      console.log("Login successful: ", [url.toString(), user]);
       return next();
     }
   }
 
+  console.log("No HTTP auth: ", [url.toString()])
   url.pathname = '/api/auth';
-  console.log({ url })
+
   return rewrite(url);
 }
