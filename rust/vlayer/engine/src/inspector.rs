891534702140434415a1ca5a12aa081fd7dc5e5f
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-07-08 11:39:20 +0200

    Move decoding and unwrapping to selector consts creation

--- rust/vlayer/engine/src/inspector.rs
+++ rust/vlayer/engine/src/inspector.rs
@@ -1,15 +1,20 @@
 use alloy_primitives::hex::decode;
 use alloy_primitives::{address, Address};
 use ethers_core::types::U256;
+use once_cell::sync::Lazy;
 use revm::{
     interpreter::{CallInputs, CallOutcome},
     Database, EvmContext, Inspector,
 };
 use tracing::info;
 
+// First 4 bytes of the call data is the selector id - the rest are arguments.
+const SELECTOR_LEN: usize = 4;
 const TRAVEL_CONTRACT_ADDR: Address = address!("1234567890AbcdEF1234567890aBcdef12345678");
-const SET_BLOCK_SELECTOR: &str = "87cea3ae";
-const SET_CHAIN_SELECTOR: &str = "1b44fd15";
+static SET_BLOCK_SELECTOR: Lazy<Vec<u8>> =
+    Lazy::new(|| decode("87cea3ae").expect("Error decoding set_block function call"));
+const SET_CHAIN_SELECTOR: Lazy<Vec<u8>> =
+    Lazy::new(|| decode("1b44fd15").expect("Error decoding set_chain function call"));
 
 #[derive(Clone, Debug, Default)]
 pub struct SetInspector {
@@ -31,20 +36,16 @@ impl<DB: Database> Inspector<DB> for SetInspector {
         match inputs.bytecode_address {
             TRAVEL_CONTRACT_ADDR => {
                 let input = &inputs.input;
-                let (selector, argument_bytes) = input.split_at(4);
+                let (selector, argument_bytes) = input.split_at(SELECTOR_LEN);
                 let argument = U256::from_big_endian(argument_bytes);
 
-                if selector
-                    == decode(SET_BLOCK_SELECTOR).expect("Error decoding set_block function call")
-                {
+                if selector == *SET_BLOCK_SELECTOR {
                     info!(
                         "Travel contract called with function: setBlock and argument: {:?}!",
                         argument
                     );
                     self.set_block = Some(argument);
-                } else if selector
-                    == decode(SET_CHAIN_SELECTOR).expect("Error decoding set_chain function call")
-                {
+                } else if selector == *SET_CHAIN_SELECTOR {
                     info!(
                         "Travel contract called with function: setChain and argument: {:?}!",
                         argument
