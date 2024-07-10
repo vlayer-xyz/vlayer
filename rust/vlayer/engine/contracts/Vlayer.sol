9f3d2ca1c724aba7bbe63a08cb13366748857386
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-10 12:43:47 +0200

    Move Vlayer.sol to contracts subfolder

--- rust/vlayer/engine/contracts/Vlayer.sol
+++ rust/vlayer/engine/contracts/Vlayer.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-library Vlayer {
-    /// @notice An ExecutionCommitment struct representing a block number and its block hash.
-    struct ExecutionCommitment {    
-        address startContractAddress;
-        bytes4 functionSelector;
-        uint256 settleBlockNumber; // Block number at which the commitment was made.
-        bytes32 settleBlockHash; // Hash of the block at the specified block number.
-    }
+/// @notice An ExecutionCommitment struct representing a block number and its block hash.
+struct ExecutionCommitment {    
+    address startContractAddress;
+    bytes4 functionSelector;
+    uint256 settleBlockNumber; // Block number at which the commitment was made.
+    bytes32 settleBlockHash; // Hash of the block at the specified block number.
+}
 
+library Vlayer {
     /// @notice Validates if the provided ExecutionCommitment matches the block hash of the given block number.
     /// @param commitment The ExecutionCommitment struct to validate.
     /// @return isValid True if the commitment's block hash matches the block hash of the block number, false otherwise.
