1027eda8a1c4ce764d4412a950f2c522feae5c63
Stanisław Durka <stanislaw@vlayer.xyz>
2024-07-11 11:49:32 +0200

    init e2e test w/ vlayerup

---
+++ bash/vlayer-init-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd $(mktemp -d)
+forge init myproject --no-commit
+cd myproject
+
+vlayer init
+test -d src/vlayer
+test -f src/vlayer/Simple.v.sol
+
+vlayer init | grep -q "ERROR"
+if [ $? -ne 0 ]; then
+exit 1
+fi
