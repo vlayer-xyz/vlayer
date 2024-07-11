7888be7c236b637f8ce91ef9b0112d981b3a3516
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-07-11 16:16:58 +0200

    Simplify SimpleTravelProver

--- examples/simple_travel/src/vlayer/SimpleTravelProver.sol
+++ examples/simple_travel/src/vlayer/SimpleTravelProver.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.13;
 
 import {Prover} from "vlayer/Prover.sol";
 
+interface IExample {
+    function exampleFunction() external returns (uint256);
+}
+
 contract SimpleTravelProver is Prover {
-    address constant EXAMPLE_ADDR = 0x742d35Cc6634C0532925a3b844Bc454e4438f44e;
+    address constant EXAMPLE_ADDR = 0x1111111111111111111111111111111111111111;
     constructor() {}
 
     function aroundTheWorld() public returns (uint256) {
-        setBlock(13);
-
-        (bool success, bytes memory output_bytes) = EXAMPLE_ADDR.call("");
-        // TODO: uncomment when we successfully intercept the call:
-        // require(success, "Call to example address should be intercepted and successful");
-        return abi.decode(output_bytes, (uint256));
+        setBlock(1);
+        return IExample(EXAMPLE_ADDR).exampleFunction();
     }
 }
