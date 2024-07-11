c8de79b91028c0e6544bef9d4c4f6959e054ae99
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-11 13:26:28 +0200

    feat: add DEV_MODE_SEAL flag into Seal encoding

--- contracts/test/Seal.t.sol
+++ contracts/test/Seal.t.sol
@@ -4,27 +4,70 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {TestHelpers} from "./helpers/TestHelpers.sol";
 
-import {Seal, SealLib} from "../src/Seal.sol";
+import {Seal, ProofMode, SealLib} from "../src/Seal.sol";
 
-contract SealLib_decodeTests is Test {
+contract SealLib_decode_Tests is Test {
     using SealLib for Seal;
 
     function test_empty() public pure {
-        Seal memory seal = Seal(0x000000000000000000000000000000000000, 0x000000000000000000000000000000000000);
+        Seal memory seal = Seal(0x000000000000000000000000000000000000, 0x00000000000000000000000000000000000000);
         assertEq(seal.decode(), SealFixtures.EMPTY());
     }
 
     function test_max() public pure {
         // SEAL_MAX = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        Seal memory seal = Seal(0xffffffffffffffffffffffffffffffffffff, 0xffffffffffffffffffffffffffffffffffff);
+        Seal memory seal = Seal(0xffffffffffffffffffffffffffffffffffff, 0xffffffffffffffffffffffffffffffffffffff);
         assertEq(seal.decode(), SealFixtures.MAX());
     }
 
     function test_sealIsDecodedFromLeftToRight() public pure {
         // SEAL_SEQ = 000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f2021222324
-        Seal memory seal = Seal(0x0102030405060708090a0b0c0d0e0f101112, 0x131415161718191a1b1c1d1e1f2021222324);
+        Seal memory seal = Seal(0x0102030405060708090a0b0c0d0e0f101112, 0x131415161718191a1b1c1d1e1f202122232400);
 
         assertEq(seal.decode(), SealFixtures.SEQ());
+    }
+
+    function test_immutability() public pure {
+        bytes18 lhv = 0x0102030405060708090a0b0c0d0e0f101112;
+        bytes19 rhv = 0x131415161718191a1b1c1d1e1f202122232401;
+        Seal memory seal = Seal(lhv, rhv);
+
+        seal.decode();
+
+        assertEq(seal.lhv, lhv);
+        assertEq(seal.rhv, rhv);
+    }
+}
+
+contract SealLib_proofMode_Tests is Test {
+    using SealLib for Seal;
+
+    function test_prodSeal() public pure {
+        Seal memory seal = Seal(0x0102030405060708090a0b0c0d0e0f101112, 0x131415161718191a1b1c1d1e1f202122232400);
+        assert(seal.proofMode() == ProofMode.GROTH16);
+    }
+
+    function test_mockSeal() public pure {
+        Seal memory seal = Seal(0x0102030405060708090a0b0c0d0e0f101112, 0x131415161718191a1b1c1d1e1f202122232401);
+        assert(seal.proofMode() == ProofMode.FAKE);
+    }
+
+    function test_invalidValueReverts() public {
+        Seal memory seal = Seal(0x0102030405060708090a0b0c0d0e0f101112, 0x131415161718191a1b1c1d1e1f202122232402);
+
+        vm.expectRevert(); // should panic with 0x21
+        seal.proofMode();
+    }
+
+    function test_immutability() public pure {
+        bytes18 lhv = 0x0102030405060708090a0b0c0d0e0f101112;
+        bytes19 rhv = 0x131415161718191a1b1c1d1e1f202122232401;
+        Seal memory seal = Seal(lhv, rhv);
+
+        seal.proofMode();
+
+        assertEq(seal.lhv, lhv);
+        assertEq(seal.rhv, rhv);
     }
 }
 
