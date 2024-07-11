77a0267f00d943394a2b1f0c8c658910d50634e3
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-07-11 14:01:07 +0200

    Add simple travel contract example

---
+++ examples/simple_travel/script/DeployVlayer.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+
+import {SimpleTravelProver} from "../src/vlayer/SimpleTravelProver.sol";
+
+contract SimpleTravelScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIV");
+        vm.startBroadcast(deployerPrivateKey);
+
+        SimpleTravelProver simpleTravelProver = new SimpleTravelProver();
+        console2.log("SimpleProver contract deployed to:", address(simpleTravelProver));
+    }
+}
