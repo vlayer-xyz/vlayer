abd59db0ddf9a00502f1530da8a181b9d2026a78
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-16 15:02:34 +0200

    feat: prevent FakeProofVerifier from being created on mainnet

---
+++ contracts/src/proof_verifier/ChainId.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+error InvalidChainId();
+
+library ChainIdLibrary {
+    function is_devnet() internal view returns (bool) {
+        return block.chainid == 31337;
+    }
+
+    function is_mainnet() internal view returns (bool) {
+        uint256[2] memory MAINNETS = [
+            uint256(1), // Ethereum
+            uint256(10) // Optimism
+        ];
+
+        for (uint256 i = 0; i < MAINNETS.length; i++) {
+            if (MAINNETS[i] == block.chainid) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
