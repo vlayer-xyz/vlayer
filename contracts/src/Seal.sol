adedb5742ab92cf963e0899ba06201d43bb37be3
vanruch <ivanruch@gmail.com>
2024-07-16 11:26:38 +0200

    Make methods on SealLib internal so that it's inlined on compilation

--- contracts/src/Seal.sol
+++ contracts/src/Seal.sol
@@ -16,21 +16,25 @@ library SealLib {
     uint256 constant SEAL_MIDDLE = SEAL_LENGTH / 2;
     uint256 constant PROOF_MODE_POSITION = SEAL_LENGTH + 1;
 
-    function decode(Seal memory seal) public pure returns (bytes memory) {
+    function decode(Seal memory seal) internal pure returns (bytes memory) {
         bytes memory sealBytes = new bytes(SEAL_LENGTH);
 
-        for (uint256 i = 0; i < SEAL_MIDDLE; i++) {
-            sealBytes[i] = bytes1(seal.lhv);
-            sealBytes[SEAL_MIDDLE + i] = bytes1(seal.rhv);
+        // we don't want to change the original seal
+        bytes18 lhv = seal.lhv;
+        bytes19 rhv = seal.rhv;
 
-            seal.lhv <<= 8;
-            seal.rhv <<= 8;
+        for (uint256 i = 0; i < SEAL_MIDDLE; i++) {
+            sealBytes[i] = bytes1(lhv);
+            sealBytes[SEAL_MIDDLE + i] = bytes1(rhv);
+
+            lhv <<= 8;
+            rhv <<= 8;
         }
 
         return sealBytes;
     }
 
-    function proofMode(Seal memory seal) public pure returns (ProofMode) {
+    function proofMode(Seal memory seal) internal pure returns (ProofMode) {
         bytes1 proofModeByte = bytes1(seal.rhv << 8 * (PROOF_MODE_POSITION - SEAL_MIDDLE - 1));
         return ProofMode(uint8(proofModeByte));
     }
