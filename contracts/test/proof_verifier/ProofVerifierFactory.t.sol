675e7fb5348ba57a3c34c7947d18235c73d097ce
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-16 14:42:53 +0200

    feat: add ProofVerifierFactory

---
+++ contracts/test/proof_verifier/ProofVerifierFactory.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {IProofVerifier} from "../../src/proof_verifier/IProofVerifier.sol";
+import {ProofVerifierFactory, InvalidChainId} from "../../src/proof_verifier/ProofVerifierFactory.sol";
+
+contract Verifier_OnlyVerified_Modifier_Tests is Test {
+    function test_producesAVerifierInDevMode() public {
+        vm.chainId(31337);
+
+        IProofVerifier verifier = ProofVerifierFactory.produce();
+        assert(verifier != IProofVerifier(address(0)));
+    }
+
+    function test_returnsAConstantForMainnets() public {
+        vm.chainId(1);
+
+        IProofVerifier verifier = ProofVerifierFactory.produce();
+        assert(verifier == IProofVerifier(address(0)));
+    }
+
+    function test_failsForOtherNetworks() public {
+        vm.chainId(11155111 + 1);
+
+        vm.expectRevert(InvalidChainId.selector);
+        ProofVerifierFactory.produce();
+    }
+}
