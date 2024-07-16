e9a860fa8706d542f176aab22410f9d8833028e3
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-16 14:39:09 +0200

    Rewrite get_or_insert_with_result to get_mut_or_insert_with_result

--- rust/host/src/utils.rs
+++ rust/host/src/utils.rs
@@ -1,17 +1,19 @@
 use std::collections::{hash_map::Entry, HashMap};
 
-pub fn get_or_insert_with_result<K, V, F, E>(map: &mut HashMap<K, V>, key: K, f: F) -> Result<V, E>
+pub fn get_mut_or_insert_with_result<K, V, F, E>(
+    map: &mut HashMap<K, V>,
+    key: K,
+    f: F,
+) -> Result<&mut V, E>
 where
     K: std::hash::Hash + Eq,
     F: FnOnce() -> Result<V, E>,
-    V: Clone,
 {
     match map.entry(key) {
-        Entry::Occupied(value) => Ok(value.get().clone()),
+        Entry::Occupied(value) => Ok(value.into_mut()),
         Entry::Vacant(entry) => {
             let value = f()?;
-            entry.insert(value.clone());
-            Ok(value)
+            Ok(entry.insert(value))
         }
     }
 }
@@ -21,21 +23,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_or_insert_with_result() {
+    fn test_get_mut_or_insert_with_result() {
         let mut map = HashMap::new();
         let key = "key";
-        let value = 42;
-        let result = get_or_insert_with_result(&mut map, key, || Ok::<_, ()>(value));
-        assert_eq!(result, Ok(value));
+        let mut value = 42;
+        let result = get_mut_or_insert_with_result(&mut map, key, || Ok::<_, ()>(value));
+        assert_eq!(result, Ok(&mut value));
         assert_eq!(map.get(key), Some(&value));
     }
 
     #[test]
-    fn test_get_or_insert_with_result_error() {
+    fn test_get_mut_or_insert_with_result_error() {
         let mut map = HashMap::<_, (), _>::new();
         let key = "key";
         let error = "error";
-        let result = get_or_insert_with_result(&mut map, key, || Err(error));
+        let result = get_mut_or_insert_with_result(&mut map, key, || Err(error));
         assert_eq!(result, Err(error));
         assert_eq!(map.get(key), None);
     }
