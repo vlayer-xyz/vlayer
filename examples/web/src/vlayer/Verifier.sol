1dadb4470a7f7ad24d3ed051932511cd536fd864
Anna Jabłonowska <ania@vlayer.xyz>
2024-07-16 17:21:58 +0200

    Add notary pub keys check

--- examples/web/src/vlayer/Verifier.sol
+++ examples/web/src/vlayer/Verifier.sol
@@ -4,21 +4,35 @@ pragma solidity ^0.8.13;
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
 import {Verifier} from "vlayer/Verifier.sol";
 import {ExecutionCommitment} from "vlayer/ExecutionCommitment.sol";
-
 import {WebProver} from "./Prover.sol";
 
 contract WebVerifier is Verifier {
-    mapping(address => bool) public authorizedMembers;
-    mapping(string => bool) public claimedUsersId;
+    mapping(address => bool) authorizedMembers;
+    mapping(string => bool) claimedUsersId;
+    mapping(string => bool) trustedPubKeys;
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier _ownerOnly() {
+        require(msg.sender == owner);
+    }
 
     function join(
         Proof proof,
         uint8[] calldata userId,
         string calldata notaryPubKey
     ) public onlyVerified(PROVER_ADDR, PROVER_FUNC_SELECTOR) {
-        require(!claimedUsersId[userId], "Claimed user Id");
+        require(!claimedUsersId[userId], "User Id claimed");
+        require(trustedPubKeys[notaryPubKey], "Notary PubKey not trusted");
 
         claimedUsersId[userId] = true;
         authorizedMembers[msg.sender] = true;
+    }
+
+    function addTrustedPubKey(String newPubKey) _ownerOnly {
+        trustedPubKeys[newPubKey] = true;
     }
 }
