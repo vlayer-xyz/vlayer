98b384561de1aa40efa4db68ace02eb260def1f5
Wiktor Gromniak <wiktor@vlayer.xyz>
2024-07-17 11:40:21 +0200

    Removed parse_tlsn_output.js script from web example.

--- examples/web/vlayer/src/parse_tlsn_output.js
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs'
-
-const file = fs.readFileSync('tlsn.json')
-const parsed = JSON.parse(file)
-console.log(parsed)
-console.log(parsed.substrings.inclusion_proof)
-console.log(parsed.substrings.openings['38'])
-console.log(new Buffer.from(parsed.substrings.openings['38'][1]["Blake3"].data).toString('utf8'))
