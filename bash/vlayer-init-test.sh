aab7e7e3526d8bee096c74839f662e024423c827
Stanisław Durka <stanislaw@vlayer.xyz>
2024-07-17 11:12:12 +0200

    refactor test script and account for updated examples

--- bash/vlayer-init-test.sh
+++ bash/vlayer-init-test.sh
@@ -1,16 +1,46 @@
 #!/usr/bin/env bash
 
-set -e
+set -ueo pipefail
 
-cd $(mktemp -d)
-forge init myproject --no-commit
-cd myproject
+function setup_foundry_project() {
+        cd $(mktemp -d)
+        forge init myproject --no-commit
+        cd myproject
+}
 
-vlayer init
-test -d src/vlayer
-test -f src/vlayer/Simple.v.sol
+function test_can_initialise_properly() {
+    setup_foundry_project
 
-vlayer init | grep -q "ERROR"
-if [ $? -ne 0 ]; then
-exit 1
-fi
+    vlayer init
+    
+    contracts=(
+        "SimpleProver.sol"
+        "SimpleProver.t.sol"
+        "SimpleVerifier.sol"
+    )
+    
+    for contract in "${contracts[@]}"; do
+        if [[ ! -f "src/vlayer/${contract}" ]] ; then
+            echo "ERROR: $contract not found" >&2
+            exit 1
+        fi
+    done
+}
+
+function test_init_is_not_idempotent() {
+    setup_foundry_project
+
+    vlayer init
+
+    # should log an error. If not, 'grep' exits with 1
+    vlayer init | grep -q "ERROR" 
+}
+
+####### SETUP
+curl -SL  https://install.vlayer.xyz | bash
+source ~/.config/.bashrc
+vlayerup
+
+####### TESTS
+test_can_initialise_properly
+test_init_is_not_idempotent
