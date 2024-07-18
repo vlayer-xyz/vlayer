a06901f242ba9f7085a004652b8c84180f2a9fc6
Adam Król <adamkrol@MacBook-Pro-Adam.local>
2024-07-18 12:42:39 +0200

    EnvBlockHeader conversions

--- rust/engine/src/block_header.rs
+++ rust/engine/src/block_header.rs
@@ -1,7 +1,12 @@
 pub mod eth;
 
+use std::any::Any;
+
+use as_any::{AsAny, Downcast};
+
 use alloy_primitives::{BlockNumber, B256};
 
+use eth::EthBlockHeader;
 use revm::primitives::BlockEnv;
 
 pub trait Hashable {
@@ -10,7 +15,7 @@ pub trait Hashable {
 }
 
 /// An EVM abstraction of a block header.
-pub trait EvmBlockHeader: Hashable {
+pub trait EvmBlockHeader: Hashable + AsAny {
     /// Returns the hash of the parent block's header.
     fn parent_hash(&self) -> &B256;
     /// Returns the block number.
@@ -21,4 +26,51 @@ pub trait EvmBlockHeader: Hashable {
     fn state_root(&self) -> &B256;
     /// Fills the EVM block environment with the header's data.
     fn fill_block_env(&self, blk_env: &mut BlockEnv);
+}
+
+pub enum BlockHeaderType {
+    Eth(EthBlockHeader),
+}
+
+impl BlockHeaderType {
+    pub fn to_box_dyn(&self) -> Box<dyn EvmBlockHeader> {
+        match self {
+            BlockHeaderType::Eth(header) => Box::new(header.clone()),
+        }
+    }
+}
+
+pub fn match_block_header_type(header: Box<dyn EvmBlockHeader>) -> Option<BlockHeaderType> {
+    if let Some(eth_header) = header.as_ref().downcast_ref::<EthBlockHeader>() {
+        Some(BlockHeaderType::Eth(eth_header.clone()))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eth_block_header_to_box_dyn() {
+        let eth_block_header = EthBlockHeader::default();
+
+        let header_type = BlockHeaderType::Eth(eth_block_header.clone());
+
+        let boxed_header: Box<dyn EvmBlockHeader> = header_type.to_box_dyn();
+        assert!(boxed_header.as_ref().is::<EthBlockHeader>());
+    }
+
+    #[test]
+    fn test_match_block_header_type() {
+        let eth_block_header = EthBlockHeader::default();
+        let header_type = BlockHeaderType::Eth(eth_block_header.clone());
+
+        let boxed_header: Box<dyn EvmBlockHeader> = header_type.to_box_dyn();
+        match match_block_header_type(boxed_header) {
+            Some(BlockHeaderType::Eth(_)) => println!("Matched EthBlockHeader"),
+            None => panic!("Did not match any block header type"),
+        }
+    }
 }
