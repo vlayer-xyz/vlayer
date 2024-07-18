ed01a759912b61f9db613f3c3d5554a4296d6f04
Leonid Logvinov <logvinov.leon@gmail.com>
2024-07-18 11:03:35 +0200

    Move CachedMultiProvider to provider::multi.rs

--- rust/host/src/provider.rs
+++ rust/host/src/provider.rs
@@ -1,22 +1,20 @@
-use alloy_primitives::{
-    Address, BlockNumber, Bytes, ChainId, StorageKey, StorageValue, TxNumber, U256,
-};
+use alloy_primitives::{Address, BlockNumber, Bytes, StorageKey, StorageValue, TxNumber, U256};
 use auto_impl::auto_impl;
-use factory::ProviderFactory;
-use std::{collections::HashMap, error::Error as StdError, rc::Rc};
+use std::error::Error as StdError;
 use vlayer_engine::evm::block_header::EvmBlockHeader;
 
 mod cache;
 pub mod ethers;
 pub mod factory;
 mod file;
+pub mod multi;
 mod null;
 
 pub use ethers::{EthersProvider, EthersProviderError};
 use ethers_providers::{Http, RetryClient};
 pub use file::{EthFileProvider, FileProvider};
 
-use crate::{host::error::HostError, proof::EIP1186Proof, utils::get_mut_or_insert_with_result};
+use crate::proof::EIP1186Proof;
 
 /// The Ethers client type.
 pub type EthersClient = ethers_providers::Provider<RetryClient<Http>>;
@@ -47,32 +45,4 @@ pub trait Provider {
         storage_keys: Vec<StorageKey>,
         block: BlockNumber,
     ) -> Result<EIP1186Proof, Self::Error>;
-}
-
-type MultiProvider<P> = HashMap<ChainId, Rc<P>>;
-
-pub struct CachedMultiProvider<P> {
-    cache: MultiProvider<P>,
-    factory: Box<dyn ProviderFactory<P>>,
-}
-
-impl<P> CachedMultiProvider<P>
-where
-    P: Provider,
-{
-    pub fn new(factory: impl ProviderFactory<P> + 'static) -> Self {
-        CachedMultiProvider {
-            cache: HashMap::new(),
-            factory: Box::new(factory),
-        }
-    }
-
-    pub fn try_get(&mut self, chain_id: ChainId) -> Result<Rc<P>, HostError> {
-        let create_provider = || Ok::<_, HostError>(Rc::new(self.factory.create(chain_id)?));
-        Ok(Rc::clone(get_mut_or_insert_with_result(
-            &mut self.cache,
-            chain_id,
-            create_provider,
-        )?))
-    }
 }
---
+++ rust/host/src/provider/multi.rs
@@ -0,0 +1,32 @@
+use super::{factory::ProviderFactory, Provider};
+use crate::{host::error::HostError, utils::get_mut_or_insert_with_result};
+use alloy_primitives::ChainId;
+use std::{collections::HashMap, rc::Rc};
+
+type MultiProvider<P> = HashMap<ChainId, Rc<P>>;
+
+pub struct CachedMultiProvider<P> {
+    cache: MultiProvider<P>,
+    factory: Box<dyn ProviderFactory<P>>,
+}
+
+impl<P> CachedMultiProvider<P>
+where
+    P: Provider,
+{
+    pub fn new(factory: impl ProviderFactory<P> + 'static) -> Self {
+        CachedMultiProvider {
+            cache: HashMap::new(),
+            factory: Box::new(factory),
+        }
+    }
+
+    pub fn try_get(&mut self, chain_id: ChainId) -> Result<Rc<P>, HostError> {
+        let create_provider = || Ok::<_, HostError>(Rc::new(self.factory.create(chain_id)?));
+        Ok(Rc::clone(get_mut_or_insert_with_result(
+            &mut self.cache,
+            chain_id,
+            create_provider,
+        )?))
+    }
+}
