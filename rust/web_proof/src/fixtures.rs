f960d8fde056d6f56b21d4f0b9957911a55f582e
Arkadiusz Konior <akonior@gmail.com>
2024-07-24 16:21:13 +0200

    Add notary_pub_key_example

--- rust/web_proof/src/fixtures.rs
+++ rust/web_proof/src/fixtures.rs
@@ -1,7 +1,13 @@
 use core::str;
+use p256::PublicKey;
+use pkcs8::DecodePublicKey;
 use serde_json::Value;
 
-pub const NOTARY_PUB_KEY_EXAMPLE: &str = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
+pub const NOTARY_PUB_KEY_PEM_EXAMPLE: &str = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
+
+pub fn notary_pub_key_example() -> PublicKey {
+    PublicKey::from_public_key_pem(NOTARY_PUB_KEY_PEM_EXAMPLE).unwrap()
+}
 
 pub fn tls_proof_example() -> Value {
     serde_json::from_str(str::from_utf8(include_bytes!("../testdata/tls_proof.json")).unwrap())
