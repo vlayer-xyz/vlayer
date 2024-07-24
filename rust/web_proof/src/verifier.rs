e29bff8a89c12755eb4ae92b4e7f3558060495c5
Anna Jabłonowska <ania@vlayer.xyz>
2024-07-24 16:46:08 +0200

    WebProof verification failing test

--- rust/web_proof/src/verifier.rs
+++ rust/web_proof/src/verifier.rs
@@ -6,10 +6,31 @@ fn _verify_proof(_web_proof: WebProof) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use tlsn_core::proof::{SessionProof, TlsProof};
+
     use crate::fixtures::{notary_pub_key_example, tls_proof_example};
     use crate::types::WebProof;
 
     use super::_verify_proof;
+
+    #[test]
+    fn fail_verification() {
+        let tls_proof = tls_proof_example();
+
+        let wrong_tls_proof = TlsProof {
+            session: SessionProof {
+                signature: None,
+                ..tls_proof.session
+            },
+            ..tls_proof
+        };
+
+        let proof = WebProof {
+            tls_proof: wrong_tls_proof,
+            notary_pub_key: notary_pub_key_example(),
+        };
+        assert_eq!(_verify_proof(proof), false)
+    }
 
     #[test]
     fn success_verification() {
