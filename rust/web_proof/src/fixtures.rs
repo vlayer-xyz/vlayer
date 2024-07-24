9fcdb9e168adc0ef21ada38d8be9f9f91775917c
Arkadiusz Konior <akonior@gmail.com>
2024-07-24 16:29:19 +0200

    Change tls_proof_example type

--- rust/web_proof/src/fixtures.rs
+++ rust/web_proof/src/fixtures.rs
@@ -1,7 +1,7 @@
 use core::str;
 use p256::PublicKey;
 use pkcs8::DecodePublicKey;
-use serde_json::Value;
+use tlsn_core::proof::TlsProof;
 
 pub const NOTARY_PUB_KEY_PEM_EXAMPLE: &str = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
 
@@ -9,7 +9,7 @@ pub fn notary_pub_key_example() -> PublicKey {
     PublicKey::from_public_key_pem(NOTARY_PUB_KEY_PEM_EXAMPLE).unwrap()
 }
 
-pub fn tls_proof_example() -> Value {
+pub fn tls_proof_example() -> TlsProof {
     serde_json::from_str(str::from_utf8(include_bytes!("../testdata/tls_proof.json")).unwrap())
         .unwrap()
 }
