2672909eb3ee41201d2a5c37f8575c871b31bbd3
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-24 12:04:03 +0200

    feat: run e2e bonsai proving only in nightly jobs

--- bash/e2e-test.sh
+++ bash/e2e-test.sh
@@ -1,23 +1,17 @@
+#!/usr/bin/env bash
+
+set -uexo pipefail
+
+PROVING_MODE=${PROVING_MODE:-dev}
+
 ./bash/prove-example.sh \
-  --mode dev \
+  --mode ${PROVING_MODE} \
   --example simple \
   --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
   --data 0xcad0899b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002 
 
 ./bash/prove-example.sh \
-  --mode prod \
-  --example simple \
-  --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
-  --data 0xcad0899b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002 
-
-./bash/prove-example.sh \
-  --mode dev \
-  --example simple_travel \
-  --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
-  --data 0xe56a41f9
-
-./bash/prove-example.sh \
-  --mode prod \
+  --mode ${PROVING_MODE} \
   --example simple_travel \
   --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
   --data 0xe56a41f9
