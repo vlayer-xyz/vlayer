39ba03dae6fe2a89b4afe775a47dd6eb52eb5d49
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-07-24 17:20:41 +0200

    Gitignore .env file

--- rust/host/src/provider/cache.rs
+++ rust/host/src/provider/cache.rs
@@ -2,7 +2,7 @@ use super::{BlockingProvider, EIP1186Proof};
 use alloy_primitives::{Address, BlockNumber, Bytes, StorageKey, StorageValue, TxNumber, U256};
 use anyhow::anyhow;
 use json::{AccountQuery, BlockQuery, JsonCache, ProofQuery, StorageQuery};
-use std::{ cell::RefCell, collections::hash_map::Entry, path::PathBuf};
+use std::{cell::RefCell, collections::hash_map::Entry, path::PathBuf};
 use vlayer_engine::block_header::EvmBlockHeader;
 
 pub mod json;
