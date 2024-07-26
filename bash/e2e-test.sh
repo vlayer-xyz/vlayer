833ee7d35cf848f5a8399c38db5ddded73656cf7
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-26 14:22:48 +0200

    feat: enable running e2e script from any directory

--- bash/e2e-test.sh
+++ bash/e2e-test.sh
@@ -2,15 +2,17 @@
 
 set -uexo pipefail
 
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+
 PROVING_MODE=${PROVING_MODE:-dev}
 
-./bash/prove-example.sh \
+${VLAYER_HOME}/bash/prove-example.sh \
   --mode ${PROVING_MODE} \
   --example simple \
   --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
   --data 0xcad0899b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002 
 
-./bash/prove-example.sh \
+${VLAYER_HOME}/bash/prove-example.sh \
   --mode ${PROVING_MODE} \
   --example simple_travel \
   --caller 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
