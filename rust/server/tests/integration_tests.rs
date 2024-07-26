981a7b9af15ee850f4dd83bf7b3580243ce3e030
Anna Jabłonowska <ania@vlayer.xyz>
2024-07-26 13:31:25 +0200

    Extract create_result and create_error_message functions

--- rust/server/tests/integration_tests.rs
+++ rust/server/tests/integration_tests.rs
@@ -35,6 +35,28 @@ mod server_tests {
     const SIMPLE_EXAMPLE_TEST_DATA: &str = "0xcad0899b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002";
     const WEB_PROOF_EXAMPLE_TEST_DATA: &str = "0xefedf8100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000096170692e782e636f6d0000000000000000000000000000000000000000000000";
 
+    fn create_result(message: &str) -> serde_json::Value {
+        json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "result": {
+                "result": message
+            }
+        })
+    }
+
+    fn create_error_response(code: i64, message: &str) -> serde_json::Value {
+        json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "error": {
+                "code": code,
+                "message": message,
+                "data": null
+            }
+        })
+    }
+
     #[tokio::test]
     async fn json_rpc_not_found() -> anyhow::Result<()> {
         let app = server(CONFIG.clone());
@@ -49,15 +71,7 @@ mod server_tests {
         assert_eq!(response.status(), StatusCode::OK);
         assert_eq!(
             body_to_json::<Value>(response.into_body()).await?,
-            json!({
-                "jsonrpc": "2.0",
-                "id": 1,
-                "error": {
-                    "code": -32601,
-                    "message": "Method `non_existent_json_rpc_method` not found",
-                    "data": null
-                }
-            })
+            create_error_response(-32601, "Method `non_existent_json_rpc_method` not found")
         );
 
         Ok(())
@@ -105,15 +119,10 @@ mod server_tests {
             assert_eq!(response.status(), StatusCode::OK);
             assert_eq!(
                 body_to_json::<Value>(response.into_body()).await?,
-                json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "error": {
-                        "code": -32602,
-                        "message": "Invalid field `to`: Odd number of digits `I am not a valid address!`",
-                        "data": null
-                    }
-                })
+                create_error_response(
+                    -32602,
+                    "Invalid field `to`: Odd number of digits `I am not a valid address!`"
+                )
             );
 
             Ok(())
@@ -135,13 +144,7 @@ mod server_tests {
             assert_eq!(response.status(), StatusCode::OK);
             assert_eq!(
                 body_to_json::<Value>(response.into_body()).await?,
-                json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "result": {
-                        "result": "prover_contract_address: 0x5FbDB2315678afecb367f032d93F642f64180aa3, function_selector: 0xcad0899b, evm_call_result: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], seal: []"
-                    }
-                })
+                create_result("prover_contract_address: 0x5FbDB2315678afecb367f032d93F642f64180aa3, function_selector: 0xcad0899b, evm_call_result: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3], seal: []")
             );
 
             Ok(())
@@ -170,15 +173,10 @@ mod server_tests {
             assert_eq!(response.status(), StatusCode::OK);
             assert_eq!(
                 body_to_json::<Value>(response.into_body()).await?,
-                json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "error": {
-                        "code": -32602,
-                        "message": "invalid type: string \"<tls proof value>\", expected struct TlsProof",
-                        "data": null
-                    }
-                })
+                create_error_response(
+                    -32602,
+                    "invalid type: string \"<tls proof value>\", expected struct TlsProof"
+                )
             );
 
             Ok(())
@@ -207,15 +205,10 @@ mod server_tests {
             assert_eq!(response.status(), StatusCode::OK);
             assert_eq!(
                 body_to_json::<Value>(response.into_body()).await?,
-                json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "error": {
-                        "code": -32602,
-                        "message": "ASN.1 error: PEM error: PEM preamble contains invalid data (NUL byte)",
-                        "data": null
-                    }
-                })
+                create_error_response(
+                    -32602,
+                    "ASN.1 error: PEM error: PEM preamble contains invalid data (NUL byte)"
+                )
             );
 
             Ok(())
@@ -245,13 +238,7 @@ mod server_tests {
             assert_eq!(response.status(), StatusCode::OK);
             assert_eq!(
                 body_to_json::<Value>(response.into_body()).await?,
-                json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "result": {
-                        "result": "prover_contract_address: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512, function_selector: 0xefedf810, evm_call_result: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], seal: []"
-                    }
-                })
+                create_result("prover_contract_address: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512, function_selector: 0xefedf810, evm_call_result: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], seal: []")
             );
 
             Ok(())
