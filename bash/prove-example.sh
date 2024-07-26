e10236d60f7b82a4d6a85ae86ed5f7caf296218d
Piotr Żelazko <piotr@vlayer.xyz>
2024-07-26 14:24:00 +0200

    feat(e2e): propagate SIGTERM signal to children

--- bash/prove-example.sh
+++ bash/prove-example.sh
@@ -1,22 +1,99 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ueo pipefail
 
-cleanup() {
+TMP_DIR=$( mktemp -d -t vlayer-$(basename $0).XXXXX )
+LOGS_DIR="${TMP_DIR}/logs"
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+
+mkdir -p "${LOGS_DIR}"
+echo ${TMP_DIR}
+
+# examples
+simple="SIMPLE_PROVER_ADDRESS"
+simple_travel="SIMPLE_TRAVEL_PROVER_ADDRESS"
+
+function cleanup() {
     echo "Cleaning up..."
     
     # Kill Anvil if running
-    if [ -n "$ANVIL_PID" ] && ps -p "$ANVIL_PID" > /dev/null; then
-        echo "Killing Anvil (PID $ANVIL_PID)..."
+    if [[ -n "${ANVIL_PID:-}" ]] && ps -p "$ANVIL_PID" > /dev/null; then
+        echo "Killing anvil (PID $ANVIL_PID)..."
         kill "$ANVIL_PID"
     fi
 
-    # Kill Vlayer server if running
-    if [ -n "$VLAYER_SERVER_PID" ] && ps -p "$VLAYER_SERVER_PID" > /dev/null; then
-        echo "Killing Vlayer server (PID $VLAYER_SERVER_PID)..."
+    # Kill vlayer server if running
+    if [[ -n "${VLAYER_SERVER_PID:-}" ]] && ps -p "$VLAYER_SERVER_PID" > /dev/null; then
+        echo "Killing vlayer server (PID $VLAYER_SERVER_PID)..."
         kill "$VLAYER_SERVER_PID"
     fi
 }
+
+check_exit_status() {
+  if [ $? -ne 0 ]; then
+    echo "$1"
+    exit 1
+  fi
+}
+
+wait_for_port() {
+    local port=$1
+    local timeout=$2
+    local service_name=$3
+    echo "Waiting for ${service_name} to be ready on localhost:${port}..."
+
+    timeout --foreground --kill-after=5s "${timeout}" bash -c "until nc -z localhost ${port}; do sleep 3; done"
+    check_exit_status "Error: Timeout reached. ${service_name} is not available on localhost:${port}."
+}
+
+function startup_anvil(){
+    echo "Starting Anvil"
+    anvil >"${LOGS_DIR}/anvil.out" &
+    ANVIL_PID=$!
+    echo "Anvil started with PID $ANVIL_PID."
+    wait_for_port 8545 30s "Anvil"
+}
+
+function startup_vlayer(){
+    echo "Starting vlayer REST server"
+    (
+        cd "${VLAYER_HOME}/rust"
+
+        RUST_LOG=info \
+        RISC0_DEV_MODE="${RISC0_DEV_MODE}" \
+        BONSAI_API_URL="${BONSAI_API_URL}" \
+        BONSAI_API_KEY="${BONSAI_API_KEY}" \
+        cargo run --bin vlayer serve \
+        >"${LOGS_DIR}/vlayer_serve.out" \
+        2>"${LOGS_DIR}/vlayer_serve.err" & 
+
+        VLAYER_SERVER_PID=$!
+
+        echo "vlayer server started with PID ${VLAYER_SERVER_PID}."
+        wait_for_port 3000 30m "vlayer server"
+
+    )
+}
+
+function deploy_contracts() {
+    local example=$1
+
+    (    
+        cd "${VLAYER_HOME}/examples/${example}"
+        source ../../bash/vlayer-deploy.sh ${example} | \
+             tee ${LOGS_DIR}/deployment.out | \
+             grep '[A-Z0-9_]\+=[a-zA-Z0-9]\+' >"${TMP_DIR}/deployment.env" 
+    ) 
+
+    # Setting up the example contracts to be called during e2e tests.
+    # When creating a new example, add here the address name corresponding to 
+    # the name of the contract in vlayer-deploy.sh that you want to call.
+    set -a ; source "${TMP_DIR}/deployment.env" ; set +a
+    eval "EXAMPLE_ADDRESS_VAR=\${${EXAMPLE}}"
+    eval "echo \${${EXAMPLE_ADDRESS_VAR}}"
+}
+
+
 
 trap cleanup EXIT ERR INT
 
@@ -26,8 +103,8 @@ CALLER=""
 DATA=""
 EXAMPLE=""
 RISC0_DEV_MODE=""
-export BONSAI_API_URL="${BONSAI_API_URL:-https://api.bonsai.xyz/}"
-export BONSAI_API_KEY="${BONSAI_API_KEY:-}"
+BONSAI_API_URL="${BONSAI_API_URL:-https://api.bonsai.xyz/}"
+BONSAI_API_KEY="${BONSAI_API_KEY:-}"
 
 # Usage function to display help for the hapless user
 usage() {
@@ -69,7 +146,7 @@ while [[ "$#" -gt 0 ]]; do
             shift 2
             ;;
         --bonsai-api-key)
-            export BONSAI_API_KEY="$2"
+            BONSAI_API_KEY="$2"
             shift 2
             ;;
         *)
@@ -86,10 +163,10 @@ if [[ -z "$MODE" || -z "$CALLER" || -z "$DATA" || -z "$EXAMPLE" ]]; then
 fi
 
 # Set the RISC0_DEV_MODE environment variable based on the mode
-if [[ "$MODE" == "dev" ]]; then
-    export RISC0_DEV_MODE=true
-elif [[ "$MODE" == "prod" ]]; then
-    export RISC0_DEV_MODE=false
+if [[ "${MODE}" == "dev" ]]; then
+    RISC0_DEV_MODE=true
+elif [[ "${MODE}" == "prod" ]]; then
+    RISC0_DEV_MODE=false
 
     # Check that BONSAI_API_URL and BONSAI_API_KEY are not empty in prod mode
     if [[ -z "$BONSAI_API_URL" || -z "$BONSAI_API_KEY" ]]; then
@@ -106,68 +183,31 @@ echo "Example: $EXAMPLE"
 echo "RISC0_DEV_MODE: $RISC0_DEV_MODE"
 echo "BONSAI_API_URL: $BONSAI_API_URL"
 
-check_exit_status() {
-  if [ $? -ne 0 ]; then
-    echo "$1"
-    exit 1
-  fi
-}
 
-wait_for_port() {
-    local port=$1
-    local timeout=$2
-    local service_name=$3
-    echo "Waiting for $service_name to be ready on localhost:$port..."
+echo
+echo "Starting services..."
+startup_anvil
+startup_vlayer
 
-    timeout "$timeout" bash -c "until nc -z localhost $port; do sleep 3; done"
-    check_exit_status "Error: Timeout reached. $service_name is not available on localhost:$port."
-}
-
-VLAYER_HOME=$(pwd)
-LOGS_DIR="${VLAYER_HOME}/logs"
-mkdir -p "$LOGS_DIR"
-
-echo "Starting Anvil"
-anvil >"${LOGS_DIR}/anvil.out" 2>"${LOGS_DIR}/anvil.err" &
-ANVIL_PID=$!
-echo "Anvil started with PID $ANVIL_PID."
-wait_for_port 8545 30s "Anvil"
-
-echo "Starting Vlayer REST server"
-cd "${VLAYER_HOME}/rust"
-RUST_LOG=info cargo run --bin vlayer serve >"${LOGS_DIR}/vlayer_serve.out" 2>"${LOGS_DIR}/vlayer_serve.err" &
-VLAYER_SERVER_PID=$!
-echo "Vlayer server started with PID ${VLAYER_SERVER_PID}."
-wait_for_port 3000 1h "vlayer server"
-
+echo
 echo "Deploying smart contracts"
-cd "${VLAYER_HOME}/examples/${EXAMPLE}"
-source ../../bash/vlayer-deploy.sh ${EXAMPLE}
+EXAMPLE_PROVER_ADDRESS=$(deploy_contracts "${EXAMPLE}")
 
-# Setting up the example contracts to be called  during e2e tests.
-# When creating a new example, add here the address name corresponding to 
-# the name of the contract in vlayer-deploy.sh that you want to call.
-simple="SIMPLE_PROVER_ADDRESS"
-simple_travel="SIMPLE_TRAVEL_PROVER_ADDRESS"
+# echo "Sending v_call request to vlayer JSON-RPC server"
+# v_call_request_json=$(cat <<EOF
+# {
+#     "method": "v_call",
+#     "params": [
+#         {"caller": "${CALLER}", "to": "${EXAMPLE_ADDRESS}", "data": "${DATA}"},
+#         {"block_no": ${BLOCK_NO}, "chain_id": 11155111}
+#     ],
+#     "id": 1,
+#     "jsonrpc": "2.0"
+# }
+# EOF
+# )
 
-eval "EXAMPLE_ADDRESS_VAR=\${${EXAMPLE}}"
-eval "EXAMPLE_ADDRESS=\${${EXAMPLE_ADDRESS_VAR}}"
-
-echo "Sending v_call request to Vlayer REST server"
-v_call_request_json=$(cat <<EOF
-{
-    "method": "v_call",
-    "params": [
-        {"caller": "${CALLER}", "to": "${EXAMPLE_ADDRESS}", "data": "${DATA}"},
-        {"block_no": ${BLOCK_NO}, "chain_id": 11155111}
-    ],
-    "id": 1,
-    "jsonrpc": "2.0"
-}
-EOF
-)
-
-curl -X POST http://127.0.0.1:3000/ -H "Content-Type: application/json" -d "$v_call_request_json"
+# curl -sX POST http://127.0.0.1:3000/ -H "Content-Type: application/json" -d "$v_call_request_json"
 
 # TODO:
 # send proof to verification smart contract
