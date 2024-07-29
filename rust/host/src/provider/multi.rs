e14c114beb590391579e590176628d424a4118c9
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-07-29 15:20:40 +0200

    Remove caller from (Server)Call and tests

--- rust/host/src/provider/multi.rs
+++ rust/host/src/provider/multi.rs
@@ -55,7 +55,7 @@ mod get {
 
     #[test]
     fn gets_cached_provider() -> anyhow::Result<()> {
-        let path_buf = PathBuf::from("testdata/mainnet_uniswap_exact_output_single_rpc_cache.json");
+        let path_buf = PathBuf::from("testdata/mainnet_uniswap_factory_owner_rpc_cache.json");
         let provider = Rc::new(FileProvider::from_file(&path_buf)?);
 
         let cache = RefCell::new(HashMap::from([(MAINNET_ID, Rc::clone(&provider))]));
@@ -78,7 +78,7 @@ mod get {
     fn gets_created_provider() -> anyhow::Result<()> {
         let rpc_file_cache = HashMap::from([(
             MAINNET_ID,
-            "testdata/mainnet_uniswap_exact_output_single_rpc_cache.json".to_string(),
+            "testdata/mainnet_uniswap_factory_owner_rpc_cache.json".to_string(),
         )]);
 
         let provider_factory = FileProviderFactory::new(rpc_file_cache);
