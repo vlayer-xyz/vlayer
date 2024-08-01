d07edec17e14a958caed69277af731d4603babd5
Piotr Żelazko <piotr@vlayer.xyz>
2024-08-01 20:16:19 +0200

    fix: use remappings aligned with foundry defaults

--- examples/erc20_balances/src/vlayer/VToyken.sol
+++ examples/erc20_balances/src/vlayer/VToyken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract VToyken is ERC20 {
     constructor() ERC20("VToyken", "VTKN") {
