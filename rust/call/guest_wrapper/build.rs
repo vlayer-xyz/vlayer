725cac576374ca3b4859228a0b5846cac464bb3b
Piotr Żelazko <piotr@vlayer.xyz>
2024-08-02 17:55:07 +0200

    feat: build ImageID to rust compilation assets

--- rust/call/guest_wrapper/build.rs
+++ rust/call/guest_wrapper/build.rs
@@ -3,15 +3,30 @@ fn main() {
     {
         use risc0_build_ethereum::generate_solidity_files;
         use std::env;
-
-        // relative path, where to put ImageID.sol file
-        const SOLIDITY_IMAGE_ID_PATH: &str = "../../contracts/src/ImageID.sol";
+        use std::fs;
+        use std::path::Path;
 
         // Generate Solidity source files for use with Forge.
-        let out_dir = env::var_os("OUT_DIR").unwrap().into_string().unwrap();
+        let build_profile = env::var_os("PROFILE").unwrap().into_string().unwrap();
+        let assets_path_str = format!("../../target/{build_profile}/assets");
+        let assets_path = Path::new(&assets_path_str);
+
+        if !assets_path.exists() {
+            println!("{:?}", assets_path);
+            fs::create_dir(assets_path).expect("Could not create assets directory");
+        }
+
+        let image_id_path = assets_path.join("ImageID.sol");
+        let elf_path = assets_path.join("Elf.sol");
+
+        fs::remove_file(&image_id_path)
+            .unwrap_or_else(|err| println!("Did not remove old ImageID file: {}", err));
+        fs::remove_file(&elf_path)
+            .unwrap_or_else(|err| println!("Did not remove old elf file: {}", err));
+
         let solidity_opts = risc0_build_ethereum::Options::default()
-            .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
-            .with_elf_sol_path(format!("{out_dir}/Elf.sol"));
+            .with_image_id_sol_path(image_id_path)
+            .with_elf_sol_path(elf_path);
 
         let guests = risc0_build::embed_methods();
         generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
