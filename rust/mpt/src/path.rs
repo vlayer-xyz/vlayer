7c1b3d8ad1b216a1b1b3a9bace2bb14223902577
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-02 15:43:10 +0200

    impl<T> From<T> for Path where T: AsRef<[u8]>

--- rust/mpt/src/path.rs
+++ rust/mpt/src/path.rs
@@ -1,48 +1,72 @@
 use nybbles::Nibbles;
 
-#[inline]
-pub fn decode_path(path: impl AsRef<[u8]>) -> (Nibbles, bool) {
-    let path = Nibbles::unpack(path);
-    assert!(path.len() >= 2);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PathKind {
+    Leaf,
+    Extension,
+}
 
-    let is_leaf = path[0] & 2 != 0;
-    let odd_nibbles = path[0] & 1 != 0;
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Path {
+    pub nibbles: Nibbles,
+    pub kind: PathKind,
+}
 
-    let prefix = if odd_nibbles { &path[1..] } else { &path[2..] };
-    (Nibbles::from_nibbles_unchecked(prefix), is_leaf)
+impl<T> From<T> for Path
+where
+    T: AsRef<[u8]>,
+{
+    fn from(path: T) -> Self {
+        let path = Nibbles::unpack(path);
+        assert!(path.len() >= 2);
+
+        let kind = if path[0] & 2 != 0 {
+            PathKind::Leaf
+        } else {
+            PathKind::Extension
+        };
+        let odd_nibbles = path[0] & 1 != 0;
+
+        let prefix = if odd_nibbles { &path[1..] } else { &path[2..] };
+        let nibbles = Nibbles::from_nibbles_unchecked(prefix);
+        Path { nibbles, kind }
+    }
 }
 
 #[cfg(test)]
 mod decode_path {
+    use super::PathKind::*;
+    use crate::path::Path;
+
     #[test]
     fn even_extension() {
-        let (path, is_leaf) = super::decode_path(&[0x00, 0x12]);
+        let path: Path = [0x00, 0x12].into();
 
-        assert!(!is_leaf);
-        assert_eq!(path.as_slice(), &[0x1, 0x2][..]);
+        assert_eq!(path.kind, Extension);
+        assert_eq!(path.nibbles.as_slice(), &[0x1, 0x2][..]);
     }
 
     #[test]
     fn odd_extension() {
-        let (path, is_leaf) = super::decode_path(&[0x11, 0x23]);
+        let path: Path = [0x11, 0x23].into();
 
-        assert!(!is_leaf);
-        assert_eq!(path.as_slice(), &[0x1, 0x2, 0x3][..]);
+        assert_eq!(path.kind, Extension);
+        assert_eq!(path.nibbles.as_slice(), &[0x1, 0x2, 0x3][..]);
     }
 
     #[test]
     fn even_leaf() {
-        let (path, is_leaf) = super::decode_path(&[0x20, 0x12]);
+        let path: Path = [0x20, 0x12].into();
 
-        assert!(is_leaf);
-        assert_eq!(path.as_slice(), &[0x1, 0x2][..]);
+        assert_eq!(path.kind, Leaf);
+        assert_eq!(path.nibbles.as_slice(), &[0x1, 0x2][..]);
     }
 
     #[test]
     fn odd_leaf() {
-        let (path, is_leaf) = super::decode_path(&[0x31, 0x23]);
+        let path: Path = [0x31, 0x23].into();
 
-        assert!(is_leaf);
-        assert_eq!(path.as_slice(), &[0x1, 0x2, 0x3][..]);
+        assert_eq!(path.kind, Leaf);
+        assert_eq!(path.nibbles.as_slice(), &[0x1, 0x2, 0x3][..]);
     }
 }
