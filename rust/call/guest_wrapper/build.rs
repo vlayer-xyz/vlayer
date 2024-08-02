508243553b2e0fe1e9248752e619fd5174be32de
Piotr Żelazko <piotr@vlayer.xyz>
2024-08-02 16:50:18 +0200

    feat: use ImageID generated by guest

--- rust/call/guest_wrapper/build.rs
+++ rust/call/guest_wrapper/build.rs
@@ -4,10 +4,13 @@ fn main() {
         use risc0_build_ethereum::generate_solidity_files;
         use std::env;
 
+        // relative path, where to put ImageID.sol file
+        const SOLIDITY_IMAGE_ID_PATH: &str = "../../contracts/src/ImageID.sol";
+
         // Generate Solidity source files for use with Forge.
         let out_dir = env::var_os("OUT_DIR").unwrap().into_string().unwrap();
         let solidity_opts = risc0_build_ethereum::Options::default()
-            .with_image_id_sol_path(format!("{out_dir}/ImageId.sol"))
+            .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
             .with_elf_sol_path(format!("{out_dir}/Elf.sol"));
 
         let guests = risc0_build::embed_methods();
