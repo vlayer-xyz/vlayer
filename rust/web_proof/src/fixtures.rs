9cf084405489e8cde01a7fccf703312ea27cddd0
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-05 13:22:43 +0200

    Remove dbg

--- rust/web_proof/src/fixtures.rs
+++ rust/web_proof/src/fixtures.rs
@@ -9,7 +9,7 @@ use crate::types::WebProof;
 pub const NOTARY_PUB_KEY_PEM_EXAMPLE: &str = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
 
 pub fn read_fixture(path: &str) -> String {
-    str::from_utf8(&fs::read(dbg!(path)).unwrap())
+    str::from_utf8(&fs::read(path).unwrap())
         .unwrap()
         .to_string()
         .replace('\n', "\r\n")
