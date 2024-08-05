1c81f7300fb3490c9e4d864bcc7399834972726d
vanruch <ivanruch@gmail.com>
2024-08-05 20:56:48 +0200

    make proofs

--- rust/test_runner/src/mock_provider.rs
+++ rust/test_runner/src/mock_provider.rs
@@ -1,17 +1,25 @@
 use alloy_sol_types::private::{Address, Bytes, U256};
+use alloy_trie::proof::ProofRetainer;
+use alloy_trie::{HashBuilder, Nibbles};
 use ethers_core::types::BlockNumber as BlockTag;
 use ethers_core::utils::keccak256;
+use forge::revm::db::DbAccount;
+use forge::revm::primitives::alloy_primitives::private::alloy_rlp;
+use forge::revm::primitives::alloy_primitives::private::alloy_rlp::Encodable;
 use forge::revm::primitives::alloy_primitives::{
     BlockNumber, ChainId, StorageKey, StorageValue, TxNumber,
 };
-use forge::revm::primitives::{b256, Account, Bytecode};
+use forge::revm::primitives::{
+    b256, Account, AccountInfo, Bytecode, EvmStorageSlot, FixedBytes, B256,
+};
 use forge::revm::{Database, Evm};
 use forge::revm::{DatabaseRef, JournaledState};
 use foundry_evm_core::backend::{Backend, FoundryEvmInMemoryDB};
 use host::host::error::HostError;
-use host::proof::EIP1186Proof;
+use host::proof::{EIP1186Proof, StorageProof};
 use host::provider::factory::ProviderFactory;
 use host::provider::BlockingProvider;
+use std::collections::HashMap;
 use std::convert::Infallible;
 use vlayer_engine::block_header::eth::EthBlockHeader;
 use vlayer_engine::block_header::EvmBlockHeader;
@@ -42,7 +50,7 @@ impl<'a> BlockingProvider for MockProvider {
     ) -> Result<Option<Box<dyn EvmBlockHeader>>, Self::Error> {
         Ok(Some(Box::new(EthBlockHeader {
             number: 15537395,
-            state_root: b256!("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"),
+            state_root: b256!("c755f1beb877e572a1a56a133898967fbc7c1a9b8bce71f1330ab841d8bc66e8"),
             ..EthBlockHeader::default()
         })))
     }
@@ -62,17 +70,11 @@ impl<'a> BlockingProvider for MockProvider {
         _storage_keys: Vec<StorageKey>,
         _block: BlockNumber,
     ) -> Result<EIP1186Proof, Self::Error> {
-        let Some(account) = self.try_get_account(address) else {
+        let Some(_) = self.try_get_account(address) else {
             return Ok(EIP1186Proof::default());
         };
 
-        Ok(EIP1186Proof {
-            balance: account.info.balance,
-            address,
-            nonce: account.info.nonce,
-            code_hash: account.info.code_hash,
-            ..EIP1186Proof::default()
-        })
+        Ok(prove_account_at(&self.state, address, _storage_keys))
     }
 
     fn get_storage_at(
@@ -92,11 +94,10 @@ impl<'a> BlockingProvider for MockProvider {
 
     fn get_transaction_count(
         &self,
-        _address: Address,
+        address: Address,
         _block: BlockNumber,
     ) -> Result<TxNumber, Self::Error> {
-        dbg!("get_transaction_count");
-        Ok(0)
+        Ok(self.try_get_account(address).unwrap_or_default().info.nonce)
     }
 }
 
@@ -110,4 +111,143 @@ impl ProviderFactory<MockProvider> for MockProviderFactory {
             state: self.state.clone(),
         })
     }
+}
+
+fn prove_account_at(
+    journaled_state: &JournaledState,
+    address: Address,
+    keys: Vec<B256>,
+) -> EIP1186Proof {
+    let state = journaled_state.state.clone();
+    let account = journaled_state.account(address);
+
+    let mut builder =
+        HashBuilder::default().with_proof_retainer(ProofRetainer::new(vec![Nibbles::unpack(
+            keccak256(address),
+        )]));
+
+    for (key, account) in trie_accounts(&state) {
+        builder.add_leaf(key, &account);
+    }
+
+    let _ = builder.root();
+
+    let proof = builder.take_proofs().values().cloned().collect::<Vec<_>>();
+    let storage_proofs = prove_storage(&account.storage, &keys);
+
+    let account_proof = EIP1186Proof {
+        address,
+        balance: account.info.balance,
+        nonce: account.info.nonce,
+        code_hash: account.info.code_hash,
+        storage_hash: storage_root(&account.storage),
+        account_proof: proof.into(),
+        storage_proof: keys
+            .into_iter()
+            .zip(storage_proofs)
+            .map(|(key, proof)| {
+                let storage_key: U256 = key.into();
+                let value = account
+                    .storage
+                    .get(&storage_key)
+                    .cloned()
+                    .unwrap_or_default()
+                    .present_value;
+                StorageProof { key, value, proof }
+            })
+            .collect(),
+    };
+
+    account_proof
+}
+
+pub fn trie_accounts(accounts: &HashMap<Address, Account>) -> Vec<(Nibbles, Vec<u8>)> {
+    let mut accounts = accounts
+        .iter()
+        .map(|(address, account)| {
+            let data = trie_account_rlp(&account.info, &account.storage);
+            (
+                Nibbles::unpack(alloy_sol_types::private::keccak256(*address)),
+                data,
+            )
+        })
+        .collect::<Vec<_>>();
+    accounts.sort_by(|(key1, _), (key2, _)| key1.cmp(key2));
+
+    accounts
+}
+
+pub fn trie_account_rlp(info: &AccountInfo, storage: &HashMap<U256, EvmStorageSlot>) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::new();
+    let list: [&dyn Encodable; 4] = [
+        &info.nonce,
+        &info.balance,
+        &storage_root(storage),
+        &info.code_hash,
+    ];
+
+    alloy_rlp::encode_list::<_, dyn Encodable>(&list, &mut out);
+
+    out
+}
+
+pub fn storage_root(storage: &HashMap<U256, EvmStorageSlot>) -> B256 {
+    build_root(trie_storage(storage))
+}
+
+pub fn build_root(values: impl IntoIterator<Item = (Nibbles, Vec<u8>)>) -> B256 {
+    let mut builder = HashBuilder::default();
+    for (key, value) in values {
+        builder.add_leaf(key, value.as_ref());
+    }
+    builder.root()
+}
+
+pub fn trie_storage(storage: &HashMap<U256, EvmStorageSlot>) -> Vec<(Nibbles, Vec<u8>)> {
+    let mut storage = storage
+        .iter()
+        .map(|(key, value)| {
+            let data = alloy_rlp::encode(value.present_value);
+            (
+                Nibbles::unpack(alloy_sol_types::private::keccak256(key.to_be_bytes::<32>())),
+                data,
+            )
+        })
+        .collect::<Vec<_>>();
+    storage.sort_by(|(key1, _), (key2, _)| key1.cmp(key2));
+
+    storage
+}
+
+pub fn prove_storage(
+    storage: &HashMap<U256, EvmStorageSlot>,
+    keys: &Vec<FixedBytes<32>>,
+) -> Vec<Vec<Bytes>> {
+    let keys: Vec<_> = keys
+        .iter()
+        .map(|key| Nibbles::unpack(alloy_sol_types::private::keccak256(key)))
+        .collect();
+
+    let mut builder = HashBuilder::default().with_proof_retainer(ProofRetainer::new(keys.clone()));
+
+    for (key, value) in trie_storage(storage) {
+        builder.add_leaf(key, &value);
+    }
+
+    let _ = builder.root();
+
+    let mut proofs = Vec::new();
+    let all_proof_nodes = builder.take_proofs();
+
+    for proof_key in keys {
+        // Iterate over all proof nodes and find the matching ones.
+        // The filtered results are guaranteed to be in order.
+        let matching_proof_nodes = all_proof_nodes
+            .iter()
+            .filter(|(path, _)| proof_key.starts_with(path))
+            .map(|(_, node)| node.clone());
+        proofs.push(matching_proof_nodes.collect());
+    }
+
+    proofs
 }
