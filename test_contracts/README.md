7e1588a075ca8375f93ae20dd39828431737122f
wgromniak2 <wiktor@vlayer.xyz>
2024-08-05 13:16:46 +0200

    Added test_contract solidity project.

---
+++ test_contracts/README.md
@@ -0,0 +1,4 @@
+# Test Contracts
+
+This is a foundry project, which consists of contracts which are used in test only
+(e.g. contracts which are use in integration tests of rust projects).
---
+++ test_contracts/src/vlayer/ExampleProver.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Prover} from "vlayer/Prover.sol";
+import {Web} from "vlayer/Web.sol";
+
+/*
+* This contract is used in rust/server integration tests. The test fixture
+* (compiled contract) is placed in rust/server/testdata/ExampleProver.json.
+* 
+* In order to update the test fixture:
+* 1. Modify this contract below.
+* 2. cd test_contracts && forge build
+* 3. cp out/ExampleProver.sol/ExampleProver.json ../rust/server/testdata
+*/
+
+contract ExampleProver is Prover {
+    constructor() {}
+
+    function sum(uint256 lhs, uint256 rhs) public pure returns (uint256) {
+        return lhs + rhs;
+    }
+
+    function web_proof(Web calldata web) public returns (bool) {
+        // require(web.url.equal(dataUrl), "Incorrect URL");
+
+        return true;
+    }
+}
