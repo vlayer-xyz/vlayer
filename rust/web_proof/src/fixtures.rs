81f0720963d4c13235fa38ebd2ed4bf32f84532b
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-05 11:50:31 +0200

    Add webproof_example function

--- rust/web_proof/src/fixtures.rs
+++ rust/web_proof/src/fixtures.rs
@@ -3,6 +3,8 @@ use p256::{ecdsa, PublicKey};
 use pkcs8::DecodePublicKey;
 use tlsn_core::proof::TlsProof;
 
+use crate::types::WebProof;
+
 pub const NOTARY_PUB_KEY_PEM_EXAMPLE: &str = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
 
 pub fn notary_pub_key_example() -> PublicKey {
@@ -20,6 +22,13 @@ pub fn received_response_example() -> String {
 pub fn tls_proof_example() -> TlsProof {
     serde_json::from_str(str::from_utf8(include_bytes!("../testdata/tls_proof.json")).unwrap())
         .unwrap()
+}
+
+pub fn webproof_example() -> WebProof {
+    WebProof {
+        tls_proof: tls_proof_example(),
+        notary_pub_key: notary_pub_key_example(),
+    }
 }
 
 pub fn invalid_tls_proof_example() -> TlsProof {
