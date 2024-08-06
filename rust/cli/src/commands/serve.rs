281da96489999c82e897eeeebf0c01aa92765f03
Stanisław Durka <stanislaw@vlayer.xyz>
2024-08-06 11:52:27 +0200

    remove hardcoded URL and port from vlayer serve

--- rust/cli/src/commands/serve.rs
+++ rust/cli/src/commands/serve.rs
@@ -2,15 +2,9 @@ use crate::errors::CLIError;
 use server::server::{serve, Config};
 use tracing::info;
 
-const URL: &str = "http://localhost:8545";
-const PORT: u16 = 3000;
-
 pub(crate) async fn run_serve() -> Result<(), CLIError> {
     info!("Running vlayer serve...");
-    let config = Config {
-        url: URL.to_string(),
-        port: PORT,
-    };
+    let config: Config = Config::default();
     serve(config).await?;
     Ok(())
 }
