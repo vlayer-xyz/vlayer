56544ab0a47a4b5bc6a86ab49eaa4f745e0ad767
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-06 14:05:24 +0200

    Introduce setup method for TestHelper

--- rust/server/tests/test_helpers/anvil.rs
+++ rust/server/tests/test_helpers/anvil.rs
@@ -12,34 +12,44 @@ use std::{sync::Arc, time::Duration};
 
 abigen!(ExampleProver, "./testdata/ExampleProver.json",);
 
+#[derive(Default)]
 pub(crate) struct _TestHelper {
-    pub _client: Arc<SignerMiddleware<Provider<Http>, Wallet<ecdsa::SigningKey>>>,
-    pub anvil: AnvilInstance,
+    pub _client: Option<Arc<SignerMiddleware<Provider<Http>, Wallet<ecdsa::SigningKey>>>>,
+    anvil: Option<AnvilInstance>,
 }
 
-pub(crate) async fn _test_helper() -> _TestHelper {
-    let (_client, anvil) = setup_anvil().await;
-    let test_helper = _TestHelper { _client, anvil };
+pub(crate) async fn test_helper() -> _TestHelper {
+    let mut test_helper = _TestHelper::default();
+    test_helper._setup().await;
     test_helper
 }
 
-async fn setup_anvil() -> (
-    Arc<SignerMiddleware<Provider<Http>, Wallet<ecdsa::SigningKey>>>,
-    AnvilInstance,
-) {
-    let anvil = Anvil::new().spawn();
-    let wallet: LocalWallet = anvil.keys()[0].clone().into();
-    let provider = Provider::<Http>::try_from(anvil.endpoint())
-        .unwrap()
-        .interval(Duration::from_millis(10u64));
-    let client = Arc::new(SignerMiddleware::new(
-        provider,
-        wallet.with_chain_id(anvil.chain_id()),
-    ));
-    ExampleProver::deploy(client.clone(), ())
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    (client, anvil)
+impl _TestHelper {
+    pub(crate) async fn _setup(&mut self) {
+        let anvil = self.setup_anvil().await;
+        self.anvil = Some(anvil);
+    }
+
+    pub(crate) fn anvil(&self) -> &AnvilInstance {
+        self.anvil.as_ref().unwrap()
+    }
+
+    async fn setup_anvil(&mut self) -> AnvilInstance {
+        let anvil = Anvil::new().spawn();
+        let wallet: LocalWallet = anvil.keys()[0].clone().into();
+        let provider = Provider::<Http>::try_from(anvil.endpoint())
+            .unwrap()
+            .interval(Duration::from_millis(10u64));
+        let client = Arc::new(SignerMiddleware::new(
+            provider,
+            wallet.with_chain_id(anvil.chain_id()),
+        ));
+        self._client = Some(client.clone());
+        ExampleProver::deploy(client, ())
+            .unwrap()
+            .send()
+            .await
+            .unwrap();
+        anvil
+    }
 }
