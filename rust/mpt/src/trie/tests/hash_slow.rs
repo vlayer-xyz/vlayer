7392605f7db5573bf4ce35bcb0462577165c6d4b
Adam Król <adamkrol@MacBook-Pro-Adam.local>
2024-08-08 16:21:57 +0200

    Set lazy static value without additional function

--- rust/mpt/src/trie/tests/hash_slow.rs
+++ rust/mpt/src/trie/tests/hash_slow.rs
@@ -5,7 +5,18 @@ use lazy_static::lazy_static;
 use nybbles::Nibbles;
 
 lazy_static! {
-    static ref MPT_WITHOUT_VALUE: MerkleTrie = setup_branch_test(None);
+    static ref MPT_BRANCH_WITH_TWO_CHILDREN: MerkleTrie = {
+        let mut children: [Option<Box<Node>>; 16] = Default::default();
+        children[0] = Some(Box::new(Node::Leaf(
+            Nibbles::from_nibbles([0; 63]),
+            vec![0].into(),
+        )));
+        children[1] = Some(Box::new(Node::Leaf(
+            Nibbles::from_nibbles([1; 63]),
+            vec![1].into(),
+        )));
+        MerkleTrie(Node::Branch(children, None))
+    };
 }
 
 #[test]
@@ -41,7 +52,7 @@ fn extension() {
 
 #[test]
 fn branch() {
-    let mpt = MPT_WITHOUT_VALUE.clone();
+    let mpt = MPT_BRANCH_WITH_TWO_CHILDREN.clone();
     assert_eq!(
         mpt.hash_slow(),
         b256!("f09860d0bbaa3a755a53bbeb7b06824cdda5ac2ee5557d14aa49117a47bd0a3e")
@@ -50,29 +61,15 @@ fn branch() {
 
 #[test]
 fn branch_with_value() {
-    let mut mpt = MPT_WITHOUT_VALUE.clone();
-    let value = vec![42u8].into();
+    let mut mpt = MPT_BRANCH_WITH_TWO_CHILDREN.clone();
 
     let MerkleTrie(Node::Branch(_, ref mut branch_value)) = mpt else {
         panic!("Expected a Branch node");
     };
-    *branch_value = Some(value);
+    *branch_value = Some(vec![42u8].into());
 
     assert_eq!(
         mpt.hash_slow(),
         b256!("81b2bafe48b11cf92d7b5d765155e3cab7b87f7e0e2fa8181c3535552cdafc40")
     );
-}
-
-fn setup_branch_test(value: Option<Box<[u8]>>) -> MerkleTrie {
-    let mut children: [Option<Box<Node>>; 16] = Default::default();
-    children[0] = Some(Box::new(Node::Leaf(
-        Nibbles::from_nibbles([0; 63]),
-        vec![0].into(),
-    )));
-    children[1] = Some(Box::new(Node::Leaf(
-        Nibbles::from_nibbles([1; 63]),
-        vec![1].into(),
-    )));
-    MerkleTrie(Node::Branch(children, value))
 }
