dd739a1115906406c4e8290f014cca6a6d6c0d0c
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-08 11:23:34 +0200

    Move all packages related to the call service to it's own subfolder to prepare for blocks service

--- rust/call/engine/src/sol/proof.rs
+++ rust/call/engine/src/sol/proof.rs
@@ -2,7 +2,7 @@ mod private {
     use crate::ExecutionCommitment;
     use crate::Seal;
 
-    alloy_sol_types::sol!("../../contracts/src/Proof.sol");
+    alloy_sol_types::sol!("../../../contracts/src/Proof.sol");
 }
 
 pub use private::Proof;
