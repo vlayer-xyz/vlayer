9b87b59e69b539371feb356d7924f1ee7f6d7084
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-08-09 12:13:11 +0200

    Deploy contract on other chain (mainnet) and call it from the prover

---
+++ examples/simple_travel/src/vlayer/OtherChainContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract OtherChainContract {
+    function example() public pure returns (uint256) {
+        return 1;
+    }
+}
