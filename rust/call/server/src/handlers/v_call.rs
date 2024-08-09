f09a27d32c4126cb44a98630775b142f78344b9f
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-09 14:07:34 +0200

    Add JsonRpcHandler trait and implement it for VCall

--- rust/call/server/src/handlers/v_call.rs
+++ rust/call/server/src/handlers/v_call.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use crate::error::AppError;
 use crate::server::ServerConfig;
+use async_trait::async_trait;
 use call_engine::io::Augmentors;
 use call_host::host::{config::HostConfig, Host};
 use call_host::Call as HostCall;
@@ -9,7 +10,40 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use types::{Call, CallContext, CallResult};
 
+use super::JsonRpcHandler;
+
 pub mod types;
+
+pub struct VCall;
+
+#[async_trait]
+impl JsonRpcHandler for VCall {
+    type Params = Params;
+    type Config = Arc<ServerConfig>;
+    type Output = CallResult;
+    type Error = AppError;
+    async fn call(params: Self::Params, config: Self::Config) -> Result<Self::Output, Self::Error> {
+        let call: HostCall = params.call.try_into()?;
+
+        let host_config = HostConfig {
+            rpc_urls: config.rpc_urls.clone(),
+            start_chain_id: params.context.chain_id,
+        };
+    
+        let return_data =
+            tokio::task::spawn_blocking(|| Host::try_new(host_config)?.run(call, params.augmentors))
+                .await??;
+    
+        Ok(CallResult {
+            result: json!({
+                "evm_call_result": return_data.guest_output.evm_call_result,
+                "function_selector": return_data.guest_output.execution_commitment.functionSelector,
+                "prover_contract_address": return_data.guest_output.execution_commitment.proverContractAddress,
+                "seal": return_data.seal
+            }),
+        })
+    }
+}
 
 #[derive(Deserialize, Serialize)]
 pub struct Params {
@@ -17,29 +51,4 @@ pub struct Params {
     context: CallContext,
     #[serde(default)]
     augmentors: Option<Augmentors>,
-}
-
-pub(crate) async fn call(
-    params: Params,
-    config: Arc<ServerConfig>,
-) -> Result<CallResult, AppError> {
-    let call: HostCall = params.call.try_into()?;
-
-    let host_config = HostConfig {
-        rpc_urls: config.rpc_urls.clone(),
-        start_chain_id: params.context.chain_id,
-    };
-
-    let return_data =
-        tokio::task::spawn_blocking(|| Host::try_new(host_config)?.run(call, params.augmentors))
-            .await??;
-
-    Ok(CallResult {
-        result: json!({
-            "evm_call_result": return_data.guest_output.evm_call_result,
-            "function_selector": return_data.guest_output.execution_commitment.functionSelector,
-            "prover_contract_address": return_data.guest_output.execution_commitment.proverContractAddress,
-            "seal": return_data.seal
-        }),
-    })
 }
