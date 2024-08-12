3eff99dd59e4e6b6b37d590de83674d6ffc69a01
Adam Król <adamkrol@MacBook-Pro-Adam.local>
2024-08-12 17:42:39 +0200

    Use From instead of new constructor for KeyNibbles

--- rust/mpt/src/node.rs
+++ rust/mpt/src/node.rs
@@ -194,13 +194,13 @@ mod node_size {
 
     #[test]
     fn leaf() {
-        let node = Node::Leaf(KeyNibbles::new([0x1]), Box::new([]));
+        let node = Node::Leaf([0x1].into(), Box::new([]));
         assert_eq!(node.size(), 1);
     }
 
     #[test]
     fn extension() {
-        let key_nibbles = KeyNibbles::new([0x1]);
+        let key_nibbles: KeyNibbles = [0x1].into();
         let leaf = Node::Leaf(key_nibbles.clone(), Box::new([]));
         let extension = Node::Extension(key_nibbles, Box::new(leaf));
         assert_eq!(extension.size(), 2);
@@ -208,7 +208,7 @@ mod node_size {
 
     #[test]
     fn branch_one_child() {
-        let leaf = Node::Leaf(KeyNibbles::new([0x1]), Box::new([]));
+        let leaf = Node::Leaf([0x1].into(), Box::new([]));
         let child = Some(Box::new(leaf));
         const NULL_CHILD: Option<Box<Node>> = None;
         let mut children = [NULL_CHILD; 16];
@@ -219,7 +219,7 @@ mod node_size {
 
     #[test]
     fn branch_many_children() {
-        let leaf = Node::Leaf(KeyNibbles::new([0x1]), Box::new([]));
+        let leaf = Node::Leaf([0x1].into(), Box::new([]));
         let child = Some(Box::new(leaf));
         let children: [_; 16] = from_fn(|_| child.clone());
         let branch = Node::Branch(children, None);
@@ -228,7 +228,7 @@ mod node_size {
 
     #[test]
     fn branch_with_value() {
-        let leaf = Node::Leaf(KeyNibbles::new([0x1]), Box::new([]));
+        let leaf = Node::Leaf([0x1].into(), Box::new([]));
         let child = Some(Box::new(leaf));
         const NULL_CHILD: Option<Box<Node>> = None;
         let mut children = [NULL_CHILD; 16];
