fc062184c0710618167b617a4b9ad1422d75efc6
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-12 16:27:50 +0200

    Move all field validation to server_utils

---
+++ rust/server_utils/src/field_validation.rs
@@ -0,0 +1,54 @@
+use alloy_primitives::hex::FromHexError as AlloyFromHexError;
+use alloy_primitives::Address;
+use hex::FromHexError;
+use thiserror::Error;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum FieldValidationError {
+    #[error("`{field}` {error} `{value}`")]
+    InvalidHex {
+        field: String,
+        value: String,
+        error: FromHexError,
+    },
+    #[error("`{field}` Invalid hex prefix `{value}`")]
+    InvalidHexPrefix { field: String, value: String },
+}
+
+pub fn parse_address_field(
+    field_name: &str,
+    address: String,
+) -> Result<Address, FieldValidationError> {
+    address
+        .parse()
+        .map_err(alloy_hex_error_to_standard_hex_error)
+        .map_err(|error| FieldValidationError::InvalidHex {
+            field: field_name.to_string(),
+            value: address,
+            error,
+        })
+}
+
+pub fn parse_hex_field(field_name: &str, hex: String) -> Result<Vec<u8>, FieldValidationError> {
+    if !hex.starts_with("0x") {
+        return Err(FieldValidationError::InvalidHexPrefix {
+            field: field_name.to_string(),
+            value: hex,
+        });
+    }
+    hex::decode(&hex[2..]).map_err(|error| FieldValidationError::InvalidHex {
+        field: field_name.to_string(),
+        value: hex,
+        error,
+    })
+}
+
+fn alloy_hex_error_to_standard_hex_error(err: AlloyFromHexError) -> FromHexError {
+    match err {
+        AlloyFromHexError::InvalidHexCharacter { c, index } => {
+            FromHexError::InvalidHexCharacter { c, index }
+        }
+        AlloyFromHexError::InvalidStringLength => FromHexError::InvalidStringLength,
+        AlloyFromHexError::OddLength => FromHexError::OddLength,
+    }
+}
--- rust/server_utils/src/lib.rs
+++ rust/server_utils/src/lib.rs
@@ -1,15 +1,13 @@
-mod error;
+mod field_validation;
 mod handler;
 mod json_rpc;
 mod layers;
 mod proof_mode;
 mod test_utils;
-mod utils;
 
-pub use error::*;
+pub use field_validation::*;
 pub use handler::*;
 pub use json_rpc::*;
 pub use layers::*;
 pub use proof_mode::*;
 pub use test_utils::*;
-pub use utils::*;
