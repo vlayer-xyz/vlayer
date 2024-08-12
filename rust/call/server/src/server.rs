7822b6d3e4683cf5555c385c7e6d0ef7cd727ebb
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-12 14:54:48 +0200

    Move json_rpc router to server_utils

--- rust/call/server/src/server.rs
+++ rust/call/server/src/server.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
-use crate::{handlers::v_call::VCall, json_rpc::route};
+use crate::handlers::v_call::VCall;
 use alloy_primitives::ChainId;
 use axum::{routing::post, Router};
 use call_engine::config::{MAINNET_ID, SEPOLIA_ID};
 use serde::{Deserialize, Serialize};
-use server_utils::{init_trace_layer, RequestIdLayer};
+use server_utils::{init_trace_layer, route, RequestIdLayer};
 use std::collections::HashMap;
 use tracing::info;
 
--- rust/server_utils/src/lib.rs
+++ rust/server_utils/src/lib.rs
@@ -1,11 +1,13 @@
 mod error;
 mod handler;
+mod json_rpc;
 mod layers;
 mod test_utils;
 mod utils;
 
 pub use error::*;
 pub use handler::*;
+pub use json_rpc::*;
 pub use layers::*;
 pub use test_utils::*;
 pub use utils::*;
