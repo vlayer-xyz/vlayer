969f60e5d5e9a2bb1ca0a4e9e01df0515f172ce1
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-12 16:06:37 +0200

    Move ProofMode to server_utils

--- rust/call/server/src/lib.rs
+++ rust/call/server/src/lib.rs
@@ -3,9 +3,8 @@ pub mod server;
 mod config;
 mod error;
 mod handlers;
-mod proof_mode;
 mod utils;
 
 pub use config::ServerConfig;
-pub use proof_mode::ProofMode;
 pub use server::{serve, server};
+pub use server_utils::ProofMode;
