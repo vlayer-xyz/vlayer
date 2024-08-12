cac091c0c95e4dfa9df29467ed4ff22f335064c5
Leonid Logvinov <logvinov.leon@gmail.com>
2024-08-12 14:51:44 +0200

    Move JsonRpcHandler to server_utils and desugar to impl Future

--- rust/call/server/src/handlers.rs
+++ rust/call/server/src/handlers.rs
@@ -1,10 +1,1 @@
-use axum_jrpc::error::JsonRpcError;
-use serde::{de::DeserializeOwned, Serialize};
-
 pub mod v_call;
-
-pub trait JsonRpcHandler {
-    type Params: DeserializeOwned;
-    type Result: Serialize;
-    async fn call(&self, params: Self::Params) -> Result<Self::Result, impl Into<JsonRpcError>>;
-}
