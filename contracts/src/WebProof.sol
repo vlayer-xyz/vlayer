cc8d4f0933bfbbaaae199ac782196655784ff65a
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-14 12:47:09 +0200

    Add library for WebProof

--- contracts/src/WebProof.sol
+++ contracts/src/WebProof.sol
@@ -4,3 +4,21 @@ pragma solidity ^0.8.13;
 struct WebProof {
     string web_proof_json;
 }
+
+library WebProofLib {
+    function verify(WebProof memory webProof) internal pure returns (bool) {
+        return true;
+    }
+
+    function url(
+        WebProof memory webProof
+    ) internal pure returns (string memory) {
+        return "";
+    }
+
+    function body(
+        WebProof memory webProof
+    ) internal pure returns (string memory) {
+        return "";
+    }
+}
