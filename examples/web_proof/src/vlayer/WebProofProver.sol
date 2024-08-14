0e70a753c790310ac81364e48be4d64613ae49da
Piotr Żelazko <piotr@vlayer.xyz>
2024-08-14 11:07:19 +0200

    fix: allow ImageID.sol directory in examples

--- examples/web_proof/src/vlayer/WebProofProver.sol
+++ examples/web_proof/src/vlayer/WebProofProver.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "openzeppelin/contracts/utils/Strings.sol";
 
 import {Prover} from "vlayer/Prover.sol";
 import {WebProof} from "vlayer/WebProof.sol";
