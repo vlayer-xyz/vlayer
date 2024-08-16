f076d9802e8dc1af349c5e9238708e44adc4ab37
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-16 10:54:41 +0200

    Update contract fixture instructions

--- test_contracts/src/vlayer/ExampleProver.sol
+++ test_contracts/src/vlayer/ExampleProver.sol
@@ -5,14 +5,14 @@ import {Prover} from "vlayer/Prover.sol";
 import {Web} from "vlayer/Web.sol";
 
 /*
-* This contract is used in rust/server integration tests. The test fixture
-* (compiled contract) is placed in rust/server/testdata/ExampleProver.json.
-* 
-* In order to update the test fixture:
-* 1. Modify this contract below.
-* 2. cd test_contracts && forge build
-* 3. cp out/ExampleProver.sol/ExampleProver.json ../rust/server/testdata
-*/
+ * This contract is used in rust/server integration tests. The test fixture
+ * (compiled contract) is placed in rust/server/testdata/ExampleProver.json.
+ *
+ * In order to update the test fixture:
+ * 1. Modify this contract below.
+ * 2. cd test_contracts && forge build
+ * 3. cp out/ExampleProver.sol/ExampleProver.json ../rust/call/server/testdata
+ */
 
 contract ExampleProver is Prover {
     constructor() {}
