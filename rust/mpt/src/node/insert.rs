871fa7e6d4a6e8ac8c5f42408857c9cb077107ef
Adam Król <adam.dawid.krol@gmail.com>
2024-08-19 14:21:52 +0200

    Handle leaf cases in Node::insert

--- rust/mpt/src/node/insert.rs
+++ rust/mpt/src/node/insert.rs
@@ -19,20 +19,12 @@ impl Node {
                 if **old_key == key {
                     panic!("Key already exists");
                 } else {
-                    let (old_key_first_nibble, remaining_old_key) = split_first_nibble(old_key);
-                    let (key_first_nibble, remaining_key) = split_first_nibble(&key);
-
-                    let mut children: [Option<Box<Node>>; 16] = Default::default();
-                    children[old_key_first_nibble as usize] = Some(Box::new(Node::Leaf(
-                        KeyNibbles::from_nibbles(remaining_old_key),
+                    *self = from_two_entries(
+                        (**old_key).clone(),
                         old_value.clone(),
-                    )));
-                    children[key_first_nibble as usize] = Some(Box::new(Node::Leaf(
-                        KeyNibbles::from_nibbles(remaining_key),
+                        key,
                         value.as_ref().into(),
-                    )));
-
-                    *self = Node::Branch(children, None);
+                    );
                 }
             }
             _ => {}
@@ -41,13 +33,62 @@ impl Node {
 }
 
 fn split_first_nibble(nibbles: &Nibbles) -> (u8, Nibbles) {
-    dbg!(nibbles);
     let splitted = nibbles.split_first().unwrap();
-    dbg!(splitted);
     let first_nibble = Nibbles::from_nibbles([*splitted.0]);
     let rest = Nibbles::from_nibbles(splitted.1);
-    dbg!(&first_nibble, &rest);
     (first_nibble[0], rest)
+}
+
+fn from_two_entries(
+    lhs_key: Nibbles,
+    lhs_value: Box<[u8]>,
+    rhs_key: Nibbles,
+    rhs_value: Box<[u8]>,
+) -> Node {
+    if lhs_key.len() <= rhs_key.len() {
+        from_two_ordered_entries(lhs_key, lhs_value, rhs_key, rhs_value)
+    } else {
+        from_two_ordered_entries(rhs_key, rhs_value, lhs_key, lhs_value)
+    }
+}
+
+fn from_two_ordered_entries(
+    lhs_key: Nibbles,
+    lhs_value: Box<[u8]>,
+    rhs_key: Nibbles,
+    rhs_value: Box<[u8]>,
+) -> Node {
+    debug_assert!(lhs_key.len() <= rhs_key.len());
+    if lhs_key == rhs_key {
+        panic!("Key already exists")
+    }
+    let (rhs_first_nibble, remaining_rhs_key) = split_first_nibble(&rhs_key);
+    dbg!(&rhs_first_nibble, &remaining_rhs_key);
+    if lhs_key.is_empty() {
+        let mut children: [Option<Box<Node>>; 16] = Default::default();
+        children[rhs_first_nibble as usize] = Some(Box::new(Node::Leaf(
+            KeyNibbles::from_nibbles(remaining_rhs_key),
+            rhs_value,
+        )));
+        Node::Branch(children, Some(lhs_value))
+    } else {
+        let (lhs_first_nibble, remaining_lhs_key) = split_first_nibble(&lhs_key);
+        dbg!(&lhs_first_nibble, &remaining_lhs_key);
+        if lhs_first_nibble != rhs_first_nibble {
+            let mut children: [Option<Box<Node>>; 16] = Default::default();
+            children[lhs_first_nibble as usize] = Some(Box::new(Node::Leaf(
+                KeyNibbles::from_nibbles(remaining_lhs_key),
+                lhs_value,
+            )));
+            children[rhs_first_nibble as usize] = Some(Box::new(Node::Leaf(
+                KeyNibbles::from_nibbles(remaining_rhs_key),
+                rhs_value,
+            )));
+            Node::Branch(children, None)
+        } else {
+            todo!()
+        }
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +126,14 @@ mod insert {
     }
 
     #[test]
+    #[should_panic(expected = "Key already exists")]
+    fn twice_same_key() {
+        let mut node = Node::Null;
+        node.insert(Nibbles::unpack([0x1]), [42]);
+        node.insert(Nibbles::unpack([0x1]), [43]);
+    }
+
+    #[test]
     fn branch_with_two_children() {
         let mut node = Node::Null;
         node.insert(Nibbles::unpack([0x11]), [42]);
@@ -94,6 +143,36 @@ mod insert {
         if let Node::Branch(ref mut children, _) = expected_branch {
             children[0x1] = Some(Box::new(Node::leaf([0x1], [42])));
             children[0x2] = Some(Box::new(Node::leaf([0x1], [43])));
+        }
+
+        assert_eq!(expected_branch, node);
+    }
+
+    #[test]
+    fn one_branch_one_leaf() {
+        let mut node = Node::Null;
+        node.insert(Nibbles::unpack([0x11]), [42]);
+        node.insert(Nibbles::unpack([]), [43]);
+        let mut expected_branch = Node::Branch(Default::default(), Some(Box::new([43])));
+
+        if let Node::Branch(ref mut children, _) = expected_branch {
+            children[0x1] = Some(Box::new(Node::leaf([0x1], [42])));
+        }
+
+        assert_eq!(expected_branch, node);
+    }
+
+    #[test]
+    //todo - make it pass
+    #[ignore]
+    fn one_leaf_one_branch() {
+        let mut node = Node::Null;
+        node.insert(Nibbles::unpack([]), [42]);
+        node.insert(Nibbles::unpack([0x11]), [43]);
+        let mut expected_branch = Node::Branch(Default::default(), Some(Box::new([42])));
+
+        if let Node::Branch(ref mut children, _) = expected_branch {
+            children[0x1] = Some(Box::new(Node::leaf([0x1], [43])));
         }
 
         assert_eq!(expected_branch, node);
--- rust/mpt/src/trie/tests/insert.rs
+++ rust/mpt/src/trie/tests/insert.rs
@@ -30,10 +30,48 @@ pub fn twice_same_key() {
 }
 
 #[test]
-pub fn two_keys() {
+pub fn two_keys_no_common_prefix() {
     let mut mpt = MerkleTrie(Node::Null);
-    mpt.insert([0x1], [42]);
+    mpt.insert([0x02], [42]);
     mpt.insert([0x10], [43]);
-    assert_eq!(Some(&[42][..]), mpt.get([0x01]));
+    assert_eq!(Some(&[42][..]), mpt.get([0x02]));
     assert_eq!(Some(&[43][..]), mpt.get([0x10]));
+}
+
+#[test]
+pub fn two_long_keys_no_common_prefix() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x12, 0x01], [42]);
+    mpt.insert([0x20, 0x01], [43]);
+    assert_eq!(Some(&[42][..]), mpt.get([0x12, 0x01]));
+    assert_eq!(Some(&[43][..]), mpt.get([0x20, 0x01]));
+}
+
+#[test]
+pub fn one_value_empty() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x01], [42]);
+    mpt.insert([], [43]);
+    assert_eq!(Some(&[42][..]), mpt.get([0x01]));
+    assert_eq!(Some(&[43][..]), mpt.get([]));
+}
+
+#[test]
+//todo - handle this case starting with branch
+#[ignore]
+pub fn one_value_empty_symmetrical() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x01], [42]);
+    mpt.insert([], [43]);
+    assert_eq!(Some(&[42][..]), mpt.get([]));
+    assert_eq!(Some(&[43][..]), mpt.get([0x01]));
+}
+
+#[test]
+pub fn one_empty_key_one_long_key() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0xff, 0xff, 0xff, 0xff], [42]);
+    mpt.insert([], [43]);
+    assert_eq!(Some(&[42][..]), mpt.get([0xff, 0xff, 0xff, 0xff]));
+    assert_eq!(Some(&[43][..]), mpt.get([]));
 }
