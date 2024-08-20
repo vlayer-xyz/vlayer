87e35da4aeebb535c580e7cedebb2b3a303718d6
Piotr Żelazko <piotr@vlayer.xyz>
2024-08-20 19:27:22 +0200

    chore: remove unused console imports

--- contracts/test/Verifier.t.sol
+++ contracts/test/Verifier.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {TestHelpers} from "./helpers/TestHelpers.sol";
 
 import {IRiscZeroVerifier, Receipt, VerificationFailed} from "risc0-ethereum/IRiscZeroVerifier.sol";
