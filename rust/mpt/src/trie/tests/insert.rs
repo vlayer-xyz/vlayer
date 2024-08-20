de3c7411f06da820aa8d85ddedf3510962d9d613
Adam Król <adam.dawid.krol@gmail.com>
2024-08-20 14:11:45 +0200

    Use expected-actual convention in insert tests

--- rust/mpt/src/trie/tests/insert.rs
+++ rust/mpt/src/trie/tests/insert.rs
@@ -4,21 +4,21 @@ use crate::{node::Node, MerkleTrie};
 pub fn empty_key() {
     let mut mpt = MerkleTrie(Node::Null);
     mpt.insert([], [42]);
-    assert_eq!(Some(&[42][..]), mpt.get([]));
+    assert_eq!(mpt.get([]), Some(&[42][..]));
 }
 
 #[test]
-pub fn odd_key() {
+pub fn one_byte_key() {
     let mut mpt = MerkleTrie(Node::Null);
     mpt.insert([0x1], [42]);
-    assert_eq!(Some(&[42][..]), mpt.get([0x01]));
+    assert_eq!(mpt.get([0x01]), Some(&[42][..]));
 }
 
 #[test]
-pub fn even_key() {
+pub fn multi_byte_key() {
     let mut mpt = MerkleTrie(Node::Null);
     mpt.insert([0x1, 0x1], [42]);
-    assert_eq!(Some(&[42][..]), mpt.get([0x1, 0x1]));
+    assert_eq!(mpt.get([0x1, 0x1]), Some(&[42][..]));
 }
 
 #[test]
@@ -34,6 +34,6 @@ pub fn two_keys() {
     let mut mpt = MerkleTrie(Node::Null);
     mpt.insert([0x1], [42]);
     mpt.insert([0x10], [43]);
-    assert_eq!(Some(&[42][..]), mpt.get([0x01]));
-    assert_eq!(Some(&[43][..]), mpt.get([0x10]));
+    assert_eq!(mpt.get([0x01]), Some(&[42][..]));
+    assert_eq!(mpt.get([0x10]), Some(&[43][..]));
 }
