d5a889cd22207bbe1d8761229727e4d27b2be9f6
Adam Król <adam.dawid.krol@gmail.com>
2024-08-22 09:09:45 +0200

    Add trie insert function

---
+++ rust/mpt/src/trie/tests/insert.rs
@@ -0,0 +1,40 @@
+use crate::{node::Node, MerkleTrie};
+
+#[test]
+pub fn empty_key() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([], [42]);
+    assert_eq!(mpt.get([]), Some(&[42][..]));
+}
+
+#[test]
+pub fn one_byte_key() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x1], [42]);
+    assert_eq!(mpt.get([0x01]), Some(&[42][..]));
+}
+
+#[test]
+pub fn multi_byte_key() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x1, 0x1], [42]);
+    assert_eq!(mpt.get([0x1, 0x1]), Some(&[42][..]));
+}
+
+#[test]
+#[should_panic(expected = "Key already exists")]
+pub fn duplicate_key() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0], [42]);
+    mpt.insert([0], [43]);
+}
+
+#[test]
+#[ignore]
+pub fn two_keys() {
+    let mut mpt = MerkleTrie(Node::Null);
+    mpt.insert([0x0], [42]);
+    mpt.insert([0x1], [43]);
+    assert_eq!(mpt.get([0x0]), Some(&[42][..]));
+    assert_eq!(mpt.get([0x1]), Some(&[43][..]));
+}
