2db23a7238b015725b2124136f0ddf1d223054c6
Artur Chmaro <artur@vlayer.xyz>
2024-08-23 12:41:37 +0200

    Add path aliases

--- packages/vlayer/sdk/src/tests/deployVerifier.ts
+++ packages/vlayer/sdk/src/tests/deployVerifier.ts
@@ -1,7 +1,7 @@
 import { type Address } from "viem";
 
 import { deployContract } from "../api/helpers";
-import fakeProofVerifier from "../../../../../contracts/out/FakeProofVerifier.sol/FakeProofVerifier.json";
+import fakeProofVerifier from "@contracts/FakeProofVerifier.sol/FakeProofVerifier.json";
 
 const addr: Address = await deployContract(fakeProofVerifier);
 console.log(`fakeProofVerifier addr: ${addr}`);
