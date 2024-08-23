3972d6ef16f3759e0961f84e80c5b87f3499721c
Anna Jabłonowska <ania@vlayer.xyz>
2024-08-23 14:28:21 +0200

    Test parsing

--- rust/web_proof/src/verifier.rs
+++ rust/web_proof/src/verifier.rs
@@ -1,6 +1,6 @@
 use std::{str::Utf8Error, string::FromUtf8Error};
 
-use httparse::{Header, Request, EMPTY_HEADER};
+use httparse::{Header, Request, Response, EMPTY_HEADER};
 use tlsn_core::{
     proof::{SessionProofError, SubstringsProofError, TlsProof},
     RedactedTranscript, ServerName,
@@ -43,7 +43,7 @@ pub fn verify_and_parse(web_proof: WebProof) -> Result<Web, VerificationError> {
     let (sent_string, _recv_string) = extract_sent_recv_strings((sent, recv))?;
 
     let mut req_headers = [EMPTY_HEADER; MAX_HEADERS_NUMBER];
-    let req = parse_tlsn_http_output(&sent_string, &mut req_headers)?;
+    let req = parse_tlsn_http_request(&sent_string, &mut req_headers)?;
 
     let host_value = find_value(req.headers, "host")?;
 
@@ -78,13 +78,22 @@ fn extract_sent_recv_strings(
     Ok((sent_string, recv_string))
 }
 
-fn parse_tlsn_http_output<'a>(
-    tlsn_http_output: &'a str,
+fn parse_tlsn_http_request<'a>(
+    tlsn_http_request: &'a str,
     headers: &'a mut [Header<'a>],
 ) -> Result<Request<'a, 'a>, VerificationError> {
     let mut req = Request::new(headers);
-    req.parse(tlsn_http_output.as_bytes())?;
+    req.parse(tlsn_http_request.as_bytes())?;
     Ok(req)
+}
+
+fn _parse_tlsn_http_response<'a>(
+    tlsn_http_response: &'a str,
+    headers: &'a mut [Header<'a>],
+) -> Result<Response<'a, 'a>, VerificationError> {
+    let mut res = Response::new(headers);
+    res.parse(tlsn_http_response.as_bytes())?;
+    Ok(res)
 }
 
 fn find_value(headers: &[Header], name: &str) -> Result<String, VerificationError> {
@@ -108,11 +117,47 @@ mod tests {
     fn fail_too_many_headers() {
         let request = read_fixture("./testdata/sent_request.txt");
         let mut headers = [EMPTY_HEADER; 1];
-        let req = parse_tlsn_http_output(&request, &mut headers);
+        let req = parse_tlsn_http_request(&request, &mut headers);
         assert_eq!(
             req.unwrap_err().to_string(),
             "Httparse error: too many headers"
         );
+    }
+
+    #[test]
+    fn correct_parsing_request() {
+        let request = read_fixture("./testdata/sent_request.txt");
+        let mut req_headers = [EMPTY_HEADER; MAX_HEADERS_NUMBER];
+        let req = parse_tlsn_http_request(&request, &mut req_headers).unwrap();
+
+        assert_eq!(req.method.unwrap(), "GET");
+        assert_eq!(req.path.unwrap(), "https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true");
+        assert_eq!(req.version.unwrap(), 1);
+        assert_eq!(req.headers.len(), 21);
+        assert_eq!(req.headers[5].name, "host");
+        assert_eq!(req.headers[5].value, "api.x.com".as_bytes());
+    }
+
+    #[test]
+    fn correct_parsing_response() {
+        let response = read_fixture("./testdata/received_response.txt");
+        let mut res_headers = [EMPTY_HEADER; MAX_HEADERS_NUMBER];
+        let res = _parse_tlsn_http_response(&response, &mut res_headers).unwrap();
+
+        assert_eq!(res.version.unwrap(), 1);
+        assert_eq!(res.code.unwrap(), 200);
+        assert_eq!(res.reason.unwrap(), "OK");
+        assert_eq!(res.headers.len(), 26);
+        assert_eq!(res.headers[4].name, "status");
+        assert_eq!(res.headers[4].value, "200 OK".as_bytes());
+    }
+
+    #[test]
+    fn fail_redacted() {
+        let redacted_request = read_fixture("./testdata/redacted_sent_request.txt");
+        let mut req_headers = [EMPTY_HEADER; MAX_HEADERS_NUMBER];
+        let req = parse_tlsn_http_request(&redacted_request, &mut req_headers);
+        assert!(req.is_err());
     }
 
     #[test]
