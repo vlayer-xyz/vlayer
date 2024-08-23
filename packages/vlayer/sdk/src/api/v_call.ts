dcf2af9dba576812c990f0d8dc62193c655e9ba7
Artur Chmaro <artur@vlayer.xyz>
2024-08-23 12:12:56 +0200

    add VCallResponse

--- packages/vlayer/sdk/src/api/v_call.ts
+++ packages/vlayer/sdk/src/api/v_call.ts
@@ -3,6 +3,19 @@ import {Hex, type Address} from "viem";
 type Calldata = string;
 export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
+export type VCallResponse = {
+    jsonrpc: string;
+    result: {
+        result: {
+        evm_call_result: number[];
+        function_selector: string;
+        prover_contract_address: string;
+        seal: number[];
+        };
+    };
+    id: number;
+};
+
 export type CallParams = {
   to: Address,
   data: Calldata
@@ -49,5 +62,5 @@ export async function v_call(call: CallParams, context: CallContext): Promise<VC
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  return await response.json() as any;
+    return response.json() as Promise<VCallResponse>;
 }
