404548f3de4669fc5bb63b3e224462444900a64f
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-08-26 16:26:33 +0200

    Add initial method_not_found handler

--- rust/block_proof/src/lib.rs
+++ rust/block_proof/src/lib.rs
@@ -1,5 +1,21 @@
-use axum::Router;
+use axum::{routing::post, Router};
+use serde_json::{from_str, json, to_string, Value};
+
+async fn method_not_found(payload: String) -> String {
+    let payload: Value = from_str(&payload).unwrap();
+    to_string(&json!({
+        "jsonrpc": "2.0",
+        "id": 1,
+        "error": {
+            "code": -32601,
+            "message": format!("Method {} not found", payload["method"]),
+            "data": null
+        }
+    }))
+    .unwrap()
+}
+
 
 pub fn server() -> Router {
-    Router::new()
+    Router::new().route("/", post(method_not_found))
 }
--- rust/block_proof/tests/integration_tests.rs
+++ rust/block_proof/tests/integration_tests.rs
@@ -3,16 +3,47 @@ use axum::{
     http::{Request, Response, StatusCode},
 };
 use block_proof::server;
+use serde::Serialize;
+use serde_json::{json, to_string};
+use server_utils::body_to_json;
 use tower::ServiceExt;
 
-async fn test_post(url: &str) -> Response<Body> {
+async fn post<T: Serialize>(url: &str, body: &T) -> Response<Body> {
     let app = server();
-    let request = Request::post(url).body(Body::from("")).unwrap();
+    let request = Request::post(url)
+        .body(Body::from(to_string(body).unwrap()))
+        .unwrap();
     app.oneshot(request).await.unwrap()
 }
 
 #[tokio::test]
-async fn not_found() {
-    let response = test_post("/non_existing").await;
+async fn http_not_found() {
+    let empty_body = json!({});
+    let response = post("/non_existing", &empty_body).await;
     assert_eq!(StatusCode::NOT_FOUND, response.status());
+}
+
+#[tokio::test]
+async fn method_not_found() {
+    let req = json!({
+        "method": "random_gibberish",
+        "params": [],
+        "id": 1,
+        "jsonrpc": "2.0",
+    });
+    let response = post("/", &req).await;
+
+    assert_eq!(StatusCode::OK, response.status());
+    assert_eq!(
+        json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "error": {
+                "code": -32601,
+                "message": "Method \"random_gibberish\" not found",
+                "data": null
+            }
+        }),
+        body_to_json(response.into_body()).await
+    );
 }
