f44b9afe1efa2738cb0361f8a469924ce376f46b
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-08-27 15:58:45 +0200

    Handle error deserializing request json into JsonRpcRequest

--- rust/block_proof/src/lib.rs
+++ rust/block_proof/src/lib.rs
@@ -1,25 +1,60 @@
-use axum::{routing::post, Json, Router};
+use axum::{
+    extract::rejection::JsonRejection,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    Json, Router,
+};
+use axum_extra::extract::WithRejection;
 use serde::Deserialize;
-use serde_json::{from_str, json, to_string, Value};
+use serde_json::{json, to_string};
+use thiserror::Error;
 
 #[derive(Deserialize)]
-struct Request {
-    method: String
+struct JsonRpcRequest {
+    method: String,
 }
 
-async fn method_not_found(Json(payload): Json<Request>) -> String {
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error(transparent)]
+    JsonExtractorRejection(#[from] JsonRejection),
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        let status = StatusCode::OK;
+        let body = match self {
+            ServerError::JsonExtractorRejection(e) => (
+                StatusCode::OK,
+                Json(json!({
+                    "jsonrpc": "2.0",
+                    "id": 1,
+                    "error": {
+                        "code": -32600,
+                        "message": "Invalid request: ".to_string() + &e.to_string()
+                    }
+                })),
+            ),
+        };
+
+        (status, body).into_response()
+    }
+}
+
+async fn method_not_found(
+    WithRejection(Json(payload), _): WithRejection<Json<JsonRpcRequest>, ServerError>,
+) -> String {
     to_string(&json!({
         "jsonrpc": "2.0",
         "id": 1,
         "error": {
             "code": -32601,
-            "message": format!("Method {} not found", payload.method),
-            "data": null
+            "message": format!("Method `{}` not found", payload.method),
         }
     }))
     .unwrap()
 }
-
 
 pub fn server() -> Router {
     Router::new().route("/", post(method_not_found))
--- rust/block_proof/tests/integration_tests.rs
+++ rust/block_proof/tests/integration_tests.rs
@@ -1,54 +1,28 @@
-use axum::{body::Body, http::StatusCode, serve};
+use axum::{
+    body::Body,
+    http::{header::CONTENT_TYPE, Request, Response, StatusCode},
+};
 use block_proof::server;
-use reqwest::{header, Client, Method, Response};
+use mime::APPLICATION_JSON;
 use serde::Serialize;
-use serde_json::{from_slice, json, to_string, Value};
+use serde_json::{json, to_string};
 use server_utils::body_to_json;
+use tower::ServiceExt;
 
-async fn post(url: &str, body: Value) -> Response {
+async fn post<T: Serialize>(url: &str, body: &T) -> Response<Body> {
     let app = server();
-    let addr = "127.0.0.1:4000";
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    serve(listener, app).await.unwrap();
-
-    Client::new()
-        .post(addr.to_string() + url)
-        .header("Content-Type", "application/json")
-        .json(&body)
-        .send()
-        .await
-        .unwrap() // TODO: handle error?
+    let request = Request::post(url)
+        .header(CONTENT_TYPE, APPLICATION_JSON.as_ref())
+        .body(Body::from(to_string(body).unwrap()))
+        .unwrap();
+    app.oneshot(request).await.unwrap()
 }
 
 #[tokio::test]
 async fn http_not_found() {
     let empty_body = json!({});
-    let response = post("/non_existing", empty_body).await;
+    let response = post("/non_existing", &empty_body).await;
     assert_eq!(StatusCode::NOT_FOUND, response.status());
-}
-
-#[tokio::test]
-async fn method_missing() {
-    let req = json!({
-        "params": [],
-        "id": 1,
-        "jsonrpc": "2.0",
-    });
-
-    let response = post("/", req).await;
-    assert_eq!(StatusCode::OK, response.status());
-    assert_eq!(
-        json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "error": {
-                "code": -32601,
-                "message": "Method missing",
-                "data": null
-            }
-        }),
-        response.json::<Value>().await.unwrap()
-    );
 }
 
 #[tokio::test]
@@ -59,7 +33,7 @@ async fn method_not_found() {
         "id": 1,
         "jsonrpc": "2.0",
     });
-    let response = post("/", req).await;
+    let response = post("/", &req).await;
 
     assert_eq!(StatusCode::OK, response.status());
     assert_eq!(
@@ -68,10 +42,32 @@ async fn method_not_found() {
             "id": 1,
             "error": {
                 "code": -32601,
-                "message": "Method \"random_gibberish\" not found",
-                "data": null
+                "message": "Method `random_gibberish` not found",
             }
         }),
-        response.json::<Value>().await.unwrap()
+        body_to_json(response.into_body()).await
+    );
+}
+
+#[tokio::test]
+async fn method_missing() {
+    let req = json!({
+        "params": [],
+        "id": 1,
+        "jsonrpc": "2.0",
+    });
+    let response = post("/", &req).await;
+
+    assert_eq!(StatusCode::OK, response.status());
+    assert_eq!(
+        json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "error": {
+                "code": -32600,
+                "message": "Invalid request: Failed to deserialize the JSON body into the target type",
+            }
+        }),
+        body_to_json(response.into_body()).await
     );
 }
