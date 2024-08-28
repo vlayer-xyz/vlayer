8ae80d6d239b15a160ecfe33172c939ad006a23e
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-08-28 11:27:54 +0200

    Refactor: use JsonRpcExtractor

--- rust/block_proof/src/lib.rs
+++ rust/block_proof/src/lib.rs
@@ -1,59 +1,9 @@
-use axum::{
-    extract::rejection::JsonRejection,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, Router,
-};
-use axum_extra::extract::WithRejection;
-use serde::Deserialize;
-use serde_json::{json, to_string};
-use thiserror::Error;
+use axum::{routing::post, Router};
+use axum_jrpc::{JrpcResult, JsonRpcExtractor};
 
-#[derive(Deserialize)]
-struct JsonRpcRequest {
-    method: String,
-}
-
-#[derive(Debug, Error)]
-pub enum ServerError {
-    #[error(transparent)]
-    JsonExtractorRejection(#[from] JsonRejection),
-}
-
-impl IntoResponse for ServerError {
-    fn into_response(self) -> Response {
-        let status = StatusCode::OK;
-        let body = match self {
-            ServerError::JsonExtractorRejection(e) => (
-                StatusCode::OK,
-                Json(json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "error": {
-                        "code": -32600,
-                        "message": "Invalid request: ".to_string() + &e.to_string()
-                    }
-                })),
-            ),
-        };
-
-        (status, body).into_response()
-    }
-}
-
-async fn method_not_found(
-    WithRejection(Json(payload), _): WithRejection<Json<JsonRpcRequest>, ServerError>,
-) -> String {
-    to_string(&json!({
-        "jsonrpc": "2.0",
-        "id": 1,
-        "error": {
-            "code": -32601,
-            "message": format!("Method `{}` not found", payload.method),
-        }
-    }))
-    .unwrap()
+async fn method_not_found(req: JsonRpcExtractor) -> JrpcResult {
+    let method = req.method();
+    Ok(req.method_not_found(method))
 }
 
 pub fn server() -> Router {
--- rust/block_proof/tests/integration_tests.rs
+++ rust/block_proof/tests/integration_tests.rs
@@ -21,17 +21,17 @@ async fn post<T: Serialize>(url: &str, body: &T) -> Response<Body> {
 #[tokio::test]
 async fn http_not_found() {
     let empty_body = json!({});
-    let response = post("/non_existing", &empty_body).await;
+    let response = post("/non-existent", &empty_body).await;
     assert_eq!(StatusCode::NOT_FOUND, response.status());
 }
 
 #[tokio::test]
 async fn method_not_found() {
     let req = json!({
-        "method": "random_gibberish",
-        "params": [],
-        "id": 1,
         "jsonrpc": "2.0",
+        "id": 1,
+        "method": "random_gibberish",
+        "params": []
     });
     let response = post("/", &req).await;
 
@@ -43,6 +43,7 @@ async fn method_not_found() {
             "error": {
                 "code": -32601,
                 "message": "Method `random_gibberish` not found",
+                "data": null
             }
         }),
         body_to_json(response.into_body()).await
@@ -52,9 +53,9 @@ async fn method_not_found() {
 #[tokio::test]
 async fn method_missing() {
     let req = json!({
-        "params": [],
-        "id": 1,
         "jsonrpc": "2.0",
+        "id": 2,
+        "params": []
     });
     let response = post("/", &req).await;
 
@@ -62,10 +63,11 @@ async fn method_missing() {
     assert_eq!(
         json!({
             "jsonrpc": "2.0",
-            "id": 1,
+            "id": null,
             "error": {
                 "code": -32600,
-                "message": "Invalid request: Failed to deserialize the JSON body into the target type",
+                "message": "missing field `method` at line 1 column 36",
+                "data": null
             }
         }),
         body_to_json(response.into_body()).await
