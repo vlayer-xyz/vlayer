60ec938e03b325290a4d417b4b17b8223ecf6e4c
vanruch <ivanruch@gmail.com>
2024-08-29 15:19:36 +0200

    web proof

--- examples/web_proof/vlayer/prove.ts
+++ examples/web_proof/vlayer/prove.ts
@@ -1,16 +1,19 @@
-import type { Address } from "viem";
-
-import { testHelpers, prove } from "@vlayer/sdk";
-import webProofProver from "../out/WebProofProver.sol/WebProofProver.json";
+import {testHelpers, completeProof} from "@vlayer/sdk";
+import webProofProver from "../out/WebProofProver.sol/WebProofProver";
+import webProofVerifier from "../out/WebProofVerifier.sol/WebProofVerifier";
 import tls_proof from './tls_proof.json';
 
 const notaryPubKey = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n"
-const webProof = { tls_proof: tls_proof, notary_pub_key: notaryPubKey }
+const webProof = {tls_proof: tls_proof, notary_pub_key: notaryPubKey}
 
-console.log("Deploying prover")
-const prover: Address = await testHelpers.deployContract(webProofProver);
-console.log(`Prover has been deployed on ${prover} address`);
+const [prover, verifier] = await testHelpers.deployProverVerifier(webProofProver, webProofVerifier);
 
 console.log("Proving...");
-const response = await prove(prover, webProofProver, 'main', [[JSON.stringify(webProof)]]);
-console.log("Response:", response)
+const {proof, returnValue} = await completeProof(prover, webProofProver.abi, 'main', [{
+  webProofJson: JSON.stringify(webProof)
+}]);
+console.log("Proof:", proof)
+
+console.log("Verifying...")
+await testHelpers.writeContract(verifier, webProofVerifier.abi, 'verify', [proof, returnValue]);
+console.log('Verified!');
