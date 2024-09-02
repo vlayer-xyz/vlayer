3139d848c39231938feba4c9fed14f762778f9d6
Arkadiusz Konior <akonior@gmail.com>
2024-09-02 15:59:55 +0200

    jsonGetString lib function (#491)

--- contracts/src/WebProof.sol
+++ contracts/src/WebProof.sol
@@ -5,29 +5,47 @@ import {Strings} from "openzeppelin/contracts/utils/Strings.sol";
 
 struct WebProof {
     string webProofJson;
-    string body;
 }
 
 library WebProofLib {
     using Strings for string;
 
     address private constant VERIFY_AND_PARSE_PRECOMPILE = address(0x100);
+    address private constant JSON_GET_STRING_PRECOMPILE = address(0x101);
 
     function verify(
         WebProof memory webProof,
         string memory dataUrl
-    ) internal view returns (bool) {
+    ) internal view returns (string memory) {
         (bool success, bytes memory returnData) = VERIFY_AND_PARSE_PRECOMPILE
             .staticcall(bytes(webProof.webProofJson));
 
         string[3] memory data = abi.decode(returnData, (string[3]));
         string memory serverName = "api.x.com";
-        webProof.body = data[2];
+        string memory body = data[2];
 
         require(success, "verify_and_parse precompile call failed");
         require(dataUrl.equal(data[0]), "Incorrect URL");
         require(serverName.equal(data[1]), "Server name not found");
 
-        return true;
+        return body;
+    }
+
+    function jsonGetString(
+        string memory body,
+        string memory jsonPath
+    ) internal view returns (string memory) {
+        require(bytes(body).length > 0, "Body is empty");
+
+        bytes memory encodedParams = abi.encode([body, jsonPath]);
+
+        (bool success, bytes memory returnData) =
+                            JSON_GET_STRING_PRECOMPILE.staticcall(encodedParams);
+
+        require(success, "json_get_string precompile call failed");
+
+        string memory jsonValue =  abi.decode(returnData, (string));
+
+        return jsonValue;
     }
 }
