b170099098df2622491f29c3ec6e82e2a3d4a0de
Leonid Logvinov <logvinov.leon@gmail.com>
2024-09-02 05:19:25 -0700

    Block proof host guest and wrapper refactor (#489)
    * Add block_proof/engine with Input structure
    
    * Add host & guest in block_proof
    
    * Move risc0 build.rs to guest_wrapper_utils
    
    * Refactor risc0_build function into Builder struct
    
    * Fix linter
    
    * Fix cargo fmt
    
    * Ignore file removal error

--- rust/call/guest_wrapper/build.rs
+++ rust/call/guest_wrapper/build.rs
@@ -1,32 +1,4 @@
 fn main() {
     #[cfg(not(clippy))]
-    {
-        use risc0_build_ethereum::generate_solidity_files;
-        use std::fs;
-        use std::path::Path;
-
-        // Generate Solidity source files for use with Forge.
-        let assets_path_str = format!("../../target/assets");
-        let assets_path = Path::new(&assets_path_str);
-
-        if !assets_path.exists() {
-            println!("{:?}", assets_path);
-            fs::create_dir(assets_path).expect("Could not create assets directory");
-        }
-
-        let image_id_path = assets_path.join("ImageID.sol");
-        let elf_path = assets_path.join("Elf.sol");
-
-        fs::remove_file(&image_id_path)
-            .unwrap_or_else(|err| println!("Did not remove old ImageID file: {}", err));
-        fs::remove_file(&elf_path)
-            .unwrap_or_else(|err| println!("Did not remove old elf file: {}", err));
-
-        let solidity_opts = risc0_build_ethereum::Options::default()
-            .with_image_id_sol_path(image_id_path)
-            .with_elf_sol_path(elf_path);
-
-        let guests = risc0_build::embed_methods();
-        generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
-    }
+    guest_wrapper_utils::Risc0Builder::new().build().unwrap();
 }
---
+++ rust/guest_wrapper_utils/src/lib.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use risc0_build::{embed_methods, GuestListEntry};
+use risc0_build_ethereum::{generate_solidity_files, Options};
+use std::fs::{create_dir_all, remove_file};
+use std::path::{Path, PathBuf};
+
+pub struct Risc0Builder {
+    assets_dir: &'static Path,
+    image_id_path: PathBuf,
+    elf_path: PathBuf,
+}
+
+#[allow(clippy::new_without_default)]
+impl Risc0Builder {
+    pub fn new() -> Self {
+        let assets_dir = Path::new("../../target/assets");
+
+        Self {
+            assets_dir,
+            image_id_path: assets_dir.join("ImageID.sol"),
+            elf_path: assets_dir.join("Elf.sol"),
+        }
+    }
+
+    pub fn build(&self) -> Result<()> {
+        let guests = embed_methods();
+
+        self.build_solidity(&guests)
+    }
+
+    // Generate Solidity source files for use with Forge.
+    fn build_solidity(&self, guests: &[GuestListEntry]) -> Result<()> {
+        create_dir_all(self.assets_dir)?;
+
+        let _ = remove_file(&self.image_id_path);
+        let _ = remove_file(&self.elf_path);
+
+        let solidity_opts = Options::default()
+            .with_image_id_sol_path(&self.image_id_path)
+            .with_elf_sol_path(&self.elf_path);
+
+        generate_solidity_files(guests, &solidity_opts)?;
+
+        Ok(())
+    }
+}
