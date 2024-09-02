05260954133adf975c3be387d5cda8bb8e28ef4c
Arkadiusz Konior <akonior@gmail.com>
2024-09-02 08:27:32 +0200

    Additional tests

--- rust/call/engine/src/precompiles/json_get_string.rs
+++ rust/call/engine/src/precompiles/json_get_string.rs
@@ -10,28 +10,26 @@ pub(crate) const JSON_GET_STRING_PRECOMPILE: Precompile = Precompile::Standard(j
 
 // TODO: set an accurate gas cost values reflecting the operation's computational complexity.
 /// The base cost of the operation.
-const JSON_GET_STRING_BASE: u64 = 10;
+const BASE_COST: u64 = 10;
 /// The cost per word.
-const JSON_GET_STRING_PER_WORD: u64 = 1;
+const PER_WORD_COST: u64 = 1;
 
 type InputType = sol_data::FixedArray<sol_data::String, 2>;
 
 fn json_get_string_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    let [body, json_path]: [String; 2] =
-        InputType::abi_decode(input, true).map_err(map_to_other)?;
+    let gas_used = gas_used(input.len(), BASE_COST, PER_WORD_COST, gas_limit)?;
 
-    let gas_used = gas_used(
-        input.len(),
-        JSON_GET_STRING_BASE,
-        JSON_GET_STRING_PER_WORD,
-        gas_limit,
-    )?;
+    let [body, json_path] = InputType::abi_decode(input, true).map_err(map_to_other)?;
+    let body = serde_json::from_str(body.as_str()).map_err(map_to_other)?;
 
-    let v: Value = serde_json::from_str(body.as_str()).map_err(map_to_other)?;
+    let value_by_path = get_value_by_path(&body, json_path.as_str())
+        .ok_or(map_to_other(format!("Missing value at path {json_path}")))?;
 
-    match get_value_by_path(&v, json_path.as_str()).ok_or(map_to_other("Missing value at paths"))? {
+    match value_by_path {
         Value::String(result) => Ok(PrecompileOutput::new(gas_used, result.to_string().into())),
-        _ => Err(map_to_other("Not a string at path")),
+        _ => Err(map_to_other(format!(
+            "Expected type 'String' at {json_path}, but found {value_by_path:?}"
+        ))),
     }
 }
 
@@ -41,27 +39,65 @@ fn get_value_by_path<'a>(value: &'a Value, path: &str) -> Option<&'a Value> {
 
 #[cfg(test)]
 mod tests {
+    use revm::precompile::{PrecompileError::Other, PrecompileErrors::Error};
     use super::*;
 
-    #[test]
-    fn success() {
-        let body = r#"
+    const TEST_JSON: &str = r#"
             {
                 "root": {
                     "level1": {
-                        "level2": "level2_string_value"
+                        "field_string": "field_string_value",
+                        "field_number": 0,
+                        "field_boolean": true,
+                        "field_array": [],
+                        "field_object": {}
+
                     }
                 }
             }
             "#;
-        let json_path = "root.level1.level2";
 
-        let abi_encoded_body_and_json_path = InputType::abi_encode(&[body, json_path]);
+    #[test]
+    fn success_string() {
+        let abi_encoded_body_and_json_path = InputType::abi_encode(&[TEST_JSON, "root.level1.field_string"]);
 
         let precompile_output =
             json_get_string_run(&abi_encoded_body_and_json_path.into(), u64::MAX).unwrap();
         let precompile_result = std::str::from_utf8(precompile_output.bytes.as_ref()).unwrap();
 
-        assert_eq!("level2_string_value", precompile_result);
+        assert_eq!("field_string_value", precompile_result);
+    }
+
+    #[test]
+    fn fail_missing() {
+        fail_with_message("root.level1.field_missing", "Missing value at path root.level1.field_missing");
+    }
+
+    #[test]
+    fn fail_number() {
+        fail_with_message("root.level1.field_number", "Expected type 'String' at root.level1.field_number, but found Number(0)");
+    }
+
+    #[test]
+    fn fail_boolean() {
+        fail_with_message("root.level1.field_boolean", "Expected type 'String' at root.level1.field_boolean, but found Bool(true)");
+    }
+
+    #[test]
+    fn fail_array() {
+        fail_with_message("root.level1.field_array", "Expected type 'String' at root.level1.field_array, but found Array []");
+    }
+
+    #[test]
+    fn fail_object() {
+        fail_with_message("root.level1.field_object", "Expected type 'String' at root.level1.field_object, but found Object {}");
+    }
+
+    fn fail_with_message(json_path: &str, error_message: &str) {
+        let abi_encoded_body_and_json_path = InputType::abi_encode(&[TEST_JSON, json_path]);
+
+        assert!(matches!(
+            json_get_string_run(&abi_encoded_body_and_json_path.into(), u64::MAX),
+        Err(Error(Other(message))) if message == error_message));
     }
 }
