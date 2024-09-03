d34e8d020195572ebbca82e6bdd1308bd7be826b
Anna Jabłonowska <62891747+aajj999@users.noreply.github.com>
2024-09-03 11:47:20 +0200

    Add body parse in Prover (#499)

--- examples/web_proof/src/vlayer/WebProofProver.sol
+++ examples/web_proof/src/vlayer/WebProofProver.sol
@@ -18,7 +18,9 @@ contract WebProofProver is Prover {
         "https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true";
 
     function main(WebProof calldata webProof) public view returns (bool) {
-        webProof.verify(dataUrl);
+        string memory body = webProof.verify(dataUrl);
+
+        require(WebProofLib.jsonGetString(body, "screen_name").equal("jab68503"), "Invalid screen_name");
 
         return true;
     }
