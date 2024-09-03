d126d136acda3b3ea21b504793379aeda2935168
Grzegorz Pociejewski <grzesiek@Grzegorzs-MacBook-Pro.local>
2024-09-03 07:47:32 +0200

    feat: use sdk in pulgin in development

--- packages/browser-plugin/src/background.ts
+++ packages/browser-plugin/src/background.ts
@@ -1,7 +1,9 @@
 import browser from "webextension-polyfill";
+import * as vlayerSDK from "@vlayer/sdk"; 
 
-console.log("Hello from the background!");
+console.log("Hello from the background!",vlayerSDK);
 
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
+
