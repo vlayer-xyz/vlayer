e405dc55b3beb4afaf2921cc487bec4093e0a3f3
Grzegorz Pociejewski <grzesiek@Grzegorzs-MacBook-Pro.local>
2024-09-03 14:10:19 +0200

    fix: eslint background.ts

--- packages/browser-plugin/src/background.ts
+++ packages/browser-plugin/src/background.ts
@@ -1,9 +1,8 @@
 import browser from "webextension-polyfill";
-import * as vlayerSDK from "@vlayer/sdk"; 
+import * as vlayerSDK from "@vlayer/sdk";
 
-console.log("Hello from the background!",vlayerSDK);
+console.log("Hello from the background!", vlayerSDK);
 
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
-
