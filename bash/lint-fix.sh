38f7c0bee3c0b9ee4f320a9eb471a08481c6bca9
vanruch <ivanruch@gmail.com>
2024-09-05 12:22:51 +0200

    Add global lint fix script

---
+++ bash/lint-fix.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+
+for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ; do
+  (
+    echo "Running eslint for: ${example}"
+    cd "${example}/vlayer"
+
+    bun install --no-save
+    bun run eslint . --fix
+    bun run lint:solidity -- --fix --noPrompt
+  )
+done
+
+cd "${VLAYER_HOME}/packages"
+bun install --no-save
+bun run lint:fix
+
+cd "${VLAYER_HOME}/contracts"
+bun install --no-save
+bun run lint:solidity -- --fix --noPrompt
