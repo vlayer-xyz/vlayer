7f9a8d891c6221831806003436ca6a6f0f103310
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-06 14:03:05 +0200

    migrate examples to soldeer: private_airdrop

--- examples/private_airdrop/src/vlayer/PrivateAirdropProver.sol
+++ examples/private_airdrop/src/vlayer/PrivateAirdropProver.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { ECDSA } from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
-import { MessageHashUtils } from "openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
-import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { Prover } from "vlayer/Prover.sol";
+import {ECDSA} from "@openzeppelin-contracts-5.0.2/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin-contracts-5.0.2/utils/cryptography/MessageHashUtils.sol";
+import {IERC20} from "@openzeppelin-contracts-5.0.2/token/ERC20/IERC20.sol";
+import {Prover} from "vlayer-contracts-0.1.0/src/Prover.sol";
 
 contract PrivateAirdropProver is Prover {
     using ECDSA for bytes32;
@@ -16,27 +16,30 @@ contract PrivateAirdropProver is Prover {
     }
 
     function nullifier(address _addr) public pure returns (bytes32) {
-        return(keccak256(abi.encodePacked(_addr)));
+        return (keccak256(abi.encodePacked(_addr)));
     }
 
-    function main(address account, bytes memory signature) public view returns (address, bytes32) {
+    function main(
+        address account,
+        bytes memory signature
+    ) public view returns (address, bytes32) {
         uint256 balance = TOKEN.balanceOf(account);
         require(balance > 0, "Insufficient balance");
 
-        require(isValidSignature(
-            account,
-            signature
-            ), "Invalid Signature"
-        );
+        require(isValidSignature(account, signature), "Invalid Signature");
 
         return (account, nullifier(account));
     }
 
-    function isValidSignature(address _account, bytes memory signature) internal pure returns (bool) {
+    function isValidSignature(
+        address _account,
+        bytes memory signature
+    ) internal pure returns (bool) {
         require(_account != address(0), "Missing Address");
 
-        bytes32 signedHash = bytes("I own ExampleToken and I want to privately claim my airdrop")
-            .toEthSignedMessageHash();
+        bytes32 signedHash = bytes(
+            "I own ExampleToken and I want to privately claim my airdrop"
+        ).toEthSignedMessageHash();
         return signedHash.recover(signature) == _account;
     }
 }
