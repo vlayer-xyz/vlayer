f26f5a24c312a4e700495655b82056b2a6b7a991
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-06 14:01:28 +0200

    migrate examples to soldeer: erc20_balances

--- examples/erc20_balances/src/vlayer/VToyken.sol
+++ examples/erc20_balances/src/vlayer/VToyken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
 
 contract VToyken is ERC20 {
     constructor() ERC20("VToyken", "VTKN") {
@@ -17,9 +17,8 @@ contract VToyken is ERC20 {
             0xa0Ee7A142d267C1f36714E4a8F75612F20a79720
         ];
 
-        for (uint256 i = 0; i < initialOwners.length; i++){
+        for (uint256 i = 0; i < initialOwners.length; i++) {
             _mint(initialOwners[i], i * 1 ether);
         }
     }
 }
-
