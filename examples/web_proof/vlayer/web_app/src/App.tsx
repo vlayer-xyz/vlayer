3debec44c0af64ccecfa154cd247f9b1c87203be
pociej <grzegorz.pociejewski@gmail.com>
2024-09-09 16:10:00 +0200

    feat: add web_app example

---
+++ examples/web_proof/vlayer/web_app/src/App.tsx
@@ -0,0 +1,14 @@
+import vlayerLogo from "/vlayer_logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <img src={vlayerLogo} alt="Vlayer Logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
