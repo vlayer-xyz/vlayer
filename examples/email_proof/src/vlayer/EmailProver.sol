d4dc4f2168f247ccf19f321dd9fa0d62a862fee5
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-10 15:45:54 +0200

    chore: migrate examples deps to soldeer - email_proof

--- examples/email_proof/src/vlayer/EmailProver.sol
+++ examples/email_proof/src/vlayer/EmailProver.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Strings} from "openzeppelin/contracts/utils/Strings.sol";
+import {Strings} from "@openzeppelin-contracts/utils/Strings.sol";
 
 import {Prover} from "vlayer/Prover.sol";
 import {EmailProof, EmailProofLib} from "vlayer/EmailProof.sol";
