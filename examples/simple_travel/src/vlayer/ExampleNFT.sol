26307ea7c1a237e05a10a1a490043cc737a56620
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-10 15:09:14 +0200

    chore: migrate examples to soldeer - simple_travel

--- examples/simple_travel/src/vlayer/ExampleNFT.sol
+++ examples/simple_travel/src/vlayer/ExampleNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin-contracts/token/ERC721/ERC721.sol";
 
 contract ExampleNFT is ERC721 {
     uint256 public currentTokenId = 1;
