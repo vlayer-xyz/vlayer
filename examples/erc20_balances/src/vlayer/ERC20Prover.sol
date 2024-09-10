e83d4c365ccc756f6eb4c37ec3a00aa8f58beb43
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-10 14:52:20 +0200

    chore: migrate examples to soldeer - erc20balances

--- examples/erc20_balances/src/vlayer/ERC20Prover.sol
+++ examples/erc20_balances/src/vlayer/ERC20Prover.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {Prover} from "vlayer/Prover.sol";
 
 contract ERC20Prover is Prover {
