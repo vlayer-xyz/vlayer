ab58fd2eb4a30ce729c0dd928dbf0ac9a662e743
pociej <grzegorz.pociejewski@gmail.com>
2024-09-10 11:05:38 +0200

    feat: add prettier recommended to linter

--- examples/web_proof/vlayer/web_app/src/App.tsx
+++ examples/web_proof/vlayer/web_app/src/App.tsx
@@ -3,9 +3,11 @@ import vlayerLogo from "/vlayer_logo.svg";
 function App() {
   return (
     <>
-      <div style={{
-        padding: "2em"
-      }}>
+      <div
+        style={{
+          padding: "2em",
+        }}
+      >
         <img src={vlayerLogo} alt="Vlayer Logo" />
       </div>
     </>
