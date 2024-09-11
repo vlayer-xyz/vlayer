d5b8cddf66d2fdb0c0e1cc5b5b1a18ea8210bc4a
Anna Jabłonowska <ania@vlayer.xyz>
2024-09-11 11:14:05 +0200

    Merge scripts

--- bash/run-web-app.sh
+++
@@ -1,5 +0,0 @@
-VLAYER_HOME=$(git rev-parse --show-toplevel)
-
-cd ${VLAYER_HOME}/examples/web_proof/vlayer
-bun run deploy.ts
-bun run dev
--- bash/run-web-extension.sh
+++
@@ -1,4 +0,0 @@
-VLAYER_HOME=$(git rev-parse --show-toplevel)
-
-cd ${VLAYER_HOME}/packages/browser-plugin
-bun run dev
