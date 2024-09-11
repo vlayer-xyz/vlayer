5913ff6aff9ec10bfe8ea853aaef045ab5a9eaa0
Anna Jabłonowska <ania@vlayer.xyz>
2024-09-11 11:09:50 +0200

    Change script name format

--- bash/run_web_example.sh
+++
@@ -1,6 +0,0 @@
-VLAYER_HOME=$(git rev-parse --show-toplevel)
-
-source ${VLAYER_HOME}/bash/run-services.sh &
-source ${VLAYER_HOME}/bash/run-web-app.sh &
-source ${VLAYER_HOME}/bash/run-web-extension.sh &
-
