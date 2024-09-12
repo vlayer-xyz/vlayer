b8bf3f5b934a1df34164eece89215482bf432d77
pociej <grzegorz.pociejewski@gmail.com>
2024-09-12 10:25:31 +0200

    fix: linting

--- packages/vlayer/web-components/src/components/theme.tsx
+++ packages/vlayer/web-components/src/components/theme.tsx
@@ -1,10 +1,6 @@
 import { Theme } from "@radix-ui/themes";
 import type { PropsWithChildren } from "react";
-import './theme.css';
-export const VlayerTheme = function (props : PropsWithChildren<{}>) {
-    return <Theme
-        accentColor="violet"
-    >
-        {props.children}
-    </Theme>
-}
+import "./theme.css";
+export const VlayerTheme = function (props: PropsWithChildren) {
+  return <Theme accentColor="violet">{props.children}</Theme>;
+};
