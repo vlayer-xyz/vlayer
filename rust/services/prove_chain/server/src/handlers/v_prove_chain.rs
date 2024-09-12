72d9b921c5cc5390d1ab57e67bdc2e9f2abe0ab9
Leonid Logvinov <logvinov.leon@gmail.com>
2024-09-12 15:38:15 +0200

    Add success test for v_prove_chain

--- rust/services/prove_chain/server/src/handlers/v_prove_chain.rs
+++ rust/services/prove_chain/server/src/handlers/v_prove_chain.rs
@@ -10,8 +10,10 @@ pub struct Params {
     block_hashes: Vec<String>,
 }
 
-#[derive(Serialize, Debug)]
-pub struct ChainProof;
+#[derive(Serialize, Debug, PartialEq)]
+pub struct ChainProof {
+    receipt: Vec<u8>,
+}
 
 pub async fn v_prove_chain(params: Params) -> Result<ChainProof, AppError> {
     let params: ValidatedParams = params.try_into()?;
@@ -35,5 +37,20 @@ mod tests {
             v_prove_chain(empty_block_hashes).await.unwrap_err(),
             AppError::NoBlockHashes
         );
+    }
+
+    #[tokio::test]
+    async fn success() -> Result<(), AppError> {
+        let parent_block_hash =
+            "0xb390d63aac03bbef75de888d16bd56b91c9291c2a7e38d36ac24731351522bd1".to_string(); // https://etherscan.io/block/19999999
+        let block_hash =
+            "0xd24fd73f794058a3807db926d8898c6481e902b7edb91ce0d479d6760f276183".to_string(); // https://etherscan.io/block/20000000
+        let params = Params {
+            block_hashes: vec![parent_block_hash, block_hash],
+        };
+
+        assert!(v_prove_chain(params).await.is_ok());
+
+        Ok(())
     }
 }
