35cb3206fd64aeb71e1bd941fc95f4db5b589c23
pociej <grzegorz.pociejewski@gmail.com>
2024-09-12 09:53:56 +0200

    feat: use web-components
    - use VlayerTheme
    - launch radix

--- examples/web_proof/vlayer/src/App.tsx
+++ examples/web_proof/vlayer/src/App.tsx
@@ -1,16 +1,20 @@
 import vlayerLogo from "/vlayer_logo.svg";
-
+import { VlayerTheme } from "@vlayer/components";
+import { Button, Grid, TextField } from "@radix-ui/themes";
 function App() {
   return (
-    <>
-      <div
-        style={{
-          padding: "2em",
-        }}
-      >
-        <img src={vlayerLogo} alt="Vlayer Logo" />
-      </div>
-    </>
+    <VlayerTheme>
+      <Grid columns="1" gapY="4" top="8">
+        <div>
+          <img src={vlayerLogo} alt="Vlayer Logo" />
+        </div>
+        <Button variant="soft">Request web proof </Button>
+
+        <TextField.Root placeholder="Provide url... ">
+          <TextField.Slot></TextField.Slot>
+        </TextField.Root>
+      </Grid>
+    </VlayerTheme>
   );
 }
 
