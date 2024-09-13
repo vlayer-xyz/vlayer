f23fe8b97486c919f71b82911a38d29b6d871b4d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-09-13 12:47:55 +0200

    Rename call_and_format to call_and_convert_to_json

--- rust/services/prove_chain/server/src/server.rs
+++ rust/services/prove_chain/server/src/server.rs
@@ -7,13 +7,13 @@ use crate::{config::ServerConfig, handlers::v_prove_chain::v_prove_chain};
 
 pub fn server(config: ServerConfig) -> Router {
     let config = Arc::new(config);
-    let call_and_format = |config: Arc<ServerConfig>, params| async move {
+    let call_and_convert_to_json = |config: Arc<ServerConfig>, params| async move {
         v_prove_chain(config.clone(), params)
             .await
             .map(|x| x.to_json())
     };
     let v_prove_chain_handler =
-        move |params| Box::pin(call_and_format(config.clone(), params)) as Pin<Box<_>>;
+        move |params| Box::pin(call_and_convert_to_json(config.clone(), params)) as Pin<Box<_>>;
     let jrpc_handler =
         move |req| async move { route(req, "v_proveChain", v_prove_chain_handler).await };
 
