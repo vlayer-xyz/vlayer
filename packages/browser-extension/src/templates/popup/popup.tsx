8317d87c2040c70b3ca5d687e4db72cfa3701620
pociej <grzegorz.pociejewski@gmail.com>
2024-09-16 06:45:30 +0200

    Add sidepanel to browser extension

--- packages/browser-extension/src/templates/popup/popup.tsx
+++ packages/browser-extension/src/templates/popup/popup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Popup from "./pages/Popup";
+import Popup from "../../pages/Popup";
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
