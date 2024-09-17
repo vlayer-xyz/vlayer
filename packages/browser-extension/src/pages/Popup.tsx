c766a2ac6c77e1f64a102b4d40b618297b0c683a
pociej <grzegorz.pociejewski@gmail.com>
2024-09-17 08:27:04 +0200

    Create sidepanel content outline

--- packages/browser-extension/src/pages/Popup.tsx
+++ packages/browser-extension/src/pages/Popup.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import * as tlsn from "tlsn-js"
-
-console.log("tlsn", tlsn)
 export default function Popup() {
   return (
     <>
       <img src="/vlayer_logo.svg" />
       <h1>Vlayer extension</h1>
-      <button onClick={() => {
-          prove('https://rickandmortyapi.com/api/character/2', {
-            method: 'GET',
-            websocketProxyUrl: 'wss://notary.pse.dev/proxy?token=rickandmortyapi.com',
-            notaryUrl: 'https://notary.pse.dev/v0.1.0-alpha.5/', 
-          }
-          )}}>
-          prove swapi
-        </button>
     </>
   );
 }
--- packages/browser-extension/src/pages/SidePanel.tsx
+++ packages/browser-extension/src/pages/SidePanel.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Button, Theme, Grid } from "@radix-ui/themes";
 export default function SidePanel() {
-  return (
-    <>
-      <img src="/vlayer_logo.svg" />
-      <h1>Vlayer extension</h1>
-    </>
-  );
+	return (
+		<Theme accentColor="violet">
+			<Grid columns="8" gapY="4" top="16" style={{ marginTop: '80px' }} >
+				<div style={{ gridColumn: 'span 1' }}></div>
+				<div style={{ gridColumn: 'span 6' }}>
+					<Grid columns="1" gapY="4">
+						<Button variant="soft"> Go to page .. </Button>
+						<Button> Make a proof  </Button>
+					</Grid>
+				</div>
+			</Grid>
+		</Theme>
+	);
 }
