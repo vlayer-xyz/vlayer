84a5caf144bfe77c8a43d85cdb7d40e4d2266897
Piotr Żelazko <piotr@vlayer.xyz>
2024-09-17 16:38:53 +0200

    chore: change default proving mode to 'fake' (#590)

--- rust/cli/src/commands/args.rs
+++ rust/cli/src/commands/args.rs
@@ -13,7 +13,7 @@ pub(crate) struct InitArgs {
     pub(crate) project_name: Option<String>,
 }
 
-#[derive(Parser)]
+#[derive(Parser, Default)]
 pub(crate) struct ServeArgs {
     #[arg(long, action = ArgAction::Append, value_parser = parse_rpc_url)]
     pub(crate) rpc_url: Vec<(ChainId, String)>,
@@ -32,11 +32,11 @@ pub(crate) enum TemplateOption {
     WebProof,
 }
 
-#[derive(Clone, Debug, ValueEnum, Default)]
+#[derive(Clone, Debug, ValueEnum, Default, PartialEq, Eq)]
 pub(crate) enum ProofModeArg {
     #[default]
-    Groth16,
     Fake,
+    Groth16,
 }
 
 impl ProofModeArg {
@@ -70,4 +70,19 @@ fn parse_rpc_url(s: &str) -> Result<(ChainId, String), String> {
         .map_err(|_| format!("Invalid chain ID: {}", parts[0]))?;
     let url = parts[1..].join(":");
     Ok((chain_id, url))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod serve_args {
+        use super::*;
+
+        #[test]
+        fn default_proving_mode_is_fake() {
+            let args: ServeArgs = Default::default();
+            assert_eq!(args.proof.unwrap_or_default(), ProofModeArg::Fake);
+        }
+    }
 }
