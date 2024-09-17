4eacc7c8f34cbe7fe670fb5bd9b7997f89f3ad5e
pociej <grzegorz.pociejewski@gmail.com>
2024-09-17 10:18:02 +0200

    fix lint

--- packages/browser-extension/src/templates/sidepanel/sidepanel.tsx
+++ packages/browser-extension/src/templates/sidepanel/sidepanel.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SidePanel from "../../pages/SidePanel"
-import '@radix-ui/themes/styles.css';
+import SidePanel from "../../pages/SidePanel";
+import "@radix-ui/themes/styles.css";
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
-    <SidePanel  />
+    <SidePanel />
   </React.StrictMode>,
 );
