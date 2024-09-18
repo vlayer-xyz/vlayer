e4eed064e2efa471f59604cf37efa374c0c5d96d
Arkadiusz Konior <akonior@gmail.com>
2024-09-18 13:35:55 +0200

    Check token owner

--- examples/web_proof/vlayer/prove.ts
+++ examples/web_proof/vlayer/prove.ts
@@ -1,8 +1,10 @@
-import { testHelpers, prove } from "@vlayer/sdk";
+import {prove, testHelpers} from "@vlayer/sdk";
 import webProofProver from "../out/WebProofProver.sol/WebProofProver";
 import webProofVerifier from "../out/WebProofVerifier.sol/WebProofVerifier";
 import tls_proof from "./tls_proof.json";
 import * as assert from "assert";
+import {encodePacked, keccak256} from 'viem';
+
 
 const notaryPubKey =
   "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
@@ -30,8 +32,6 @@ console.log("Verified!");
 
 const [account] = await testHelpers.client().getAddresses();
 
-const name = await testHelpers.call(webProofVerifier.abi, verifier, "name");
-const symbol = await testHelpers.call(webProofVerifier.abi, verifier, "symbol");
 const balance = await testHelpers.call(
   webProofVerifier.abi,
   verifier,
@@ -39,6 +39,17 @@ const balance = await testHelpers.call(
   [account],
 );
 
-assert.strictEqual(name, "TwitterNFT");
-assert.strictEqual(symbol, "TNFT");
 assert.strictEqual(balance, 1n);
+
+const tokenOwnerAddress = await testHelpers.call(
+  webProofVerifier.abi,
+  verifier,
+  "ownerOf",
+  [generateTokenId("jab68503")],
+);
+
+assert.strictEqual(account, tokenOwnerAddress);
+
+function generateTokenId(username: string): bigint {
+  return BigInt(keccak256(encodePacked(['string'], [username])));
+}
