59496549a4001bbf3dd6ead234751dfa1852b567
Arkadiusz Konior <akonior@gmail.com>
2024-09-18 13:59:48 +0200

    Explicitly use twitterUserAddress

--- examples/web_proof/vlayer/prove.ts
+++ examples/web_proof/vlayer/prove.ts
@@ -1,10 +1,9 @@
-import {prove, testHelpers} from "@vlayer/sdk";
+import { prove, testHelpers } from "@vlayer/sdk";
 import webProofProver from "../out/WebProofProver.sol/WebProofProver";
 import webProofVerifier from "../out/WebProofVerifier.sol/WebProofVerifier";
 import tls_proof from "./tls_proof.json";
 import * as assert from "assert";
-import {encodePacked, keccak256} from 'viem';
-
+import { encodePacked, keccak256 } from "viem";
 
 const notaryPubKey =
   "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
@@ -23,20 +22,23 @@ const { proof, returnValue } = await prove(prover, webProofProver.abi, "main", [
 ]);
 console.log("Proof:", proof);
 
-console.log("Verifying...");
-await testHelpers.writeContract(verifier, webProofVerifier.abi, "verify", [
-  proof,
-  returnValue,
-]);
-console.log("Verified!");
+const twitterUserAddress = (await testHelpers.getTestAddresses())[0];
 
-const [account] = await testHelpers.client().getAddresses();
+console.log("Verifying...");
+await testHelpers.writeContract(
+  verifier,
+  webProofVerifier.abi,
+  "verify",
+  [proof, returnValue],
+  twitterUserAddress,
+);
+console.log("Verified!");
 
 const balance = await testHelpers.call(
   webProofVerifier.abi,
   verifier,
   "balanceOf",
-  [account],
+  [twitterUserAddress],
 );
 
 assert.strictEqual(balance, 1n);
@@ -48,8 +50,8 @@ const tokenOwnerAddress = await testHelpers.call(
   [generateTokenId("jab68503")],
 );
 
-assert.strictEqual(account, tokenOwnerAddress);
+assert.strictEqual(twitterUserAddress, tokenOwnerAddress);
 
 function generateTokenId(username: string): bigint {
-  return BigInt(keccak256(encodePacked(['string'], [username])));
+  return BigInt(keccak256(encodePacked(["string"], [username])));
 }
