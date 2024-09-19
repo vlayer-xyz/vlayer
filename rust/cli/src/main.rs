43ab30a1ac39a34fa42628bb442a2fc8c1c64d5c
Stanisław Durka <stanislaw@vlayer.xyz>
2024-09-19 12:00:25 +0200

    chore: change logging to info and discard RUST_LOG flag

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -33,7 +33,7 @@ enum Commands {
 async fn main() {
     // In order to view logs, run `RUST_LOG=info cargo run`
     tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .with_max_level(tracing::Level::INFO)
         .init();
 
     match run().await {
