a3e696e1d9119e380b046d899204fcbe678e0dc2
vanruch <ivanruch@gmail.com>
2024-09-19 11:10:14 +0200

    fix rebase mistake

--- examples/email_proof/src/vlayer/EmailProver.sol
+++ examples/email_proof/src/vlayer/EmailProver.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
 
 import {Prover} from "vlayer-0.1.0/src/Prover.sol";
-import {Email, MimeEmail, EmailProofLib} from "vlayer-0.1.0/src/EmailProof.sol";
+import {VerifiedEmail, MimeEmail, EmailProofLib} from "vlayer-0.1.0/src/EmailProof.sol";
 
 interface IExample {
     function exampleFunction() external returns (uint256);
@@ -15,7 +15,7 @@ contract EmailProver is Prover {
     using EmailProofLib for MimeEmail;
 
     function main(MimeEmail calldata emailProof) public view returns (bool) {
-        Email memory email = emailProof.verify();
+        VerifiedEmail memory email = emailProof.verify();
 
         require(email.subject.equal("Is dinner ready?"), "subject must be 'Is dinner ready?'");
 
