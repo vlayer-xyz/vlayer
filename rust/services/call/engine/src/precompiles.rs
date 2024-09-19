dce03155d2d208ebf0004a1ec3f4c23b77fd8802
vanruch <ivanruch@gmail.com>
2024-09-19 09:30:58 +0200

    Gracefully handle precompile reverts

--- rust/services/call/engine/src/precompiles.rs
+++ rust/services/call/engine/src/precompiles.rs
@@ -8,7 +8,7 @@ use crate::precompiles::verify_and_parse_email::VERIFY_EMAIL_PRECOMPILE;
 use revm::precompile::Error::OutOfGas;
 use revm::precompile::{calc_linear_cost_u32, u64_to_address};
 use revm::{
-    precompile::{PrecompileError::Other, PrecompileErrors::Error, PrecompileWithAddress},
+    precompile::{PrecompileErrors::Error, PrecompileWithAddress},
     primitives::PrecompileErrors,
 };
 
@@ -19,7 +19,9 @@ pub(crate) const VLAYER_PRECOMPILES: [PrecompileWithAddress; 3] = [
 ];
 
 fn map_to_other<E: ToString>(err: E) -> PrecompileErrors {
-    Error(Other(err.to_string()))
+    PrecompileErrors::Fatal {
+        msg: err.to_string(),
+    }
 }
 
 fn gas_used(len: usize, base: u64, word: u64, gas_limit: u64) -> Result<u64, PrecompileErrors> {
