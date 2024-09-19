ea9bcbfafc2859da7f133d8887a19a9ddc24d07f
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-09-19 09:45:20 +0200

    Fix deploy.ts script (#595)
    * Fix deploy.ts script
    
    - use Bun utils
    - make sure it doesnt ovewrite other envs
    
    * Make .env.* files gitignored

--- examples/web_proof/vlayer/deploy.ts
+++ examples/web_proof/vlayer/deploy.ts
@@ -1,10 +1,7 @@
 import { testHelpers } from "@vlayer/sdk";
-import fs from "node:fs";
+import Bun from "bun";
 import path from "node:path";
-
-//@ts-expect-error - sol file is not a module
 import webProofProver from "../out/WebProofProver.sol/WebProofProver";
-//@ts-expect-error - sol file is not a module
 import webProofVerifier from "../out/WebProofVerifier.sol/WebProofVerifier";
 
 const [prover, verifier] = await testHelpers.deployProverVerifier(
@@ -13,12 +10,40 @@ const [prover, verifier] = await testHelpers.deployProverVerifier(
 );
 
 const envPath = path.resolve(__dirname, ".env.development");
-const envContent = `VITE_PROVER_ADDRESS=${prover}\nVITE_VERIFIER_ADDRESS=${verifier}\n`;
 
-fs.writeFile(envPath, envContent, (err) => {
-  if (err) {
-    console.error("Error writing to .env.dev file:", err);
-  } else {
-    console.log("Successfully wrote to .env.dev file");
+try {
+  const envFile = Bun.file(envPath);
+  let envContent = await envFile.text();
+
+  if (!envContent) {
+    envContent = "";
   }
-});
+
+  envContent = `${envContent}\n`;
+
+  const proverRegex = /^VITE_PROVER_ADDRESS=.*/m;
+  const verifierRegex = /^VITE_VERIFIER_ADDRESS=.*/m;
+
+  if (proverRegex.test(envContent)) {
+    envContent = envContent.replace(
+      proverRegex,
+      `VITE_PROVER_ADDRESS=${prover}`,
+    );
+  } else {
+    envContent += `VITE_PROVER_ADDRESS=${prover}\n`;
+  }
+
+  if (verifierRegex.test(envContent)) {
+    envContent = envContent.replace(
+      verifierRegex,
+      `VITE_VERIFIER_ADDRESS=${verifier}`,
+    );
+  } else {
+    envContent += `VITE_VERIFIER_ADDRESS=${verifier}\n`;
+  }
+
+  await Bun.write(envPath, envContent);
+  console.log("Successfully updated the .env.development file");
+} catch (err) {
+  console.error("Error updating the .env.development file:", err);
+}
