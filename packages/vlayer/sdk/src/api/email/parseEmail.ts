9aaa4092a5966bb6bfd8acc695fe554c5cb33469
vanruch <ivanruch@gmail.com>
2024-09-23 10:16:12 +0200

    custom err type

--- packages/vlayer/sdk/src/api/email/parseEmail.ts
+++ packages/vlayer/sdk/src/api/email/parseEmail.ts
@@ -1,5 +1,12 @@
 import { simpleParser, HeaderValue, type ParsedMail } from "mailparser";
 
+export class DkimParsingError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "DkimParsingError";
+  }
+}
+
 export async function parseEmail(mime: string) {
   return simpleParser(mime.trim(), {
     skipHtmlToText: true,
@@ -11,7 +18,7 @@ export async function parseEmail(mime: string) {
 
 export function getDkimSigners(mail: ParsedMail) {
   const dkimHeader = mail.headers.get("dkim-signature");
-  if (!dkimHeader) throw new Error("No DKIM header found");
+  if (!dkimHeader) throw new DkimParsingError("No DKIM header found");
   if (Array.isArray(dkimHeader)) {
     return dkimHeader.map(parseHeader);
   }
@@ -20,15 +27,15 @@ export function getDkimSigners(mail: ParsedMail) {
 
 function parseHeader(header: HeaderValue) {
   if (typeof header === "string" || !("params" in header)) {
-    throw new Error(`Invalid dkim header ${header}`);
+    throw new DkimParsingError(`Invalid DKIM header ${header}`);
   }
 
   if (!header.params.d) {
-    throw new Error("DKIM header missing domain");
+    throw new DkimParsingError("DKIM header missing domain");
   }
 
   if (!header.params.s) {
-    throw new Error("DKIM header missing selector");
+    throw new DkimParsingError("DKIM header missing selector");
   }
 
   return {
