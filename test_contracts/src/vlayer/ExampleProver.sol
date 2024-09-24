3ce00c4656a874721ef9771d08f7367419122359
pociej <grzegorz.pociejewski@gmail.com>
2024-09-24 14:13:11 +0200

    Launch soldeer in testcontracts

--- test_contracts/src/vlayer/ExampleProver.sol
+++ test_contracts/src/vlayer/ExampleProver.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import {Prover} from "vlayer/Prover.sol";
 import {Web, WebProof, WebProofLib, WebLib} from "vlayer/WebProof.sol";
-import "openzeppelin/contracts/utils/Strings.sol";
 
+import "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
 /*
  * This contract is used in rust/server integration tests. The test fixture
  * (compiled contract) is placed in rust/server/testdata/ExampleProver.json.
@@ -12,7 +12,7 @@ import "openzeppelin/contracts/utils/Strings.sol";
  * In order to update the test fixture:
  * 1. Modify this contract below.
  * 2. cd test_contracts && forge build
- * 3. cp out/ExampleProver.sol/ExampleProver.json ../rust/call/server/testdata
+ * 3. cp out/ExampleProver.sol/ExampleProver.json ../rust/services/call/server/testdata
  */
 
 contract ExampleProver is Prover {
