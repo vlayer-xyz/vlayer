66c5c1d6d52d1903699fc41f9ace00d3449e5613
Arkadiusz Konior <akonior@gmail.com>
2024-09-24 11:16:41 +0200

    Solidity format

--- contracts/test/vlayer/WebProofLib.t.sol
+++ contracts/test/vlayer/WebProofLib.t.sol
@@ -9,7 +9,8 @@ contract WebProverTest is VTest {
 
     function test_verifiesWebProof() public {
         WebProof memory webProof = WebProof(vm.readFile("testdata/web_proof.json"));
-        string memory dataUrl = "https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true";
+        string memory dataUrl =
+            "https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true";
 
         callProver();
         Web memory web = webProof.verify(dataUrl);
