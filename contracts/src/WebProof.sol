cf646bf12d3695edae5df8f4a64f787319a72136
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-09-25 13:14:46 +0200

    Add json precompiles (#640)
    * Add json precompiles
    
    * Reorder precompiles
    
    * Use module instead of comments
    
    * Launch soldeer in testcontracts
    
    * Organise precompiles properly
    
    * Remove unnecessary comments
    
    * Update addresses in solidity contracts

--- contracts/src/WebProof.sol
+++ contracts/src/WebProof.sol
@@ -15,7 +15,7 @@ library WebProofLib {
     using Strings for string;
 
     address private constant VERIFY_AND_PARSE_PRECOMPILE = address(0x100);
-    address private constant JSON_GET_STRING_PRECOMPILE = address(0x101);
+    address private constant JSON_GET_STRING_PRECOMPILE = address(0x102);
 
     string private constant NOTARY_PUB_KEY =
         "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
@@ -35,7 +35,7 @@ library WebProofLib {
 }
 
 library WebLib {
-    address private constant JSON_GET_STRING_PRECOMPILE = address(0x101);
+    address private constant JSON_GET_STRING_PRECOMPILE = address(0x102);
 
     function jsonGetString(Web memory web, string memory jsonPath) internal view returns (string memory) {
         require(bytes(web.body).length > 0, "Body is empty");
