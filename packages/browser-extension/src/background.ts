cc436cff9656cbd5e4e349f5a32f35537cedb2c6
pociej <grzegorz.pociejewski@gmail.com>
2024-09-26 10:05:24 +0200

    Remove unnecessary notification

--- packages/browser-extension/src/background.ts
+++ packages/browser-extension/src/background.ts
@@ -2,14 +2,6 @@ import browser from "webextension-polyfill";
 import { MESSAGE } from "./constants/message";
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-  console.log("Actives tab changed", activeInfo);
-  chrome.notifications.create({
-    type: "basic",
-    title: "Action Triggered",
-    message: "You clicked the extension icon!",
-    iconUrl: "./vlayer_logo.svg",
-  });
-
   windowId = activeInfo.windowId;
 });
 
