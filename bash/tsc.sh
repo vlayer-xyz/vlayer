ecb5625f94e782a2e6e71ea25347ea8bf05c9e6e
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-09-26 10:27:17 +0200

    Update bash/tsc.sh
    Co-authored-by: Arkadiusz Konior <akonior@gmail.com>

--- bash/tsc.sh
+++ bash/tsc.sh
@@ -5,6 +5,6 @@ set -ueo pipefail
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 
 cd "${VLAYER_HOME}/packages/vlayer/sdk"
-echo "Running tsc  for: $VLAYER_HOME/packages/vlayer/sdk"
+echo "Running tsc for: $VLAYER_HOME/packages/vlayer/sdk"
 bun install
 tsc  --noEmit
