2dae79bd6b5c1c2880c33e742017bd297f6a4e69
Ivan Rukhavets <ivanruch@gmail.com>
2024-09-26 13:40:36 +0200

    Pass single dns record into the contract (#656)

--- rust/email_proof/src/dkim/static_resolver.rs
+++ rust/email_proof/src/dkim/static_resolver.rs
@@ -3,17 +3,23 @@ use mail_auth::common::resolve::{IntoFqdn, Resolve, UnwrapTxtRecord};
 use mail_auth::Txt;
 use std::sync::Arc;
 
-pub struct StaticResolver {}
+pub struct StaticResolver<'a> {
+    dns_record: &'a [u8],
+}
 
-const HARDCODED_DNS_RECORD: &str =
-    "newengland._domainkey.example.com v=DKIM1; \
-p=MIGJAoGBALVI635dLK4cJJAH3Lx6upo3X/Lm1tQz3mezcWTA3BUBnyIsdnRf57aD5BtNmhPrYYDlWlzw3UgnKisIxktkk5+iMQMlFtAS10JB8L3YadXNJY+JBcbeSi5TgJe4WFzNgW95FWDAuSTRXSWZfA/8xjflbTLDx0euFZOM7C4T0GwLAgMBAAE=";
+impl<'a> StaticResolver<'a> {
+    pub fn new(dns_record: &'a str) -> Self {
+        Self {
+            dns_record: dns_record.as_bytes(),
+        }
+    }
+}
 
-impl Resolve for StaticResolver {
+impl Resolve for StaticResolver<'_> {
     async fn txt_lookup<'x, T: TxtRecordParser + Into<Txt> + UnwrapTxtRecord>(
         &self,
         _key: impl IntoFqdn<'x>,
     ) -> mail_auth::Result<Arc<T>> {
-        Ok(Arc::new(T::parse(HARDCODED_DNS_RECORD.as_bytes())?))
+        Ok(Arc::new(T::parse(self.dns_record)?))
     }
 }
