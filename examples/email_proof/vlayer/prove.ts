742fb304c498f480b640f76542069e04f22ba6f6
vanruch <ivanruch@gmail.com>
2024-09-26 09:55:08 +0200

    use preverify email

--- examples/email_proof/vlayer/prove.ts
+++ examples/email_proof/vlayer/prove.ts
@@ -1,11 +1,11 @@
 import fs from "fs";
-import { testHelpers, prove } from "@vlayer/sdk";
+import { testHelpers, prove, enrichEmail } from "@vlayer/sdk";
 import emailProofProver from "../out/EmailProver.sol/EmailProver";
 import emailProofVerifier from "../out/EmailProofVerifier.sol/EmailProofVerifier";
 
 const mimeEmail = fs.readFileSync("../testdata/test_email.txt").toString();
 
-const unverifiedEmail = { email: mimeEmail };
+const unverifiedEmail = await enrichEmail(mimeEmail);
 
 const [prover, verifier] = await testHelpers.deployProverVerifier(
   emailProofProver,
