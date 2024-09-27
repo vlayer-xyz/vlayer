d995352f99b21cd8a4e9bebfdcaffc1970fdbce8
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2024-09-27 14:47:56 +0200

    Implement alloy_rlp::Decode & alloy_rlp::Decode for Node (#675)

--- rust/mpt/src/node/rlp.rs
+++ rust/mpt/src/node/rlp.rs
@@ -1,5 +1,5 @@
 use alloy_primitives::B256;
-use alloy_rlp::{Decodable, Encodable, Header, EMPTY_STRING_CODE};
+use alloy_rlp::{BufMut, Decodable, Encodable, Header};
 use rlp as legacy_rlp;
 
 use crate::{
@@ -10,6 +10,79 @@ use crate::{
 
 use super::Node;
 
+impl Encodable for Node {
+    fn encode(&self, out: &mut dyn BufMut) {
+        match self {
+            Node::Null => "".encode(out),
+            Node::Leaf(prefix, value) => {
+                let path = prefix.encode_path_leaf(true);
+                encode_header(true, path.length() + value.length(), out);
+                path.encode(out);
+                value.encode(out);
+            }
+            Node::Extension(prefix, child) => {
+                let path = prefix.encode_path_leaf(false);
+                let node_ref = NodeRef::from_node(child);
+                encode_header(true, path.length() + node_ref.length(), out);
+                path.encode(out);
+                node_ref.encode(out);
+            }
+            Node::Branch(children, value) => {
+                let mut child_refs: [NodeRef; 16] = Default::default();
+                let mut payload_length = 0;
+
+                for (i, child) in children.iter().enumerate() {
+                    match child.as_deref() {
+                        Some(node) => {
+                            let node_ref = NodeRef::from_node(node);
+                            payload_length += node_ref.length();
+                            child_refs[i] = node_ref;
+                        }
+                        None => payload_length += 1,
+                    }
+                }
+
+                payload_length += value
+                    .as_ref()
+                    .map_or(1 /* EMPTY_STRING_CODE */, |v| v.len());
+
+                encode_header(true, payload_length, out);
+                child_refs.iter().for_each(|child| child.encode(out));
+
+                let value = value.as_deref().unwrap_or(&[]);
+                value.encode(out);
+            }
+            Node::Digest(digest) => digest.encode(out),
+        }
+    }
+}
+
+impl Decodable for Node {
+    fn decode(buf: &mut &[u8]) -> alloy_rlp::Result<Self> {
+        let rlp = legacy_rlp::Rlp::new(buf);
+        legacy_rlp::Decodable::decode(&rlp).map_err(map_rlp_error)
+    }
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn map_rlp_error(err: legacy_rlp::DecoderError) -> alloy_rlp::Error {
+    match err {
+        rlp::DecoderError::RlpIsTooBig | rlp::DecoderError::RlpInconsistentLengthAndData => {
+            alloy_rlp::Error::UnexpectedLength
+        }
+        rlp::DecoderError::RlpIsTooShort => alloy_rlp::Error::InputTooShort,
+        rlp::DecoderError::RlpDataLenWithZeroPrefix
+        | rlp::DecoderError::RlpListLenWithZeroPrefix => alloy_rlp::Error::LeadingZero,
+        rlp::DecoderError::RlpInvalidLength => alloy_rlp::Error::Overflow,
+        rlp::DecoderError::RlpExpectedToBeList => alloy_rlp::Error::Custom("list expected"),
+        rlp::DecoderError::RlpExpectedToBeData => alloy_rlp::Error::Custom("data expected"),
+        rlp::DecoderError::RlpIncorrectListLen => alloy_rlp::Error::Custom("incorrect list length"),
+        rlp::DecoderError::RlpInvalidIndirection => alloy_rlp::Error::Custom("invalid indirection"),
+        rlp::DecoderError::Custom(str) => alloy_rlp::Error::Custom(str),
+    }
+}
+
+// TODO: Remove `legacy_rlp` dependency and rewrite this with `alloy_rlp`
 impl legacy_rlp::Decodable for Node {
     fn decode(rlp: &legacy_rlp::Rlp) -> Result<Self, legacy_rlp::DecoderError> {
         use legacy_rlp::{Decodable, DecoderError, Prototype};
@@ -59,55 +132,8 @@ impl legacy_rlp::Decodable for Node {
 
 impl Node {
     /// Returns the RLP encoding of the node.
-    pub(crate) fn rlp_encoded(&self) -> Vec<u8> {
-        match self {
-            Node::Null => vec![EMPTY_STRING_CODE],
-            Node::Leaf(prefix, value) => {
-                let path = prefix.encode_path_leaf(true);
-                let mut out = encoded_header(true, path.length() + value.length());
-                path.encode(&mut out);
-                value.encode(&mut out);
-
-                out
-            }
-            Node::Extension(prefix, child) => {
-                let path = prefix.encode_path_leaf(false);
-                let node_ref = NodeRef::from_node(child);
-                let mut out = encoded_header(true, path.length() + node_ref.length());
-                path.encode(&mut out);
-                node_ref.encode(&mut out);
-
-                out
-            }
-            Node::Branch(children, value) => {
-                let mut child_refs: [NodeRef; 16] = Default::default();
-                let mut payload_length = 0;
-
-                for (i, child) in children.iter().enumerate() {
-                    match child.as_deref() {
-                        Some(node) => {
-                            let node_ref = NodeRef::from_node(node);
-                            payload_length += node_ref.length();
-                            child_refs[i] = node_ref;
-                        }
-                        None => payload_length += 1,
-                    }
-                }
-
-                payload_length += value
-                    .as_ref()
-                    .map_or(1 /* EMPTY_STRING_CODE */, |v| v.len());
-
-                let mut out = encoded_header(true, payload_length);
-                child_refs.iter().for_each(|child| child.encode(&mut out));
-
-                let value = value.as_deref().unwrap_or(&[]);
-                value.encode(&mut out);
-
-                out
-            }
-            Node::Digest(digest) => alloy_rlp::encode(digest),
-        }
+    pub fn rlp_encoded(&self) -> Vec<u8> {
+        alloy_rlp::encode(self)
     }
 
     pub(crate) fn to_rlp_nodes(&self) -> Vec<Vec<u8>> {
@@ -128,13 +154,11 @@ impl Node {
 }
 
 #[inline]
-fn encoded_header(list: bool, payload_length: usize) -> Vec<u8> {
+fn encode_header(list: bool, payload_length: usize, out: &mut dyn BufMut) {
     debug_assert!(payload_length > 0);
     let header = Header {
         list,
         payload_length,
     };
-    let mut out = Vec::with_capacity(header.length() + payload_length);
-    header.encode(&mut out);
-    out
+    header.encode(out);
 }
