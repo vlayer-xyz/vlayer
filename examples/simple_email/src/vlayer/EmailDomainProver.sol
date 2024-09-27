9288c9a975cd4700612480167784ca448c59c8da
Artur Chmaro <artur@vlayer.xyz>
2024-09-27 15:05:56 +0200

    move contains helper to library

--- examples/simple_email/src/vlayer/EmailDomainProver.sol
+++ examples/simple_email/src/vlayer/EmailDomainProver.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.13;
 import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
 import {Prover} from "vlayer-0.1.0/src/Prover.sol";
 import {VerifiedEmail, UnverifiedEmail, EmailProofLib} from "vlayer-0.1.0/src/EmailProof.sol";
+import {EmailStrings} from "./EmailStrings.sol";
 
 contract EmailDomainProver is Prover {
     using Strings for string;
+    using EmailStrings for string;
     using EmailProofLib for UnverifiedEmail;
 
     string targetDomain;
@@ -22,26 +24,9 @@ contract EmailDomainProver is Prover {
     {
         VerifiedEmail memory email = unverifiedEmail.verify();
 
-        require(contains(email.from, targetDomain), "incorrect sender domain");
+        require(email.from.contains(targetDomain), "incorrect sender domain");
         require(email.subject.equal("Verify me for company NFT"), "incorrect subject");
 
         return (sha256(abi.encodePacked(email.from)), targetWallet);
-    }
-
-    function contains(string memory _string, string memory _substring) public pure returns (bool) {
-        bytes memory stringBytes = bytes(_string);
-        bytes memory substringBytes = bytes(_substring);
-
-        for (uint256 i = 0; i <= stringBytes.length - substringBytes.length; i++) {
-            bool found = true;
-            for (uint256 j = 0; j < substringBytes.length; j++) {
-                if (stringBytes[i + j] != substringBytes[j]) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) return true;
-        }
-        return false;
     }
 }
