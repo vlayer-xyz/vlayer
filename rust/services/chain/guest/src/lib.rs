b5f9a42bc0f346ce450d1dbdcf42e312d978f96e
Leonid Logvinov <logvinov.leon@gmail.com>
2024-09-30 14:44:30 +0200

    Use RLP for block number encoding

--- rust/services/chain/guest/src/lib.rs
+++ rust/services/chain/guest/src/lib.rs
@@ -1,3 +1,4 @@
+use alloy_rlp::encode_fixed_size;
 use block_header::EvmBlockHeader;
 pub use chain_engine::Input;
 use mpt::MerkleTrie;
@@ -6,7 +7,8 @@ use serde::Serialize;
 
 pub fn initialize(elf_id: Digest, block: &dyn EvmBlockHeader) -> impl Serialize {
     let mut mpt = MerkleTrie::new();
-    mpt.insert(block.number().to_ne_bytes(), block.hash_slow())
+    let encoded_block_num = encode_fixed_size(&block.number());
+    mpt.insert(encoded_block_num, block.hash_slow())
         .expect("insert block number");
 
     (mpt.hash_slow(), elf_id)
