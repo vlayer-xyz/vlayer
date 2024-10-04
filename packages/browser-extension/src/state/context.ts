273c9253dfe608a54aa1f863ad0dc2b17e3e15f4
pociej <grzegorz.pociejewski@gmail.com>
2024-10-04 12:07:31 +0200

    Add history recording mechanism

--- packages/browser-extension/src/state/context.ts
+++ packages/browser-extension/src/state/context.ts
@@ -3,9 +3,9 @@ import browser from "webextension-polyfill";
 
 type HistoryItem = {
   url: string;
-  headers: {
-    [key: string]: string;
-  };
+  headers?: browser.WebRequest.HttpHeadersItemType[];
+  cookies?: browser.Cookies.Cookie[];
+  ready?: boolean;
 };
 
 type Context = {
@@ -13,18 +13,48 @@ type Context = {
   history: HistoryItem[];
 };
 
-export class ContextManager extends Store<Context> {
-  static #instance: Store<Context>;
+export class ContextManager {
+  static #instance: ContextManager;
+  store: Store<Context>;
 
-  public static get instance(): Store<Context> {
+  constructor() {
+    this.store = new Store<Context>(browser.storage.local);
+  }
+
+  public static get instance(): ContextManager {
     if (!ContextManager.#instance) {
-      ContextManager.#instance = new Store<Context>(browser.storage.local);
+      ContextManager.#instance = new ContextManager();
     }
     return ContextManager.#instance;
   }
 
-  async pushHistory(item: HistoryItem): Promise<void> {
-    const history = await ContextManager.instance.get("history");
-    return ContextManager.instance.set("history", [...history, item]);
+  getUrls = () => {
+    //this should return list of urls that should be recorded based on passed setup
+    return ["x.com"];
+  };
+
+  async updateHistory(item: HistoryItem): Promise<void> {
+    let newItem = item;
+    let history = await contextManager.store.get("history");
+    const existingItemIndex = history.findIndex((i) => i.url === item.url);
+
+    // Add cookies and headers and mark eventually as ready
+    if (existingItemIndex) {
+      const existingItem = history[existingItemIndex];
+      newItem = {
+        url: item.url,
+        headers: [...(existingItem.headers || []), ...(item.headers || [])],
+        cookies: [...(existingItem.cookies || []), ...(item.cookies || [])],
+        ready: item.ready ?? existingItem.ready ?? false,
+      };
+      history = history.map((historyItem, index) => {
+        return index === existingItemIndex ? newItem : historyItem;
+      });
+    } else {
+      history = [...history, newItem];
+    }
+    return contextManager.store.set("history", history);
   }
 }
+
+export const contextManager = ContextManager.instance;
