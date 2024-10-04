eebac69e4aa5378319d323f4a9ab53d47f18d207
Arkadiusz Konior <akonior@gmail.com>
2024-10-04 11:23:55 +0200

    Run tsc on all packages (#748)
    * Run tsc on all packages
    
    * Fix ts errors

--- bash/tsc.sh
+++ bash/tsc.sh
@@ -4,7 +4,7 @@ set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 
-cd "${VLAYER_HOME}/packages/vlayer/sdk"
-echo "Running tsc for: $VLAYER_HOME/packages/vlayer/sdk"
+cd "${VLAYER_HOME}/packages"
+echo "Running tsc for: $VLAYER_HOME/packages"
 bun install
 bun tsc  --noEmit
