09d2e5a03efc13fffe7c158335261e3a47c21223
Anna Jabłonowska <ania@vlayer.xyz>
2024-10-04 12:09:56 +0200

    Fix lint ts

--- examples/web_proof/vlayer/prove.ts
+++ examples/web_proof/vlayer/prove.ts
@@ -3,7 +3,7 @@ import webProofProver from "../out/WebProofProver.sol/WebProofProver";
 import webProofVerifier from "../out/WebProofVerifier.sol/WebProofVerifier";
 import tls_proof from "./tls_gp_proof.json";
 import * as assert from "assert";
-import { encodePacked, keccak256, type Address } from "viem";
+import { encodePacked, keccak256 } from "viem";
 
 const notaryPubKey =
   "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExpX/4R4z40gI6C/j9zAM39u58LJu\n3Cx5tXTuqhhu/tirnBi5GniMmspOTEsps4ANnPLpMmMSfhJ+IFHbc3qVOA==\n-----END PUBLIC KEY-----\n";
