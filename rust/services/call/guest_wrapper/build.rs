34d1d5aac5c755103c54cba4a63ff91cb2e2ca98
Piotr Żelazko <piotr@vlayer.xyz>
2024-10-08 15:08:23 +0200

    feat: enable injection of methods.rs and ImageId.sol

--- rust/services/call/guest_wrapper/build.rs
+++ rust/services/call/guest_wrapper/build.rs
@@ -6,38 +6,46 @@ fn main() -> Result<()> {
         use std::{
             env,
             fs::{copy, create_dir_all, remove_file},
+            io,
             path::Path,
         };
 
         use risc0_build_ethereum::{generate_solidity_files, Options};
 
+        let out_dir = env::var("OUT_DIR").expect("'OUT_DIR' is not set");
+        let out_dir_path = Path::new(&out_dir);
+        let assets_dir = Path::new("../../../target/assets");
+
+        let image_id_sol_output_path = assets_dir.join("ImageID.sol");
+        let elf_sol_output_path = assets_dir.join("Elf.sol");
+        let solidity_opts = Options::default()
+            .with_image_id_sol_path(&image_id_sol_output_path)
+            .with_elf_sol_path(&elf_sol_output_path);
+
+        let clean_assets_dir = || -> io::Result<()> {
+            create_dir_all(&assets_dir)?;
+            let _ = remove_file(&image_id_sol_output_path);
+            let _ = remove_file(&elf_sol_output_path);
+            Ok(())
+        };
+
         if env::var("RISC0_SKIP_BUILD").is_ok() {
             return Ok(());
         }
 
-        let guests = risc0_build::embed_methods();
+        clean_assets_dir()?;
 
-        let assets_dir = Path::new("../../../target/assets");
-        let image_id_path = assets_dir.join("ImageID.sol");
-        let elf_path = assets_dir.join("Elf.sol");
+        if let Ok(guest_artifacts_path) = env::var("RISC0_EXISTING_CALL_GUEST") {
+            let guest_artifacts_path = Path::new(&guest_artifacts_path);
 
-        create_dir_all(&assets_dir)?;
+            let methods_rs_path = guest_artifacts_path.join("methods.rs");
+            let image_id_path = guest_artifacts_path.join("ImageID.sol");
 
-        let _ = remove_file(&image_id_path);
-        let _ = remove_file(&elf_path);
-
-        let solidity_opts = Options::default()
-            .with_image_id_sol_path(&image_id_path)
-            .with_elf_sol_path(&elf_path);
-
-        generate_solidity_files(&*guests, &solidity_opts)?;
-
-        if let Ok(method_rs_replacement) = env::var("RISC0_REPLACE_METHOD_RS_PATH") {
-            let method_rs_replacement_path = Path::new(&method_rs_replacement);
-
-            let out_dir = env::var("OUT_DIR").expect("'OUT_DIR' is not set");
-            let out_dir_path = Path::new(&out_dir);
-            copy(&method_rs_replacement_path, out_dir_path.join("methods.rs"))?;
+            copy(methods_rs_path, out_dir_path.join("methods.rs"))?;
+            copy(image_id_path, image_id_sol_output_path)?;
+        } else {
+            let guests = risc0_build::embed_methods();
+            generate_solidity_files(&guests, &solidity_opts)?;
         }
     }
 
