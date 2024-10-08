7b3cb0407ac557217be461ed215f83457504510a
Anna Jabłonowska <ania@vlayer.xyz>
2024-10-08 16:11:39 +0200

    Fix lint

--- packages/vlayer/sdk/src/api/lib/client.ts
+++ packages/vlayer/sdk/src/api/lib/client.ts
@@ -3,16 +3,18 @@ import { WebProofProvider } from "types/webProofProvider";
 
 import { prove } from "../prover";
 import { createExtensionWebProofProvider } from "../webProof";
-export const createVlayerClient = ({
-  url = "127.0.0.1:3000",
-  webProofProvider = createExtensionWebProofProvider(),
-}: {
-  url?: string;
-  webProofProvider?: WebProofProvider;
-} = {
-  url: "127.0.0.1:3000",
-  webProofProvider: createExtensionWebProofProvider(),
-}): VlayerClient => {
+export const createVlayerClient = (
+  {
+    url = "127.0.0.1:3000",
+    webProofProvider = createExtensionWebProofProvider(),
+  }: {
+    url?: string;
+    webProofProvider?: WebProofProvider;
+  } = {
+    url: "127.0.0.1:3000",
+    webProofProvider: createExtensionWebProofProvider(),
+  },
+): VlayerClient => {
   // TODO : implement high level api
   console.log("createVlayerClient with", url, webProofProvider);
   return {
