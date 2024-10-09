6e72e74c33a619184be68af3d0d91bf6de696255
Anna Jabłonowska <ania@vlayer.xyz>
2024-10-09 15:16:13 +0200

    Remove prove export

--- packages/vlayer/sdk/src/index.ts
+++ packages/vlayer/sdk/src/index.ts
@@ -2,7 +2,7 @@ export { v_call } from "./api/v_call";
 export type { CallParams, CallContext } from "types/vlayer";
 export type { ContractSpec } from "types/ethereum";
 
-export { getContractSpec, prove } from "./api/prover";
+export { getContractSpec } from "./api/prover";
 export * as testHelpers from "./api/helpers";
 export { client as createTestClient } from "./api/helpers";
 export { preverifyEmail } from "./api/email/preverify.ts";
