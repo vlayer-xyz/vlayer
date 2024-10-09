f4cf51feac4300d2b609b53fcc611921e17fd84b
Arkadiusz Konior <akonior@gmail.com>
2024-10-09 13:13:13 +0200

    Improve useProvenUrl

--- packages/browser-extension/src/hooks/useProvenUrl.ts
+++ packages/browser-extension/src/hooks/useProvenUrl.ts
@@ -1,17 +1,23 @@
 import { useBrowsingHistory } from "./useBrowsingHistory";
 import { useMemo } from "react";
 import { useProvingSessionConfig } from "./useProvingSessionConfig";
+import { HistoryItem } from "../state/history";
 
 // NOTE this will need to be refactored
 // if one day we will decide to support multiple parallel proves scenario
 
-export const useProvenUrl = () => {
+export function useProvenUrl(): HistoryItem | undefined {
   const [config] = useProvingSessionConfig();
   const steps = config?.steps || [];
   const [browsingHistory] = useBrowsingHistory();
   return useMemo(() => {
-    const provenUrlAddress =
-      steps?.find(({ step }) => step === "notarize")?.url || "";
-    return browsingHistory.find((item) => item.url.includes(provenUrlAddress));
+    const provenUrlAddress = steps.find(({ step }) => step === "notarize")?.url;
+    if (provenUrlAddress) {
+      return browsingHistory.find((item) =>
+        item.url.includes(provenUrlAddress),
+      );
+    } else {
+      return undefined;
+    }
   }, [steps, browsingHistory]);
-};
+}
