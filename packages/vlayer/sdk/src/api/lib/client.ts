83580b8cae6035906370ce71d70fe6063079056e
Anna Jabłonowska <62891747+aajj999@users.noreply.github.com>
2024-10-09 13:15:53 +0200

    Fix default url in v_call (#799)
    Fix url

--- packages/vlayer/sdk/src/api/lib/client.ts
+++ packages/vlayer/sdk/src/api/lib/client.ts
@@ -5,13 +5,13 @@ import { prove } from "../prover";
 import { createExtensionWebProofProvider } from "../webProof";
 export const createVlayerClient = (
   {
-    url = "127.0.0.1:3000",
+    url = "http://127.0.0.1:3000",
     webProofProvider = createExtensionWebProofProvider(),
   }: {
     url?: string;
     webProofProvider?: WebProofProvider;
   } = {
-    url: "127.0.0.1:3000",
+    url: "http://127.0.0.1:3000",
     webProofProvider: createExtensionWebProofProvider(),
   },
 ): VlayerClient => {
