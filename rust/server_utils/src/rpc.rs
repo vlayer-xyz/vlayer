40b9b1d2a712204f2fef1d008e71e05f7fd97711
Adam Król <adam.dawid.krol@gmail.com>
2024-10-10 16:51:41 +0200

    Use impl Serialize instead of Option<Value>

--- rust/server_utils/src/rpc.rs
+++ rust/server_utils/src/rpc.rs
@@ -1,6 +1,7 @@
 use httpmock::{Mock, MockServer};
 use reqwest::blocking::Client;
-use serde_json::{json, Map, Value};
+use serde::Serialize;
+use serde_json::{json, Value};
 
 pub struct RpcServerMock {
     mock_server: MockServer,
@@ -8,12 +9,12 @@ pub struct RpcServerMock {
 }
 
 impl RpcServerMock {
-    pub fn start(method: &str) -> Self {
+    pub fn start(method: impl AsRef<str>) -> Self {
         let mock_server = MockServer::start();
 
         RpcServerMock {
             mock_server,
-            method: method.into(),
+            method: method.as_ref().to_string(),
         }
     }
 
@@ -21,18 +22,19 @@ impl RpcServerMock {
         self.mock_server.base_url()
     }
 
-    pub fn mock<P>(&self, params: Option<P>, result: impl Into<Value>) -> Mock
-    where
-        P: Into<Value>,
-    {
+    pub fn mock(&self, params: impl Serialize, result: impl Into<Value>) -> Mock {
         let result = result.into();
-        
-        let request_body = self.build_request_body(params);
+
         let mock = self.mock_server.mock(|when, then| {
             when.method("POST")
                 .path("/")
                 .header("Content-Type", "application/json")
-                .json_body_partial(request_body);
+                .json_body(json!({
+                    "jsonrpc": "2.0",
+                    "method": self.method,
+                    "params": params,
+                    "id": 1
+                }));
 
             then.status(200)
                 .header("Content-Type", "application/json")
@@ -47,20 +49,6 @@ impl RpcServerMock {
         });
 
         mock
-    }
-
-    fn build_request_body<P>(&self, params: Option<P>) -> String
-    where
-        P: Into<Value>,
-    {
-        let mut request_map = Map::new();
-        request_map.insert("method".to_string(), Value::String(self.method.clone()));
-
-        if let Some(params) = params {
-            request_map.insert("params".to_string(), params.into());
-        }
-
-        serde_json::to_string(&Value::Object(request_map)).unwrap()
     }
 }
 
@@ -79,57 +67,34 @@ impl RpcClient {
         }
     }
 
-    pub fn call(&self, params: Option<Value>) -> Result<Value, reqwest::Error> {
-        let id = 1; // Fixed ID for simplicity
-        let request_body = if let Some(params) = params {
-            json!({
-                "jsonrpc": "2.0",
-                "method": self.method,
-                "params": params,
-                "id": id
-            })
-        } else {
-            json!({
-                "jsonrpc": "2.0",
-                "method": self.method,
-                "id": id
-            })
-        };
+    pub fn call(&self, params: impl Serialize) -> Result<Value, reqwest::Error> {
+        let request_body = json!({
+            "jsonrpc": "2.0",
+            "method": self.method,
+            "params": params,
+            "id": 1
+        });
 
-        let response = self
+        let mut response = self
             .client
             .post(&self.url)
             .header("Content-Type", "application/json")
             .json(&request_body)
             .send()?
+            .error_for_status()?
             .json::<Value>()?;
 
         if let Some(error) = response.get("error") {
             panic!("JSON-RPC Error: {}", error);
         }
 
-        Ok(response["result"].clone())
+        Ok(response["result"].take())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn mock_without_params() -> anyhow::Result<()> {
-        let rpc_mock = RpcServerMock::start("get_data");
-        let mock = rpc_mock.mock(None::<Value>, json!({"data": "some data"}));
-        let rpc_client = RpcClient::new(&rpc_mock.url(), "get_data");
-
-        let result = rpc_client.call(None)?;
-
-        mock.assert();
-
-        assert_eq!(result, json!({"data": "some data"}));
-
-        Ok(())
-    }
 
     #[test]
     fn mock_with_params() -> anyhow::Result<()> {
@@ -144,5 +109,28 @@ mod tests {
         assert_eq!(result, json!({"data": "some data"}));
 
         Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = "No request has been received by the mock server.")]
+    fn mock_not_called_panics() {
+        let rpc_mock = RpcServerMock::start("get_data");
+        let mock = rpc_mock.mock(Some(json!({"key": "value"})), json!({"data": "some data"}));
+        RpcClient::new(&rpc_mock.url(), "get_data");
+
+        mock.assert();
+    }
+
+    #[test]
+    fn call_without_mock_returns_error() {
+        let rpc_mock = RpcServerMock::start("get_data");
+        let rpc_client = RpcClient::new(&rpc_mock.url(), "unmocked_method");
+
+        let result = rpc_client.call(Some(json!({"key": "value"})));
+
+        assert!(
+            result.unwrap_err().status().is_some(),
+            "Expected an HTTP status code in the error, but it was missing."
+        );
     }
 }
