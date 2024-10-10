9ac2e4dff7279f5b5aae0dbe414035206b79c90b
Leonid Logvinov <logvinov.leon@gmail.com>
2024-10-10 01:24:08 -0700

    Worker skeleton with test (#807)
    * Add Worker skeleton with test
    
    * Simplify

--- rust/services/chain/worker/src/main.rs
+++ rust/services/chain/worker/src/main.rs
@@ -1,30 +1,73 @@
-use std::collections::HashMap;
+use alloy_primitives::{ChainId, B256};
+use chain_db::{ChainDb, ChainInfo, ChainUpdate, Database};
+use lazy_static::lazy_static;
+use thiserror::Error;
 
-use alloy_primitives::ChainId;
-use anyhow::Result;
-use clap::Parser;
-use provider::{BlockTag, BlockingProvider, EthersProviderFactory, ProviderFactory};
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(long)]
-    chain_id: ChainId,
-    #[arg(long)]
-    rpc_url: String,
+fn main() -> anyhow::Result<()> {
+    Ok(())
 }
 
-fn main() -> Result<()> {
-    let Args { chain_id, rpc_url } = Args::parse();
+#[allow(dead_code)]
+struct Worker<DB: for<'a> Database<'a>> {
+    chain_id: ChainId,
+    db: ChainDb<DB>,
+    provider: (),
+}
 
-    let rpc_urls = HashMap::from([(chain_id, rpc_url)]);
-    let provider_factory = EthersProviderFactory::new(rpc_urls);
-    let provider = provider_factory.create(chain_id)?;
-    let latest_block = provider
-        .get_block_header(BlockTag::Latest)?
-        .expect("block not found");
+lazy_static! {
+    static ref EMPTY_PROOF: Vec<u8> = vec![];
+    static ref SOME_CHAIN_INFO: ChainInfo =
+        ChainInfo::new(1..=2, B256::with_last_byte(1), EMPTY_PROOF.as_slice());
+    static ref SOME_CHAIN_UPDATE: ChainUpdate = ChainUpdate::new(SOME_CHAIN_INFO.clone(), [], []);
+}
 
-    println!("{:?}", latest_block.number());
+#[derive(Debug, Error)]
+enum WorkerError {
+    #[error("ChainDB error: {0}")]
+    ChainDb(#[from] chain_db::ChainDbError),
+}
 
-    Ok(())
+impl<DB> Worker<DB>
+where
+    DB: for<'a> Database<'a>,
+{
+    #[allow(dead_code)]
+    pub fn new(db: ChainDb<DB>, chain_id: ChainId) -> Self {
+        Worker {
+            chain_id,
+            db,
+            provider: (),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn init(&mut self) -> Result<(), WorkerError> {
+        Ok(self
+            .db
+            .update_chain(self.chain_id, SOME_CHAIN_UPDATE.clone())?)
+    }
+
+    #[allow(dead_code)]
+    pub fn db(&self) -> &ChainDb<DB> {
+        &self.db
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use chain_db::InMemoryDatabase;
+
+    use super::*;
+
+    #[test]
+    fn db_has_chain_info_after_init() -> Result<()> {
+        let mut worker = Worker::new(ChainDb::new(InMemoryDatabase::new()), 1);
+
+        worker.init()?;
+
+        assert_eq!(worker.db().get_chain_info(1)?, Some(SOME_CHAIN_INFO.clone()));
+
+        Ok(())
+    }
 }
