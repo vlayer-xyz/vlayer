abff529d215752cdba1a6639d3ae16254dfe9438
pociej <grzegorz.pociejewski@gmail.com>
2024-10-10 18:26:29 +0200

    Use __dirname properly

--- packages/browser-extension/tests/fixtures.ts
+++ packages/browser-extension/tests/fixtures.ts
@@ -5,7 +5,9 @@ import {
   type BrowserContext,
 } from "@playwright/test";
 import path from "path";
-
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
 // NOTE: this env make sidepanel accessible vis context.pages()
 // https://github.com/microsoft/playwright/blob/8f3353865d8d98e9b40c15497e60d5e2583410b6/packages/playwright-core/src/server/chromium/crBrowser.ts#L169C11-L169C27
 process.env.PW_CHROMIUM_ATTACH_TO_OTHER = "1";
@@ -16,8 +18,7 @@ export const test = base.extend<{
 }>({
   // eslint-disable-next-line
   context: async ({}, use) => {
-    console.log("Import meta", import.meta);
-    const pathToExtension = path.join(import.meta.dirname, "../dist");
+    const pathToExtension = path.join(__dirname, "../dist");
 
     const context = await chromium.launchPersistentContext(
       "",
