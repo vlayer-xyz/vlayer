ba70d634b3d32dc021528b88a69c1dea31c18878
pociej <grzegorz.pociejewski@gmail.com>
2024-10-15 14:09:52 +0200

    Add server side tlsn rust provider

---
+++ examples/web_proof/vlayer/serverSideProvider/prove.ts
@@ -0,0 +1,51 @@
+import { testHelpers, createVlayerClient } from "@vlayer/sdk";
+
+//NOTE:
+//@ts-expect-error This should be replaced by actual  provider contract
+import webProofProver from "../../out/WebProofProver.sol/WebProofProver";
+//NOTE:
+//@ts-expect-error This should be replaced by actual  verifier contract
+import webProofVerifier from "../../out/WebProofVerifier.sol/WebProofVerifier";
+import { serverSideTlsnProofProvider } from "./serverSideTlsProofProvider";
+
+//NOTE  this uses tlsn fixture public key as serverSideTlsProofProvider uses default lokal tlsn provider for now
+//TODO: make serverSideTlsProofProvider able to accept notary server data as input
+
+const notaryPubKey =
+  "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEBv36FI4ZFszJa0DQFJ3wWCXvVLFr\ncRzMG5kaTeHGoSzDu6cFqx3uEWYpFGo6C0EOUgf+mEgbktLrXocv5yHzKg==\n-----END PUBLIC KEY-----\n";
+
+const [prover /*,_verifier*/] = await testHelpers.deployProverVerifier(
+  webProofProver,
+  webProofVerifier,
+);
+
+const vlayer = createVlayerClient();
+
+async function testSuccessProving() {
+  console.log("Started proving...");
+  //TODO : this should accept headers so private data can be accessed
+  const tls_proof =
+    await serverSideTlsnProofProvider.getWebProof("Your URL here");
+
+  const webProof = { tls_proof: tls_proof, notary_pub_key: notaryPubKey };
+
+  const { hash } = await vlayer.prove({
+    address: prover,
+    functionName: "main",
+    proverAbi: webProofProver.abi,
+    args: [
+      {
+        webProofJson: JSON.stringify(webProof),
+      },
+    ],
+  });
+
+  const { proof, result } = await vlayer.waitForProvingResult({ hash });
+
+  console.log("Proof generated!", proof, result);
+  return { proof, result };
+}
+
+testSuccessProving().catch((e) => {
+  console.error("Error in provind", e);
+});
