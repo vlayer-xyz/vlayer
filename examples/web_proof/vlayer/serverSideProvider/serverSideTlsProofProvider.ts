625931b6a97cdf824d1d583c37a0acc0ea03b7c4
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-10-15 15:32:12 +0200

    Add server side tlsn rust provider (#859)

---
+++ examples/web_proof/vlayer/serverSideProvider/serverSideTlsProofProvider.ts
@@ -0,0 +1,28 @@
+// import { createVlayerClient } from "@vlayer/sdk";
+
+import { spawn } from "node:child_process";
+import fs from "node:fs";
+// Path to your Rust executable
+const rustExecutable = "./rust/target/release/tlsn-provider"; // Adjust the path and name
+
+async function runRustAndStreamOutput(url: string) {
+  return new Promise<string>((resolve) => {
+    const rustProcess = spawn(rustExecutable, [url]); // Pass the URL as an argument
+    rustProcess.stdout.pipe(process.stdout);
+    rustProcess.stderr.pipe(process.stderr);
+
+    rustProcess.on("close", () => {
+      const json = fs.readFileSync("./proof.json", "utf8");
+      fs.unlinkSync("./proof.json");
+      resolve(JSON.parse(json));
+    });
+  });
+}
+
+// Run the function
+// runRustAndStreamOutput();
+export const serverSideTlsnProofProvider = {
+  getWebProof(url: string) {
+    return runRustAndStreamOutput(url);
+  },
+};
