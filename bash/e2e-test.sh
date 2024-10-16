b5fa888924e7c26579b914136ba8a48003fc8c41
Artur Chmaro <artur@vlayer.xyz>
2024-10-16 11:04:27 +0200

    skip tests locally if no env present

--- bash/e2e-test.sh
+++ bash/e2e-test.sh
@@ -22,19 +22,30 @@ if [[  -n "${RUNNER_OS:-}" ]] && [[ -z "${ALCHEMY_API_KEY:-}" ]] && [[ -z "${EXA
   exit 1
 fi
 
+EXAMPLES_REQUIRING_ALCHEMY=("simple_time_travel" "simple_teleport")
+EXAMPLES_REQUIRING_PRIV_KEY=("simple_time_travel")
+
 for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ; do
-  (
-    example_name=$(basename "${example}")
+  example_name=$(basename "${example}")
 
-    echo "Running tests of: ${example}"
-    cd "${example}"
-    forge soldeer install
-    forge clean
-    forge build
+  if [[ "${EXAMPLES_REQUIRING_ALCHEMY[@]}" =~ "${example_name}" ]] && [[ -z "${ALCHEMY_API_KEY:-}" ]]; then
+    echo "Skipping: ${example} (configure ALCHEMY_API_KEY to run it)"
+    continue
+  fi
 
-    cd vlayer
-    bun install
-    bun run prove.ts 
-  )
+  if [[ "${EXAMPLES_REQUIRING_PRIV_KEY[@]}" =~ "${example_name}" ]] && [[ -z "${EXAMPLES_TEST_PRIVATE_KEY:-}" ]]; then
+    echo "Skipping: ${example} (configure EXAMPLES_TEST_PRIVATE_KEY to run it)"
+    continue
+  fi
+
+  echo "Running tests of: ${example}"
+  cd "${example}"
+  forge soldeer install
+  forge clean
+  forge build
+
+  cd vlayer
+  bun install
+  bun run prove.ts 
 done
 
