e78a4117d69269e359a7b2bb174b0be4cb1b9c7c
pociej <grzegorz.pociejewski@gmail.com>
2024-10-21 15:01:46 +0200

    WIP

--- packages/browser-extension/src/hooks/useTlsnProver.tsx
+++ packages/browser-extension/src/hooks/useTlsnProver.tsx
@@ -55,7 +55,7 @@ export const TlsnProofContextProvider = ({ children }: PropsWithChildren) => {
 
       const tlsnProof = await tlsnProve(removeQueryParams(provenUrl?.url), {
         notaryUrl: provingSessionConfig.notaryUrl,
-        websocketProxyUrl: `${provingSessionConfig.wsProxyUrl}?token=${new URL(provenUrl.url).host}`,
+        websocketProxyUrl: `${provingSessionConfig.wsProxyUrl}`,
         method: "GET",
         headers: formattedHeaders?.headers,
         secretHeaders: formattedHeaders?.secretHeaders,
--- packages/test-web-app/src/Source.tsx
+++ packages/test-web-app/src/Source.tsx
@@ -4,7 +4,26 @@ import {
   notarize,
   startPage,
 } from "@vlayer/sdk";
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
+
+const requestWebProof = async () => {
+  const provider = createExtensionWebProofProvider({
+    notaryUrl: "http://localhost:7047",
+    wsProxyUrl: "ws://localhost:55688",
+  });
+  const loginUrl = `${window.location.origin}${import.meta.env.BASE_URL}login`;
+  const targetUrl = `${window.location.origin}${import.meta.env.BASE_URL}target`;
+
+  await provider.getWebProof({
+    //@ts-expect-error this part is not implemented yet in our tlsn flow
+    proverCallCommitment: {},
+    steps: [
+      startPage(loginUrl, "Go to login"),
+      expectUrl(targetUrl, "Logged in and appear at target page"),
+      notarize("https://swapi.dev/api/people/1", "GET", "Prove"),
+    ],
+  });
+};
 
 function Source() {
   const [hasProof, setHasProof] = useState(true);
