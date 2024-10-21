55f04ac009b619d239b7725286e5482f9e8507e9
pociej <grzegorz.pociejewski@gmail.com>
2024-10-21 15:55:42 +0200

    Generate proof in e2e test

--- packages/browser-extension/playwright.config.ts
+++ packages/browser-extension/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5000,
+  timeout: 70000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
