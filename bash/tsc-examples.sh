39f77467b4092189492a4d9e2cf14eff7a9d5f31
rzadp <przemek@vlayer.xyz>
2024-10-21 09:55:28 +0200

    Fix bun arg

--- bash/tsc-examples.sh
+++ bash/tsc-examples.sh
@@ -6,7 +6,7 @@ VLAYER_HOME=$(git rev-parse --show-toplevel)
 
 cd "${VLAYER_HOME}/packages"
 echo "Running bun install for: $VLAYER_HOME/packages"
-bun install --frozen-lock-file
+bun install --frozen-lockfile
 
 EXAMPLE="${VLAYER_HOME}/examples/web_proof"
 cd ${EXAMPLE}
@@ -20,5 +20,5 @@ ${VLAYER_HOME}/bash/build_ts_types.sh >/dev/null
 cd ${EXAMPLE}/vlayer
 
 echo "Running tsc for: ${EXAMPLE}"
-bun install --frozen-lock-file
+bun install --frozen-lockfile
 bun tsc --noEmit
