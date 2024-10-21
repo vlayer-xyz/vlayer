ac8ac4bc3e1015e04d88e8933cb05636097d1264
Anna Jabłonowska <ania@vlayer.xyz>
2024-10-21 11:03:44 +0200

    Remove no-undef lint rule in packages

--- packages/eslint.config.js
+++ packages/eslint.config.js
@@ -9,8 +9,6 @@ export default [
   prettierRecommended,
   {
     rules: {
-      "no-undef": "warn",
-
        // Rules enabled as a baseline after enabling recommendedTypeChecked.
        // These should be eventually upgraded to "error" and all related issues should be resolved.
       "@typescript-eslint/no-unsafe-argument": "warn",
