22f67efa106afdca1661bd2cf56a3dd4d53ec1c9
Leonid Logvinov <logvinov.leon@gmail.com>
2024-10-22 10:34:45 +0200

    Imports in tracing

--- rust/services/chain/worker/src/trace.rs
+++ rust/services/chain/worker/src/trace.rs
@@ -1,13 +1,11 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    fmt::layer, layer::SubscriberExt, registry, util::SubscriberInitExt, EnvFilter,
+};
 
 const TRACING_CONFIG: &str = "info";
 
 pub(crate) fn init_tracing() {
-    let env_filter =
-        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or(TRACING_CONFIG.into());
-    let formatting_layer = tracing_subscriber::fmt::layer().with_ansi(true);
-    tracing_subscriber::registry()
-        .with(env_filter)
-        .with(formatting_layer)
-        .init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or(TRACING_CONFIG.into());
+    let formatting_layer = layer().with_ansi(true);
+    registry().with(env_filter).with(formatting_layer).init();
 }
