bcac23b573a15b866936eeefb2822c4144e0d179
Piotr Żelazko <piotr@vlayer.xyz>
2024-10-22 15:15:59 +0200

    refactor: make proof decoding more readable

--- contracts/src/Verifier.sol
+++ contracts/src/Verifier.sol
@@ -10,8 +10,9 @@ import {CallAssumptionsLib} from "./CallAssumptions.sol";
 abstract contract Verifier {
     uint256 private constant SELECTOR_LEN = 4;
     uint256 private constant PROOF_OFFSET = SELECTOR_LEN;
-    uint256 private constant JOURNAL_OFFSET = PROOF_OFFSET + ProofLib.CALL_ASSUMPTIONS_OFFSET;
-    uint256 private constant CALL_ASSUMPTIONS_END = JOURNAL_OFFSET + CallAssumptionsLib.CALL_ASSUMPTIONS_ENCODING_LENGTH;
+    uint256 private constant CALL_ASSUMPTIONS_BEGIN = PROOF_OFFSET + ProofLib.CALL_ASSUMPTIONS_OFFSET;
+    uint256 private constant CALL_ASSUMPTIONS_END =
+        CALL_ASSUMPTIONS_BEGIN + CallAssumptionsLib.CALL_ASSUMPTIONS_ENCODING_LENGTH;
 
     IProofVerifier public verifier;
 
@@ -32,12 +33,15 @@ abstract contract Verifier {
     function _decodeCalldata() private pure returns (Proof memory, bytes32) {
         Proof memory proof = abi.decode(msg.data[PROOF_OFFSET:], (Proof));
 
-        uint256 journalEnd = JOURNAL_OFFSET + proof.length - ProofLib.PROOF_ENCODING_LENGTH;
+        uint256 paramsBegin = CALL_ASSUMPTIONS_END;
+        uint256 paramsLen =
+            proof.length - ProofLib.PROOF_ENCODING_LENGTH - CallAssumptionsLib.CALL_ASSUMPTIONS_ENCODING_LENGTH;
+        uint256 paramsEnd = paramsBegin + paramsLen;
 
-        bytes memory callAssumptions = msg.data[JOURNAL_OFFSET:CALL_ASSUMPTIONS_END];
-        bytes memory param = msg.data[CALL_ASSUMPTIONS_END:journalEnd];
+        bytes memory callAssumptions = msg.data[CALL_ASSUMPTIONS_BEGIN:CALL_ASSUMPTIONS_END];
+        bytes memory params = msg.data[paramsBegin:paramsEnd];
 
-        bytes memory journalWithEmptyProof = bytes.concat(callAssumptions, abi.encode(ProofLib.emptyProof()), param);
+        bytes memory journalWithEmptyProof = bytes.concat(callAssumptions, abi.encode(ProofLib.emptyProof()), params);
         bytes32 journalHash = sha256(journalWithEmptyProof);
 
         return (proof, journalHash);
