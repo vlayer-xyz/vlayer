a194c9fb3c18e165cb4db1dbea66f37173b767f7
rzadp <przemek@vlayer.xyz>
2024-10-24 15:51:52 +0200

    group stuff

--- bash/test_release.sh
+++ bash/test_release.sh
@@ -2,24 +2,33 @@
 
 set -ueo pipefail
 
+echo '::group::foundry installation'
 curl -L https://foundry.paradigm.xyz | bash
 export PATH="$PATH:/home/runner/.config/.foundry/bin"
 foundryup
+echo '::endgroup::'
 
+echo '::group::vlayer installation'
 curl -SL https://install.vlayer.xyz | bash
 export PATH="$PATH:/home/runner/.config/.vlayer/bin"
 vlayerup
+echo '::endgroup::'
 
+echo '::group::risczero installation'
 curl -L https://risczero.com/install | bash
 export PATH="$PATH:/home/runner/.risc0/bin"
 rzup install cargo-risczero v1.0.5
+echo '::endgroup::'
 
+echo '::group::bun installation'
 curl -fsSL https://bun.sh/install | bash
 export PATH="$PATH:~/.bun/bin"
+echo '::endgroup::'
 
 git config --global user.email "test@example.com"
 git config --global user.name "Github Runner"
 
+echo '::group::vlayer template initialization'
 mkdir /home/runner/web_proof_test
 cd /home/runner/web_proof_test
 
@@ -28,3 +37,4 @@ forge build
 
 cd vlayer
 bun install
+echo '::endgroup::'
