67a1f615084bed48ebb8be382892827f313421e6
Adam Król <adam.dawid.krol@gmail.com>
2024-10-28 15:46:35 +0100

    Proper error message for TravelCallExecutorError in HostError

--- rust/services/call/host/src/host/error.rs
+++ rust/services/call/host/src/host/error.rs
@@ -13,7 +13,7 @@ pub enum HostError {
     #[error("Invalid input")]
     CreatingInput(String),
 
-    #[error("Engine error: {0}")]
+    #[error("TravelCallExecutor error: {0}")]
     Engine(#[from] TravelCallExecutorError),
 
     #[error("Provider factory error: {0}")]
