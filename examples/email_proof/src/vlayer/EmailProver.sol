415bcb3196bccc27b5dc22cca981eab97a10bc6e
vanruch <ivanruch@gmail.com>
2024-10-28 11:33:52 +0100

    rm useless import

--- examples/email_proof/src/vlayer/EmailProver.sol
+++ examples/email_proof/src/vlayer/EmailProver.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
 
 import {Proof} from "vlayer-0.1.0/Proof.sol";
 import {Prover} from "vlayer-0.1.0/Prover.sol";
 import {RegexLib} from "vlayer-0.1.0/Regex.sol";
 import {VerifiedEmail, UnverifiedEmail, EmailProofLib} from "vlayer-0.1.0/EmailProof.sol";
-import "vlayer-0.1.0/testing/VTest.sol";
 
 interface IExample {
     function exampleFunction() external returns (uint256);
