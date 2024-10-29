40a5c7ab46036b2ce8a069aa780433eef1716d5a
vanruch <ivanruch@gmail.com>
2024-10-29 14:10:55 +0100

    Wrap test execution error into CLIError

--- rust/cli/src/commands/test.rs
+++ rust/cli/src/commands/test.rs
@@ -5,10 +5,10 @@ use crate::errors::CLIError;
 
 pub async fn run_test(cmd: Box<TestArgs>) -> Result<(), CLIError> {
     info!("Running vlayer tests");
-    let test_result = cmd.run().await.unwrap();
+    let test_result = cmd.run().await?;
     let failed_tests_count = test_result.failed();
     if !test_result.allow_failure && failed_tests_count > 0 {
-        return Err(CLIError::TestFailed(failed_tests_count));
+        return Err(CLIError::TestsFailed(failed_tests_count));
     }
     Ok(())
 }
--- rust/test_runner/src/lib.rs
+++ rust/test_runner/src/lib.rs
@@ -4,4 +4,5 @@ pub(crate) mod composite_inspector;
 pub(crate) mod forked;
 mod proof;
 mod providers;
+pub use color_eyre::Report;
 pub use forked::cli;
