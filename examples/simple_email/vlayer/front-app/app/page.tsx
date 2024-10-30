342357a04e8134b639d921dd8fa83b7138aed526
Artur Chmaro <artur@vlayer.xyz>
2024-10-30 08:56:35 +0100

    use vite instead of next

--- examples/simple_email/vlayer/front-app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <button className="btn btn-secondary">Hello Button</button>
-    </div>
-  );
-}
---
+++ examples/simple_email/vlayer/front-app/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <button className="btn btn-primary">Hello World</button>;
+}
+
+export default App;
