596b0bc6f0718cc50625489d779c9cdc533132cf
Artur Chmaro <artur@vlayer.xyz>
2024-10-31 13:35:36 +0100

    move js to the top

---
+++ book/src/static/appendixMenu.js
@@ -0,0 +1,24 @@
+// Hide appendx/architecture stuff from menu when not authenticated
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  if(!getCookie('authenticated')) {
+    const spacerElement = document.querySelector('.spacer');
+    if (spacerElement) {
+      let nextElement = spacerElement.nextElementSibling;
+      for (let i = 0; i < 2 && nextElement; i++) {
+        nextElement.style.display = 'none';
+        nextElement = nextElement.nextElementSibling;
+      }
+    }
+    spacerElement.style.display = 'none';
+  }
+});
+
+
+
