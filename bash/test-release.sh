63b80cd2a6b555f9232e819225329b6610ee245a
Arkadiusz Konior <akonior@gmail.com>
2024-11-05 15:52:52 +0100

    Run `bun run prove.ts` on post release testing (#1040)
    * Run bun run prove.ts on post release testing
    
    * Delete debug workflow

--- bash/test-release.sh
+++ bash/test-release.sh
@@ -14,12 +14,6 @@ export PATH="$PATH:$HOME/.config/.vlayer/bin"
 vlayerup
 echo '::endgroup::'
 
-echo '::group::risczero installation'
-curl -L https://risczero.com/install | bash
-export PATH="$PATH:$HOME/.risc0/bin"
-rzup install cargo-risczero v1.0.5
-echo '::endgroup::'
-
 echo '::group::bun installation'
 curl -fsSL https://bun.sh/install | bash
 export PATH="$PATH:~/.bun/bin"
@@ -29,12 +23,16 @@ git config --global user.email "test@example.com"
 git config --global user.name "Github Runner"
 
 echo '::group::vlayer template initialization'
-mkdir ${HOME}/web_proof_test
-cd ${HOME}/web_proof_test
+VLAYER_TEMP_DIR=$(mktemp -d -t vlayer-test-release-XXXXXX-)
+cd ${VLAYER_TEMP_DIR}
 
 vlayer init --template web-proof
 forge build
 
 cd vlayer
 bun install
+echo '::endgroup::'
+
+echo '::group::vlayer run prove.ts'
+bun run prove.ts
 echo '::endgroup::'
