7d5590e774de3686a9c0d13d143592d6ae5f057d
Adam Król <adam.dawid.krol@gmail.com>
2024-11-07 18:04:08 +0100

    Create Add EvmBlockFetcher

---
+++ rust/services/chain/host/src/host/evm_block_fetcher.rs
@@ -0,0 +1,60 @@
+use std::ops::RangeInclusive;
+
+use ethers::{
+    middleware::Middleware,
+    providers::{Http, JsonRpcClient, Provider},
+    types::BlockNumber as BlockTag,
+};
+use futures::future::join_all;
+use provider::{to_eth_block_header, EvmBlockHeader};
+use tracing::{info, instrument};
+
+use super::HostError;
+
+pub struct EvmBlockFetcher<P>
+where
+    P: JsonRpcClient,
+{
+    pub(crate) provider: Provider<P>,
+}
+
+impl EvmBlockFetcher<Http> {
+    pub fn new(rpc_url: String) -> Self {
+        let provider =
+            Provider::<Http>::try_from(rpc_url).expect("could not instantiate HTTP Provider");
+        EvmBlockFetcher { provider }
+    }
+}
+
+impl<P> EvmBlockFetcher<P>
+where
+    P: JsonRpcClient,
+{
+    pub fn from_provider(provider: Provider<P>) -> Self {
+        EvmBlockFetcher { provider }
+    }
+
+    #[instrument(skip(self))]
+    pub async fn get_blocks_range(
+        &self,
+        range: &RangeInclusive<u64>,
+    ) -> Result<Vec<Box<dyn EvmBlockHeader>>, HostError> {
+        let blocks = join_all(range.clone().map(|n| self.get_block(n.into()))).await;
+        blocks.into_iter().collect()
+    }
+
+    #[instrument(skip(self))]
+    pub async fn get_block(&self, number: BlockTag) -> Result<Box<dyn EvmBlockHeader>, HostError> {
+        info!("Fetching block {}", number);
+        let ethers_block = self
+            .provider
+            .get_block(number)
+            .await
+            .map_err(|err| HostError::Provider(err.to_string()))?
+            .ok_or(HostError::BlockNotFound(number))?;
+        let block = to_eth_block_header(ethers_block)
+            .map_err(|e| HostError::BlockConversion(e.to_string()))?;
+        info!("Fetched block {}", block.number());
+        Ok(Box::new(block))
+    }
+}
