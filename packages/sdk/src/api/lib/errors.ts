89817a6a4616384f909d682c06de59e194938804
vanruch <ivanruch@gmail.com>
2024-11-08 10:50:48 +0100

    move error parser

--- packages/sdk/src/api/lib/errors.ts
+++ packages/sdk/src/api/lib/errors.ts
@@ -4,3 +4,16 @@ export class VersionError extends Error {
     this.name = "VersionError";
   }
 }
+
+export function parseVCallResponseError({
+  message,
+}: {
+  message: string | undefined;
+}): Error {
+  if (message?.startsWith("Unsupported CallGuestID")) {
+    return new VersionError(`${message}
+    vlayer uses the daily release cycle, and SDK version must match the proving server version.
+    Please run "vlayer update" to update the SDK to the latest version.`);
+  }
+  return new Error(`Error response: ${message ?? "unknown error"}`);
+}
--- packages/sdk/src/api/v_call.ts
+++ packages/sdk/src/api/v_call.ts
@@ -1,5 +1,5 @@
 import { CallContext, CallParams, VCallResponse } from "types/vlayer";
-import { VersionError } from "./lib/errors";
+import { parseVCallResponseError } from "./lib/errors";
 
 function v_callBody(call: CallParams, context: CallContext) {
   return {
@@ -28,19 +28,12 @@ export async function v_call(
   assertObject(response_json);
 
   if ("error" in response_json) {
-    throw parseError(response_json.error as { message: string | undefined });
+    throw parseVCallResponseError(
+      response_json.error as { message: string | undefined },
+    );
   }
 
   return response_json as Promise<VCallResponse>;
-}
-
-function parseError({ message }: { message: string | undefined }): Error {
-  if (message?.startsWith("Unsupported CallGuestID")) {
-    return new VersionError(`${message}
-    vlayer uses the daily release cycle, and SDK version must match the proving server version.
-    Please run "vlayer update" to update the SDK to the latest version.`);
-  }
-  return new Error(`Error response: ${message ?? "unknown error"}`);
 }
 
 function assertObject(x: unknown): asserts x is object {
