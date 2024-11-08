83ebb114271dcc32bf6ad15e1fbf10f42916f0eb
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-08 12:23:05 +0100

    Serialize Bytes as Hex in RpcChainProof

--- rust/services/chain/common/src/lib.rs
+++ rust/services/chain/common/src/lib.rs
@@ -2,6 +2,7 @@ use alloy_primitives::bytes::Bytes;
 use block_trie::BlockTrie;
 use mpt::{MerkleTrie, ParseNodeError};
 use serde::{Deserialize, Serialize};
+use serde_with::{hex::Hex, serde_as};
 
 #[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ChainProof {
@@ -9,9 +10,12 @@ pub struct ChainProof {
     pub block_trie: BlockTrie,
 }
 
+#[serde_as]
 #[derive(Serialize, Default, Deserialize, Debug, PartialEq)]
 pub struct RpcChainProof {
+    #[serde_as(as = "Hex")]
     pub proof: Bytes,
+    #[serde_as(as = "Vec<Hex>")]
     pub nodes: Vec<Bytes>,
 }
 
