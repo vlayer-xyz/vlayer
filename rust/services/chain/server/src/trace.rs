b5ad4ba93a5ac84d0c714e5066d27bd3362b20ca
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-12 10:11:59 +0100

    Rename TRACING_CONFIG to DEFAULT_RUST_LOG

--- rust/services/chain/server/src/trace.rs
+++ rust/services/chain/server/src/trace.rs
@@ -2,10 +2,10 @@ use tracing_subscriber::{
     fmt::layer, layer::SubscriberExt, registry, util::SubscriberInitExt, EnvFilter,
 };
 
-const TRACING_CONFIG: &str = "info";
+const DEFAULT_RUST_LOG: &str = "info";
 
 pub(crate) fn init_tracing() {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or(TRACING_CONFIG.into());
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or(DEFAULT_RUST_LOG.into());
     let formatting_layer = layer().with_ansi(true);
     registry().with(env_filter).with(formatting_layer).init();
 }
