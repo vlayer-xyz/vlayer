4e48ec06ea2098bb25b4e9f4e9e7386f785e0864
Wiktor Gromniak <wiktor@vlayer.xyz>
2024-11-12 08:57:02 +0100

    Use config defaults when envs not specified in web_proof example.

--- examples/web_proof/vlayer/config.ts
+++ examples/web_proof/vlayer/config.ts
@@ -14,7 +14,9 @@ const DEFAULT_CONFIG: Config = {
   proverUrl: "http://127.0.0.1:3000",
   jsonRpcUrl: "",
   envPath: "",
-  privateKey: "0x",
+  // anvil default private key
+  privateKey:
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
 };
 
 export const getConfig = (envPath?: string): Config => {
@@ -23,12 +25,16 @@ export const getConfig = (envPath?: string): Config => {
   dotenv.config({ path: envPath, override: true });
   dotenv.config({ path: `${envPath}.local`, override: true });
 
-  const chainName = process.env.CHAIN_NAME;
+  const chainName = process.env.CHAIN_NAME || DEFAULT_CONFIG.chainName;
+
+  const privateKey =
+    (process.env.EXAMPLES_TEST_PRIVATE_KEY as `0x${string}`) ||
+    DEFAULT_CONFIG.privateKey;
 
   return {
     ...Object.assign(DEFAULT_CONFIG, {
       chainName,
-      privateKey: process.env.EXAMPLES_TEST_PRIVATE_KEY as `0x${string}`,
+      privateKey,
       jsonRpcUrl: process.env.JSON_RPC_URL,
       envPath,
     }),
