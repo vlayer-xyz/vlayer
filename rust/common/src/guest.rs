2a01d4abe17963a2dbe9f28956994e27d4a0c420
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-13 14:28:34 +0100

    Derive stuff

--- rust/common/src/guest.rs
+++ rust/common/src/guest.rs
@@ -1,6 +1,8 @@
 use bytes::Bytes;
+use derive_new::new;
 use risc0_zkp::core::digest::Digest;
 
+#[derive(Debug, Clone, new, PartialEq, PartialOrd, Eq, Ord, Default)]
 pub struct Guest {
     pub id: Digest,
     pub elf: Bytes,
