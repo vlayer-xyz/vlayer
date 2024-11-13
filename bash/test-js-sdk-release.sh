220347f8c0a12d7f110b80c6143200641f3cb931
Anna Jabłonowska <62891747+aajj999@users.noreply.github.com>
2024-11-13 09:56:23 +0100

    Test js sdk post release (#1089)
    * Add js sdk testing script
    
    * Add js sdk testing to worflow

---
+++ bash/test-js-sdk-release.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+echo '::group::creating js project'
+mkdir ${HOME}/js_project_test
+cd ${HOME}/js_project_test
+echo "{
+  \"type\": \"module\"
+}" >> package.json
+echo '::endgroup::'
+
+echo '::group::vlayer sdk installation'
+npm install @vlayer/sdk
+echo '::endgroup::'
+
+echo '::group::creating simple code using sdk'
+
+echo "import { createVlayerClient } from '@vlayer/sdk';
+import { strict as assert } from 'assert';
+
+try {
+    const vlayerClient = createVlayerClient();
+    const hash = await vlayerClient.prove({
+        proverAbi: [{
+            'type': 'function',
+            'name': 'main',
+        }],
+    });
+    await vlayerClient.waitForProvingResult(hash);
+    throw Error('Exception expected');
+} catch (error) {
+    assert.equal(error.message, 'Error response: missing field \`to\`');
+    console.log('Vlayer server called');
+}
+" >> index.js
+
+echo '::endgroup::'
+
+echo '::group::running code'
+node index
+echo '::endgroup::'
