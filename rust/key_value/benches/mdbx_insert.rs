498e5495470865cce6f29c4595cc83b4e41cf0ce
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2024-11-13 16:59:23 +0100

    Review comments

--- rust/key_value/benches/mdbx_insert.rs
+++ rust/key_value/benches/mdbx_insert.rs
@@ -7,20 +7,22 @@ const KEY_SIZE: usize = 32;
 const VALUE_SIZE: usize = 1024;
 const TEST_TABLE: &str = "test";
 
-fn generate_random_key(rng: &mut StdRng) -> [u8; KEY_SIZE] {
-    let mut key = [0u8; KEY_SIZE];
+fn generate_random_entry(rng: &mut StdRng) -> ([u8; KEY_SIZE], Vec<u8>) {
+    let mut key = [0; KEY_SIZE];
+    let mut value = vec![0; VALUE_SIZE];
     rng.fill_bytes(&mut key);
-    key
-}
-
-fn generate_random_value(rng: &mut StdRng) -> Vec<u8> {
-    let mut value = vec![0u8; VALUE_SIZE];
     rng.fill_bytes(&mut value);
-    value
+    (key, value)
 }
 
 fn benchmark_insert(c: &mut Criterion) {
     c.bench_function("mdbx_insert_10000", |b| {
+        let seed = 42;
+        let mut rng = StdRng::seed_from_u64(seed);
+        let entries: Vec<_> = (0..10_000)
+            .map(|_| generate_random_entry(&mut rng))
+            .collect();
+
         b.iter(|| {
             let temp_dir = tempdir().unwrap();
             let db_path = temp_dir.path().to_path_buf();
@@ -30,14 +32,10 @@ fn benchmark_insert(c: &mut Criterion) {
             tx.create_table(TEST_TABLE).unwrap();
             tx.commit().unwrap();
 
-            let seed: u64 = 42;
-            let mut rng = StdRng::seed_from_u64(seed);
-
-            for _ in 0..10_000 {
-                let key = generate_random_key(&mut rng);
-                let value = generate_random_value(&mut rng);
+            for (key, value) in &entries {
                 let mut tx = db.begin_rw().unwrap();
-                black_box(tx.insert(TEST_TABLE, &key, &value).unwrap());
+                #[allow(clippy::unit_arg)]
+                black_box(tx.insert(TEST_TABLE, key, value).unwrap());
                 tx.commit().unwrap();
             }
         })
