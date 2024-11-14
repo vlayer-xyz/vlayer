4cf9625a1c88aadb23ac736c6104a8566b6b2a85
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-14 14:32:39 +0100

    Fix build

--- rust/services/call/guest_wrapper/risc0_guest/src/main.rs
+++ rust/services/call/guest_wrapper/risc0_guest/src/main.rs
@@ -3,14 +3,14 @@
 risc0_zkvm::guest::entry!(main);
 
 use alloy_sol_types::SolValue;
-use chain_guest_wrapper::chain_guest;
+use chain_guest_wrapper::GUEST;
 use risc0_zkvm::guest::env;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let input = env::read();
 
-    let output = call_guest::main(input, chain_guest().id).await;
+    let output = call_guest::main(input, GUEST.id).await;
 
     env::commit_slice(&output.call_assumptions.abi_encode());
     env::commit_slice(&output.evm_call_result);
