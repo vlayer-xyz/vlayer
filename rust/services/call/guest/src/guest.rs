daa94f653d77f20dc91782975db929a16139cdbc
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-14 01:10:17 +0100

    Bind chain guest id in call_wrapper and not call_guest

--- rust/services/call/guest/src/guest.rs
+++ rust/services/call/guest/src/guest.rs
@@ -3,7 +3,7 @@ use call_engine::{
     GuestOutput, Input,
 };
 use chain_client::CachedClient;
-use chain_guest_wrapper::chain_guest;
+use risc0_zkvm::sha::Digest;
 use env::{verify_input, VerifiedEnv};
 
 mod env;
@@ -17,10 +17,11 @@ pub async fn main(
         chain_proofs,
         call,
     }: Input,
+    chain_guest_id: Digest,
 ) -> GuestOutput {
     let input_verifier = chain_proofs.map(|chain_proofs| {
         let chain_client = CachedClient::new(chain_proofs);
-        let chain_proof_verifier = ZkChainProofVerifier::new(chain_guest().id, GuestVerifier);
+        let chain_proof_verifier = ZkChainProofVerifier::new(chain_guest_id, GuestVerifier);
         guest_input::ZkVerifier::new(chain_client, chain_proof_verifier)
     });
     let verified_input = verify_input(input_verifier, multi_evm_input).await;
--- rust/services/call/guest_wrapper/risc0_guest/src/main.rs
+++ rust/services/call/guest_wrapper/risc0_guest/src/main.rs
@@ -4,12 +4,13 @@ risc0_zkvm::guest::entry!(main);
 
 use alloy_sol_types::SolValue;
 use risc0_zkvm::guest::env;
+use chain_guest_wrapper::chain_guest;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let input = env::read();
 
-    let output = call_guest::main(input).await;
+    let output = call_guest::main(input, chain_guest().id).await;
 
     env::commit_slice(&output.call_assumptions.abi_encode());
     env::commit_slice(&output.evm_call_result);
