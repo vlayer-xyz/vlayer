de22b407a6c69e7a4773ddd56cc7b7922d27f475
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-11-14 09:44:22 +0100

    Migrate simple_time_travel (#1110)

--- examples/simple_time_travel/vlayer/prove.ts
+++ examples/simple_time_travel/vlayer/prove.ts
@@ -1,127 +1,57 @@
 import { optimismSepolia } from "viem/chains";
-import {
-  createPublicClient,
-  createWalletClient,
-  http,
-  type Address,
-} from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-
 import { createVlayerClient } from "@vlayer/sdk";
-import averageBalance from "../out/AverageBalance.sol/AverageBalance";
-import averageBalanceVerifier from "../out/AverageBalanceVerifier.sol/AverageBalanceVerifier";
+import proverSpec from "../out/AverageBalance.sol/AverageBalance";
+import verifierSpec from "../out/AverageBalanceVerifier.sol/AverageBalanceVerifier";
+import {
+  createContext,
+  deployVlayerContracts,
+  getConfig,
+  waitForTransactionReceipt,
+} from "@vlayer/sdk/config";
 
-let privateKey = process.env.EXAMPLES_TEST_PRIVATE_KEY;
+const tokenOwner = "0xE6b08c02Dbf3a0a4D3763136285B85A9B492E391"; // Owner of the USDC token at OP Sepolia
+const usdcTokenAddr = "0x5fd84259d66Cd46123540766Be93DFE6D43130D7"; // Test USDC at OP Sepolia
+const startBlock = 17915294n;
+const endBlock = 17985294n;
+const step = 9000n;
 
-if (!privateKey) {
-  throw new Error("EXAMPLES_TEST_PRIVATE_KEY environment variable is not set.");
-}
-privateKey = privateKey.startsWith("0x") ? privateKey : `0x${privateKey}`;
-const deployer = privateKeyToAccount(privateKey as `0x${string}`);
+const config = getConfig();
+const { ethClient, account } = await createContext(config);
 
-const tokenOwner = "0xE6b08c02Dbf3a0a4D3763136285B85A9B492E391";
-
-const walletClient = createWalletClient({
-  chain: optimismSepolia,
-  transport: http("https://sepolia.optimism.io"),
+const { prover, verifier } = await deployVlayerContracts({
+  proverSpec,
+  verifierSpec,
+  proverArgs: [usdcTokenAddr, startBlock, endBlock, step],
+  verifierArgs: [],
 });
 
-const client = createPublicClient({
-  chain: optimismSepolia,
-  transport: http("https://sepolia.optimism.io"),
-});
-
-const deployProver = async () => {
-  const usdcTokenAddr = "0x5fd84259d66Cd46123540766Be93DFE6D43130D7"; // Test USDC at OP Sepolia
-  const startBlock = 17915294n;
-  const endBlock = 17985294n;
-  const step = 9000n;
-
-  const txHash = await walletClient.deployContract({
-    abi: averageBalance.abi,
-    bytecode: averageBalance.bytecode.object,
-    account: deployer,
-    args: [usdcTokenAddr, startBlock, endBlock, step],
-    chain: optimismSepolia,
-  });
-
-  const receipt = await client.waitForTransactionReceipt({
-    hash: txHash,
-    confirmations: 5,
-  });
-
-  if (receipt.status != "success") {
-    throw new Error(
-      `Contract deployment failed with status: ${receipt.status}`,
-    );
-  }
-
-  return receipt.contractAddress as Address;
-};
-
-const deployVerifier = async (prover: Address) => {
-  const txHash = await walletClient.deployContract({
-    abi: averageBalanceVerifier.abi,
-    bytecode: averageBalanceVerifier.bytecode.object,
-    account: deployer,
-    args: [prover],
-    chain: optimismSepolia,
-  });
-
-  const receipt = await client.waitForTransactionReceipt({
-    hash: txHash,
-    confirmations: 5,
-  });
-
-  if (receipt.status != "success") {
-    throw new Error(
-      `Contract deployment failed with status: ${receipt.status}`,
-    );
-  }
-
-  return receipt.contractAddress as Address;
-};
-
-console.log("Deploying Prover...");
-const proverAddr = await deployProver();
-console.log(
-  `Prover deployed: https://sepolia-optimism.etherscan.io/address/${proverAddr}`,
-);
-
-console.log("Deploying Verifier...");
-const verifierAddr = await deployVerifier(proverAddr);
-console.log(
-  `Verifier deployed: https://sepolia-optimism.etherscan.io/address/${verifierAddr}`,
-);
-
-console.log("Proving...");
 const vlayer = createVlayerClient();
 
-const hash = await vlayer.prove({
-  address: proverAddr,
-  proverAbi: averageBalance.abi,
+const provingHash = await vlayer.prove({
+  address: prover,
+  proverAbi: proverSpec.abi,
   functionName: "averageBalanceOf",
   args: [tokenOwner],
   chainId: optimismSepolia.id,
 });
-console.log("Waiting for proving result: ", hash);
 
-const result = await vlayer.waitForProvingResult(hash);
-console.log("Response:", result);
+console.log("Waiting for proving result: ");
 
-const txHash = await walletClient.writeContract({
-  address: verifierAddr,
-  abi: averageBalanceVerifier.abi,
+const result = await vlayer.waitForProvingResult(provingHash);
+
+console.log("Proof:", result[0]);
+console.log("Verifying...");
+
+const verificationHash = await ethClient.writeContract({
+  address: verifier,
+  abi: verifierSpec.abi,
   functionName: "claim",
   args: result,
-  account: deployer,
+  account,
 });
-console.log(
-  `Verification tx: https://sepolia-optimism.etherscan.io/tx/${txHash}`,
-);
-const receipt = await client.waitForTransactionReceipt({
-  hash: txHash,
-  confirmations: 5,
+
+const receipt = await waitForTransactionReceipt({
+  hash: verificationHash,
 });
 
 console.log(`Verification result: ${receipt.status}`);
