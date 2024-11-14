be1057bd4ea353c7f348f76bef0ea93e6699a92e
wgromniak2 <wiktor@vlayer.xyz>
2024-11-14 19:01:34 +0100

    Added testing of all examples to test-release.sh. (#1112)
    * Added testing of all examples to test-release.sh.
    
    * Run test-release workflow for updates of test-release.sh.
    
    * Run test-release OP Sepolia with remote prover.
    
    * Skip examples not supporting dev/testnet in test-release.sh.
    
    * Fixed path to .env files in test-release.sh.
    
    * Add teleport RPC URLs in docker compose config for release test.
    
    * tmp don't stop test-release on single test failure.
    
    * Make test-release.sh fail on failed test.

--- bash/test-release.sh
+++ bash/test-release.sh
@@ -2,6 +2,11 @@
 
 set -ueo pipefail
 
+if [ -z "${VLAYER_ENV:-}" ]; then
+    echo "Error: VLAYER_ENV is not set."
+    exit 1
+fi
+
 echo '::group::foundry installation'
 curl -L https://foundry.paradigm.xyz | bash
 export PATH="$PATH:$HOME/.config/.foundry/bin"
@@ -22,18 +27,29 @@ echo '::endgroup::'
 git config --global user.email "test@example.com"
 git config --global user.name "Github Runner"
 
-echo '::group::vlayer template initialization'
-VLAYER_TEMP_DIR=$(mktemp -d -t vlayer-test-release-XXXXXX-)
-cd ${VLAYER_TEMP_DIR}
+VLAYER_HOME=$(git rev-parse --show-toplevel)
 
-vlayer init --template web-proof
-forge build
-vlayer test
+for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ; do
+    example_name=$(basename "${example}"  | tr '_' '-')
 
-cd vlayer
-bun install
-echo '::endgroup::'
+    if [ ! -f "${example}/vlayer/.env.${VLAYER_ENV}" ]; then
+        echo "Skipping ${example_name} as .env.${VLAYER_ENV} file is not defined"
+        continue
+    fi
 
-echo '::group::vlayer run prove.ts'
-bun run prove.ts
-echo '::endgroup::'
+    echo "::group::Initializing vlayer template: ${example_name}"
+    VLAYER_TEMP_DIR=$(mktemp -d -t vlayer-test-release-XXXXXX-)
+    cd ${VLAYER_TEMP_DIR}
+
+    vlayer init --template "${example_name}"
+    forge build
+    vlayer test
+
+    cd vlayer
+    bun install
+    echo '::endgroup::'
+
+    echo "::group::vlayer run prove.ts: ${example_name}"
+    bun run prove.ts
+    echo '::endgroup::'
+done
