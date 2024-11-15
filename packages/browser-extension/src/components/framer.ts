95ff9aca581f63242efb23d500c7c865b2486591
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2024-11-15 12:49:05 +0100

    Update packages/browser-extension/src/components/framer.ts
    Co-authored-by: Arkadiusz Konior <akonior@gmail.com>

--- packages/browser-extension/src/components/framer.ts
+++ packages/browser-extension/src/components/framer.ts
@@ -1,4 +1,6 @@
-export const collapseAnimation = {
+import { Variants } from "framer-motion";
+
+export const collapseAnimation: Variants = {
   initial: { opacity: 0, height: 0 },
   animate: { opacity: 1, height: "auto" },
   exit: { opacity: 0, height: 0 },
