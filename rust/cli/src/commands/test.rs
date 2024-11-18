08e3756bf8f78939e54808d605e7411fc805d0af
vanruch <ivanruch@gmail.com>
2024-11-18 20:46:05 +0700

    liiiiiiiint

--- rust/cli/src/commands/test.rs
+++ rust/cli/src/commands/test.rs
@@ -5,7 +5,7 @@ use crate::errors::CLIError;
 
 pub async fn run_test(cmd: Box<TestArgs>) -> Result<(), CLIError> {
     info!("Running vlayer tests");
-    let test_result = cmd.run().await?;
+    let test_result = Box::pin(cmd.run()).await?;
     let failed_tests_count = test_result.failed();
     if !test_result.allow_failure && failed_tests_count > 0 {
         return Err(CLIError::TestsFailed(failed_tests_count));
