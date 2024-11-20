8f520af90989cc1f766f6d3fae1cd3bfc7664697
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-20 17:48:30 +0100

    Prepare to set more empty accounts

--- rust/services/call/engine/src/db.rs
+++ rust/services/call/engine/src/db.rs
@@ -1,9 +1,12 @@
-use alloy_primitives::address;
+use alloy_primitives::Address;
 use revm::db::CacheDB;
 
+use crate::config::DEFAULT_CALLER;
+
+static EMPTY_ACCOUNTS: &[Address] = &[DEFAULT_CALLER];
+
 pub fn seed_cache_db_with_trusted_data<D>(db: &mut CacheDB<D>) {
-    db.insert_account_info(
-        address!("1111111111111111111111111111111111111111"),
-        Default::default(),
-    );
+    for address in EMPTY_ACCOUNTS {
+        db.insert_account_info(*address, Default::default());
+    }
 }
