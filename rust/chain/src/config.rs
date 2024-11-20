32076a6fd3b344c4d68470de2987cd984ba66f21
Adam Król <adam.dawid.krol@gmail.com>
2024-11-20 12:14:17 +0100

    Delete unused chains

--- rust/chain/src/config.rs
+++ rust/chain/src/config.rs
@@ -54,22 +54,8 @@ pub static CHAIN_MAP: Lazy<HashMap<ChainId, &'static Lazy<ChainSpec>>> = Lazy::n
         (421614, &ARBITRUM_SEPOLIA_CHAIN_SPEC),
         (324, &ZKSYNC_CHAIN_SPEC),
         (300, &ZKSYNC_SEPOLIA_CHAIN_SPEC),
-        (747, &FLOW_MAINNET_CHAIN_SPEC),
-        (545, &FLOW_TESTNET_CHAIN_SPEC),
-        (534352, &SCROLL_MAINNET_CHAIN_SPEC),
-        (534351, &SCROLL_SEPOLIA_CHAIN_SPEC),
-        (5000, &MANTLE_MAINNET_CHAIN_SPEC),
         (59144, &LINEA_MAINNET_CHAIN_SPEC),
         (59141, &LINEA_SEPOLIA_CHAIN_SPEC),
-        (96, &BITKUB_CHAIN_CHAIN_SPEC),
-        (25925, &BITKUB_CHAIN_TESTNET_CHAIN_SPEC),
-        (7887, &KINTO_CHAIN_SPEC),
-        (42220, &CELO_CHAIN_SPEC),
-        (48900, &ZIRCUIT1_MAINNET_CHAIN_SPEC),
-        (48899, &ZIRCUIT1_TESTNET_CHAIN_SPEC),
-        (100, &GNOSIS_MAINNET_CHAIN_SPEC),
-        (10200, &GNOSIS_CHIADO_CHAIN_SPEC),
-        (8008135, &PHENIX_CHAIN_SPEC),
     ])
 });
 
@@ -88,22 +74,8 @@ pub static CHAIN_NAME_TO_ID: Lazy<HashMap<String, ChainId>> = Lazy::new(|| {
         ("arbitrum-sepolia".into(), 421614),
         ("zksync".into(), 324),
         ("zksync-sepolia".into(), 300),
-        ("flow".into(), 747),
-        ("flow-testnet".into(), 545),
-        ("scroll".into(), 534352),
-        ("scroll-sepolia".into(), 534351),
-        ("mantle".into(), 5000),
         ("linea".into(), 59144),
         ("linea-sepolia".into(), 59141),
-        ("bitkub-chain".into(), 96),
-        ("bitkub-chain-testnet".into(), 25925),
-        ("kinto".into(), 7887),
-        ("celo".into(), 42220),
-        ("zircuit1".into(), 48900),
-        ("zircuit1-testnet".into(), 48899),
-        ("gnosis".into(), 100),
-        ("gnosis-chiado".into(), 10200),
-        ("phenix".into(), 8008135),
     ])
 });
 
@@ -148,43 +120,3 @@ pub static LINEA_SEPOLIA_CHAIN_SPEC: Lazy<ChainSpec> = Lazy::new(|| CHAIN_SPECS[
 
 pub static TESTING_CHAIN_SPEC: Lazy<ChainSpec> =
     Lazy::new(|| ChainSpec::new_single(TEST_CHAIN_ID, MERGE));
-
-pub static FLOW_MAINNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(747, CANCUN));
-
-pub static FLOW_TESTNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(545, CANCUN));
-
-pub static SCROLL_MAINNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(534352, CANCUN));
-
-pub static SCROLL_SEPOLIA_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(534351, CANCUN));
-
-pub static MANTLE_MAINNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(5000, CANCUN));
-
-pub static BITKUB_CHAIN_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(96, CANCUN));
-
-pub static BITKUB_CHAIN_TESTNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(25925, CANCUN));
-
-pub static KINTO_CHAIN_SPEC: Lazy<ChainSpec> = Lazy::new(|| ChainSpec::new_single(7887, CANCUN));
-
-pub static CELO_CHAIN_SPEC: Lazy<ChainSpec> = Lazy::new(|| ChainSpec::new_single(42220, CANCUN));
-
-pub static ZIRCUIT1_MAINNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(48900, CANCUN));
-
-pub static ZIRCUIT1_TESTNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(48899, CANCUN));
-
-pub static GNOSIS_MAINNET_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(100, CANCUN));
-
-pub static GNOSIS_CHIADO_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(10200, CANCUN));
-
-pub static PHENIX_CHAIN_SPEC: Lazy<ChainSpec> =
-    Lazy::new(|| ChainSpec::new_single(8008135, CANCUN));
