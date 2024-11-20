c704dbc441db59c16a1d0ba7bcc7aa9ff5a81560
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-20 15:38:36 +0100

    Add USDT profiling with snap

--- rust/services/call/host/src/host/tests/number_of_rpc_calls.rs
+++ rust/services/call/host/src/host/tests/number_of_rpc_calls.rs
@@ -1,6 +1,7 @@
 use std::{path::PathBuf, sync::Arc};
 
-use alloy_chains::NamedChain::OptimismSepolia;
+use alloy_chains::NamedChain::{Mainnet, OptimismSepolia};
+use alloy_primitives::address;
 use call_engine::{
     evm::env::{cached::CachedEvmEnv, location::ExecutionLocation},
     travel_call_executor::TravelCallExecutor,
@@ -10,7 +11,9 @@ use provider::{profiling, CachedMultiProvider, CachedProvider};
 use crate::{
     evm_env::factory::HostEvmEnvFactory,
     test_harness::{
-        contracts::{AVERAGE_BALANCE_OF_CALL, SIMPLE_TIME_TRAVEL},
+        contracts::{
+            AVERAGE_BALANCE_OF_CALL, IERC20::balanceOfCall, SIMPLE_TIME_TRAVEL, USDT, USDT_BLOCK_NO,
+        },
         rpc_snapshot_file,
     },
     Call,
@@ -31,6 +34,29 @@ async fn time_travel() -> anyhow::Result<()> {
     let _ = TravelCallExecutor::new(&envs).call(&call, location);
 
     assert_eq!(profiling_provider.state().total_count(), 88);
+    insta::with_settings!({sort_maps => true}, {
+        insta::assert_yaml_snapshot!(profiling_provider.state())
+    });
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn usdt_erc20_balance_of() -> anyhow::Result<()> {
+    let location: ExecutionLocation = (USDT_BLOCK_NO, Mainnet).into();
+    let binance_8 = address!("F977814e90dA44bFA03b6295A0616a897441aceC");
+    let call = Call::new(USDT, &balanceOfCall { account: binance_8 });
+
+    let rpc_file = PathBuf::from(rpc_snapshot_file("mainnet", "usdt_erc20_balance_of"));
+    let provider = CachedProvider::from_file(&rpc_file)?;
+    let profiling_provider = Arc::new(profiling::Provider::new(provider));
+    let multi_provider =
+        CachedMultiProvider::from_provider(location.chain_id, profiling_provider.clone());
+    let envs = CachedEvmEnv::from_factory(HostEvmEnvFactory::new(multi_provider));
+
+    let _ = TravelCallExecutor::new(&envs).call(&call, location);
+
+    assert_eq!(profiling_provider.state().total_count(), 7);
     insta::with_settings!({sort_maps => true}, {
         insta::assert_yaml_snapshot!(profiling_provider.state())
     });
