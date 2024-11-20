ff806a86d1e6a0fb617e5ff458032c7da674259f
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-20 11:02:10 +0100

    Make sure anvil is present in server config by default

--- rust/common/src/guest.rs
+++ rust/common/src/guest.rs
@@ -3,7 +3,7 @@ use derive_more::AsRef;
 use risc0_zkp::core::digest::Digest;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Serialize, Deserialize, AsRef)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Serialize, Deserialize, AsRef, Default)]
 pub struct GuestElf {
     #[as_ref]
     pub id: Digest,
@@ -17,9 +17,5 @@ impl GuestElf {
             id: Digest::new(id),
             elf: Bytes::from_static(elf),
         }
-    }
-
-    pub const fn default() -> Self {
-        Self::new([0; 8], &[])
     }
 }
