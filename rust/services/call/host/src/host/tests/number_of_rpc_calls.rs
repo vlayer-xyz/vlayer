404ffc939b73ebb4fe7873081971b33a40487b80
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-20 16:21:02 +0100

    USDT single block profiling (#1190)
    * Add USDT profiling with snap
    
    * Extract common profiling utils
    
    * Fix linter
    
    * Rearrange tests

--- rust/services/call/host/src/host/tests/number_of_rpc_calls.rs
+++ rust/services/call/host/src/host/tests/number_of_rpc_calls.rs
@@ -1,6 +1,7 @@
 use std::{path::PathBuf, sync::Arc};
 
-use alloy_chains::NamedChain::OptimismSepolia;
+use alloy_chains::NamedChain::{Mainnet, OptimismSepolia};
+use alloy_primitives::address;
 use call_engine::{
     evm::env::{cached::CachedEvmEnv, location::ExecutionLocation},
     travel_call_executor::TravelCallExecutor,
@@ -10,29 +11,58 @@ use provider::{profiling, CachedMultiProvider, CachedProvider};
 use crate::{
     evm_env::factory::HostEvmEnvFactory,
     test_harness::{
-        contracts::{AVERAGE_BALANCE_OF_CALL, SIMPLE_TIME_TRAVEL},
+        contracts::{
+            AVERAGE_BALANCE_OF_CALL, IERC20::balanceOfCall, SIMPLE_TIME_TRAVEL, USDT, USDT_BLOCK_NO,
+        },
         rpc_snapshot_file,
     },
     Call,
 };
 
-#[tokio::test]
-async fn time_travel() -> anyhow::Result<()> {
-    let location: ExecutionLocation = (20_064_547_u64, OptimismSepolia).into();
-    let call = Call::new(SIMPLE_TIME_TRAVEL, &AVERAGE_BALANCE_OF_CALL);
-
-    let rpc_file = PathBuf::from(rpc_snapshot_file("op_sepolia", "simple_time_travel"));
+fn profile(
+    chain: &str,
+    test_name: &str,
+    location: ExecutionLocation,
+    call: &Call,
+) -> anyhow::Result<profiling::State> {
+    let rpc_file = PathBuf::from(rpc_snapshot_file(chain, test_name));
     let provider = CachedProvider::from_file(&rpc_file)?;
     let profiling_provider = Arc::new(profiling::Provider::new(provider));
     let multi_provider =
         CachedMultiProvider::from_provider(location.chain_id, profiling_provider.clone());
     let envs = CachedEvmEnv::from_factory(HostEvmEnvFactory::new(multi_provider));
 
-    let _ = TravelCallExecutor::new(&envs).call(&call, location);
+    let _ = TravelCallExecutor::new(&envs).call(call, location);
 
-    assert_eq!(profiling_provider.state().total_count(), 88);
+    Ok(profiling_provider.state())
+}
+
+#[tokio::test]
+async fn usdt_erc20_balance_of() -> anyhow::Result<()> {
+    let location: ExecutionLocation = (USDT_BLOCK_NO, Mainnet).into();
+    let binance_8 = address!("F977814e90dA44bFA03b6295A0616a897441aceC");
+    let call = Call::new(USDT, &balanceOfCall { account: binance_8 });
+
+    let state = profile("mainnet", "usdt_erc20_balance_of", location, &call)?;
+
+    assert_eq!(state.total_count(), 7);
     insta::with_settings!({sort_maps => true}, {
-        insta::assert_yaml_snapshot!(profiling_provider.state())
+        insta::assert_yaml_snapshot!(state)
+    });
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn time_travel() -> anyhow::Result<()> {
+    let location: ExecutionLocation = (20_064_547_u64, OptimismSepolia).into();
+    let call = Call::new(SIMPLE_TIME_TRAVEL, &AVERAGE_BALANCE_OF_CALL);
+
+    let state = profile("op_sepolia", "simple_time_travel", location, &call)?;
+
+    assert_eq!(state.total_count(), 88);
+    insta::with_settings!({sort_maps => true}, {
+        insta::assert_yaml_snapshot!(state)
     });
 
     Ok(())
