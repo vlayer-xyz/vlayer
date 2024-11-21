231130ad872015fbb34a4e60deeb7c67433907cf
pociej <grzegorz.pociejewski@gmail.com>
2024-11-21 18:31:35 +0100

    Add email fixtures

---
+++ contracts/fixtures/src/vlayer/EmailProofVerifier.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {EmailProver} from "./EmailProver.sol";
+
+import {Proof} from "vlayer/Proof.sol";
+import {Verifier} from "vlayer/Verifier.sol";
+
+contract EmailProofVerifier is Verifier {
+    address public prover;
+
+    constructor(address _prover) {
+        prover = _prover;
+    }
+
+    function verify(Proof calldata, address wallet) public view onlyVerified(prover, EmailProver.main.selector) {
+        require(wallet == msg.sender, "Must be called from the same wallet as the registered address");
+    }
+}
