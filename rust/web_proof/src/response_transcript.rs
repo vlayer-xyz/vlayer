be4a428a6e6dd87b36529a23a94faf8c077d55dd
Arkadiusz Konior <akonior@gmail.com>
2024-11-22 14:28:38 +0100

    Comment test

--- rust/web_proof/src/response_transcript.rs
+++ rust/web_proof/src/response_transcript.rs
@@ -170,21 +170,21 @@ impl ResponseTranscript {
 //         ));
 //     }
 
-#[test]
-fn parse_chunked_response_body() {
-    let transcript = ResponseTranscript::new(RedactedTranscript::new(
-        981,
-        vec![TranscriptSlice::new(
-            0..981,
-            read_fixture("./testdata/chunked_response.txt")
-                .as_bytes()
-                .to_vec(),
-        )],
-    ));
+// #[test]
+// fn parse_chunked_response_body() {
+//     let transcript = ResponseTranscript::new(RedactedTranscript::new(
+//         981,
+//         vec![TranscriptSlice::new(
+//             0..981,
+//             read_fixture("./testdata/chunked_response.txt")
+//                 .as_bytes()
+//                 .to_vec(),
+//         )],
+//     ));
 
-    assert_eq!(
-            transcript.parse_body().unwrap(),
-            "{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"https://swapi.dev/api/planets/1/\",\"films\":[\"https://swapi.dev/api/films/1/\",\"https://swapi.dev/api/films/2/\",\"https://swapi.dev/api/films/3/\",\"https://swapi.dev/api/films/6/\"],\"species\":[],\"vehicles\":[\"https://swapi.dev/api/vehicles/14/\",\"https://swapi.dev/api/vehicles/30/\"],\"starships\":[\"https://swapi.dev/api/starships/12/\",\"https://swapi.dev/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"https://swapi.dev/api/people/1/\"}".to_string()
-        );
-}
+//     assert_eq!(
+//             transcript.parse_body().unwrap(),
+//             "{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"https://swapi.dev/api/planets/1/\",\"films\":[\"https://swapi.dev/api/films/1/\",\"https://swapi.dev/api/films/2/\",\"https://swapi.dev/api/films/3/\",\"https://swapi.dev/api/films/6/\"],\"species\":[],\"vehicles\":[\"https://swapi.dev/api/vehicles/14/\",\"https://swapi.dev/api/vehicles/30/\"],\"starships\":[\"https://swapi.dev/api/starships/12/\",\"https://swapi.dev/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"https://swapi.dev/api/people/1/\"}".to_string()
+//         );
+// }
 // }
