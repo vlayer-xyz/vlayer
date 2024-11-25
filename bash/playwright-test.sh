c8b5fba1a1d5edecd3dfa8e47967dabd98a5bfcb
Jakub Konka <kubkon@jakubkonka.com>
2024-11-25 17:51:43 +0100

    ci: startup only selected services with docker compose
    Revert docker/web-proof/docker-compose-release.yaml so that we don't
    break folks' local development envs if they use it.
    
    Split up playwright-test.sh into smaller chunks for easier remote-ssh
    debuggability.

--- bash/playwright-test.sh
+++ bash/playwright-test.sh
@@ -7,36 +7,21 @@ VLAYER_HOME=$(git rev-parse --show-toplevel)
 echo Run services
 source ${VLAYER_HOME}/bash/run-services.sh
 
-echo Install dependencies
-cd ${VLAYER_HOME}/packages && bun install --frozen-lockfile && cd -
-
-echo Build SDK
-cd ${VLAYER_HOME}/packages/sdk && bun run build && cd -
-
-echo Install dependencies of test-web-app
-cd ${VLAYER_HOME}/packages/test-web-app && bun install --frozen-lockfile && cd -
-
-echo Build extension
-cd ${VLAYER_HOME}/packages/browser-extension && bun run build && cd -
-
-echo Install playwright browsers
-cd ${VLAYER_HOME}/packages/browser-extension && bunx playwright install --with-deps chromium && cd -
-
 echo Mock ImageId.sol
 source ${VLAYER_HOME}/bash/mock-imageid.sh
 
 echo Run Forge build vlayer
-cd ${VLAYER_HOME}/contracts/vlayer
+pushd ${VLAYER_HOME}/contracts/vlayer
 forge soldeer install
 forge build --sizes
-cd -
+popd
 
 echo Run Forge build fixtures
-cd ${VLAYER_HOME}/contracts/fixtures
+pushd ${VLAYER_HOME}/contracts/fixtures
 forge soldeer install
 forge build --sizes
-cd -
+popd
 
 echo Run playwright tests
-cd ${VLAYER_HOME}/packages && bun run test:headless && cd -
+pushd ${VLAYER_HOME}/packages && bun run test:headless && popd
 
