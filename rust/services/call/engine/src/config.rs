e038ad6a74a1defc6fb48c7b10f86a63ed28e6ee
Leonid Logvinov <logvinov.leon@gmail.com>
2024-11-25 10:50:39 +0100

    Hardcode optimism coinbase account as empty

--- rust/services/call/engine/src/config.rs
+++ rust/services/call/engine/src/config.rs
@@ -1,3 +1,5 @@
 use alloy_primitives::{address, Address};
 
 pub const DEFAULT_CALLER: Address = address!("1111111111111111111111111111111111111111");
+// Has the same meaning as coinbase in Ethereum.
+pub const OPTIMISM_SEQUENCER_VAULT: Address = address!("4200000000000000000000000000000000000011");
