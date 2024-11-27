22e7671847f1a7f0e8d72c040bb6ed7153a0a691
Adam Król <adam.dawid.krol@gmail.com>
2024-11-27 15:38:58 +0100

    Use ACCOUNT_TO_STORAGE in seed_cache_db_with_trusted_data

--- rust/services/call/engine/src/db.rs
+++ rust/services/call/engine/src/db.rs
@@ -1,10 +1,11 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, fmt::Debug};
 
-use alloy_primitives::{Address, U256};
+use alloy_primitives::Address;
 use revm::{db::CacheDB, DatabaseRef};
 
 use crate::config::{
-    BASE_FEE_VAULT, DEFAULT_CALLER, L1_BLOCK, L1_FEE_VAULT, OPTIMISM_SEQUENCER_VAULT,
+    Storage, ACCOUNT_TO_STORAGE, BASE_FEE_VAULT, DEFAULT_CALLER, L1_BLOCK, L1_FEE_VAULT,
+    OPTIMISM_SEQUENCER_VAULT,
 };
 
 static EMPTY_ACCOUNTS: &[Address] =
@@ -23,42 +24,28 @@ where
     D: DatabaseRef,
     <D as DatabaseRef>::Error: Debug,
 {
-    for address in EMPTY_ACCOUNTS {
-        db.insert_account_info(*address, Default::default());
-    }
+    insert_accounts_info(db, EMPTY_ACCOUNTS);
+    insert_accounts_storage(db, &ACCOUNT_TO_STORAGE.clone());
+}
 
-    let storage_keys = [1u64, 3u64, 5u64, 7u64];
-    for key in &storage_keys {
-        db.insert_account_storage(L1_BLOCK, U256::from(*key), Default::default())
-            .expect("Failed to insert account storage");
+fn insert_accounts_info<D>(db: &mut CacheDB<D>, accounts: &[Address])
+where
+    D: DatabaseRef,
+{
+    for address in accounts {
+        db.insert_account_info(*address, Default::default());
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use revm::db::EmptyDB;
-
-    use super::*;
-
-    #[test]
-    fn seeds_address_to_some_data() {
-        let mut db = CacheDB::new(EmptyDB::default());
-        seed_cache_db_with_trusted_data(&mut db);
-
-        for address in EMPTY_ACCOUNTS {
-            assert!(db.accounts.get(address).unwrap().info.code.is_some());
+fn insert_accounts_storage<D>(db: &mut CacheDB<D>, address_to_storage: &HashMap<Address, Storage>)
+where
+    D: DatabaseRef,
+    <D as DatabaseRef>::Error: Debug,
+{
+    for (&address, storage) in address_to_storage {
+        for (&key, &value) in storage {
+            db.insert_account_storage(address, key, value)
+                .expect("failed to insert account storage");
         }
-    }
-
-    #[test]
-    fn seed_storage_of_l1_block_to_zeros() {
-        let mut db = CacheDB::new(EmptyDB::default());
-        seed_cache_db_with_trusted_data(&mut db);
-
-        let l1_block_storage = db.accounts.get(&L1_BLOCK).unwrap().storage.clone();
-        assert_eq!(l1_block_storage.get(&U256::from(1)).unwrap(), &U256::ZERO);
-        assert_eq!(l1_block_storage.get(&U256::from(3)).unwrap(), &U256::ZERO);
-        assert_eq!(l1_block_storage.get(&U256::from(5)).unwrap(), &U256::ZERO);
-        assert_eq!(l1_block_storage.get(&U256::from(7)).unwrap(), &U256::ZERO);
     }
 }
