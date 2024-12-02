672754b0b1b7151ffd45b6eeb555dcc35691b8a9
Artur Chmaro <artur@vlayer.xyz>
2024-12-02 12:15:09 +0100

    move to envs

--- examples/simple_time_travel/vlayer/prove.ts
+++ examples/simple_time_travel/vlayer/prove.ts
@@ -7,32 +7,18 @@ import {
   getConfig,
   waitForTransactionReceipt,
 } from "@vlayer/sdk/config";
-import { $ } from "bun";
 import { type Address } from "viem";
 
 const config = getConfig();
 const { ethClient, account, proverUrl } = await createContext(config);
 
-let tokenOwner: Address;
-let usdcTokenAddr: Address;
-let startBlock: bigint;
-let endBlock: bigint;
-let step: bigint;
+const tokenOwner = process.env.TOKEN_OWNER as Address;
+const usdcTokenAddr = process.env.USDC_TOKEN_ADDR as Address;
+const startBlock = BigInt(process.env.START_BLOCK as string);
+const endBlock = BigInt(process.env.END_BLOCK as string);
+const step = BigInt(process.env.STEP as string);
 
-if (process.env.VLAYER_ENV === "dev") {
-  await $`forge script --chain anvil scripts/AnvilSetup.s.sol:AnvilSetup --rpc-url ${config.jsonRpcUrl} --broadcast --private-key ${config.privateKey}`;
-  tokenOwner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // Owner of the USDC token at anvil
-  usdcTokenAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // USDC at anvil
-  startBlock = 1n;
-  endBlock = 40n;
-  step = 10n;
-} else {
-  tokenOwner = "0xE6b08c02Dbf3a0a4D3763136285B85A9B492E391"; // Owner of the USDC token at OP Sepolia
-  usdcTokenAddr = "0x5fd84259d66Cd46123540766Be93DFE6D43130D7"; // Test USDC at OP Sepolia
-  startBlock = 17915294n;
-  endBlock = 17985294n;
-  step = 9000n;
-}
+console.log({ tokenOwner, usdcTokenAddr, startBlock, endBlock, step });
 
 const { prover, verifier } = await deployVlayerContracts({
   proverSpec,
