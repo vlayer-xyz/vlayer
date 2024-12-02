1866d05d1c4507788292f1f894032836a5e21017
Artur Chmaro <artur@vlayer.xyz>
2024-12-02 16:43:47 +0100

    wip

--- examples/simple_time_travel/vlayer/loadFixtures.ts
+++ examples/simple_time_travel/vlayer/loadFixtures.ts
@@ -1,6 +1,55 @@
-import { $ } from "bun";
 import { getConfig } from "@vlayer/sdk/config";
+import { createTestClient, http, publicActions, walletActions } from "viem";
+import { foundry } from "viem/chains";
+import MockERC20 from "../out/MockERC20.sol/MockERC20";
+import { privateKeyToAccount } from "viem/accounts";
 
 const config = getConfig();
 
-await $`forge script --chain anvil scripts/LoadFixtures.s.sol:LoadFixtures --rpc-url ${config.jsonRpcUrl} --broadcast --private-key ${config.privateKey}`;
+export const testClient = createTestClient({
+  chain: foundry,
+  mode: "anvil",
+  transport: http(config.jsonRpcUrl),
+})
+  .extend(publicActions)
+  .extend(walletActions);
+
+const [john] = await testClient.getAddresses();
+
+const account = privateKeyToAccount(config.privateKey as `0x${string}`);
+
+const hash = await testClient.deployContract({
+  abi: MockERC20.abi,
+  code: MockERC20.bytecode.object,
+  account,
+});
+
+const receipt = await testClient.waitForTransactionReceipt({ hash });
+const erc20addr = receipt.contractAddress as `0x${string}`;
+
+await testClient.writeContract({
+  address: erc20addr,
+  abi: MockERC20.abi,
+  functionName: "mint",
+  args: [account.address, 1000n],
+  account,
+});
+await testClient.mine({ blocks: 10 });
+
+// await testClient.writeContract({
+//   address: erc20addr,
+//   abi: MockERC20.abi,
+//   functionName: "transfer",
+//   args: [john, 100n],
+//   account
+// });
+await testClient.mine({ blocks: 10 });
+
+// await testClient.writeContract({
+//   address: erc20addr,
+//   abi: MockERC20.abi,
+//   functionName: "transfer",
+//   args: [john, 100n],
+//   account,
+// });
+await testClient.mine({ blocks: 20 });
