35cdd8d5acc9f2af748eae004589ec7fe044f0a0
pociej <grzegorz.pociejewski@gmail.com>
2024-12-04 15:43:10 +0100

    Revert change in Imageid

--- contracts/vlayer/src/ImageID.sol
+++ contracts/vlayer/src/ImageID.sol
@@ -1,6 +1,1 @@
-pragma solidity ^0.8.20;
-
-library ImageID {
-    bytes32 public constant RISC0_CALL_GUEST_ID =
-        bytes32(0x0000000000000000000000000000000000000000000000000000000000000000);
-}
+../../../rust/target/assets/ImageID.sol
