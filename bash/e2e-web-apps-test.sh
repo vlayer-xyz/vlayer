f18c1010a485f0b96ab194e2099c9fc37f55492e
Artur Chmaro <artur@vlayer.xyz>
2024-12-10 14:28:24 +0100

    cleanup sh for e2e tests

--- bash/e2e-web-apps-test.sh
+++ bash/e2e-web-apps-test.sh
@@ -12,19 +12,11 @@ ${VLAYER_HOME}/bash/build-ts-types.sh >/dev/null
 echo Running services...
 source ${VLAYER_HOME}/bash/run-services.sh
 
-echo Setting up SDK 
-cd ${VLAYER_HOME}/packages/sdk && bun install --frozen-lockfile
-
-echo "::group::Building sdk"
-cd "${VLAYER_HOME}/packages/sdk"
-bun run build
-echo '::endgroup::'
-
-echo 'going to install playwright deps'
-bunx playwright install chromium --with-deps
-
 for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ; do
   example_name=$(basename "${example}")
+
+  echo "make snapshot of anvil"
+  ANVIL_SNAPSHOT_ID=$(cast rpc evm_snapshot)
 
   echo "::group::Running tests of: ${example}"
   cd "${example}"
@@ -35,6 +27,9 @@ for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ;
   cd vlayer
   bun install --frozen-lockfile
   bun run test:"${VLAYER_ENV}"
+
+  echo "revert anvil to initial state"
+  cast rpc evm_revert "${ANVIL_SNAPSHOT_ID}"
   echo '::endgroup::'
 done
 
