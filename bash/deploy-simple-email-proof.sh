45c5a4a6da433e22651cc0f641b797ef899c3620
Artur <artur@vlayer.xyz>
2024-12-10 11:22:01 +0100

    Deploy email proof to vercel (#1314)
    * Deploy email proof to vercel
    
    * lint + chmod
    
    * add more envs
    
    * test cli deploy
    
    * change dir
    
    * rm obsolete files
    
    * add github comment, include secrets
    
    * linter fix
    
    * build on CI

---
+++ bash/deploy-simple-email-proof.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+example_name="simple_email_proof"
+
+cd "${VLAYER_HOME}/examples/${example_name}"
+mkdir -p .vercel
+echo "{\"projectId\":\"${VERCEL_PROJECT_ID}\",\"orgId\":\"${VERCEL_ORG_ID}\"}" > .vercel/project.json
+
+if [ "$VERCEL_ENV" == "production" ]; then
+  vercel env pull ./vlayer/.env.testnet --token "$VERCEL_TOKEN" --prod
+  vercel env pull ./vlayer/.env.testnet.local --token "$VERCEL_TOKEN" --prod
+  vercel --token "$VERCEL_TOKEN" --prod --yes --cwd ./vlayer/dist --scope "$VERCEL_ORG_ID" | tail -1
+  echo "simple-email-proof production deployment available at: $DEPLOYMENT_URL"
+else
+  vercel env pull ./vlayer/.env.testnet --token "$VERCEL_TOKEN"
+  vercel env pull ./vlayer/.env.testnet.local --token "$VERCEL_TOKEN"
+  vercel --token "$VERCEL_TOKEN" --yes --cwd ./ --scope "$VERCEL_ORG_ID" | tail -1
+  DEPLOYMENT_URL=$(vercel --token $VERCEL_TOKEN --yes --cwd ./vlayer/dist --scope $VERCEL_ORG_ID | tail -1)
+  COMMENT_BODY="The preview of the simple-email-proof example app is available at: $DEPLOYMENT_URL"
+  curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    -X POST \
+    -d "{\"body\":\"$COMMENT_BODY\"}" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments"
+fi
