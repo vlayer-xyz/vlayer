e3dda85c51716c1cdbc244e8ee95f52ae6a101dc
Artur Chmaro <artur@vlayer.xyz>
2024-12-10 11:40:02 +0100

    hotifx for production deployments

--- bash/deploy-simple-email-proof.sh
+++ bash/deploy-simple-email-proof.sh
@@ -10,14 +10,9 @@ mkdir -p .vercel
 echo "{\"projectId\":\"${VERCEL_PROJECT_ID}\",\"orgId\":\"${VERCEL_ORG_ID}\"}" > .vercel/project.json
 
 if [ "$VERCEL_ENV" == "production" ]; then
-  vercel env pull ./vlayer/.env.testnet --token "$VERCEL_TOKEN" --prod
-  vercel env pull ./vlayer/.env.testnet.local --token "$VERCEL_TOKEN" --prod
   vercel --token "$VERCEL_TOKEN" --prod --yes --cwd ./vlayer/dist --scope "$VERCEL_ORG_ID" | tail -1
   echo "simple-email-proof production deployment available at: $DEPLOYMENT_URL"
 else
-  vercel env pull ./vlayer/.env.testnet --token "$VERCEL_TOKEN"
-  vercel env pull ./vlayer/.env.testnet.local --token "$VERCEL_TOKEN"
-  vercel --token "$VERCEL_TOKEN" --yes --cwd ./ --scope "$VERCEL_ORG_ID" | tail -1
   DEPLOYMENT_URL=$(vercel --token $VERCEL_TOKEN --yes --cwd ./vlayer/dist --scope $VERCEL_ORG_ID | tail -1)
   COMMENT_BODY="The preview of the simple-email-proof example app is available at: $DEPLOYMENT_URL"
   curl -s -H "Authorization: token $GITHUB_TOKEN" \
