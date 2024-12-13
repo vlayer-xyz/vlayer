a9bae912e4d287f55e81a12c461a1171625cf2ba
Piotr Żelazko <piotr@vlayer.xyz>
2024-12-13 12:55:50 +0100

    feat: add r0vm

--- flake.nix
+++ flake.nix
@@ -5,50 +5,74 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     foundry.url = "github:shazow/foundry.nix";
-    risc0.url = "github:kubkon/risc0-flake";
   };
 
 
-  outputs = { 
-    flake-utils, 
-    nixpkgs, 
-    nixpkgs-unstable,
-    foundry, 
-    risc0,
-    ... 
-  } @ inputs: let
-    overlays = [ foundry.overlay ] ++ [
-      (final: prev: {
-        risc0pkgs = risc0.packages.${prev.system};
-      })
-    ];
-    systems = [ "aarch64-darwin" ];
-  in 
+  outputs =
+    { flake-utils
+    , nixpkgs
+    , nixpkgs-unstable
+    , foundry
+    , ...
+    }:
+    let
+      systems = [ "aarch64-darwin" ];
+    in
     flake-utils.lib.eachSystem systems (
-      system: let
+      system:
+      let
+        overlays = [ foundry.overlay ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
         pkgsUnstable = import nixpkgs-unstable {
           inherit system overlays;
         };
-        buildInputs = with pkgs; [
-          risc0pkgs.rzup
-          rustup
-          libiconv
-          foundry-bin
-          pkgsUnstable.bun
-          nodejs
-          darwin.apple_sdk.frameworks.AppKit # these would not be required if we used rust-overlay... just saying!
-          darwin.apple_sdk.frameworks.SystemConfiguration
-          darwin.apple_sdk.frameworks.Security
+
+        risc0-version = "1.2.0";
+        risc0 = (import ./nix/risc0.nix { inherit system pkgs; }).risc0.${risc0-version};
+
+        darwinInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          pkgs.darwin.apple_sdk.frameworks.Security
+          pkgs.darwin.apple_sdk.frameworks.AppKit
         ];
-      in rec {
+
+        devTools = [
+          risc0.r0vm
+
+          pkgs.foundry-bin
+
+          pkgs.nodejs
+          pkgsUnstable.bun
+
+          pkgs.mdbook
+
+          pkgs.cargo-sort
+          pkgs.cargo-machete
+        ];
+
+        buildInputs = [
+          risc0.toolchain
+
+          pkgs.rustup
+          pkgs.libiconv
+
+        ] ++ darwinInputs;
+
+
+      in
+      {
+
         devShells.default = pkgs.mkShell {
-          name = "vlayer";
-          buildInputs = buildInputs;
+          name = "vlayer-shell";
+          buildInputs = buildInputs ++ devTools;
+
           CC_riscv32im_risc0_zkvm_elf = "${pkgs.pkgsCross.riscv32-embedded.stdenv.cc}/bin/riscv32-none-elf-cc";
           CFLAGS_riscv32im_risc0_zkvm_elf = "-march=rv32im -nostdlib -DRING_CORE_NOSTDLIBINC=1 -D__ILP32__=1";
+
+          CARGO_NET_GIT_FETCH_WITH_CLI = "true";
+
           RUST_BACKTRACE = 1;
         };
       }
