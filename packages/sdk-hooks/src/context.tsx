fabeb730099575fe91c08ccf3b1c6629881cf65e
pociej <grzegorz.pociejewski@gmail.com>
2024-12-16 16:36:54 +0100

    Remove error boundary

--- packages/sdk-hooks/src/context.tsx
+++ packages/sdk-hooks/src/context.tsx
@@ -1,14 +1,9 @@
 import { createVlayerClient } from "@vlayer/sdk";
 import { type VlayerContextType } from "./types";
-import { createContext, useCallback, type PropsWithChildren } from "react";
+import { createContext, type PropsWithChildren } from "react";
 import { createExtensionWebProofProvider } from "@vlayer/sdk/web_proof";
 import "viem/window";
-import { ErrorBoundary } from "react-error-boundary";
-import { WagmiProviderNotFoundError } from "wagmi";
 export const VlayerContext = createContext<VlayerContextType | null>(null);
-
-const NO_WAGMI_PROVIDER_ERROR_MESSAGE =
-  "Wagmi provider is required but not found. Please make sure you have connected a Wagmi provider.";
 
 const DEFAULT_PROVER_URL = "https://test-prover.vlayer.xyz";
 const DEFAULT_NOTARY_URL = "https://test-notary.vlayer.xyz";
@@ -38,36 +33,14 @@ export const VlayerProvider = ({
     webProofProvider,
   });
 
-  const handleError = useCallback((error: Error) => {
-    if (error instanceof WagmiProviderNotFoundError) {
-      console.error(`@vlayer/react: ${NO_WAGMI_PROVIDER_ERROR_MESSAGE}`);
-    }
-  }, []);
   return (
-    <ErrorBoundary
-      FallbackComponent={VlayerErrorFallback}
-      onError={handleError}
+    <VlayerContext.Provider
+      value={{
+        vlayerClient,
+        webProofProvider,
+      }}
     >
-      <VlayerContext.Provider
-        value={{
-          vlayerClient,
-          webProofProvider,
-        }}
-      >
-        {children}
-      </VlayerContext.Provider>
-    </ErrorBoundary>
+      {children}
+    </VlayerContext.Provider>
   );
 };
-
-function VlayerErrorFallback({ error }: { error: Error }) {
-  if (error instanceof WagmiProviderNotFoundError) {
-    return (
-      <div style={{ textAlign: "center", fontSize: "1.2rem" }}>
-        {NO_WAGMI_PROVIDER_ERROR_MESSAGE}
-      </div>
-    );
-  }
-
-  return <div>Vlayer Error: {error.message}</div>;
-}
