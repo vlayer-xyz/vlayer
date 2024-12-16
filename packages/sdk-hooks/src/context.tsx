08fc24313c6efe04bb4ee29462d0b14dc0d1d73b
pociej <grzegorz.pociejewski@gmail.com>
2024-12-16 09:21:15 +0100

    Add example usage of provider

--- packages/sdk-hooks/src/context.tsx
+++ packages/sdk-hooks/src/context.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { createVlayerClient } from "@vlayer/sdk";
 import { createContext, type PropsWithChildren } from "react";
 import { createExtensionWebProofProvider } from "@vlayer/sdk/web_proof";
+<<<<<<< HEAD
 import { type VlayerContextType } from "./types";
+=======
+import "viem/window";
+import { anvil } from "viem/chains";
+import { type Config } from "@vlayer/sdk/config";
+
+>>>>>>> 916b4880 (Add example usage of provider)
 export const VlayerContext = createContext<VlayerContextType | null>(null);
 
 const DEFAULT_CONFIG = {
@@ -15,6 +22,7 @@ export const VlayerProvider = ({
   config,
   children,
 }: PropsWithChildren<{
+<<<<<<< HEAD
   config: Partial<typeof DEFAULT_CONFIG>;
 }>) => {
   const { proverUrl, notaryUrl, wsProxyUrl } = { ...DEFAULT_CONFIG, ...config };
@@ -34,6 +42,29 @@ export const VlayerProvider = ({
       value={{
         vlayerClient,
         webProofProvider,
+=======
+  config: Config;
+}>) => {
+  const useWindowEthereumTransport = config.chainName !== anvil.name;
+  return (
+    <VlayerContext.Provider
+      value={{
+        vlayerClient: createVlayerClient({
+          url: config.proverUrl,
+        }),
+        webProofProvider: createExtensionWebProofProvider(),
+        chainContext: createVlayerChainContext(
+          {
+            chainName: config.chainName,
+            proverUrl: config.proverUrl,
+            jsonRpcUrl: config.jsonRpcUrl,
+            privateKey: config.privateKey,
+          },
+          useWindowEthereumTransport && window.ethereum
+            ? customTransport(window.ethereum)
+            : undefined,
+        ),
+>>>>>>> 916b4880 (Add example usage of provider)
       }}
     >
       {children}
