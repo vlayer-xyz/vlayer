6eb96f29e5d8dd78ad564c773aeda2ad9e93d9ae
pociej <grzegorz.pociejewski@gmail.com>
2024-12-16 14:44:55 +0100

    Add useWebproof hook

--- packages/sdk-hooks/src/interface.test.ts
+++ packages/sdk-hooks/src/interface.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import * as hooks from "./index";
+import { hooks } from "./index";
 
 describe("interface", () => {
   it("should export all must have hooks", () => {
