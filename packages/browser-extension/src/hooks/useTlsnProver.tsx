8b71f591dd80babd7eb0ae7ae9f6320a512a1cfd
wgromniak2 <wiktor@vlayer.xyz>
2024-12-18 10:50:12 +0100

    Refactored tlsnProve function outside of useTlsnProver in browser-extension. (#1408)
    * Refactored tlsnProve function outside of useTlsnProver in browser-extension.

--- packages/browser-extension/src/hooks/useTlsnProver.tsx
+++ packages/browser-extension/src/hooks/useTlsnProver.tsx
@@ -1,9 +1,3 @@
-import {
-  NotaryServer,
-  Prover as TProver,
-  Presentation as TPresentation,
-} from "tlsn-js";
-import { Reveal } from "tlsn-wasm";
 import React, {
   createContext,
   PropsWithChildren,
@@ -12,51 +6,21 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import * as Comlink from "comlink";
 import { formatTlsnHeaders } from "lib/formatTlsnHeaders";
 import { isDefined, ExtensionMessageType } from "../web-proof-commons";
 import { useProvingSessionConfig } from "./useProvingSessionConfig";
 import { useProvenUrl } from "./useProvenUrl";
 import { useTrackHistory } from "hooks/useTrackHistory";
-import { removeQueryParams } from "lib/removeQueryParams";
 import sendMessageToServiceWorker from "lib/sendMessageToServiceWorker";
 import { LOADING } from "@vlayer/extension-hooks";
-
-type ProverConfig = {
-  serverDns: string;
-  maxSentData?: number;
-  maxRecvData?: number;
-  maxRecvDataOnline?: number;
-  deferDecryptionFromStart?: boolean;
-};
-
-type PresentationConfig = {
-  attestationHex: string;
-  secretsHex: string;
-  notaryUrl?: string;
-  websocketProxyUrl?: string;
-  reveal: Reveal;
-};
-
-interface TLSNWorker {
-  init: (options: { loggingLevel: string }) => Promise<void>;
-  Prover: new (config: ProverConfig) => Promise<TProver>;
-  Presentation: new (config: PresentationConfig) => Promise<TPresentation>;
-}
-
-// tlsn-wasm needs to run in a worker
-const worker = new Worker(new URL("./tlsnWorker.ts", import.meta.url), {
-  type: "module",
-});
-const { init, Prover, Presentation } = Comlink.wrap(
-  worker,
-) as unknown as TLSNWorker;
+import { tlsnProve } from "./tlsnProve/tlsnProve";
 
 const TlsnProofContext = createContext({
   prove: async () => {},
   proof: null as object | null,
   isProving: false,
 });
+
 export const TlsnProofContextProvider = ({ children }: PropsWithChildren) => {
   const [proof, setProof] = useState<object | null>(null);
   const [isProving, setIsProving] = useState(false);
@@ -93,54 +57,22 @@ export const TlsnProofContextProvider = ({ children }: PropsWithChildren) => {
       isDefined(provingSessionConfig, "Missing proving session config");
 
       const hostname = new URL(provenUrl.url).hostname;
-
-      await init({ loggingLevel: "Debug" });
-      const notary = NotaryServer.from(
+      const notaryUrl =
         provingSessionConfig !== LOADING
           ? provingSessionConfig.notaryUrl || ""
-          : "",
-      );
-      const prover = await new Prover({
-        serverDns: hostname,
-        maxSentData: 4096,
-        maxRecvData: 16384,
-      });
-
-      const sessionUrl = await notary.sessionUrl();
-      await prover.setup(sessionUrl);
-
-      const res = await prover.sendRequest(
+          : "";
+      const wsProxyUrl =
         provingSessionConfig !== LOADING
           ? provingSessionConfig.wsProxyUrl + `?token=${hostname}`
-          : "",
-        {
-          url: removeQueryParams(provenUrl.url),
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            ...formattedHeaders?.headers,
-          },
-        },
+          : "";
+
+      const tlsnProof = await tlsnProve(
+        notaryUrl,
+        hostname,
+        wsProxyUrl,
+        provenUrl.url,
+        formattedHeaders,
       );
-
-      console.log("Received response", res);
-
-      const transcript = await prover.transcript();
-      const commit = {
-        sent: [transcript.ranges.sent.all],
-        recv: [transcript.ranges.recv.all],
-      };
-      const notarizationOutputs = await prover.notarize(commit);
-
-      const presentation = await new Presentation({
-        attestationHex: notarizationOutputs.attestation,
-        secretsHex: notarizationOutputs.secrets,
-        notaryUrl: notarizationOutputs.notaryUrl,
-        websocketProxyUrl: notarizationOutputs.websocketProxyUrl,
-        reveal: commit,
-      });
-
-      const tlsnProof = await presentation.json();
 
       void sendMessageToServiceWorker({
         type: ExtensionMessageType.ProofDone,
