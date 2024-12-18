1b40a11f8f8424c3c4c1a73bee7adfe10b45f880
Artur <artur@vlayer.xyz>
2024-12-18 14:06:39 +0100

    Deploy simple-web-proof to Vercel (#1389)
    * fix problem with top level await
    
    * gitignore .vercel
    
    * make vercel deploy script reusable
    
    * action for deploy web example
    
    * fix action syntax and linter issue
    
    * action linter
    
    * Update .github/workflows/deploy_simple_email_proof.yaml
    
    Co-authored-by: Przemek Rzad <przemek@vlayer.xyz>
    
    * Update .github/workflows/deploy_simple_web_proof.yaml
    
    Co-authored-by: Przemek Rzad <przemek@vlayer.xyz>
    
    * add concurrency
    
    * deploy script for examples
    
    * typo
    
    * remove console.log
    
    * fix env and if statement
    
    * use github vars instead of inputs
    
    * pass json rpc url
    
    * pass json rpc and github token
    
    ---------
    
    Co-authored-by: Przemek Rzad <przemek@vlayer.xyz>

--- bash/deploy-to-vercel.sh
+++ bash/deploy-to-vercel.sh
@@ -3,18 +3,17 @@
 set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
-example_name="simple_email_proof"
 
-cd "${VLAYER_HOME}/examples/${example_name}"
+cd "${VLAYER_HOME}/examples/${EXAMPLE_NAME}"
 mkdir -p .vercel
 echo "{\"projectId\":\"${VERCEL_PROJECT_ID}\",\"orgId\":\"${VERCEL_ORG_ID}\"}" > .vercel/project.json
 
 if [ "$VERCEL_ENV" == "production" ]; then
   DEPLOYMENT_URL=$(vercel --token "$VERCEL_TOKEN" --prod --yes --cwd ./vlayer/dist --scope "$VERCEL_ORG_ID" | tail -1)
-  echo "simple-email-proof production deployment available at: $DEPLOYMENT_URL"
+  echo "${EXAMPLE_NAME} production deployment available at: $DEPLOYMENT_URL"
 else
   DEPLOYMENT_URL=$(vercel --token $VERCEL_TOKEN --yes --cwd ./vlayer/dist --scope $VERCEL_ORG_ID | tail -1)
-  COMMENT_BODY="The preview of the simple-email-proof example app is available at: $DEPLOYMENT_URL"
+  COMMENT_BODY="The preview of the ${EXAMPLE_NAME} example app is available at: $DEPLOYMENT_URL"
   curl -s -H "Authorization: token $GITHUB_TOKEN" \
     -X POST \
     -d "{\"body\":\"$COMMENT_BODY\"}" \
