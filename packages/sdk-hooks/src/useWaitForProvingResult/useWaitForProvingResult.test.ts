2247ca35b4d1f8ebd66c9a43ad5c6d1c778bb63d
pociej <grzegorz.pociejewski@gmail.com>
2024-12-20 09:51:30 +0100

    Remove useProver duplication

--- packages/sdk-hooks/src/useWaitForProvingResult/useWaitForProvingResult.test.ts
+++ packages/sdk-hooks/src/useWaitForProvingResult/useWaitForProvingResult.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, waitFor } from "@testing-library/react";
-import { describe, it, expect, vi, beforeEach } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import {
   useWaitForProvingResult,
   WaitForProvingResultStatus,
