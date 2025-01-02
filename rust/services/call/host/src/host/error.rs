527cb1629e54aba40ea9d7c13ec0fdcc496438fb
Leonid Logvinov <logvinov.leon@gmail.com>
2025-01-02 14:23:47 +0100

    Remove two unused errors

--- rust/services/call/host/src/host/error.rs
+++ rust/services/call/host/src/host/error.rs
@@ -1,4 +1,3 @@
-use alloy_primitives::ChainId;
 use call_engine::{
     evm::env::factory::Error as EvmEnvFactoryError,
     travel_call_executor::Error as TravelCallExecutorError, verifier::guest_input,
@@ -24,9 +23,6 @@ pub enum Error {
     #[error("Block not found: {0}")]
     BlockNotFound(BlockTag),
 
-    #[error("Error creating client: {0}")]
-    NewClient(#[from] url::ParseError),
-
     #[error("Proving error: {0}")]
     Proving(#[from] proving::Error),
 
@@ -36,14 +32,8 @@ pub enum Error {
     #[error("Host output does not match guest output: {0:?} {1:?}")]
     HostGuestOutputMismatch(Vec<u8>, Vec<u8>),
 
-    #[error("No rpc url for chain: {0}")]
-    NoRpcUrl(ChainId),
-
     #[error("Chain error: {0}")]
     Chain(#[from] chain::Error),
-
-    #[error("Abi encode error: {0}")]
-    AbiEncode(String),
 
     #[error("Evm env factory error: {0}")]
     EvmEnvFactory(#[from] EvmEnvFactoryError),
