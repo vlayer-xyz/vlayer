710932bde1e3743a3f047a8896bd2ffe3b10ee59
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-03 15:33:03 +0100

    feat: add server state

--- rust/services/dns/server/src/server.rs
+++ rust/services/dns/server/src/server.rs
@@ -7,19 +7,43 @@ use tracing::info;
 
 use crate::config::Config;
 
+#[derive(Clone)]
+struct AppState;
+
+impl AppState {
+    fn new() -> Self {
+        Self
+    }
+}
+
 pub async fn serve(config: Config) -> anyhow::Result<()> {
     let listener = TcpListener::bind(config.socket_addr()).await?;
 
+    let state = AppState::new();
+
     info!("Listening on {}", listener.local_addr()?);
-    axum::serve(listener, server()).await?;
+    axum::serve(listener, server(state)).await?;
 
     Ok(())
 }
-pub fn server() -> axum::Router {
+
+fn server(state: AppState) -> Router {
     handlers::handlers()
         .into_iter()
         .fold(Router::new(), |router, (path, handler)| router.route(path, handler))
+        .with_state(state)
         .layer(cors())
         .layer(init_trace_layer())
         .layer(RequestIdLayer)
+}
+
+#[cfg(test)]
+mod test_helpers {
+
+    use super::*;
+
+    pub fn app() -> Router {
+        let state = AppState::new();
+        server(state)
+    }
 }
