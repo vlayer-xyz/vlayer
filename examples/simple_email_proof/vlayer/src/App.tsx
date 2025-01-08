8ea197a1cb415dac8f5747f1a1460e51b9d4fb05
Artur Chmaro <artur@vlayer.xyz>
2025-01-08 11:53:02 +0100

    Better providers

--- examples/simple_email_proof/vlayer/src/App.tsx
+++ examples/simple_email_proof/vlayer/src/App.tsx
@@ -3,21 +3,28 @@ import { ProofProvider } from "@vlayer/react";
 import EmlUploadForm from "./containers/EmlUploadForm";
 
 import { optimismSepolia, foundry } from "wagmi/chains";
+import { metaMask } from "wagmi/connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const wagmiConfig = createConfig({
+const wagmiConfig = createConfig({
   chains: [optimismSepolia, foundry],
+  connectors: [metaMask()],
   transports: {
     [optimismSepolia.id]: http(),
     [foundry.id]: http(),
   },
 });
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <WagmiProvider config={wagmiConfig}>
-      <ProofProvider>
-        <EmlUploadForm />
-      </ProofProvider>
+      <QueryClientProvider client={queryClient}>
+        <ProofProvider>
+          <EmlUploadForm />
+        </ProofProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
