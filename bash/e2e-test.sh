7d738481a8db74cc330b6cff6c020d49ce3b62c7
Leonid Logvinov <logvinov.leon@gmail.com>
2025-01-09 12:33:17 +0100

    Inline run_example(s)

--- bash/e2e-test.sh
+++ bash/e2e-test.sh
@@ -13,4 +13,20 @@ PROVING_MODE=${PROVING_MODE:-dev}
 generate_ts_bindings
 build_sdk
 
-run_examples
+echo "::group::Running examples"
+for example in $(find ${VLAYER_HOME}/examples -type d -maxdepth 1 -mindepth 1) ; do
+export EXAMPLE_NAME=$(basename "${example}")
+
+echo Running services...
+source ${VLAYER_HOME}/bash/run-services.sh
+
+pushd "${example}"
+  echo "::group::Running tests of: ${example}"
+  silent_unless_fails build_contracts
+  run_prover_script
+popd
+
+cleanup
+rm -rf "${VLAYER_TMP_DIR}/chain_db"
+done
+echo '::endgroup::Running examples'
