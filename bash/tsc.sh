5a6ad47c59c276cf7e65ab86022988da7ba3d1b9
pociej <grzegorz.pociejewski@gmail.com>
2025-01-09 14:49:16 +0100

    Change install directory in tsc.sh

--- bash/tsc.sh
+++ bash/tsc.sh
@@ -4,7 +4,7 @@ set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 
+bun install --frozen-lockfile
 cd "${VLAYER_HOME}/packages"
 echo "Running tsc for: $VLAYER_HOME/packages"
-bun install --frozen-lockfile
 bun tsc --build --noEmit
--- packages/browser-extension/vite.config.ts
+++ packages/browser-extension/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 function generateManifest() {
   const manifest = readJsonFile("src/manifest.json") as object;
@@ -38,6 +38,7 @@ export default defineConfig({
       watchFilePaths: ["package.json", "manifest.json"],
       webExtConfig: {
         startUrl: "http://localhost:5174",
+        target: "chromium",
       },
     }),
     viteStaticCopy({
@@ -64,7 +65,7 @@ export default defineConfig({
     }),
     nodePolyfills({
       // buffer is required by tlsn-js internals
-      include: ['buffer']
+      include: ["buffer"],
     }),
   ],
 });
