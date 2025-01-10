313cb4913e2c557c7c1b8d01940521792864e122
pociej <grzegorz.pociejewski@gmail.com>
2025-01-10 10:19:59 +0100

    Adjust path in extension build

--- packages/browser-extension/vite.config.ts
+++ packages/browser-extension/vite.config.ts
@@ -45,20 +45,20 @@ export default defineConfig({
       targets: [
         // copying to src/hooks/tlsnProve is require to run in dev mode
         {
-          src: `${__dirname}/../node_modules/tlsn-js/build/54ec0a0a2728a8a41850.wasm`,
+          src: `${__dirname}/../../node_modules/tlsn-js/build/54ec0a0a2728a8a41850.wasm`,
           dest: "src/hooks/tlsnProve/",
         },
         {
-          src: `${__dirname}/../node_modules/tlsn-js/build/112.js`,
+          src: `${__dirname}/../../node_modules/tlsn-js/build/112.js`,
           dest: "src/hooks/tlsnProve/",
         },
         // copying to assets is required for prod build
         {
-          src: `${__dirname}/../node_modules/tlsn-js/build/54ec0a0a2728a8a41850.wasm`,
+          src: `${__dirname}/../../node_modules/tlsn-js/build/54ec0a0a2728a8a41850.wasm`,
           dest: "assets/",
         },
         {
-          src: `${__dirname}/../node_modules/tlsn-js/build/112.js`,
+          src: `${__dirname}/../../node_modules/tlsn-js/build/112.js`,
           dest: "assets/",
         },
       ],
