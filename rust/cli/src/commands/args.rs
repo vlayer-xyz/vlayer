7e4b1c53e89341b260ae60784fdb35c8bff37a4a
Jakub Konka <jakub@vlayer.xyz>
2025-01-16 14:38:14 +0100

    cli: allow overriding vlayer version locally; attach error context to mostly io::Error paths (#1537)
    * cli: allow overriding vlayer version locally
    
    * cli: use common formatting for TemplateOptions
    
    Previously we would use clap default ValueEnum formatting as expected
    CLI input, such as `simple-web-proof`, but we would then display the
    installed template in logs as `simple_web_proof`.
    
    This change ensures we only use one formatting in both cases (currently
    the default clap formatting).
    
    * cli: minor refactor
    
    * cli: attach error context to mostly io::Error paths
    
    * cli: allow specifying custom workdir for templates
    
    * cli: allow for target (examples, contracts) version override only in cli
    
    * cli: add docs for each init flag

--- rust/cli/src/commands/args.rs
+++ rust/cli/src/commands/args.rs
@@ -1,15 +1,21 @@
-use std::fmt;
+use std::{fmt, path::PathBuf};
 
 use clap::{Parser, ValueEnum};
 
 #[derive(Clone, Debug, Parser)]
 pub(crate) struct InitArgs {
+    /// Template to use for the project
     #[arg(long, value_enum)]
     pub(crate) template: Option<TemplateOption>,
+    /// Force init in existing project location
     #[arg(long)]
     pub(crate) existing: bool,
+    /// Name of the project
     #[arg()]
     pub(crate) project_name: Option<String>,
+    /// Directory where the templates will be unpacked into (useful for debugging)
+    #[arg(long, env = "VLAYER_WORK_DIR")]
+    pub(crate) work_dir: Option<PathBuf>,
 }
 
 #[derive(Clone, Debug, ValueEnum, Default)]
@@ -24,12 +30,10 @@ pub(crate) enum TemplateOption {
 
 impl fmt::Display for TemplateOption {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            TemplateOption::Simple => write!(f, "simple"),
-            TemplateOption::SimpleEmailProof => write!(f, "simple_email_proof"),
-            TemplateOption::SimpleTeleport => write!(f, "simple_teleport"),
-            TemplateOption::SimpleTimeTravel => write!(f, "simple_time_travel"),
-            TemplateOption::SimpleWebProof => write!(f, "simple_web_proof"),
-        }
+        let as_value = self
+            .to_possible_value()
+            .expect("no TemplateOption variant should be skipped");
+        let name = as_value.get_name();
+        write!(f, "{name}")
     }
 }
