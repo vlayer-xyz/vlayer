547fcc6a9c65a2ef0d8f6713c662798acdaba486
Leonid Logvinov <logvinov.leon@gmail.com>
2025-01-16 12:43:40 +0100

    Revert risc0 v1.2.1-rc.1

--- bash/test-release-remote-prover.sh
+++ bash/test-release-remote-prover.sh
@@ -23,7 +23,7 @@ echo '::endgroup::'
 
 echo '::group::risczero installation'
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall -y cargo-risczero@1.2.1-rc.1
+cargo binstall -y cargo-risczero@1.2.0
 cargo risczero install
 echo '::endgroup::'
 
