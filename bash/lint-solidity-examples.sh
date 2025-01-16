44a3174a2d558faf4fc41699fab53436d9b8e96c
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2025-01-16 05:38:13 +0100

    Fix linting contracts (#1535)
    Fix contract linting

--- bash/lint-solidity-examples.sh
+++ bash/lint-solidity-examples.sh
@@ -5,11 +5,11 @@ set -ueo pipefail
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 source "$(dirname "${BASH_SOURCE[0]}")/lib/examples.sh"
 
+bun install --frozen-lockfile
+
 for example in $(get_examples); do (
     echo "Running solhint of: ${example}"
-    cd "$VLAYER_HOME/examples/$example"
-
-    bun install --frozen-lockfile
+    cd "$VLAYER_HOME/examples/$example/vlayer"
     bun run lint:solidity
 ) done
  
