2300c2899c118fbc25cf15b1ed91a8cfa6c0d452
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2025-01-17 15:51:17 +0100

    Chain guest ELF ID history (#1542)
    * guest_wrapper builder struct & data_layout macro
    * Chain guest ID in hex with history
    * Remove unused dependencies
    * Move guest build utils to a separate crate
    * Remove deprecated gitignore line
    * Add docstrings and remove unnecessary struct

---
+++ rust/guest_wrapper/build_utils/src/risc0_builder.rs
@@ -0,0 +1,150 @@
+use std::{collections::HashMap, fs, io};
+
+use risc0_build::{embed_methods_with_options, DockerOptions, GuestListEntry, GuestOptions};
+
+use crate::{
+    chain_guest_id, data_layout, path_from_env, remove_file_if_exists, use_bool_var, PROJECT_ROOT,
+};
+
+data_layout!(DataLayout {
+    project_root: PROJECT_ROOT.into(),
+    out_dir: path_from_env("OUT_DIR").unwrap(),
+} {
+    (project_root / "target/assets") => solidity_assets_dir,
+    (project_root / "target/assets/Elf.sol") => elf_sol_output,
+    (project_root / "target/assets/ImageID.sol") => image_id_sol_output,
+    (out_dir / "methods.rs") => rust_methods,
+});
+
+data_layout!(ExistingGuestLayout {
+    artifacts_dir: path_from_env("RISC0_EXISTING_GUEST")?,
+} {
+    (artifacts_dir / "methods.rs") => rust_methods,
+    (artifacts_dir / "ImageID.sol") => image_id_sol,
+});
+
+pub struct Builder {
+    data_layout: DataLayout,
+    existing_guest: Option<ExistingGuestLayout>,
+    skip_build: bool,
+    use_docker: bool,
+    update_guest_elf: bool,
+}
+
+impl Builder {
+    pub fn from_env() -> Self {
+        Self {
+            data_layout: DataLayout::new().unwrap(),
+            existing_guest: ExistingGuestLayout::new(),
+            skip_build: use_bool_var("RISC0_SKIP_BUILD"),
+            use_docker: use_bool_var("RISC0_USE_DOCKER"),
+            update_guest_elf: use_bool_var("UPDATE_GUEST_ELF_ID"),
+        }
+    }
+
+    pub fn build(&self) -> anyhow::Result<()> {
+        if self.skip_build {
+            println!("cargo::warning=Skipped build of guest_wrapper");
+            return Ok(());
+        }
+
+        self.prepare_assets_dir()?;
+
+        if self.copy_existing_guest_artifacts()? {
+            return Ok(());
+        }
+
+        let (call_guest, chain_guest) = self.build_guests()?;
+        self.check_or_update_chain_guest_id(&chain_guest)?;
+        self.generate_guest_sol_files(call_guest)?;
+
+        Ok(())
+    }
+
+    /// Create directory for assets, clean up old artifacts.
+    fn prepare_assets_dir(&self) -> io::Result<()> {
+        fs::create_dir_all(self.data_layout.solidity_assets_dir())?;
+        remove_file_if_exists(&self.data_layout.image_id_sol_output())?;
+        remove_file_if_exists(&self.data_layout.elf_sol_output())?;
+        Ok(())
+    }
+
+    /// Copy pre-built guest artifacts, if `RISC0_EXISTING_GUEST` is set.
+    /// Returns true iff artifacts were copied.
+    fn copy_existing_guest_artifacts(&self) -> io::Result<bool> {
+        let Some(existing_guest) = self.existing_guest.as_ref() else {
+            return Ok(false);
+        };
+
+        println!(
+            "cargo::warning=Using existing guest artifacts from {}",
+            existing_guest.artifacts_dir().display()
+        );
+
+        fs::copy(existing_guest.rust_methods(), self.data_layout.rust_methods())?;
+        fs::copy(existing_guest.image_id_sol(), self.data_layout.image_id_sol_output())?;
+
+        Ok(true)
+    }
+
+    fn get_guest_options(&self) -> GuestOptions {
+        let use_docker = self.use_docker.then_some(DockerOptions {
+            root_dir: Some(self.data_layout.project_root().into()),
+            env: vec![
+                ("CC_riscv32im_risc0_zkvm_elf".to_string(), "clang".to_string()),
+                (
+                    "CFLAGS_riscv32im_risc0_zkvm_elf".to_string(),
+                    "-nostdlibinc -DRING_CORE_NOSTDLIBINC=1 -target riscv32-unknown-elf -march=rv32im -D__ILP32__=1".to_string()
+                ),
+            ],
+        });
+        GuestOptions {
+            use_docker,
+            ..Default::default()
+        }
+    }
+
+    fn build_guests(&self) -> anyhow::Result<(GuestListEntry, GuestListEntry)> {
+        let guest_options = self.get_guest_options();
+        let mut guests = embed_methods_with_options(HashMap::from([
+            ("risc0_call_guest", guest_options.clone()),
+            ("risc0_chain_guest", guest_options),
+        ]));
+        let call_guest = remove_guest(&mut guests, "risc0_call_guest")?;
+        let chain_guest = remove_guest(&mut guests, "risc0_chain_guest")?;
+        Ok((call_guest, chain_guest))
+    }
+
+    /// Verify that chain guest ID is unchanged, or generate new one if `UPDATE_GUEST_ELF_ID` is set.
+    /// Generate rust file with current and historical chain guest IDs.
+    fn check_or_update_chain_guest_id(&self, chain_guest: &GuestListEntry) -> anyhow::Result<()> {
+        let generated_id = chain_guest.image_id.into();
+
+        if self.update_guest_elf {
+            anyhow::ensure!(self.use_docker, "`UPDATE_GUEST_ELF_ID` requires `RISC0_USE_DOCKER`");
+            chain_guest_id::update(generated_id)?;
+        } else if self.use_docker {
+            chain_guest_id::assert(generated_id)?;
+        }
+
+        Ok(())
+    }
+
+    /// Generate solidity files with call guest image ID and ELF.
+    fn generate_guest_sol_files(&self, call_guest: GuestListEntry) -> anyhow::Result<()> {
+        let solidity_opts = risc0_build_ethereum::Options::default()
+            .with_image_id_sol_path(self.data_layout.image_id_sol_output())
+            .with_elf_sol_path(self.data_layout.elf_sol_output());
+        risc0_build_ethereum::generate_solidity_files(&[call_guest], &solidity_opts)?;
+        Ok(())
+    }
+}
+
+/// Remove `GuestListEntry` with given name from vector.
+fn remove_guest(guests: &mut Vec<GuestListEntry>, name: &str) -> anyhow::Result<GuestListEntry> {
+    let idx = guests
+        .iter()
+        .position(|g| g.name == name)
+        .ok_or_else(|| anyhow::anyhow!("Guest {name} not found"))?;
+    Ok(guests.remove(idx))
+}
--- rust/guest_wrapper/risc0_call_guest/src/main.rs
+++ rust/guest_wrapper/risc0_call_guest/src/main.rs
@@ -5,12 +5,13 @@ risc0_zkvm::guest::entry!(main);
 use alloy_sol_types::SolValue;
 use risc0_zkvm::{guest::env, sha::Digest};
 
+include!(concat!(env!("OUT_DIR"), "/guest_id.rs"));
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let input = env::read();
-    let chain_guest_elf_id: Digest = (*include_bytes!("../../chain_guest_elf_id")).into();
 
-    let output = call_guest::main(input, chain_guest_elf_id).await;
+    let output = call_guest::main(input, Digest::from_bytes(CHAIN_GUEST_ELF_ID)).await;
 
     env::commit_slice(&output.call_assumptions.abi_encode());
     env::commit_slice(&output.evm_call_result);
