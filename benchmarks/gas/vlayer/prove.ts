4dbf0f151ebd2b4b2a67ae94e5973431ecaa7ce3
Jakub Konka <jakub@vlayer.xyz>
2025-01-18 09:39:49 +0100

    Deploy only prover contract

--- benchmarks/gas/vlayer/prove.ts
+++ benchmarks/gas/vlayer/prove.ts
@@ -1,17 +1,15 @@
-import { mean, median, sampleStandardDeviation } from "simple-statistics";
-import { createVlayerClient, ProofReceipt, Metrics } from "@vlayer/sdk";
-import nftSpec from "../out/ExampleNFT.sol/ExampleNFT";
+import { mean, sampleStandardDeviation } from "simple-statistics";
+import { createVlayerClient, ProofReceipt } from "@vlayer/sdk";
 import tokenSpec from "../out/ExampleToken.sol/ExampleToken";
 import { isAddress } from "viem";
 import {
   getConfig,
   createContext,
-  deployVlayerContracts,
+  deployProver,
   waitForContractDeploy,
 } from "@vlayer/sdk/config";
 
 import proverSpec from "../out/SimpleProver.sol/SimpleProver";
-import verifierSpec from "../out/SimpleVerifier.sol/SimpleVerifier";
 
 class MetricsUnpacked {
   gas: Array<number> = [];
@@ -58,13 +56,7 @@ type MetricsStats = {
 };
 
 const config = getConfig();
-const {
-  chain,
-  ethClient,
-  account: john,
-  proverUrl,
-  confirmations,
-} = createContext(config);
+const { chain, ethClient, account: john, proverUrl } = createContext(config);
 
 const INITIAL_TOKEN_SUPPLY = BigInt(10_000_000);
 
@@ -79,23 +71,7 @@ const tokenAddress = await waitForContractDeploy({
   hash: tokenDeployTransactionHash,
 });
 
-const nftDeployTransactionHash = await ethClient.deployContract({
-  abi: nftSpec.abi,
-  bytecode: nftSpec.bytecode.object,
-  account: john,
-  args: [],
-});
-
-const nftContractAddress = await waitForContractDeploy({
-  hash: nftDeployTransactionHash,
-});
-
-const { prover, verifier } = await deployVlayerContracts({
-  proverSpec,
-  verifierSpec,
-  proverArgs: [tokenAddress],
-  verifierArgs: [nftContractAddress],
-});
+const prover = await deployProver({ proverSpec, proverArgs: [tokenAddress] });
 
 const vlayer = createVlayerClient({
   url: proverUrl,
@@ -121,32 +97,14 @@ for (let i = 0; i < 10; i++) {
     chainId: chain.id,
     token: config.token,
   });
+
   const result = await vlayer.waitForProvingResult({
     hash,
     callback,
   });
-  const [proof, owner, balance] = result;
 
-  if (!isAddress(owner)) {
-    throw new Error(`${owner} is not a valid address`);
-  }
-
-  const verificationHash = await ethClient.writeContract({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "claimWhale",
-    args: [proof, owner, balance],
-    account: john,
-  });
-
-  const receipt = await ethClient.waitForTransactionReceipt({
-    hash: verificationHash,
-    confirmations,
-    retryCount: 60,
-    retryDelay: 1000,
-  });
-  if (receipt.status !== "success") {
-    throw Error("Verification failed!");
+  if (!isAddress(result[1])) {
+    throw new Error(`${result.owner} is not a valid address`);
   }
 
   console.log("  ...success");
