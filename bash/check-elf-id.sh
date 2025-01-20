dbf175a472426967b82cb494c6a3bc63dd64d32b
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2025-01-20 10:46:20 +0100

    Automate adding and checking chain_guest changelog entries

--- bash/check-elf-id.sh
+++ bash/check-elf-id.sh
@@ -5,6 +5,7 @@ set -ueo pipefail
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 CURRENT_ELF_ID_PATH="${VLAYER_HOME}/rust/guest_wrapper/artifacts/chain_guest/elf_id"
 ELF_ID_HISTORY_PATH="${VLAYER_HOME}/rust/guest_wrapper/artifacts/chain_guest/elf_id_history"
+CHANGELOG_PATH="${VLAYER_HOME}/rust/guest_wrapper/artifacts/chain_guest/CHANGELOG.md"
 
 CURRENT_ELF_ID=$(cat "${CURRENT_ELF_ID_PATH}")
 PREV_ELF_ID=$(git show --raw HEAD~1:rust/guest_wrapper/artifacts/chain_guest/elf_id)
@@ -14,7 +15,13 @@ if [[ "${CURRENT_ELF_ID}" == "${PREV_ELF_ID}" ]]; then
     exit 0
 fi
 
-grep "${PREV_ELF_ID}" "${ELF_ID_HISTORY_PATH}" > /dev/null && exit 0
+grep "${PREV_ELF_ID}" "${ELF_ID_HISTORY_PATH}" > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Previous ELF ID ${PREV_ELF_ID} not found in history"
+  exit 1
+fi
 
-echo "Previous ELF ID ${PREV_ELF_ID} not found in history"
-exit 1
+if ! grep "${CURRENT_ELF_ID}" "${CHANGELOG_PATH}" | grep -vq "TODO"; then
+  echo "No changelog entry for ${CURRENT_ELF_ID}"
+  exit 1
+fi
