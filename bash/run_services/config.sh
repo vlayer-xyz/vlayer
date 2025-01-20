c5d2cd3c932aa376f89390770d36d7ae8e6a9d84
Adam Król <adam.dawid.krol@gmail.com>
2025-01-20 14:50:06 +0100

    Bash lint

--- bash/run_services/config.sh
+++ bash/run_services/config.sh
@@ -19,7 +19,7 @@ function set_proof_mode() {
 }
 
 function set_external_rpc_urls() {
-    if [[ -n "${QUICKNODE_API_KEY:-}" && -n "${QUICKNODE_ENDPOINT:-}" ]] ; then
+    if [[ -n "${QUICKNODE_API_KEY:-}" && -n "${QUICKNODE_ENDPOINT:-}" ]]; then
         EXTERNAL_RPC_URLS=(
             "--rpc-url" "1:https://${QUICKNODE_ENDPOINT}.quiknode.pro/${QUICKNODE_API_KEY}"
             "--rpc-url" "11155111:https://${QUICKNODE_ENDPOINT}.ethereum-sepolia.quiknode.pro/${QUICKNODE_API_KEY}"
@@ -47,14 +47,14 @@ function set_testnet_chain_worker_args() {
 
     if [[ "${EXAMPLE_NAME:-}" == "simple_time_travel" ]]; then
         # Time travel example needs to travel 10 block back
-        start_op_sepolia_block=$(( $latest_op_sepolia_block - 10 ))
+        start_op_sepolia_block=$(($latest_op_sepolia_block - 10))
     else
         start_op_sepolia_block=$latest_op_sepolia_block
     fi
 
     CHAIN_WORKER_ARGS=()
 
-    if ["${EXAMPLE_NAME:-}" == "simple_time_travel"]; then
+    if [ "${EXAMPLE_NAME:-}" == "simple_time_travel" ]; then
         CHAIN_WORKER_ARGS+=(
             "https://${QUICKNODE_ENDPOINT}.optimism-sepolia.quiknode.pro/${QUICKNODE_API_KEY} 11155420 ${start_op_sepolia_block} ${latest_op_sepolia_block}"
         )
@@ -62,7 +62,7 @@ function set_testnet_chain_worker_args() {
 }
 
 function set_chain_worker_args() {
-    if [[ -z "${QUICKNODE_API_KEY:-}" || -z "${QUICKNODE_ENDPOINT:-}" ]] ; then
+    if [[ -z "${QUICKNODE_API_KEY:-}" || -z "${QUICKNODE_ENDPOINT:-}" ]]; then
         echo QUICKNODE_API_KEY is not configured. Starting devnet chain workers. >&2
         set_devnet_chain_worker_args
     else
