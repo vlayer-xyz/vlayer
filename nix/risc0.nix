dfa73ade48bdc48f0d548f79dd83e4a986dde702
Jakub Konka <jakub@vlayer.xyz>
2025-01-21 12:36:21 +0100

    nix: update to risc0 1.2.1 (#1577)

--- nix/risc0.nix
+++ nix/risc0.nix
@@ -2,6 +2,12 @@
 
 let
   releases = {
+    "1.2.1" = {
+      "aarch64-darwin" = {
+        arch = "aarch64-apple-darwin";
+        hash = "sha256-q3+mhhOO8E0z8sDNkHowMFXN/87fcenPOl/3ML9UILg=";
+      };
+    };
     "1.2.0" = {
       "aarch64-darwin" = {
         arch = "aarch64-apple-darwin";
@@ -9,7 +15,8 @@ let
       };
     };
   };
-  artifacts = system: version:
+  artifacts =
+    system: version:
     let
       inherit (releases.${version}.${system}) arch hash;
     in
@@ -18,37 +25,41 @@ let
       url = "https://github.com/risc0/risc0/releases/download/v${version}/cargo-risczero-${arch}.tgz";
     };
 
-  toolchain = version: pkgs.stdenv.mkDerivation {
-    name = "cargo-risczero-v${version}";
-    version = version;
-    src = artifacts system version;
-    sourceRoot = ".";
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r ./cargo-risczero $out/bin/
-    '';
-  };
-  r0vm = version: pkgs.stdenv.mkDerivation {
-    name = "risc0-r0vm-v${version}";
-    version = version;
-    src = artifacts system version;
-    sourceRoot = ".";
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r ./r0vm $out/bin/
-    '';
-  };
+  toolchain =
+    version:
+    pkgs.stdenv.mkDerivation {
+      name = "cargo-risczero-v${version}";
+      version = version;
+      src = artifacts system version;
+      sourceRoot = ".";
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -r ./cargo-risczero $out/bin/
+      '';
+    };
+  r0vm =
+    version:
+    pkgs.stdenv.mkDerivation {
+      name = "risc0-r0vm-v${version}";
+      version = version;
+      src = artifacts system version;
+      sourceRoot = ".";
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -r ./r0vm $out/bin/
+      '';
+    };
   package = version: {
-    ${version} =
-      {
-        toolchain = toolchain version;
-        r0vm = r0vm version;
-      };
+    ${version} = {
+      toolchain = toolchain version;
+      r0vm = r0vm version;
+    };
   };
 in
 rec {
   risc0 = {
     default = risc0."1.2.1";
+    inherit (package "1.2.1") "1.2.1";
     inherit (package "1.2.0") "1.2.0";
   };
 }
