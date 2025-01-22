798efa1c649227e104335b05a49e56837d478190
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-22 19:04:31 +0100

    feat: implement adding and revoking support for ImageID

--- contracts/vlayer/test/proof_verifier/ImageIdRepository.t.sol
+++ contracts/vlayer/test/proof_verifier/ImageIdRepository.t.sol
@@ -6,6 +6,8 @@ import {Test, console} from "forge-std-1.9.4/src/Test.sol";
 import {ImageID} from "../../src/ImageID.sol";
 import {ImageIdRepository} from "../../src/proof_verifier/ImageIdRepository.sol";
 
+bytes32 constant MOCK_IMAGE_ID = bytes32(0x1111111111111111111111111111111111111111111111111111111111111111);
+
 contract ImageIdRepository_addSupport_Tests is Test {
     ImageIdRepository public repository;
 
@@ -14,15 +16,72 @@ contract ImageIdRepository_addSupport_Tests is Test {
     }
 
     function test_byDefaultCurrentImageIdIsNotSupported() public view {
-        assertTrue(!repository.isSupported(bytes32(0x1111111111111111111111111111111111111111111111111111111111111111)));
+        assertTrue(!repository.isSupported(MOCK_IMAGE_ID));
         assertTrue(!repository.isSupported(ImageID.RISC0_CALL_GUEST_ID));
     }
 
     function test_onceAddedSupportImageIsSupported() public {
-        assertTrue(!repository.isSupported(bytes32(0x1111111111111111111111111111111111111111111111111111111111111111)));
+        assertTrue(!repository.isSupported(MOCK_IMAGE_ID));
 
-        repository.addSupport(bytes32(0x1111111111111111111111111111111111111111111111111111111111111111));
+        repository.addSupport(MOCK_IMAGE_ID);
 
-        assertTrue(repository.isSupported(bytes32(0x1111111111111111111111111111111111111111111111111111111111111111)));
+        assertTrue(repository.isSupported(MOCK_IMAGE_ID));
+    }
+
+    function test_canSupportMultipleImageIds() public {
+        repository.addSupport(MOCK_IMAGE_ID);
+        repository.addSupport(bytes32(0x2222222222222222222222222222222222222222222222222222222222222222));
+
+        assertTrue(repository.isSupported(MOCK_IMAGE_ID));
+        assertTrue(repository.isSupported(bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)));
+    }
+
+    function test_cannotAddSupportForAnAlreadySupportedImageId() public {
+        repository.addSupport(MOCK_IMAGE_ID);
+
+        vm.expectRevert("ImageID is already supported");
+        repository.addSupport(MOCK_IMAGE_ID);
+    }
+
+    function test_emitsNewImageIdEvent() public {
+        vm.expectEmit();
+        emit ImageIdRepository.AddedImageIDSupport(MOCK_IMAGE_ID);
+        repository.addSupport(MOCK_IMAGE_ID);
+    }
+}
+
+contract ImageIdRepository_revokeSupport_Tests is Test {
+    ImageIdRepository public repository;
+
+    function setUp() public {
+        repository = new ImageIdRepository();
+    }
+
+    function test_canRevokeSupport() public {
+        repository.addSupport(MOCK_IMAGE_ID);
+
+        repository.revokeSupport(MOCK_IMAGE_ID);
+        assertTrue(!repository.isSupported(MOCK_IMAGE_ID));
+    }
+
+    function test_failsToRevokeForUnsupportedImageId() public {
+        vm.expectRevert("Cannot revoke unsupported ImageID");
+        repository.revokeSupport(MOCK_IMAGE_ID);
+    }
+
+    function test_emitsNewImageIdEvent() public {
+        repository.addSupport(MOCK_IMAGE_ID);
+
+        vm.expectEmit();
+        emit ImageIdRepository.RevokedImageIDSupport(MOCK_IMAGE_ID);
+        repository.revokeSupport(MOCK_IMAGE_ID);
+    }
+
+    function test_canAddSupportAgainOnceRevoked() public {
+        repository.addSupport(MOCK_IMAGE_ID);
+        repository.revokeSupport(MOCK_IMAGE_ID);
+
+        repository.addSupport(MOCK_IMAGE_ID);
+        assertTrue(repository.isSupported(MOCK_IMAGE_ID));
     }
 }
