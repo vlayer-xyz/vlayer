3e28f71ba6d36daad1d5decbafdf4b9689355d66
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-23 15:19:20 +0100

    ImageIdRepository: implement adding and revoking support for ImageID (#1605)
    feat: implement adding and revoking support for ImageID

--- contracts/vlayer/src/proof_verifier/ImageIdRepository.sol
+++ contracts/vlayer/src/proof_verifier/ImageIdRepository.sol
@@ -2,10 +2,23 @@
 pragma solidity ^0.8.21;
 
 contract ImageIdRepository {
+    event AddedImageIDSupport(bytes32 imageID);
+    event RevokedImageIDSupport(bytes32 imageID);
+
     mapping(bytes32 => bool) private imageIds;
 
     function addSupport(bytes32 imageId) external {
+        require(!isSupported(imageId), "ImageID is already supported");
+
         imageIds[imageId] = true;
+        emit AddedImageIDSupport(imageId);
+    }
+
+    function revokeSupport(bytes32 imageId) external {
+        require(isSupported(imageId), "Cannot revoke unsupported ImageID");
+
+        imageIds[imageId] = false;
+        emit RevokedImageIDSupport(imageId);
     }
 
     function isSupported(bytes32 imageId) public view returns (bool) {
