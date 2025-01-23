96b1965368661889f63ab4f47dfc362070fbf971
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-23 10:38:52 +0100

    feat: add role-based access control

--- contracts/vlayer/src/proof_verifier/ImageIdRepository.sol
+++ contracts/vlayer/src/proof_verifier/ImageIdRepository.sol
@@ -1,20 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-contract ImageIdRepository {
+import {AccessControlEnumerable} from "openzeppelin-contracts/access/extensions/AccessControlEnumerable.sol";
+
+contract ImageIdRepository is AccessControlEnumerable {
     event AddedImageIDSupport(bytes32 imageID);
     event RevokedImageIDSupport(bytes32 imageID);
 
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     mapping(bytes32 => bool) private imageIds;
 
-    function addSupport(bytes32 imageId) external {
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(OWNER_ROLE, msg.sender);
+    }
+
+    function transferAdminRole(address newAdmin) public {
+        grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function transferOwnership(address newOwner) public {
+        address owner = getRoleMember(OWNER_ROLE, 0);
+        revokeRole(OWNER_ROLE, owner);
+        grantRole(OWNER_ROLE, newOwner);
+    }
+
+    function addSupport(bytes32 imageId) external onlyRole(OWNER_ROLE) {
         require(!isSupported(imageId), "ImageID is already supported");
 
         imageIds[imageId] = true;
         emit AddedImageIDSupport(imageId);
     }
 
-    function revokeSupport(bytes32 imageId) external {
+    function revokeSupport(bytes32 imageId) external onlyRole(OWNER_ROLE) {
         require(isSupported(imageId), "Cannot revoke unsupported ImageID");
 
         imageIds[imageId] = false;
