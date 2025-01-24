4f3ed099e48f73eebb03256d95bcbe4ae806b8c0
Wiktor Gromniak <wiktor@vlayer.xyz>
2025-01-24 12:12:36 +0100

    Added trivial playwright tests to simple-web-proof example.

---
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+test("displays button", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByRole("button", { name: "Start" })).toBeVisible({ timeout: 60000 });
+});
