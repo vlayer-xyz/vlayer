268eba6c7ac222500f4a0bcc67830bfff42e7e33
Arkadiusz Konior <akonior@gmail.com>
2025-01-24 09:24:58 +0100

    Add support for empty value redaction (#1604)
    * Add failing test
    
    * Fix empty array and empty object
    
    * Remove println

--- rust/web_proof/src/utils/json.rs
+++ rust/web_proof/src/utils/json.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use serde_json::Value;
 
 use crate::{errors::ParsingError, redaction::RedactedTranscriptNameValue};
@@ -14,16 +16,26 @@ pub(crate) fn json_to_redacted_transcript(
 
 fn flatten_json(json: &Value, prefix: String) -> Vec<(String, String)> {
     match json {
-        Value::Object(obj) => obj
-            .iter()
-            .flat_map(|(key, value)| flatten_json(value, format!("{prefix}.{key}")))
-            .collect(),
+        Value::Object(obj) => {
+            if obj.is_empty() {
+                vec![(prefix, String::new())]
+            } else {
+                obj.iter()
+                    .flat_map(|(key, value)| flatten_json(value, format!("{prefix}.{key}")))
+                    .collect()
+            }
+        }
 
-        Value::Array(arr) => arr
-            .iter()
-            .enumerate()
-            .flat_map(|(index, value)| flatten_json(value, format!("{prefix}[{index}]")))
-            .collect(),
+        Value::Array(arr) => {
+            if arr.is_empty() {
+                vec![(prefix, String::new())]
+            } else {
+                arr.iter()
+                    .enumerate()
+                    .flat_map(|(index, value)| flatten_json(value, format!("{prefix}[{index}]")))
+                    .collect()
+            }
+        }
 
         Value::String(s) => vec![(prefix, s.to_string())],
 
@@ -45,7 +57,9 @@ mod test {
             "object": {
                 "nested_string": "Nested",
                 "nested_number": 99.99
-            }
+            },
+            "empty_object": {},
+            "empty_array": []
         }"#;
         let result = json_to_redacted_transcript(json_str).unwrap();
         let mut result = result.iter().map(ToString::to_string).collect::<Vec<_>>();
@@ -57,6 +71,8 @@ mod test {
             "$.array[2]: 3",
             "$.array[3].array_string: four",
             "$.boolean: true",
+            "$.empty_array: ",
+            "$.empty_object: ",
             "$.number: 42",
             "$.object.nested_number: 99.99",
             "$.object.nested_string: Nested",
