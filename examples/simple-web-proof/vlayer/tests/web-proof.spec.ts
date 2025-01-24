4a7b683dcf5302f1f4144cc832e95f765d3c23ef
Wiktor Gromniak <wiktor@vlayer.xyz>
2025-01-24 12:34:24 +0100

    Lint fix.

--- examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("displays button", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("button", { name: "Start" })).toBeVisible({ 
-    timeout: 60000
+  await expect(page.getByRole("button", { name: "Start" })).toBeVisible({
+    timeout: 60000,
   });
 });
