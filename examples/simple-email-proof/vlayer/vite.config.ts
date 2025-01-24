814ffbff3221df0df24f3d8f56cef6c396ce26d9
Github Runner <grzegorz.pociejewski@gmail.com>
2025-01-24 10:08:43 +0100

    Use srv version of sdk in gas-benchmarks

--- examples/simple-email-proof/vlayer/vite.config.ts
+++ examples/simple-email-proof/vlayer/vite.config.ts
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve:
-    process.env.VLAYER_ENV === "dev" ? { conditions: ["development"] } : {},
 });
