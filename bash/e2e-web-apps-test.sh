fedc416d2cb1206b5a076527035110487ba314cd
Wiktor Gromniak <wiktor@vlayer.xyz>
2025-01-24 14:11:09 +0100

    Enable playwright example tests on testnet.

--- bash/e2e-web-apps-test.sh
+++ bash/e2e-web-apps-test.sh
@@ -10,25 +10,19 @@ PROVING_MODE=${PROVING_MODE:-dev}
 echo Generating typescript bidings ...
 ${VLAYER_HOME}/bash/build-ts-types.sh >/dev/null
 
-echo Running services...
-source ${VLAYER_HOME}/bash/run-services.sh
-
 for example in $(get_examples); do
-  echo "make snapshot of anvil"
-  ANVIL_SNAPSHOT_ID=$(cast rpc evm_snapshot)
+
+  echo Running services...
+  source ${VLAYER_HOME}/bash/run-services.sh
 
   echo "::group::Running tests of: ${example}"
   cd "$VLAYER_HOME/examples/$example"
-  forge soldeer install
-  forge clean
   forge build
 
   cd vlayer
   bun install --frozen-lockfile
   bun run test:"${VLAYER_ENV}"
 
-  echo "revert anvil to initial state"
-  cast rpc evm_revert "${ANVIL_SNAPSHOT_ID}"
-  echo '::endgroup::'
+  cleanup
 done
 
