c526d6878cb099a99da4cd3238c4bf93337f87d7
wgromniak2 <wiktor@vlayer.xyz>
2025-01-24 13:26:54 +0100

    Add trivial playwright tests to simple-web-proof example. (#1617)
    * Added trivial playwright tests to simple-web-proof example.
    
    * Lint fix.
    
    * Lint fix.

---
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("displays button", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByRole("button", { name: "Start" })).toBeVisible({
+    timeout: 60000,
+  });
+});
