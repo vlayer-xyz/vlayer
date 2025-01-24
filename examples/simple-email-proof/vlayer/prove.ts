5aca421c3ef9da790e078711db0662bd4f588867
Ivan Rukhavets <ivanruch@gmail.com>
2025-01-24 17:13:48 +0100

    Update examples/simple-email-proof/vlayer/prove.ts

--- examples/simple-email-proof/vlayer/prove.ts
+++ examples/simple-email-proof/vlayer/prove.ts
@@ -31,7 +31,6 @@ console.log("Proving...");
 const vlayer = createVlayerClient({
   url: proverUrl,
 });
-console.log(await preverifyEmail(mimeEmail, "http://127.0.0.1:3002/dns-query"));
 const hash = await vlayer.prove({
   address: prover,
   proverAbi: proverSpec.abi,
