5a52990299f6a6ff0f74cec4f4d61dfd34fb455b
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2025-01-27 10:48:41 +0100

    Use local version of sdk in development.  (#1614)
    * use local version of sdk in development
    
    * Use local version of sdk in gas-benchmarks

--- examples/simple-web-proof/vlayer/vite.config.ts
+++ examples/simple-web-proof/vlayer/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), react()],
   build: {
     target: "esnext",
   },
+  resolve:
+    process.env.VLAYER_ENV === "dev" ? { conditions: ["development"] } : {},
 });
