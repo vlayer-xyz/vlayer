1091eae8e4def60f98d6de4cd88863b65ba612a0
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-27 21:33:41 +0100

    Contracts stable deployment scripts (#1644)
    * feat: add verifier deployer script
    
    * feat: add repository image id admin scripts

---
+++ contracts/vlayer/script/ImageIdAdministration.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {console, Script} from "forge-std-1.9.4/src/Script.sol";
+
+import {ImageID} from "../src/ImageID.sol";
+import {IImageIdRepository} from "../src/Repository.sol";
+import {ChainIdLibrary} from "../src/proof_verifier/ChainId.sol";
+
+contract AddSupportForCurrentImageId is Script {
+    function run(IImageIdRepository repository) external {
+        AddImageIdSupport addImageIdSupportScript = new AddImageIdSupport();
+        addImageIdSupportScript.run(repository, ImageID.RISC0_CALL_GUEST_ID);
+    }
+}
+
+contract AddImageIdSupport is Script {
+    function run(IImageIdRepository repository, bytes32 imageId) external {
+        uint256 ownerPrivateKey = vm.envUint("REPOSITORY_OWNER_PRIVATE_KEY");
+
+        console.log("REPOSITORY_ADDRESS=%s", address(repository));
+        console.log("IMAGE_ID=");
+        console.logBytes32(imageId);
+
+        vm.startBroadcast(ownerPrivateKey);
+        repository.addImageIdSupport(imageId);
+        vm.stopBroadcast();
+    }
+}
+
+contract RevokeImageIdSupport is Script {
+    function run(IImageIdRepository repository, bytes32 imageId) external {
+        uint256 ownerPrivateKey = vm.envUint("REPOSITORY_OWNER_PRIVATE_KEY");
+
+        console.log("REPOSITORY_ADDRESS=%s", address(repository));
+        console.log("IMAGE_ID=");
+        console.logBytes32(imageId);
+
+        vm.startBroadcast(ownerPrivateKey);
+        repository.revokeImageIdSupport(imageId);
+        vm.stopBroadcast();
+    }
+}
