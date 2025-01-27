bc19cfed64d3b870eb6cd159eaea62da5dda3d06
vanruch <ivanruch@gmail.com>
2025-01-27 16:07:51 +0100

    undo fixtures format

--- contracts/fixtures/src/vlayer/ArithOpProver.sol
+++ contracts/fixtures/src/vlayer/ArithOpProver.sol
@@ -5,27 +5,42 @@ import {Proof} from "vlayer/Proof.sol";
 import {Prover} from "vlayer/Prover.sol";
 
 contract ArithOpProver is Prover {
-    function add(uint256 lhs, uint256 rhs) public pure returns (Proof memory, uint256) {
+    function add(
+        uint256 lhs,
+        uint256 rhs
+    ) public pure returns (Proof memory, uint256) {
         uint256 res = lhs + rhs;
         return (proof(), res);
     }
 
-    function mul(uint256 lhs, uint256 rhs) public pure returns (Proof memory, uint256) {
+    function mul(
+        uint256 lhs,
+        uint256 rhs
+    ) public pure returns (Proof memory, uint256) {
         uint256 res = lhs * rhs;
         return (proof(), res);
     }
 
-    function sub(int256 lhs, int256 rhs) public pure returns (Proof memory, int256) {
+    function sub(
+        int256 lhs,
+        int256 rhs
+    ) public pure returns (Proof memory, int256) {
         int256 res = lhs - rhs;
         return (proof(), res);
     }
 
-    function div(uint256 lhs, uint256 rhs) public pure returns (Proof memory, uint256) {
+    function div(
+        uint256 lhs,
+        uint256 rhs
+    ) public pure returns (Proof memory, uint256) {
         uint256 res = lhs / rhs;
         return (proof(), res);
     }
 
-    function sdiv(int256 lhs, int256 rhs) public pure returns (Proof memory, int256) {
+    function sdiv(
+        int256 lhs,
+        int256 rhs
+    ) public pure returns (Proof memory, int256) {
         int256 res = lhs / rhs;
         return (proof(), res);
     }
