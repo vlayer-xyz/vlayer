4cb501bdf0e90c33d5a4e7c0703ce4dfe740b49f
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2025-01-28 15:03:22 +0100

    Revert Risc0 v1.2.1 to v1.2.0

--- rust/guest_wrapper/artifacts/chain_guest/CHANGELOG.md
+++ rust/guest_wrapper/artifacts/chain_guest/CHANGELOG.md
@@ -1,6 +1,6 @@
 ## Chain guest ELF ID changelog
   * `e6fbd753afc2d27cc55b4f6a47cea4f2f0bf6ce8a900a22800521e8d5a09d2ea` – Initial version
   * `e8175d54bac30389a87a8dd39ac43fe22f621066d4e657eee10a30328bdfb05f` – Downgrade revm from 19.2.0 to 19.0.0 [#1557](https://github.com/vlayer-xyz/vlayer/pull/1557)
-  * `ed5b4117fe05afa4f5136dbbac0e3a6c0a51394715e5f7fec64cf2dfd6319fa1` – Risc0 v1.2.1 (with accelerated keccaks) [#1551](https://github.com/vlayer-xyz/vlayer/pull/1551)
   * `2dfd3adf61327dc1ad47a637f5771496458ad5dc9a10de41b05cb348fa5d2eee` – Use old guest IDs in chain guest [#1584](https://github.com/vlayer-xyz/vlayer/pull/1584)
   * `acbedbbeb664cdc1c58abd3d1c6c5a6e66d7cd206d752e3ddb7bf133f5f04ce7` – Use old guest IDs in chain host [#1603](https://github.com/vlayer-xyz/vlayer/pull/1603)
+  * `d35081d72b7d4875780f69da408127523669dfb2f17a05c23dc2506b74a92c83` – Revert Risc0 v1.2.1 to v1.2.0
--- rust/zkvm-benchmarks/benchmarks/src/benchmarks/block_trie.rs
+++ rust/zkvm-benchmarks/benchmarks/src/benchmarks/block_trie.rs
@@ -89,27 +89,27 @@ pub fn benchmarks() -> Vec<Benchmark> {
         Benchmark::new(
             "append_single",
             with_fixture!(append_single::fixture(), append_single::run),
-            16_673,
+            113_026,
         ),
         Benchmark::new(
             "prepend_single",
             with_fixture!(prepend_single::fixture(), prepend_single::run),
-            16_677,
+            112_947,
         ),
         Benchmark::new(
             "append_10",
             with_fixture!(append_batch::fixture(10), append_batch::run),
-            181_543,
+            1_145_303,
         ),
         Benchmark::new(
             "prepend_10",
             with_fixture!(prepend_batch::fixture(10), prepend_batch::run),
-            195_716,
+            1_159_542,
         ),
         Benchmark::new(
             "prepend_20",
             with_fixture!(prepend_batch::fixture(20), prepend_batch::run),
-            397_352,
+            2_325_021,
         ),
         Benchmark::new("hash_100", with_fixture!(hash_slow::fixture(100), hash_slow::run), 348_705),
     ]
