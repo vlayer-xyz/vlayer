c1fc2e60b677b4d09c6a1f162fbc3130ab378796
Ivan Rukhavets <ivanruch@gmail.com>
2025-01-28 10:01:27 +0100

    Introduce notary repo (#1648)

--- contracts/vlayer/script/PushImageId.s.sol
+++ contracts/vlayer/script/PushImageId.s.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.21;
 
 import {ImageID} from "../src/ImageID.sol";
 import {Script, console} from "forge-std-1.9.4/src/Script.sol";
-import {Repository} from "../src/Repository.sol";
+import {IImageIdRepository} from "../src/Repository.sol";
 
 contract PushImageId is Script {
     function run() external {
         bytes32 imageId = ImageID.RISC0_CALL_GUEST_ID;
         address repositoryAddress = vm.envAddress("REPOSITORY_CONTRACT_ADDRESS");
         require(repositoryAddress != address(0), "IMAGE_ID_REPOSITORY not set");
-        Repository repository = Repository(repositoryAddress);
+        IImageIdRepository repository = IImageIdRepository(repositoryAddress);
 
         uint256 ownerPrivateKey = vm.envUint("REPOSITORY_OWNER_PRIVATE_KEY");
         vm.startBroadcast(ownerPrivateKey);
