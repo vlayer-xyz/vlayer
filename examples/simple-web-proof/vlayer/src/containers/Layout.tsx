8dc423e62c2f48bb92d50cebd8e1a0d7f12c7837
pociej <grzegorz.pociejewski@gmail.com>
2025-01-28 17:12:56 +0100

    Remove extention check component temporary

--- examples/simple-web-proof/vlayer/src/containers/Layout.tsx
+++ examples/simple-web-proof/vlayer/src/containers/Layout.tsx
@@ -2,12 +2,21 @@
 import { Outlet } from "react-router";
 import { Modal } from "../components/Modal";
 import { ProgressBar } from "../components/ProgressBar";
+import { useState } from "react";
+import { AnimatePresence } from "motion/react";
 
 export const Layout = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const closeModal = () => setIsModalOpen(false);
+  const showModal = () => setIsModalOpen(true);
   return (
-    <Modal>
-      <ProgressBar />
-      <Outlet /> {/* Content specific to the route will be rendered here */}
-    </Modal>
+    <AnimatePresence>
+      {isModalOpen && (
+        <Modal>
+          <ProgressBar />
+          <Outlet context={{ isModalOpen, closeModal, showModal }} />
+        </Modal>
+      )}
+    </AnimatePresence>
   );
 };
--- examples/simple-web-proof/vlayer/src/containers/WalletContainer.tsx
+++ examples/simple-web-proof/vlayer/src/containers/WalletContainer.tsx
@@ -1,9 +1,20 @@
 import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
 import { ConnectWallet } from "../components/ConnectWallet";
+import { useOutletContext } from "react-router";
 
 export const WalletContainer = () => {
   const { open } = useAppKit();
   const { isConnected } = useAppKitAccount();
-
-  return <ConnectWallet isWalletConnected={isConnected} openWallet={open} />;
+  const { closeModal, showModal } = useOutletContext<{
+    closeModal: () => void;
+    showModal: () => void;
+  }>();
+  return (
+    <ConnectWallet
+      isWalletConnected={isConnected}
+      openWallet={open}
+      showModal={showModal}
+      closeModal={closeModal}
+    />
+  );
 };
