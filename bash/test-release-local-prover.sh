5adc2d75f82daada5c6e0c9e3c49e60b8961d7fa
Wiktor Gromniak <wiktor@vlayer.xyz>
2025-01-28 15:00:49 +0100

    Turn off playwright in devnet post-release test temporarily.

--- bash/test-release-local-prover.sh
+++ bash/test-release-local-prover.sh
@@ -52,8 +52,4 @@ for example in $(get_examples); do
     echo "::group::vlayer run prove.ts: ${example}"
     run_prover_script
     echo '::endgroup::'
-
-    echo "::group::vlayer run Playwright test: ${example}"
-    run_playwright_tests
-    echo '::endgroup::'
 done
