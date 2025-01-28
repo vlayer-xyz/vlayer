d95ef26a164ead43f5fef060d37f7363b21b6a59
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-28 21:31:58 +0100

    feat: add push-image-id bash script

---
+++ bash/verifiers_management/push-image-id.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+set -ueo pipefail
+
+SCRIPT="ImageIdAdministration.s.sol"
+CONTARCT="AddSupportForCurrentImageId"
+SCRIPT_INVOCATION="${CONTRACTS_DIR}/script/${SCRIPT}:${CONTARCT}"
+REPOSITORY_ADDRESS=$(jq -r <"${CONTRACTS_DIR}/deployed_contracts.json" '.contracts[] | select(.contractName == "Repository") | .contractAddress')
+
+cd "${CONTRACTS_DIR}"
+
+cleanup
+REPOSITORY_CONTRACT_ADDRESS=${REPOSITORY_ADDRESS} run_forge_script "${SCRIPT_INVOCATION}"
