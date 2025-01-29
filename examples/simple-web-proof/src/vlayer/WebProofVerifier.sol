abe8a090105914d8f8e1a23db0150ca79e12e812
Artur Chmaro <artur@vlayer.xyz>
2025-01-29 14:03:31 +0100

    Add meta for Web Proof NFT

--- examples/simple-web-proof/src/vlayer/WebProofVerifier.sol
+++ examples/simple-web-proof/src/vlayer/WebProofVerifier.sol
@@ -11,6 +11,8 @@ import {ERC721} from "@openzeppelin-contracts-5.0.1/token/ERC721/ERC721.sol";
 contract WebProofVerifier is Verifier, ERC721 {
     address public prover;
 
+    mapping(uint256 => string) public tokenIdToMetadataUri;
+
     constructor(address _prover) ERC721("TwitterNFT", "TNFT") {
         prover = _prover;
     }
@@ -23,5 +25,13 @@ contract WebProofVerifier is Verifier, ERC721 {
         require(_ownerOf(tokenId) == address(0), "User has already minted a TwitterNFT");
 
         _safeMint(account, tokenId);
+        tokenIdToMetadataUri[tokenId] = string.concat(
+            "https://faucet.vlayer.xyz/api/xBadgeMeta?handle=",
+            username
+        );
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return tokenIdToMetadataUri[tokenId];
     }
 }
