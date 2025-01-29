f6470ca1477eae3b2684956becd911d6d48966c1
Piotr Żelazko <piotr@vlayer.xyz>
2025-01-29 11:58:04 +0100

    Update image id on release (#1676)
    * chore: rename owner key variable to be aligned with other places
    
    * chore: extract common functions in bash forge scripts
    
    * feat: add push-image-id bash script
    
    * feat: use bash script in gh action to push image id
    
    * feat: enable pushing image id at release

---
+++ bash/verifiers_management/common.sh
@@ -0,0 +1,40 @@
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+CONTRACTS_DIR="${VLAYER_HOME}/contracts/vlayer"
+
+DRY_RUN=${DRY_RUN:-true}
+NETWORKS=( "sepolia" "base-sepolia" "optimism-sepolia" )
+
+echo "DRY_RUN: ${DRY_RUN}"
+echo "NETWORKS: ${NETWORKS[@]}"
+
+function broadcast_flag(){
+  if [[ "${DRY_RUN}" == "false" ]]; then
+      echo "--broadcast"
+  fi
+}
+
+function cleanup(){
+  echo "Cleaning up artifacts..."
+  if [[ -d "./broadcast" ]] ; then 
+    rm -r ./broadcast ./cache
+  fi
+  forge clean
+
+}
+
+function build_contracts(){
+  echo "Building contracts..."
+  forge build 
+}
+
+function run_forge_script(){
+  local script_invocation=$1
+
+  for NETWORK in "${NETWORKS[@]}" ; do
+    echo "Running script on ${NETWORK}"
+    forge script "${script_invocation}" \
+      --rpc-url "${NETWORK}" \
+      --slow \
+      "$(broadcast_flag)" 
+  done
+}
--- bash/verifiers_management/deploy-verifiers.sh
+++ bash/verifiers_management/deploy-verifiers.sh
@@ -1,34 +1,15 @@
 #!/usr/bin/env bash
 
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
 set -ueo pipefail
 
-VLAYER_HOME=$(git rev-parse --show-toplevel)
-CONTRACTS_DIR="${VLAYER_HOME}/contracts/vlayer"
 DEPLOYER_SCRIPT="VerifierDeployer.s.sol"
 DEPLOYER_CONTARCT="VerifierDeployer"
 DEPLOYER_SCRIPT_INVOCATION="${CONTRACTS_DIR}/script/${DEPLOYER_SCRIPT}:${DEPLOYER_CONTARCT}"
 
-DRY_RUN=${DRY_RUN:-true}
-
-NETWORKS=( "sepolia" "base-sepolia" "optimism-sepolia" )
-echo "DRY_RUN: ${DRY_RUN}"
-echo "NETWORKS: ${NETWORKS[@]}"
-
 function deploy_contracts(){
-  local dry_run=$1
-  local broadcast_flag=""
-  if [[ "${dry_run}" == "false" ]]; then
-      broadcast_flag="--broadcast"
-  fi
-
-  for NETWORK in "${NETWORKS[@]}" ; do
-    echo "Deploying to ${NETWORK}"
-    forge script "${DEPLOYER_SCRIPT_INVOCATION}" \
-      --chain "${NETWORK}" \
-      --rpc-url "${NETWORK}" \
-      --slow \
-      "${broadcast_flag}" 
-  done
+  run_forge_script "${DEPLOYER_SCRIPT_INVOCATION}" 
 }
 
 function verify_deployment_is_stable_across_networks(){
@@ -38,7 +19,7 @@ function verify_deployment_is_stable_across_networks(){
     if [[ "${stable_deployment}" != "${result}" ]] ; then
       echo "Deployments are not consistent ❌" >&2
       echo "--------------------------" >&2
-      echo "${stable_deployment}" | jq >&2
+      echo "${stable_deployment}" | jq  >&2
       echo "--------------------------" >&2
       echo "${result}" | jq >&2
       echo "--------------------------" >&2
@@ -48,19 +29,9 @@ function verify_deployment_is_stable_across_networks(){
   echo "Deployments has been verified ✅"
 }
 
-function cleanup(){
-  echo "Cleaning up artifacts..."
-  if [[ -d "./broadcast" ]] ; then 
-    rm -r ./broadcast ./cache
-  fi
-  forge clean
-
-  echo "Building contracts..."
-  forge build 
-}
-
 cd "${CONTRACTS_DIR}"
 
 cleanup
-deploy_contracts "${DRY_RUN}"
+build_contracts
+deploy_contracts
 verify_deployment_is_stable_across_networks
