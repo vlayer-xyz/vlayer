116214ff9fc14940ef6e6c9cb9944fca0934d24f
Leonid Logvinov <logvinov.leon@gmail.com>
2025-01-30 12:38:09 +0100

    Add test for get_destinations

--- rust/services/call/engine/src/verifier/tests/teleport.rs
+++ rust/services/call/engine/src/verifier/tests/teleport.rs
@@ -24,3 +24,46 @@ mod ensure_latest_teleport_location_is_confirmed {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod get_destinations {
+    use std::collections::HashMap;
+
+    use alloy_primitives::{BlockHash, BlockNumber, ChainId, B256};
+
+    use crate::{evm::env::BlocksByChain, verifier::teleport::get_destinations};
+
+    #[test]
+    fn success_no_teleport() {
+        let chain_id: ChainId = 1;
+        let block: BlockNumber = 1;
+        let blocks_by_chain: BlocksByChain =
+            HashMap::from([(chain_id, vec![(block, B256::ZERO)])]).into();
+        let start_exec_location = (chain_id, block).into();
+
+        let destinations: HashMap<ChainId, Vec<(BlockNumber, BlockHash)>> =
+            get_destinations(blocks_by_chain, start_exec_location).collect();
+
+        assert_eq!(destinations.len(), 0);
+    }
+
+    #[test]
+    fn success_teleport() {
+        let src_chain_id: ChainId = 1;
+        let src_block: BlockNumber = 1;
+        let dest_chain_id: ChainId = 10;
+        let dest_block: BlockNumber = 1;
+        let blocks_by_chain: BlocksByChain = HashMap::from([
+            (src_chain_id, vec![(src_block, B256::ZERO)]),
+            (dest_chain_id, vec![(dest_block, B256::ZERO)]),
+        ])
+        .into();
+        let start_exec_location = (src_chain_id, src_block).into();
+
+        let destinations: HashMap<ChainId, Vec<(BlockNumber, BlockHash)>> =
+            get_destinations(blocks_by_chain, start_exec_location).collect();
+
+        assert_eq!(destinations.len(), 1);
+        assert_eq!(destinations[&dest_chain_id], vec![(dest_block, B256::ZERO)]);
+    }
+}
