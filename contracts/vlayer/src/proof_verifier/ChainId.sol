b3c01ae9c5aa88e475e5504cf8bdbc39461b7c33
vanruch <ivanruch@gmail.com>
2025-02-03 09:36:56 +0100

    sort chain ids

--- contracts/vlayer/src/proof_verifier/ChainId.sol
+++ contracts/vlayer/src/proof_verifier/ChainId.sol
@@ -11,24 +11,24 @@ library ChainIdLibrary {
 
     function isTestnet() internal view returns (bool) {
         return block.chainid == 11155111 // Ethereum Sepolia
-            || block.chainid == 11155420 // Optimism Sepolia
+            || block.chainid == 300 // zkSync Sepolia
+            || block.chainid == 4801 // Worldchain Sepolia
+            || block.chainid == 59141 // Linea Sepolia
+            || block.chainid == 80002 // Polygon Amoy
             || block.chainid == 84532 // Base Sepolia
             || block.chainid == 421614 // Arbitrum Sepolia
-            || block.chainid == 80002 // Polygon Amoy
-            || block.chainid == 300 // zkSync Sepolia
-            || block.chainid == 59141 // Linea Sepolia
-            || block.chainid == 4801; // Worldchain Sepolia
+            || block.chainid == 11155420; // Optimism Sepolia
     }
 
     function isMainnet() internal view returns (bool) {
         return block.chainid == 1 // Ethereum
             || block.chainid == 10 // Optimism
+            || block.chainid == 137 // Polygon
+            || block.chainid == 324 // zkSync
+            || block.chainid == 480 // Worldchain
             || block.chainid == 8453 // Base
             || block.chainid == 42161 // Arbitrum One
             || block.chainid == 42170 // Arbitrum Nova
-            || block.chainid == 137 // Polygon
-            || block.chainid == 324 // zkSync
-            || block.chainid == 59144 // Linea
-            || block.chainid == 480; // Worldchain
+            || block.chainid == 59144; // Linea
     }
 }
