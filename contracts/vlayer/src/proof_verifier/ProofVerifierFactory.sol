800d29ba689b4df09c0d4b85de519b8fea499baf
Piotr Żelazko <piotr@vlayer.xyz>
2025-02-03 14:38:25 +0100

    chore: bump addresses

--- contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
+++ contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
@@ -28,11 +28,11 @@ library ProofVerifierFactory {
         returns (Repository, FakeProofVerifier, Groth16ProofVerifier, ProofVerifierRouter)
     {
         Repository repository = Repository(address(0xc9708B07ae9906b92FF19281Fd660FB19206a8fA));
-        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x67bAcaAf3B7E9B3e86B05479A9EE5b6ad611e34A));
+        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x1737776D145af312f24F51fFF1F0B22f2f7b9082));
         Groth16ProofVerifier groth16ProofVerifier =
             Groth16ProofVerifier(address(0x39599aC412c14F9635f5b5Bf8f4D4C1aeeCF6307));
         ProofVerifierRouter proofVerifierRouter =
-            ProofVerifierRouter(address(0x782860fFa50B0cd234453612A56cf699f77a0687));
+            ProofVerifierRouter(address(0xE3443ab33ba5C406056FE10715dA20c8619d4137));
 
         return (repository, fakeProofVerifier, groth16ProofVerifier, proofVerifierRouter);
     }
