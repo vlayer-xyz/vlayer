5ca23e91bb5e95568725db7478168481e6ee74a0
vanruch <ivanruch@gmail.com>
2025-02-04 14:02:51 +0100

    lint please stop

--- contracts/vlayer/src/testing/TestVerifierRouterDeployer.sol
+++ contracts/vlayer/src/testing/TestVerifierRouterDeployer.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import {ProofVerifierRouter, FakeProofVerifier, Groth16ProofVerifier, IImageIdRepository} from "../proof_verifier/ProofVerifierRouter.sol";
+import {ProofVerifierRouter, FakeProofVerifier, Groth16ProofVerifier} from "../proof_verifier/ProofVerifierRouter.sol";
 import {Repository} from "../Repository.sol";
 import {ImageID} from "../ImageID.sol";
 import {Verifier} from "../Verifier.sol";
 
 contract TestVerifierRouterDeployer {
     function swapProofVerifier(Verifier verifier) external {
-        Repository repository = new Repository(
-            msg.sender,
-            msg.sender
-        );
+        Repository repository = new Repository(msg.sender, msg.sender);
         repository.addImageIdSupport(ImageID.RISC0_CALL_GUEST_ID);
 
         verifier._setTestVerifier(
