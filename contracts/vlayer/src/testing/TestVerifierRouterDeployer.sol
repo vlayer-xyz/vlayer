792546f3da72217409232365ea50e1c974d15f0c
vanruch <ivanruch@gmail.com>
2025-02-04 13:58:34 +0100

    reduce size of deployer contract a bit

--- contracts/vlayer/src/testing/TestVerifierRouterDeployer.sol
+++ contracts/vlayer/src/testing/TestVerifierRouterDeployer.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import {ProofVerifierRouter, FakeProofVerifier, Groth16ProofVerifier} from "../proof_verifier/ProofVerifierRouter.sol";
+import {ProofVerifierRouter, FakeProofVerifier, Groth16ProofVerifier, IImageIdRepository} from "../proof_verifier/ProofVerifierRouter.sol";
 import {Repository} from "../Repository.sol";
 import {ImageID} from "../ImageID.sol";
 import {Verifier} from "../Verifier.sol";
 
 contract TestVerifierRouterDeployer {
-    bool public constant IS_SCRIPT = true;
-
     function swapProofVerifier(Verifier verifier) external {
-        Repository currentRepo = Repository(address(verifier.verifier().imageIdRepository()));
-
         Repository repository = new Repository(
-            currentRepo.getRoleMember(currentRepo.DEFAULT_ADMIN_ROLE(), 0),
-            currentRepo.getRoleMember(currentRepo.OWNER_ROLE(), 0)
+            msg.sender,
+            msg.sender
         );
         repository.addImageIdSupport(ImageID.RISC0_CALL_GUEST_ID);
 
