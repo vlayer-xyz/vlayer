c5ac2a97cccc9c2b13e27c434a47572451732fbf
Artur <artur@vlayer.xyz>
2025-02-04 12:42:05 +0100

    Add Sentry error catching to example apps (Web and Email) (#1720)
    * add sentry
    * submit sentry dsn in workflow

--- examples/simple-web-proof/vlayer/src/main.tsx
+++ examples/simple-web-proof/vlayer/src/main.tsx
@@ -4,6 +4,14 @@ import { Analytics } from "@vercel/analytics/react";
 import "./main.css";
 
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [],
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
