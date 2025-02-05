58a86043926544166a374409943923bbf114045c
Piotr Żelazko <piotr@vlayer.xyz>
2025-02-05 12:36:52 +0100

    test: ensure verifiers addresses are stable on main (#1674)
    * feat: deploy new testnet contracts
    
    * teset: run test contracts workflow on its changes
    
    * test: ensure verifiers addresses are stable
    
    * chore: skip address stability tests for now
    
    * feat: add stable deployment definition
    
    * chore: bump addresses

--- contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
+++ contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
@@ -21,4 +21,19 @@ library ProofVerifierFactory {
 
         revert InvalidChainId();
     }
+
+    function testnetStableDeployment()
+        internal
+        pure
+        returns (Repository, FakeProofVerifier, Groth16ProofVerifier, ProofVerifierRouter)
+    {
+        Repository repository = Repository(address(0xc9708B07ae9906b92FF19281Fd660FB19206a8fA));
+        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x1737776D145af312f24F51fFF1F0B22f2f7b9082));
+        Groth16ProofVerifier groth16ProofVerifier =
+            Groth16ProofVerifier(address(0x39599aC412c14F9635f5b5Bf8f4D4C1aeeCF6307));
+        ProofVerifierRouter proofVerifierRouter =
+            ProofVerifierRouter(address(0xE3443ab33ba5C406056FE10715dA20c8619d4137));
+
+        return (repository, fakeProofVerifier, groth16ProofVerifier, proofVerifierRouter);
+    }
 }
