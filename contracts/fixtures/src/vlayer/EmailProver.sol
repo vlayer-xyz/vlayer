f16af8751590dddd0d8e3a24697a32dde7d71724
vanruch <ivanruch@gmail.com>
2025-02-05 13:04:02 +0100

    undo weird import reorder

--- contracts/fixtures/src/vlayer/EmailProver.sol
+++ contracts/fixtures/src/vlayer/EmailProver.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
+import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
 
-import {AddressParser} from "./utils/AddressParser.sol";
 import {Proof} from "vlayer/Proof.sol";
 import {Prover} from "vlayer/Prover.sol";
 import {RegexLib} from "vlayer/Regex.sol";
-
-import {Strings} from "@openzeppelin-contracts-5.0.1/utils/Strings.sol";
-import {VerifiedEmail, UnverifiedEmail, EmailProofLib} from "vlayer/EmailProof.sol";
+import {EmailProofLib, VerifiedEmail, UnverifiedEmail} from "vlayer/EmailProof.sol";
 import {IVDnsKeyRepository} from "vlayer/Repository.sol";
+
+import {AddressParser} from "./utils/AddressParser.sol";
 
 interface IExample {
     function exampleFunction() external returns (uint256);
