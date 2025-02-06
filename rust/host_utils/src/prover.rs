46b5ac7c77ab2a3d428707afd6bd85cd10007971
Adam Wierzbicki <adam.wierzbicki@vlayer.xyz>
2025-02-06 14:10:53 +0100

    Log proving time (#1751)

--- rust/host_utils/src/prover.rs
+++ rust/host_utils/src/prover.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use derivative::Derivative;
 use risc0_zkvm::{
     BonsaiProver, ExecutorEnv, ExternalProver, ProveInfo, Prover as ProverTrait, ProverOpts,
@@ -29,43 +31,50 @@ impl Prover {
     }
 
     pub fn prove(&self, env: ExecutorEnv<'_>, elf: &[u8]) -> Result<ProveInfo> {
-        let prove_info = match self.mode {
+        let (prove_info, elapsed) = match self.mode {
             ProofMode::Groth16 => prove_bonsai(env, elf, &ProverOpts::groth16()),
             ProofMode::Succinct => prove_bonsai(env, elf, &ProverOpts::succinct()),
             ProofMode::Fake => prove_fake(env, elf),
         }?;
-        log_stats(&prove_info.stats);
+        log_stats(&prove_info.stats, &elapsed);
         Ok(prove_info)
     }
 }
 
-fn log_stats(stats: &SessionStats) {
+fn log_stats(stats: &SessionStats, elapsed: &Duration) {
     let SessionStats {
         total_cycles,
         user_cycles,
         segments,
         ..
     } = stats;
-    info!("Prover stats. Segments: {segments}, cycles: {total_cycles}, user cycles: {user_cycles}");
+    let elapsed_sec = elapsed.as_secs_f32();
+    info!("Prover stats. Segments: {segments}, cycles: {total_cycles}, user cycles: {user_cycles} elapsed: {elapsed_sec:.1} s");
 }
 
-fn prove_bonsai(env: ExecutorEnv<'_>, elf: &[u8], opts: &ProverOpts) -> Result<ProveInfo> {
+fn prove_bonsai(
+    env: ExecutorEnv<'_>,
+    elf: &[u8],
+    opts: &ProverOpts,
+) -> Result<(ProveInfo, Duration)> {
     info!("Proving with Bonsai");
     let bonsai_prover = BonsaiProver::new("vlayer: bonsai");
+    let start = tokio::time::Instant::now();
     // block_in_place is used to avoid tokio runtime panic, since bonsai_prover.prove_with_opts is blocking.
     // https://github.com/risc0/risc0/issues/2049
     let prove_info = tokio::task::block_in_place(|| bonsai_prover.prove_with_opts(env, elf, opts))?;
     info!("Proving with Bonsai done");
-    Ok(prove_info)
+    Ok((prove_info, start.elapsed()))
 }
 
-fn prove_fake(env: ExecutorEnv<'_>, elf: &[u8]) -> Result<ProveInfo> {
+fn prove_fake(env: ExecutorEnv<'_>, elf: &[u8]) -> Result<(ProveInfo, Duration)> {
     // Fake proof mode cannot be forced in any other way, since all risc0-zkvm modules, that could be reused here, are only crate-public.
     // Following is a temporary solution, that sets RISC0_DEV_MODE always to the same value, so race conditions are not a risk here.
     // Setting this env variable will be moved directly to ExternalProver, once it supports injection of config.
     unsafe {
         std::env::set_var("RISC0_DEV_MODE", "1");
     }
-
-    Ok(ExternalProver::new("vlayer: ipc", "r0vm").prove(env, elf)?)
+    let start = tokio::time::Instant::now();
+    let prove_info = ExternalProver::new("vlayer: ipc", "r0vm").prove(env, elf)?;
+    Ok((prove_info, start.elapsed()))
 }
