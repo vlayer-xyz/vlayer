974d120e7e3aa0d56760fe2752fe18146dcbda28
Piotr Żelazko <piotr@vlayer.xyz>
2025-02-06 17:33:30 +0100

    feat: return stable deployment for testnets (#1663)

--- contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
+++ contracts/vlayer/src/proof_verifier/ProofVerifierFactory.sol
@@ -3,15 +3,20 @@ pragma solidity ^0.8.21;
 
 import {ChainIdLibrary, InvalidChainId} from "./ChainId.sol";
 import {FakeProofVerifier} from "./FakeProofVerifier.sol";
+import {ProofVerifierRouter} from "./ProofVerifierRouter.sol";
 import {IProofVerifier} from "./IProofVerifier.sol";
 import {ImageID} from "../ImageID.sol";
 import {Repository} from "../Repository.sol";
+import {TestnetStableDeployment} from "../TestnetStableDeployment.sol";
 
 library ProofVerifierFactory {
     function produce() internal returns (IProofVerifier) {
         if (ChainIdLibrary.isMainnet()) {
             return IProofVerifier(address(0));
-        } else if (ChainIdLibrary.isDevnet() || ChainIdLibrary.isTestnet()) {
+        } else if (ChainIdLibrary.isTestnet()) {
+            (,, ProofVerifierRouter proofVerifierRouter) = TestnetStableDeployment.verifiers();
+            return proofVerifierRouter;
+        } else if (ChainIdLibrary.isDevnet()) {
             Repository repository = new Repository(address(this), address(this));
             repository.addImageIdSupport(ImageID.RISC0_CALL_GUEST_ID);
             return new FakeProofVerifier(repository);
