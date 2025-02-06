23434058b0da93cb96f2ca6e5e4b679558b54674
Wiktor Gromniak <wiktor@vlayer.xyz>
2025-02-06 08:58:51 +0100

    Get rid of confusing test failure message in simple-web-proof.

--- examples/simple-web-proof/vlayer/prove.ts
+++ examples/simple-web-proof/vlayer/prove.ts
@@ -139,7 +139,6 @@ async function testFailedProving() {
       "Preflight failed with error: Preflight error: TravelCallExecutor error: EVM transact error: Verification error: Presentation error: presentation error: attestation error caused by: attestation proof error: signature error caused by: signature verification failed: invalid secp256k1 signature",
       `Error with wrong message returned: ${error.message}`,
     );
-    console.log("Proving failed as expected with message:", error.message);
   }
 }
 
