ed6d8983db81a712d7f60cd5f1ca0e57b648ff9e
Jakub Konka <jakub@vlayer.xyz>
2025-02-11 12:21:03 +0100

    call_server: fix parsing --proof fake

--- rust/server_utils/src/proof_mode.rs
+++ rust/server_utils/src/proof_mode.rs
@@ -5,6 +5,7 @@ use strum::{Display, EnumString};
 #[derive(
     Debug, Clone, Deserialize, Serialize, Copy, PartialEq, Eq, Display, EnumString, Default,
 )]
+#[strum(ascii_case_insensitive)]
 pub enum ProofMode {
     #[default]
     Fake,
