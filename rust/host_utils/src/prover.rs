4b7acb3c5ebbb26ff86930d4c94e1ab62e6d9653
Jakub Konka <jakub@vlayer.xyz>
2025-02-11 14:11:30 +0100

    Add link to an open issue with risc0

--- rust/host_utils/src/prover.rs
+++ rust/host_utils/src/prover.rs
@@ -58,6 +58,7 @@ pub fn set_risc0_dev_mode() {
     // Fake proof mode cannot be forced in any other way, since all risc0-zkvm modules, that could be reused here, are only crate-public.
     // Following is a temporary solution, that sets RISC0_DEV_MODE always to the same value, so race conditions are not a risk here.
     // Setting this env variable will be moved directly to ExternalProver, once it supports injection of config.
+    // https://github.com/risc0/risc0/issues/2814
     unsafe {
         std::env::set_var("RISC0_DEV_MODE", "1");
     }
