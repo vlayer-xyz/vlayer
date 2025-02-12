3314bf5ec4f98c5c157a67be30f0c7e42680fc4c
Arkadiusz Konior <akonior@gmail.com>
2025-02-12 11:44:15 +0100

    Dump playwrite config

--- examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeAll(({ playwright }) => {
+  console.log("Resolved Playwright Config:", JSON.stringify(playwright._initializer, null, 2));
+});
+
 test("displays button", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByTestId("start-page-button")).toBeVisible({
