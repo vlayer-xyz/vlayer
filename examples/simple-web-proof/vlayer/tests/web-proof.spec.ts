6182d31100ab1257afd4c8451452b1f046ede3f2
Arkadiusz Konior <akonior@gmail.com>
2025-02-12 11:50:57 +0100

    console log page

--- examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeAll(({ playwright }) => {
-  console.log("Resolved Playwright Config:", JSON.stringify(playwright._initializer, null, 2));
-});
-
 test("displays button", async ({ page }) => {
+  console.log("Resolved Playwright Config:", JSON.stringify(page));
   await page.goto("/");
   await expect(page.getByTestId("start-page-button")).toBeVisible({
     timeout: 60000,
