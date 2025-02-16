c6e4270cadee2caf39d655ce93bc853bc6ecca31
pociej <grzegorz.pociejewski@gmail.com>
2025-02-16 19:58:14 +0100

    Clanup

--- contracts/vlayer/src/ImageID.sol
+++ contracts/vlayer/src/ImageID.sol
@@ -1,6 +1,1 @@
-pragma solidity ^0.8.20;
-
-library ImageID {
-    bytes32 public constant RISC0_CALL_GUEST_ID =
-        bytes32(0x0000000000000000000000000000000000000000000000000000000000000000);
-}
+../../../target/assets/ImageID.sol
