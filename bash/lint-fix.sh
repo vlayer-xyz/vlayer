26c29bd5f351669568b6d52cf08d65bc9b378072
Anna Jabłonowska <ania@vlayer.xyz>
2025-02-21 21:06:05 +0100

    Use prepare-to-lint  in lint-fix.sh

--- bash/lint-fix.sh
+++ bash/lint-fix.sh
@@ -4,23 +4,22 @@ set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 source "$(dirname "${BASH_SOURCE[0]}")/lib/examples.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/lib/prepare-to-lint.sh"
+
+prepare-to-lint
 
 for example in $(get_examples); do
   (
     cd "$VLAYER_HOME/examples/$example/vlayer"
-
-    bun install --frozen-lockfile --no-save --silent
     bun run --silent eslint . --fix
     bun run --silent lint:solidity -- --fix --noPrompt
   )
 done
 
 cd "${VLAYER_HOME}/packages"
-bun install --frozen-lockfile --no-save --silent
 bun run --silent lint:fix
 
 cd "${VLAYER_HOME}/contracts/vlayer"
-bun install --frozen-lockfile --no-save --silent
 bun run --silent lint:solidity -- --fix --noPrompt
 
 forge fmt "${VLAYER_HOME}/examples" -- --write
