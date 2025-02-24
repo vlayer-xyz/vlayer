84fe14bd13ddb1991595dc41d2ac68a709baa51d
Arkadiusz Konior <akonior@gmail.com>
2025-02-24 11:18:51 +0100

    Disable wallet connection in devnet tests (#1866)
    * Disable wallet connection in devnet tests
    
    * Remove data-testid; split test to steps

--- examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test("displays button", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByTestId("start-page-button")).toBeVisible({
-    timeout: 60000,
+  await test.step("Click Start", async () => {
+    await page.goto("/");
+
+    await expect(page.getByTestId("start-page-button")).toBeVisible();
+    await page.getByTestId("start-page-button").click();
+
+    await expect(page).toHaveURL("/connect-wallet");
+  });
+
+  await test.step("Click Start Proving", async () => {
+    await expect(page.getByText("Start Proving")).toBeVisible();
+    await page.getByText("Start Proving").click();
+
+    await expect(page).toHaveURL("/install-extension");
   });
 });
