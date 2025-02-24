ab7488a3e62c93669e5650e0a0b18491dffe4144
Arkadiusz Konior <akonior@gmail.com>
2025-02-24 13:46:47 +0100

    Mock twitter auth_token

--- examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
+++ examples/simple-web-proof/vlayer/tests/web-proof.spec.ts
@@ -1,6 +1,16 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "./config";
+import { sidePanel } from "./helpers";
 
-test("displays button", async ({ page }) => {
+test("web proof flow", async ({ page, context }) => {
+  await context.addCookies([
+    {
+      name: "auth_token",
+      value: "4469c0e31f1e2054cd3ac1a6e468e2fba0bfc41e",
+      path: "/",
+      domain: ".x.com",
+    },
+  ]);
+
   await test.step("Click Start", async () => {
     await page.goto("/");
 
@@ -14,6 +24,27 @@ test("displays button", async ({ page }) => {
     await expect(page.getByText("Start Proving")).toBeVisible();
     await page.getByText("Start Proving").click();
 
-    await expect(page).toHaveURL("/install-extension");
+    await expect(page).toHaveURL("/start-proving");
+  });
+
+  await test.step("Open extension", async () => {
+    await expect(page.getByText("Open Extension")).toBeVisible();
+    await page.getByText("Open Extension").click();
+
+    const extension = await sidePanel(context);
+    expect(extension).toBeDefined();
+  });
+
+  await test.step("Redirect to x.com", async () => {
+    const extension = await sidePanel(context);
+    expect(extension).toBeDefined();
+
+    const redirectButton = extension.getByTestId("start-page-button");
+    const [newPage] = await Promise.all([
+      context.waitForEvent("page"),
+      redirectButton.click(),
+    ]);
+
+    await expect(newPage).toHaveURL("https://x.com/home");
   });
 });
