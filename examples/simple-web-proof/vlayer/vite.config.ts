0c7beaa56b1930ef224bf620c5fbc362cf7bbc2c
Anna Jabłonowska <62891747+aajj999@users.noreply.github.com>
2025-03-03 14:14:37 +0100

    Add comment  explaining quick fix for web proof example (#1915)
    * Add comment  explaining quick fix for web proof example
    
    * Add isuue link and dependency name

--- examples/simple-web-proof/vlayer/vite.config.ts
+++ examples/simple-web-proof/vlayer/vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   build: {
     target: "esnext",
   },
+  // This is added as a temporary fix for the `process is not defined` issue
+  // (https://github.com/reown-com/appkit/issues/3926)
+  // appearing in our dependency: reown/appkit
   define: {
     "process.env": {},
   },
