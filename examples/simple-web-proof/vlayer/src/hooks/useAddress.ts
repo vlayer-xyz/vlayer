efeaae754e1ab0e6d0ca8b12a651c6cf20c87506
Anna Jabłonowska <ania@vlayer.xyz>
2025-03-04 11:14:14 +0100

    Use enum

--- examples/simple-web-proof/vlayer/src/hooks/useAddress.ts
+++ examples/simple-web-proof/vlayer/src/hooks/useAddress.ts
@@ -1,6 +1,11 @@
 import { useAppKitAccount } from "@reown/appkit/react";
 import { privateKeyToAccount } from "viem/accounts";
 
+export enum ClientAuthMode {
+  ENV_PRIVATE_KEY = "envPrivateKey",
+  WALLET = "wallet",
+}
+
 export const useAddress = () => {
   let address = "";
   let error: string | null = null;
@@ -8,22 +13,27 @@ export const useAddress = () => {
   const authMode = import.meta.env.VITE_CLIENT_AUTH_MODE;
 
   try {
-    if (authMode == "envPrivateKey") {
-      const envPrivateKey = import.meta.env.VITE_PRIVATE_KEY;
-      if (!envPrivateKey) {
-        throw new Error("No private key found");
-      } else {
-        address = privateKeyToAccount(envPrivateKey as "0x").address;
+    switch (authMode) {
+      case ClientAuthMode.ENV_PRIVATE_KEY: {
+        const envPrivateKey = import.meta.env.VITE_PRIVATE_KEY;
+        if (!envPrivateKey) {
+          throw new Error("No private key found");
+        } else {
+          address = privateKeyToAccount(envPrivateKey as "0x").address;
+        }
+        break;
       }
-    } else if (authMode == "wallet") {
-      const addressFromWallet = useAppKitAccount().address;
-      if (addressFromWallet) {
-        address = addressFromWallet;
-      } else {
-        throw new Error("No address found in wallet");
+      case ClientAuthMode.WALLET: {
+        const addressFromWallet = useAppKitAccount().address;
+        if (addressFromWallet) {
+          address = addressFromWallet;
+        } else {
+          throw new Error("No address found in wallet");
+        }
+        break;
       }
-    } else {
-      throw new Error("Invalid VITE_CLIENT_AUTH_MODE");
+      default:
+        throw new Error("Invalid VITE_CLIENT_AUTH_MODE");
     }
   } catch (e) {
     error = (e as Error).message;
--- examples/simple-web-proof/vlayer/src/vite-env.d.ts
+++ examples/simple-web-proof/vlayer/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import { ClientAuthMode } from "./hooks/useAddress.ts";
+
+interface ImportMetaEnv {
+  readonly VITE_CLIENT_AUTH_MODE: ClientAuthMode;
+}
