ea20c4028a0147552110972ed230aebfcea80cd9
Artur Chmaro <artur@vlayer.xyz>
2025-03-03 18:25:36 -0700

    bring back tests

--- examples/simple-email-proof/vlayer/tests/WelcomePage.spec.ts
+++ examples/simple-email-proof/vlayer/tests/WelcomePage.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from "@playwright/test";
-import { steps } from "../src/app/router/steps";
+import { stepsMeta, STEP_KIND } from "../src/app/router/types";
 
 test("navigates from / to wallet connection page", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByText(steps[0].title)).toBeVisible();
-  await expect(page.getByText(steps[0].description)).toBeVisible();
+  await expect(
+    page.getByText(stepsMeta[STEP_KIND.WELCOME].title),
+  ).toBeVisible();
+  await expect(
+    page.getByText(stepsMeta[STEP_KIND.WELCOME].description),
+  ).toBeVisible();
   await page.getByTestId("start-page-button").click();
-  await expect(page).toHaveURL("/connect-wallet");
+  await expect(page).toHaveURL(stepsMeta[STEP_KIND.CONNECT_WALLET].path);
 });
