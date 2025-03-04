d8c729f929332ecb752a3334f4089c51b94fd5fa
Anna Jabłonowska <ania@vlayer.xyz>
2025-03-04 14:43:29 +0100

    Add build_contracts function with argument

--- bash/lib/contracts.sh
+++ bash/lib/contracts.sh
@@ -1,31 +1,21 @@
 source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
-function build_vlayer_contracts() {
-  echo "::group::Building vlayer contracts"
-  pushd "${VLAYER_HOME}/contracts/vlayer"
-  silent_unless_fails forge soldeer install
-  forge clean
-  silent_unless_fails forge build
-  popd
-  echo '::endgroup::Building vlayer contracts'
-}
-
-function build_fixtures_contracts() {
-  echo "::group::Building fixtures contracts"
-  pushd "${VLAYER_HOME}/contracts/fixtures"
-  silent_unless_fails forge soldeer install
-  forge clean
-  silent_unless_fails forge build
-  popd
-  echo '::endgroup::Building fixtures contracts'
-}
-
 function build_contracts() {
+    echo "::group::Building ${1} contracts"
+    pushd "${VLAYER_HOME}/contracts/${1}"
+    silent_unless_fails forge soldeer install
+    forge clean
+    silent_unless_fails forge build
+    popd
+    echo "::endgroup::Building ${1} contracts"
+}
+
+function build_all_contracts() {
   echo "::group::Building contracts"
 
   mock_imageid
-  build_vlayer_contracts
-  build_fixtures_contracts
+  build_contracts vlayer
+  build_contracts fixtures
   generate_ts_bindings
 
   echo '::endgroup::Building contracts'
