141e6932e15dbc82779776534c9fe2e5e445596a
Leonid Logvinov <logvinov.leon@gmail.com>
2025-03-04 12:21:08 +0100

    Disable e-mail benchmark

--- rust/zkvm-benchmarks/benchmarks/src/benchmarks/precompiles.rs
+++ rust/zkvm-benchmarks/benchmarks/src/benchmarks/precompiles.rs
@@ -1,7 +1,11 @@
 use crate::{benchmarks::merge, Benchmark};
+#[allow(dead_code)]
 mod email;
 mod url_pattern;
 
 pub fn benchmarks() -> Vec<Benchmark> {
-    merge([("email", email::benchmarks()), ("url_pattern", url_pattern::benchmarks())])
+    merge([
+        // ("email", email::benchmarks()),
+        ("url_pattern", url_pattern::benchmarks()),
+    ])
 }
