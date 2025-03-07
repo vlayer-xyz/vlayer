cbef625dfd7d9f030f879c11d02b349e2ea8f203
pociej <grzegorz.pociejewski@gmail.com>
2025-03-07 17:30:18 +0100

    Restore bash script from main

--- bash/e2e-web-apps-test.sh
+++ bash/e2e-web-apps-test.sh
@@ -1,33 +1,34 @@
+#!/usr/bin/env bash
+
 set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 source "$(dirname "${BASH_SOURCE[0]}")/lib/proving_mode.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/lib/examples.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/lib/e2e.sh"
 
 set_proving_mode
 
 echo Generating typescript bidings ...
 ${VLAYER_HOME}/bash/build-ts-types.sh >/dev/null
-bun install --frozen-lockfile
 
 echo '::group::Build extension'
-pushd "$VLAYER_HOME/packages/browser-extension" > /dev/null
+cd "$VLAYER_HOME/packages/browser-extension"
+bun install --frozen-lockfile
 bun run build
-popd > /dev/null
 echo '::endgroup::'
 
-install_chromium
-
 for example in $(get_examples); do
+
   echo Running services...
   source ${VLAYER_HOME}/bash/run-services.sh
+
   echo "::group::Running tests of: ${example}"
   cd "$VLAYER_HOME/examples/$example"
   forge build
+
   cd vlayer
-  echo "Running tests for ${example}"
-  pushd "$VLAYER_HOME/examples/$example/" > /dev/null
-    run_playwright_tests 
-  popd > /dev/null
+  bun install --frozen-lockfile
+  bun run test:"${VLAYER_ENV}"
+
+  cleanup
 done
