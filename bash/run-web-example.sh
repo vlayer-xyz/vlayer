fbd73b6d9bd1f93a6588492be75f9fd17f3cb7ad
Anna Jabłonowska <ania@vlayer.xyz>
2025-03-10 11:50:05 +0100

    Add flow logs to run-web-example.sh

--- bash/run-web-example.sh
+++ bash/run-web-example.sh
@@ -7,27 +7,35 @@ set -ueo pipefail
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 
 function install_deps {
+    echo "::group::Installing dependencies"
     build_react_sdk_with_deps
     
     cd ${VLAYER_HOME}/examples/simple-web-proof/vlayer
     rm -rf node_modules
     bun install --frozen-lockfile
+    echo "::endgroup::Installing dependencies"
 }
 
-function build_example_contracts {    
+function build_example_contracts {
+    echo "::group::Building example contracts" 
     cd ${VLAYER_HOME}/examples/simple-web-proof
     forge clean
     forge build
+    echo "::endgroup::Building example contracts"
 }
 
 function run_web_app {
+    echo "::group::Running web app"
     cd ${VLAYER_HOME}/examples/simple-web-proof/vlayer
     bun run web:dev &
+    echo "::endgroup::Running web app"
 }
 
 function run_browser_extension {
+    echo "::group::Running browser extension"
     cd ${VLAYER_HOME}/packages/browser-extension
     bun run dev
+    echo "::endgroup::Running browser extension"
 }
 
 install_deps
