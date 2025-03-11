273aa1c6ae21b7c12c8efcce544e12d3c2fce7f8
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2025-03-11 12:26:51 +0100

    Upload artifcacts from test_e2e_web_apps_testnet (#1948)
    * Upload artifcacts from test_e2e_web_apps_testnet
    
    * Upload artifacts for relase test
    
    * Restore bash script from main
    
    * Restore bash script from main
    
    * Remove pr triggers from test_e2e_web_apps_testnet
    rename back test-web to test
    
    * fixup! Upload artifacts for relase test

--- bash/test-release-remote-prover.sh
+++ bash/test-release-remote-prover.sh
@@ -47,29 +47,47 @@ echo '::endgroup::'
 echo "Starting VDNS server"
 docker compose -f ${VLAYER_HOME}/docker/docker-compose.devnet.yaml up -d vdns_server
 
+TEMP_DIR=$(mktemp -d -t vlayer-test-release-XXXXXX)
+mkdir -p "${TEMP_DIR}/packages/browser-extension"
+cp -a "${VLAYER_HOME}/packages/browser-extension/dist" "${TEMP_DIR}/packages/browser-extension"
+
+echo '::group::Installing playwright chromium'
+install_chromium
+echo '::endgroup::'
+
+# We need to first create a temporary directory and perform operations there.
+# After that we copy the results to the VLAYER_HOME directory.
+# This is necessary because initializing in the repository directly causes
+# `forge init (called by vlayer init)` error because git is already initialized. 
+# This is a workaround to avoid this and make all the playwright reports available
+# in the VLAYER_HOME directory.
+
 for example in $(get_examples); do
-
     echo "::group::Initializing vlayer template: ${example}"
-    TEMP_DIR=$(mktemp -d -t vlayer-test-release-XXXXXX-)
-    cd ${TEMP_DIR}
-
+    cd "${TEMP_DIR}"
     mkdir -p examples/${example}
-    mkdir -p packages/browser-extension
-    cp -a ${VLAYER_HOME}/packages/browser-extension/dist packages/browser-extension
+    echo "Current directory: $(pwd)"
 
-    VLAYER_TEMP_DIR=${TEMP_DIR}/examples/${example}
-    cd ${VLAYER_TEMP_DIR}
-
+    VLAYER_TEMP_DIR="examples/${example}"
+    cd "${VLAYER_TEMP_DIR}"
+ 
     vlayer init --template "${example}"
+    echo "Current directory: $(pwd)"
     forge build
     vlayer test
-    echo '::endgroup::'
+    echo "::endgroup::"
 
     echo "::group::vlayer run prove.ts: ${example}"
     run_prover_script
-    echo '::endgroup::'
+    echo "::endgroup::"
 
     echo "::group::vlayer run Playwright test: ${example}"
-    WEB_SERVER_URL="https://web-proofs-demo.vlayer.xyz/" run_playwright_tests
-    echo '::endgroup::'
+    run_playwright_tests
+    echo "::endgroup::"
 done
+
+# Copy the TEMP_DIR to the final destination after the loop
+mkdir -p "${VLAYER_HOME}/vlayer-test-release"
+cp -a "${TEMP_DIR}"/* "${VLAYER_HOME}/vlayer-test-release"
+echo "Copying from ${TEMP_DIR} to ${VLAYER_HOME}/vlayer-test-release"
+find "${VLAYER_HOME}/vlayer-test-release" -name "playwright-report"
