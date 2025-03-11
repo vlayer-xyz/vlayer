a9f384271f97c54de00399377972958e7fedccda
Anna Jabłonowska <ania@vlayer.xyz>
2025-03-11 14:47:05 +0100

    Use generate_ts_bindings

--- bash/e2e-web-proof-example-test.sh
+++ bash/e2e-web-proof-example-test.sh
@@ -6,12 +6,11 @@ VLAYER_HOME=$(git rev-parse --show-toplevel)
 source "$(dirname "${BASH_SOURCE[0]}")/lib/proving_mode.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/lib/build_packages.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/lib/e2e.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/lib/utils.sh"
 
 set_proving_mode
 
-echo '::group::Generating typescript bidings'
-${VLAYER_HOME}/bash/build-ts-types.sh >/dev/null
-echo '::endgroup::'
+generate_ts_bindings
 
 echo '::group::Running services'
 source ${VLAYER_HOME}/bash/run-services.sh
