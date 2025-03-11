cf9f6840b4b9b1c5edd6a391647fcb71b42be17b
Grzegorz Pociejewski <grzegorz.pociejewski@gmail.com>
2025-03-11 14:06:18 +0100

    Revert "Rename test -> test-web where needed (#1960)"
    This reverts commit 0f21f59eac6479092a1c86c1d12959656f39ddfb.

--- bash/playwright-test.sh
+++ bash/playwright-test.sh
@@ -66,9 +66,9 @@ echo_color BLUE "Run playwright tests"
 pushd ${VLAYER_HOME}/packages
 if [ "$headed_mode" = true ]; then
     echo_color BLUE "Running in headed mode"
-    VLAYER_ENV=${VLAYER_ENV} bun run test-web:headed
+    VLAYER_ENV=${VLAYER_ENV} bun run test:headed
 else
     echo_color BLUE "Running in headless mode"
-    VLAYER_ENV=${VLAYER_ENV} bun run test-web:headless
+    VLAYER_ENV=${VLAYER_ENV} bun run test:headless
 fi
 popd
