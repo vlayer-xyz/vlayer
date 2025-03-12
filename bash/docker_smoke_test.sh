b15166665cdfaf8fdebcc72d1050c2587f6a5255
Anna Jabłonowska <62891747+aajj999@users.noreply.github.com>
2025-03-12 13:03:33 +0100

    Refactor color script (#1974)
    * Use echo_color
    
    * Print without color when NO_COLOR

--- bash/docker_smoke_test.sh
+++ bash/docker_smoke_test.sh
@@ -16,12 +16,12 @@ while [ $SECONDS -lt $END ]; do
     echo -e "Health Status - \033[32mHEALTHY: $HEALTHY\033[0m | \033[34mSTARTING: $STARTING\033[0m | \033[31mUNHEALTHY: $UNHEALTHY\033[0m | \033[33mNO HEALTHCHECK: $NO_HEALTHCHECK\033[0m | TOTAL: $TOTAL"
 
     if [[ "$UNHEALTHY" -gt 0 ]]; then
-        echo -e "\n\033[31mUnhealthy services:\033[0m"
+        echo_color RED "\nUnhealthy services:"
         docker ps --filter health=unhealthy --format "{{.Names}}"
     fi
 
     if [[ "$HEALTHY" -eq $((TOTAL-NO_HEALTHCHECK)) ]]; then
-        echo -e "\n\033[32m✓ All services with health checks are healthy!\033[0m"
+        echo_color GREEN "\n✓ All services with health checks are healthy!"
         exit 0
     fi
 
@@ -29,14 +29,14 @@ while [ $SECONDS -lt $END ]; do
     if [ ${#DOTS} -gt 3 ]; then
         DOTS=""
     fi
-    echo -e "\n\033[33mWaiting for services to become healthy${DOTS}\033[0m"
+    echo_color YELLOW "\nWaiting for services to become healthy${DOTS}"
     sleep $INTERVAL
 done
 
 UNHEALTHY_SERVICES=$(docker ps --filter health=unhealthy --format "{{.Names}}")
 STARTING_SERVICES=$(docker ps --filter health=starting --format "{{.Names}}")
 
-echo -e "\n\033[31mError: Services not healthy after $TIMEOUT seconds.\033[0m" >&2
+echo_color RED "\nError: Services not healthy after $TIMEOUT seconds." >&2
 [ -n "$UNHEALTHY_SERVICES" ] && echo -e "\033[31mUnhealthy services:\033[0m\n$UNHEALTHY_SERVICES" >&2
 [ -n "$STARTING_SERVICES" ] && echo -e "\033[34mStill starting:\033[0m\n$STARTING_SERVICES" >&2
 echo "Exiting..."
