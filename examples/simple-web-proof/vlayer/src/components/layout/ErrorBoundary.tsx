373bea3fc2f69581b4e043348778db72e8e384e2
Anna Jabłonowska <ania@vlayer.xyz>
2025-03-18 15:06:23 +0100

    Update Component to use KnownAppError

--- examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
+++ examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
@@ -1,25 +1,21 @@
 import { FallbackProps } from "react-error-boundary";
-import { match } from "ts-pattern";
+import { KnownAppError } from "../../errors";
 
 export const ErrorBoundaryComponent = ({ error }: FallbackProps) => {
-  const errorMessage = error.message;
-  console.error(errorMessage);
+  console.error(error.message);
 
-  return match(error.name)
-    .with("AlreadyMintedError", () => {
-      return (
-        <div data-testid="Error display">
-          <p style={{ color: "black" }}>
-            NFT has already been minted for this account.
-          </p>
-        </div>
-      );
-    })
-    .otherwise(() => {
-      return (
-        <div data-testid="Error display">
-          <p style={{ color: "red" }}>Something went wrong</p>
-        </div>
-      );
-    });
+  if (error instanceof KnownAppError) {
+    return (
+      <div data-testid="Error display">
+        <p style={{ color: "red" }}>{error.name}</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+      </div>
+    );
+  } else {
+    return (
+      <div data-testid="Error display">
+        <p style={{ color: "red" }}>Something went wrong</p>
+      </div>
+    );
+  }
 };
