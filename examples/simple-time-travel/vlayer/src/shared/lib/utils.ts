c8677cb7464bec0a31893db3ad3af47e04388c3e
Artur Chmaro <artur@vlayer.xyz>
2025-03-19 10:14:52 +0100

    better naming

--- examples/simple-time-travel/vlayer/src/shared/lib/utils.ts
+++ examples/simple-time-travel/vlayer/src/shared/lib/utils.ts
@@ -1,5 +1,5 @@
 export const usePrivateKey =
   import.meta.env.VITE_USE_WINDOW_ETHEREUM_TRANSPORT === "false";
 
-export const truncateHashOrAddr = (hash: string | null) =>
-  hash ? `${hash.slice(0, 4)}...${hash.slice(-4)}` : "";
+export const shortenAndFormatHash = (hash: string | null) =>
+  hash ? `${hash.slice(0, 4)}...${hash.slice(-4)}` : ""; // 0x00...012 instead of long hash
