3460b3400fac52745a737791cb2b8f9a6c3af502
Artur Chmaro <artur@vlayer.xyz>
2025-03-19 11:45:27 +0100

    Allow ranges instead of hardcoded steps

--- examples/simple-time-travel/vlayer/deploy.ts
+++ examples/simple-time-travel/vlayer/deploy.ts
@@ -16,8 +16,8 @@ declare const process: {
     PROVER_START_BLOCK: bigint;
     PROVER_END_BLOCK: bigint | "latest";
     PROVER_TRAVEL_RANGE: bigint;
-    PROVER_ERC20_CONTRACT_ADDR: Address;
-    PROVER_ERC20_HOLDER_ADDR: Address;
+    PROVER_ERC20_CONTRACT_ADDR: string;
+    PROVER_ERC20_HOLDER_ADDR: string;
     PROVER_STEP: bigint;
     USE_WINDOW_ETHEREUM_TRANSPORT: string;
   };
@@ -25,15 +25,21 @@ declare const process: {
 
 const useLatestBlock = process.env.PROVER_END_BLOCK === "latest";
 const latestBlock = await ethClient.getBlockNumber();
-const endBlock = useLatestBlock ? latestBlock : process.env.PROVER_END_BLOCK;
+const endBlock = useLatestBlock
+  ? latestBlock
+  : BigInt(process.env.PROVER_END_BLOCK);
 
-const startBlock = useLatestBlock
-  ? latestBlock - process.env.PROVER_TRAVEL_RANGE
-  : process.env.PROVER_START_BLOCK;
+let startBlock;
 
-const usdcTokenAddr = process.env.PROVER_ERC20_CONTRACT_ADDR;
+if (process.env.PROVER_TRAVEL_RANGE) {
+  startBlock = latestBlock - BigInt(process.env.PROVER_TRAVEL_RANGE);
+} else {
+  startBlock = BigInt(process.env.PROVER_START_BLOCK);
+}
 
-const step = process.env.PROVER_STEP;
+const usdcTokenAddr = process.env.PROVER_ERC20_CONTRACT_ADDR as Address;
+
+const step = BigInt(process.env.PROVER_STEP);
 
 const { prover, verifier } = await deployVlayerContracts({
   proverSpec,
