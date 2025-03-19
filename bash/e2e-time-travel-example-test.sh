f55bfd1268e5bd68cfbc7f73957f90bc6f35d5fb
Artur Chmaro <artur@vlayer.xyz>
2025-03-19 10:14:28 +0100

    e2e script cleanup

--- bash/e2e-time-travel-example-test.sh
+++ bash/e2e-time-travel-example-test.sh
@@ -4,25 +4,18 @@ set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
 source "$(dirname "${BASH_SOURCE[0]}")/lib/proving_mode.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/lib/utils.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/lib/e2e.sh"
 
 set_proving_mode
 
-echo '::group::Generating typescript bidings'
-${VLAYER_HOME}/bash/build-ts-types.sh >/dev/null
-echo '::endgroup::'
+generate_ts_bindings
 
 echo '::group::Running services'
 source ${VLAYER_HOME}/bash/run-services.sh
 echo '::endgroup::'
 
-echo '::group::Running tests of: simple-time-travel'
-cd "$VLAYER_HOME/examples/simple-time-travel"
-forge build
-
-cd vlayer
-bun install --frozen-lockfile
-bun run test:"${VLAYER_ENV}"
-echo '::endgroup::'
+run_web_tests simple-time-travel
 
 echo '::group::Cleanup'
 cleanup
