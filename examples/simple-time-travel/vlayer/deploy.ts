a37ca5961e73c2e5f7f8eec1d9f98252b00afb95
Artur Chmaro <artur@vlayer.xyz>
2025-03-19 10:14:17 +0100

    type fixes

--- examples/simple-time-travel/vlayer/deploy.ts
+++ examples/simple-time-travel/vlayer/deploy.ts
@@ -16,25 +16,24 @@ declare const process: {
     PROVER_START_BLOCK: bigint;
     PROVER_END_BLOCK: bigint | "latest";
     PROVER_TRAVEL_RANGE: bigint;
-    PROVER_ERC20_CONTRACT_ADDR: string;
-    PROVER_ERC20_HOLDER_ADDR: string;
+    PROVER_ERC20_CONTRACT_ADDR: Address;
+    PROVER_ERC20_HOLDER_ADDR: Address;
     PROVER_STEP: bigint;
     USE_WINDOW_ETHEREUM_TRANSPORT: string;
   };
 };
 
 const useLatestBlock = process.env.PROVER_END_BLOCK === "latest";
-const endBlock = useLatestBlock
-  ? await ethClient.getBlockNumber()
-  : BigInt(process.env.PROVER_END_BLOCK);
+const latestBlock = await ethClient.getBlockNumber();
+const endBlock = useLatestBlock ? latestBlock : process.env.PROVER_END_BLOCK;
 
 const startBlock = useLatestBlock
-  ? endBlock - BigInt(process.env.PROVER_TRAVEL_RANGE)
-  : BigInt(process.env.PROVER_START_BLOCK);
+  ? latestBlock - process.env.PROVER_TRAVEL_RANGE
+  : process.env.PROVER_START_BLOCK;
 
-const usdcTokenAddr = process.env.PROVER_ERC20_CONTRACT_ADDR as Address;
+const usdcTokenAddr = process.env.PROVER_ERC20_CONTRACT_ADDR;
 
-const step = BigInt(process.env.PROVER_STEP);
+const step = process.env.PROVER_STEP;
 
 const { prover, verifier } = await deployVlayerContracts({
   proverSpec,
