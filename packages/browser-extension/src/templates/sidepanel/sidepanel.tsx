774d9c175109a3e91b8064172da9a917751d1a81
rzadp <przemek@vlayer.xyz>
2025-03-19 15:07:52 +0100

    Add Sentry to side panel

--- packages/browser-extension/src/templates/sidepanel/sidepanel.tsx
+++ packages/browser-extension/src/templates/sidepanel/sidepanel.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@radix-ui/themes/styles.css";
+import * as Sentry from "@sentry/react";
 import { SidePanel } from "components/pages/SidePanel";
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [],
+    release: chrome.runtime.getManifest().version_name,
+  });
+}
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
