866eeb64ac323311cd54d97dce7023e3bd8f950e
Adam Król <adam.dawid.krol@gmail.com>
2025-03-20 12:55:56 +0100

    Correct lint-solidity script

--- bash/lint/lint-solidity.sh
+++ bash/lint/lint-solidity.sh
@@ -2,10 +2,12 @@
 
 set -ueo pipefail
 
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 VLAYER_HOME=$(git rev-parse --show-toplevel)
-source "$(dirname "${BASH_SOURCE[0]}")/lib/examples.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/lib/colors.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/lib/utils.sh"
+
+source "$SCRIPT_DIR/../lib/examples.sh"
+source "$SCRIPT_DIR/../lib/colors.sh"
+source "$SCRIPT_DIR/../lib/utils.sh"
 
 usage() {
     echo_color YELLOW "Usage: $0 [OPTIONS]"
@@ -41,7 +43,7 @@ handle_options "$@"
 bun install --frozen-lockfile
 mock_imageid
 
-echo "::group::Runing solhint for examples"
+echo "::group::Running solhint for examples"
 for example in $(get_examples); do (
     echo "::group::Running solhint for: ${example}"
     pushd "$VLAYER_HOME/examples/$example/vlayer"
@@ -49,7 +51,7 @@ for example in $(get_examples); do (
     popd
     echo "::endgroup::Running solhint for: ${example}"
 ) done
-echo "::endgroup::Runing solhint for examples"
+echo "::endgroup::Running solhint for examples"
 
 echo "::group::Running solhint for contracts directory"
 pushd "$VLAYER_HOME/contracts"
