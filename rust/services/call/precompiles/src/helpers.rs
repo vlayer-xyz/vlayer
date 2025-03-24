b31e2fa79cb5e83bb2f3341fb7904e537b9d7d0a
Leonid Logvinov <logvinov.leon@gmail.com>
2025-03-24 13:53:45 +0100

    Enable is_vlayer_test precompile only when feature flag is enabled

--- rust/services/call/precompiles/src/helpers.rs
+++ rust/services/call/precompiles/src/helpers.rs
@@ -9,22 +9,8 @@ pub(super) fn map_to_fatal<E: ToString>(err: E) -> PrecompileErrors {
     }
 }
 
-macro_rules! generate_precompiles {
-    ($(($address:literal, $func:ident, $base_cost:literal, $byte_cost:literal, $category:expr),)*) => {
-        [
-            $(
-                generate_precompiles!(($address, $func, $base_cost, $byte_cost, $category)),
-            )*
-        ]
-    };
+macro_rules! generate_precompile {
     (($address:literal, $func:ident, $base_cost:literal, $byte_cost:literal, $category:expr)) => {{
-        use alloy_primitives::Bytes;
-        use revm::precompile::{
-            u64_to_address, Precompile as RawPrecompile, PrecompileOutput, PrecompileResult, PrecompileWithAddress,
-        };
-
-        use crate::precompile::{gas_used, Precompile};
-
         fn run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
             let gas_used = gas_used(input.len(), gas_limit, $base_cost, $byte_cost)?;
             let bytes = $func(input)?;
@@ -33,6 +19,18 @@ macro_rules! generate_precompiles {
         let inner = PrecompileWithAddress(u64_to_address($address), RawPrecompile::Standard(run));
         Precompile::new(inner, $category)
     }};
+}
+
+pub(super) use generate_precompile;
+
+macro_rules! generate_precompiles {
+    ($(($address:literal, $func:ident, $base_cost:literal, $byte_cost:literal, $category:expr),)*) => {
+        [
+            $(
+                generate_precompile!(($address, $func, $base_cost, $byte_cost, $category)),
+            )*
+        ]
+    };
 }
 
 pub(super) use generate_precompiles;
--- rust/services/call/precompiles/src/lib.rs
+++ rust/services/call/precompiles/src/lib.rs
@@ -7,35 +7,59 @@ pub mod system;
 pub mod url_pattern;
 mod web_proof;
 
-use alloy_primitives::Address;
+use alloy_primitives::{Address, Bytes};
+use cfg_if::cfg_if;
 use email_proof::verify as email_proof;
-use helpers::generate_precompiles;
+use helpers::{generate_precompile, generate_precompiles};
 use json::{
     get_array_length as json_get_array_length, get_bool as json_get_bool, get_int as json_get_int,
     get_string as json_get_string,
 };
-use precompile::{Precompile, Tag};
+use precompile::{gas_used, Precompile, Tag};
 use regex::{capture as regex_capture, is_match as regex_is_match};
-use system::is_vlayer_test;
+use revm::precompile::{
+    u64_to_address, Precompile as RawPrecompile, PrecompileOutput, PrecompileResult,
+    PrecompileWithAddress,
+};
 use url_pattern::test as url_pattern_test;
 use web_proof::verify as web_proof;
 
-const NUM_PRECOMPILES: usize = 10;
+cfg_if!(
+    if #[cfg(feature = "vlayer_test")] {
+        use system::is_vlayer_test;
 
-#[rustfmt::skip]
-pub const PRECOMPILES: [Precompile; NUM_PRECOMPILES] = generate_precompiles![
-    // (address, precompile, base_cost, byte_cost, tag)
-    (0x100, web_proof,             1000, 10, Tag::WebProof),
-    (0x101, email_proof,           1000, 10, Tag::EmailProof),
-    (0x102, json_get_string,       1000, 10, Tag::JsonGetString),
-    (0x103, json_get_int,          1000, 10, Tag::JsonGetInt),
-    (0x104, json_get_bool,         1000, 10, Tag::JsonGetBool),
-    (0x105, json_get_array_length, 1000, 10, Tag::JsonGetArrayLength),
-    (0x110, regex_is_match,        1000, 10, Tag::RegexIsMatch),
-    (0x111, regex_capture,         1000, 10, Tag::RegexCapture),
-    (0x120, url_pattern_test,      1000, 10, Tag::UrlPatternTest),
-    (0x130, is_vlayer_test,        1000, 10, Tag::IsVlayerTest),
-];
+        const NUM_PRECOMPILES: usize = 10;
+        #[rustfmt::skip]
+        pub const PRECOMPILES: [Precompile; NUM_PRECOMPILES] = generate_precompiles![
+            // (address, precompile, base_cost, byte_cost, tag)
+            (0x100, web_proof,             1000, 10, Tag::WebProof),
+            (0x101, email_proof,           1000, 10, Tag::EmailProof),
+            (0x102, json_get_string,       1000, 10, Tag::JsonGetString),
+            (0x103, json_get_int,          1000, 10, Tag::JsonGetInt),
+            (0x104, json_get_bool,         1000, 10, Tag::JsonGetBool),
+            (0x105, json_get_array_length, 1000, 10, Tag::JsonGetArrayLength),
+            (0x110, regex_is_match,        1000, 10, Tag::RegexIsMatch),
+            (0x111, regex_capture,         1000, 10, Tag::RegexCapture),
+            (0x120, url_pattern_test,      1000, 10, Tag::UrlPatternTest),
+            (0x130, is_vlayer_test,        1000, 10, Tag::IsVlayerTest),
+        ];
+    } else {
+        const NUM_PRECOMPILES: usize = 9;
+        #[rustfmt::skip]
+        pub const PRECOMPILES: [Precompile; NUM_PRECOMPILES] = generate_precompiles![
+            // (address, precompile, base_cost, byte_cost, tag)
+            (0x100, web_proof,             1000, 10, Tag::WebProof),
+            (0x101, email_proof,           1000, 10, Tag::EmailProof),
+            (0x102, json_get_string,       1000, 10, Tag::JsonGetString),
+            (0x103, json_get_int,          1000, 10, Tag::JsonGetInt),
+            (0x104, json_get_bool,         1000, 10, Tag::JsonGetBool),
+            (0x105, json_get_array_length, 1000, 10, Tag::JsonGetArrayLength),
+            (0x110, regex_is_match,        1000, 10, Tag::RegexIsMatch),
+            (0x111, regex_capture,         1000, 10, Tag::RegexCapture),
+            (0x120, url_pattern_test,      1000, 10, Tag::UrlPatternTest),
+        ];
+    }
+);
 
 pub fn precompile_by_address(address: &Address) -> Option<Precompile> {
     PRECOMPILES
