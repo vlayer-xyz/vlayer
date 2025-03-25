8992a099a8bfdbe4fff14e565cc60f2d8fdf2868
Artur Chmaro <artur@vlayer.xyz>
2025-03-25 14:38:48 +0100

    remove extra step

--- examples/simple-time-travel/vlayer/tests/TimeTravel.spec.ts
+++ examples/simple-time-travel/vlayer/tests/TimeTravel.spec.ts
@@ -13,7 +13,6 @@ test("Simple time travel flow", async ({ page }) => {
   });
 
   await test.step("navigates to show balance page", async () => {
-    await page.goto("/");
     await page.getByRole("button").click();
     await expect(
       page.getByText(stepsMeta[StepKind.showBalance].title),
