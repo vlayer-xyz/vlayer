feb9f06f10ee55ff1520b305394976330190db61
Arkadiusz Konior <akonior@gmail.com>
2025-04-01 14:57:34 +0200

    Fix integration test

--- rust/web_prover/src/presentation.rs
+++ rust/web_prover/src/presentation.rs
@@ -1,5 +1,6 @@
 use tlsn_core::{attestation::Attestation, presentation::Presentation, CryptoProvider, Secrets};
 use tracing::debug;
+use utils::range::RangeSet;
 
 pub async fn create_presentation(
     attestation: Attestation,
@@ -7,7 +8,12 @@ pub async fn create_presentation(
 ) -> Result<Presentation, Box<dyn std::error::Error>> {
     debug!("Creating presentation");
 
-    let builder = secrets.transcript_proof_builder();
+    let mut builder = secrets.transcript_proof_builder();
+
+    let transcript = secrets.transcript();
+
+    builder.reveal_sent(&RangeSet::from(0..transcript.sent().len()))?;
+    builder.reveal_recv(&RangeSet::from(0..transcript.received().len()))?;
 
     let transcript_proof = builder.build()?;
 
