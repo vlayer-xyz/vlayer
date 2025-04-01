08bd91bbd3473600e058f854459dded5e266c6aa
Krzysztof Jelski <krzysztof@vlayer.xyz>
2025-04-01 11:01:40 +0200

    Commit to full transcript

--- rust/web_prover/src/presentation.rs
+++ rust/web_prover/src/presentation.rs
@@ -1,19 +1,21 @@
 use tlsn_core::{attestation::Attestation, presentation::Presentation, CryptoProvider, Secrets};
 use tlsn_formats::http::HttpTranscript;
+use tracing::debug;
 
 pub async fn create_presentation(
     attestation: Attestation,
     secrets: Secrets,
 ) -> Result<Presentation, Box<dyn std::error::Error>> {
-    let transcript = HttpTranscript::parse(secrets.transcript())?;
+    debug!("Creating presentation");
+    // let transcript = HttpTranscript::parse(secrets.transcript())?;
 
     let mut builder = secrets.transcript_proof_builder();
 
-    let request = &transcript.requests[0];
-    builder.reveal_sent(request)?;
+    // let request = &transcript.requests[0];
+    // builder.reveal_sent(request)?;
 
-    let response = &transcript.responses[0];
-    builder.reveal_recv(response)?;
+    // let response = &transcript.responses[0];
+    // builder.reveal_recv(response)?;
 
     let transcript_proof = builder.build()?;
 
