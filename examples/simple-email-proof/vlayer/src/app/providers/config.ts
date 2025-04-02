719a2b0acd0dd29b042fb1041793066b4bc6d78f
Artur Chmaro <artur@vlayer.xyz>
2025-04-02 10:16:51 +0200

    use new walletmock

--- examples/simple-email-proof/vlayer/src/app/providers/config.ts
+++ examples/simple-email-proof/vlayer/src/app/providers/config.ts
@@ -1,32 +1,14 @@
 import { http, createConfig } from "wagmi";
 import { optimismSepolia, anvil } from "wagmi/chains";
-import { metaMask } from "wagmi/connectors";
-import { useEnvPrivateKey } from "../../shared/lib/clientAuthMode";
-import { mockConnector } from "../../shared/lib/mockConnector";
 
-const chain =
-  import.meta.env.VITE_CHAIN_NAME === "anvil" ? anvil : optimismSepolia;
-
-const wagmiConfig = useEnvPrivateKey()
-  ? createConfig({
-      connectors: [mockConnector(chain)],
-      chains: [chain],
-      transports: {
-        [anvil.id]: http(),
-        [optimismSepolia.id]: http(),
-      },
-    })
-  : createConfig({
-      chains:
-        import.meta.env.VITE_CHAIN_NAME === "anvil"
-          ? [anvil]
-          : [optimismSepolia],
-      connectors: [metaMask()],
-      transports: {
-        [anvil.id]: http(),
-        [optimismSepolia.id]: http(),
-      },
-    });
+const wagmiConfig = createConfig({
+  chains:
+    import.meta.env.VITE_CHAIN_NAME === "anvil" ? [anvil] : [optimismSepolia],
+  transports: {
+    [anvil.id]: http(),
+    [optimismSepolia.id]: http(),
+  },
+});
 
 const proverConfig = {
   proverUrl: import.meta.env.VITE_PROVER_URL,
