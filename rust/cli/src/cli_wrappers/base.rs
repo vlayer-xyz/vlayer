d2be7f3cdb7333a90b5bbf6d143900b423708870
Leonid Logvinov <logvinov.leon@gmail.com>
2025-04-03 12:52:44 +0200

    Attach error context

--- rust/cli/src/cli_wrappers/base.rs
+++ rust/cli/src/cli_wrappers/base.rs
@@ -9,8 +9,8 @@ struct Output(std::process::Output);
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Failed to spawn command: {0}")]
-    Spawn(#[from] std::io::Error),
+    #[error("Failed to spawn command: {0} {1}")]
+    Spawn(std::io::Error, String),
     #[error("Command returned non-zero exit code: {0} {1}")]
     NonZeroExitCode(ExitStatus, String),
 }
@@ -19,7 +19,10 @@ pub struct Cli;
 
 impl Cli {
     pub fn run(cmd: &str, args: &[&str]) -> Result<String, Error> {
-        let output = std::process::Command::new(cmd).args(args).output()?;
+        let output = std::process::Command::new(cmd)
+            .args(args)
+            .output()
+            .map_err(|err| Error::Spawn(err, format!("{cmd} {}", args.join(" "))))?;
         if !output.status.success() {
             let stderr = String::from_utf8_lossy(&output.stderr).into_owned();
             return Err(Error::NonZeroExitCode(output.status, stderr));
