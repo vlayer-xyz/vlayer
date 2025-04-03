b1465b448db5a3f01fe0790658ce9bb16f5ac6a8
Artur Chmaro <artur@vlayer.xyz>
2025-04-03 13:36:57 +0200

    First draft for Kraken Server Side Web Proof

---
+++ examples/kraken-web-proof/vlayer/prove.ts
@@ -0,0 +1,88 @@
+import { createVlayerClient } from "@vlayer/sdk";
+import proverSpec from "../out/KrakenProver.sol/KrakenProver";
+import verifierSpec from "../out/KrakenVerifier.sol/KrakenVerifier";
+import {
+  getConfig,
+  createContext,
+  deployVlayerContracts,
+  writeEnvVariables,
+} from "@vlayer/sdk/config";
+
+const config = getConfig();
+const { chain, ethClient, account, proverUrl, confirmations, notaryUrl } =
+  await createContext(config);
+
+const URL_TO_PROVE = "https://api.kraken.com/0/public/Ticker?pair=ETHUSD";
+
+async function generateWebProof() {
+  try {
+    console.log("⏳ Generating web proof...");
+    const result =
+      await Bun.$`${process.env.PATH_TO_VLAYER ?? "vlayer"} web-proof-fetch --notary ${notaryUrl} --url ${URL_TO_PROVE}`;
+    return result.stdout.toString();
+  } catch (error) {
+    console.error("Command failed:", error);
+    return null;
+  }
+}
+
+const webProof = await generateWebProof();
+
+if (!webProof) {
+  throw new Error("Failed to generate web proof");
+}
+
+console.log("⏳ Deploying contracts...");
+
+const { prover, verifier } = await deployVlayerContracts({
+  proverSpec,
+  verifierSpec,
+  proverArgs: [],
+  verifierArgs: [],
+});
+writeEnvVariables(".env", {
+  VITE_PROVER_ADDRESS: prover,
+  VITE_VERIFIER_ADDRESS: verifier,
+});
+
+console.log("✅ Contracts deployed", { prover, verifier });
+
+const vlayer = createVlayerClient({
+  url: proverUrl,
+  token: config.token,
+});
+
+console.log("⏳ Proving...");
+const hash = await vlayer.prove({
+  address: prover,
+  functionName: "main",
+  proverAbi: proverSpec.abi,
+  args: [
+    {
+      webProofJson: webProof.toString(),
+    },
+  ],
+  chainId: chain.id,
+});
+const result = await vlayer.waitForProvingResult({ hash });
+const [proof, avgPrice] = result;
+console.log("✅ Proof generated");
+
+console.log("⏳ Verifying...");
+const txHash = await ethClient.writeContract({
+  address: verifier,
+  abi: verifierSpec.abi,
+  functionName: "verify",
+  args: [proof, avgPrice],
+  chain,
+  account,
+});
+
+await ethClient.waitForTransactionReceipt({
+  hash: txHash,
+  confirmations,
+  retryCount: 60,
+  retryDelay: 1000,
+});
+
+console.log("✅ Verified!");
