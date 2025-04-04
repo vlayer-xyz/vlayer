beff323e8babba172e294dcb70cc369d8ca494ff
Artur Chmaro <artur@vlayer.xyz>
2025-04-04 08:46:21 +0200

    use reown

--- examples/simple-email-proof/vlayer/src/app/providers/config.ts
+++ examples/simple-email-proof/vlayer/src/app/providers/config.ts
@@ -1,12 +1,33 @@
-import { http, createConfig } from "wagmi";
-import { optimismSepolia, anvil } from "wagmi/chains";
+import { optimismSepolia, anvil, Chain } from "wagmi/chains";
+import { createAppKit } from "@reown/appkit/react";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 
-const wagmiConfig = createConfig({
-  chains:
-    import.meta.env.VITE_CHAIN_NAME === "anvil" ? [anvil] : [optimismSepolia],
-  transports: {
-    [anvil.id]: http(),
-    [optimismSepolia.id]: http(),
+const appKitProjectId = `0716afdbbb2cc3df69721a879b92ad5b`;
+const chain =
+  import.meta.env.VITE_CHAIN_NAME === "anvil" ? anvil : optimismSepolia;
+const chains: [Chain, ...Chain[]] = [chain];
+const networks = chains;
+
+const wagmiAdapter = new WagmiAdapter({
+  projectId: appKitProjectId,
+  chains,
+  networks,
+});
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  projectId: appKitProjectId,
+  networks,
+  defaultNetwork: chain,
+  metadata: {
+    name: "vlayer-email-proof-example",
+    description: "vlayer Email Proof Example",
+    url: "https://vlayer.xyz",
+    icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  },
+  themeVariables: {
+    "--w3m-color-mix": "#551fbc",
+    "--w3m-color-mix-strength": 40,
   },
 });
 
@@ -14,5 +35,7 @@ const proverConfig = {
   proverUrl: import.meta.env.VITE_PROVER_URL,
   token: import.meta.env.VITE_VLAYER_API_TOKEN,
 };
+
+const { wagmiConfig } = wagmiAdapter;
 
 export { wagmiConfig, proverConfig };
