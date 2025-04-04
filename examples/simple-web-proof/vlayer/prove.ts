f6e529fa764a718d594aca023e1d0ab64df3491b
Arkadiusz Konior <akonior@gmail.com>
2025-04-04 09:16:29 +0200

    Revert fixtures to simple-web-proof prove.ts

--- examples/simple-web-proof/vlayer/prove.ts
+++ examples/simple-web-proof/vlayer/prove.ts
@@ -1,8 +1,8 @@
 import { createVlayerClient } from "@vlayer/sdk";
 import proverSpec from "../out/WebProofProver.sol/WebProofProver";
 import verifierSpec from "../out/WebProofVerifier.sol/WebProofVerifier";
-import web_proof from "../testdata/0.1.0-alpha.8/web_proof.json";
-import web_proof_invalid_signature from "../testdata/0.1.0-alpha.8/web_proof_invalid_notary_pub_key.json";
+import web_proof from "./web_proof.json";
+import web_proof_invalid_signature from "./web_proof_invalid_signature.json";
 import * as assert from "assert";
 import { encodePacked, isAddress, keccak256 } from "viem";
 
