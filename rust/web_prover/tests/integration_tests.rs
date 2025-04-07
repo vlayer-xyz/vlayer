c06b811f3eca328920011f8139271083548ac522
Arkadiusz Konior <akonior@gmail.com>
2025-04-07 13:59:10 +0200

    Test body in integration tests

--- rust/web_prover/tests/integration_tests.rs
+++ rust/web_prover/tests/integration_tests.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-// #[cfg(feature = "integration-tests")]
+#[cfg(feature = "integration-tests")]
 mod integration_tests {
     use std::{collections::HashMap, error::Error};
 
@@ -21,6 +21,7 @@ mod integration_tests {
                 .server_port(3011_u16)
                 .uri("/auth_header_require")
                 .headers(HashMap::from([("Authorization".to_string(), "s3cret_t0ken".to_string())]))
+                .body("body content")
                 .build()
                 .unwrap(),
         ))
@@ -34,9 +35,13 @@ mod integration_tests {
 
         let verification_result = verify_presentation(to_presentation(&web_proof_result)).unwrap();
 
-        assert_eq!(verification_result.sent, "GET /auth_header_require HTTP/1.1\r\nhost: lotr-api.online\r\naccept: */*\r\naccept-encoding: identity\r\nconnection: close\r\nuser-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36\r\nauthorization: s3cret_t0ken\r\n\r\n");
-        let regex = Regex::new("^HTTP/1\\.1 200 OK\\r\\nAccess-Control-Allow-Credentials: true\\r\\nVary: \\*\\r\\nAccess-Control-Allow-Origin: \\*\\r\\nAccess-Control-Allow-Methods: GET\\r\\nAccess-Control-Allow-Headers: host, accept, accept-encoding, connection, user-agent, authorization\\r\\nAccess-Control-Expose-Headers: host, accept, accept-encoding, connection, user-agent, authorization\\r\\nContent-Type: application/json;charset=utf-8\\r\\nDate: [A-Za-z]{3}, \\d{2} [A-Za-z]{3} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT\\r\\nContent-Length: \\d+\\r\\n\\r\\n\\{\"success\":true,\"name\":\"Tom Bombadil\",\"greeting\":\"Old Tom Bombadil is a merry fellow!\"}$");
-        assert!(regex.unwrap().is_match(&verification_result.recv));
+        assert_eq!(verification_result.sent, "GET /auth_header_require HTTP/1.1\r\nhost: lotr-api.online\r\naccept: */*\r\naccept-encoding: identity\r\nconnection: close\r\nuser-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36\r\nauthorization: s3cret_t0ken\r\ncontent-length: 12\r\n\r\nbody content");
+        let regex = Regex::new("^HTTP/1\\.1 200 OK\\r\\nAccess-Control-Allow-Credentials: true\\r\\nVary: \\*\\r\\nAccess-Control-Allow-Origin: \\*\\r\\nAccess-Control-Allow-Methods: GET\\r\\nAccess-Control-Allow-Headers: host, accept, accept-encoding, connection, user-agent, authorization, content-length\\r\\nAccess-Control-Expose-Headers: host, accept, accept-encoding, connection, user-agent, authorization, content-length\\r\\nContent-Type: application/json;charset=utf-8\\r\\nDate: [A-Za-z]{3}, \\d{2} [A-Za-z]{3} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT\\r\\nContent-Length: \\d+\\r\\n\\r\\n\\{\"success\":true,\"name\":\"Tom Bombadil\",\"greeting\":\"Old Tom Bombadil is a merry fellow!\"}$");
+        assert!(
+            regex.unwrap().is_match(&verification_result.recv,),
+            "Got response: {}",
+            verification_result.recv,
+        );
         assert_eq!(verification_result.server_name, ServerName::new("lotr-api.online".to_string()));
         assert_eq!(
             verification_result.key,
