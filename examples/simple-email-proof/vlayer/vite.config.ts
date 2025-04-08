e42b8e8c8a0f1035e143fd8ff534176ba6dee633
Jakub Konka <jakub@vlayer.xyz>
2025-04-08 11:22:53 +0200

    simple-email-proof: backport reown temp fix

--- examples/simple-email-proof/vlayer/vite.config.ts
+++ examples/simple-email-proof/vlayer/vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This is added as a temporary fix for the `process is not defined` issue
+  // (https://github.com/reown-com/appkit/issues/3926)
+  // appearing in our dependency: reown/appkit
+  define: {
+    "process.env": {},
+  },
 });
