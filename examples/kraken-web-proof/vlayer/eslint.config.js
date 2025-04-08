23af62a45d28908162ee5160cdce417376bea054
Artur Chmaro <artur@vlayer.xyz>
2025-04-08 10:55:03 +0200

    add eslint

--- examples/kraken-web-proof/vlayer/eslint.config.js
+++ examples/kraken-web-proof/vlayer/eslint.config.js
@@ -1,1 +1,23 @@
-../../../packages/eslint.config.js
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettierRecommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      curly: "error",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
