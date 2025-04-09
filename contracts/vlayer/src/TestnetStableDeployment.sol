842e4b2b7b81c177cfab6002d5a08ba878a492e1
rzadp <przemek@vlayer.xyz>
2025-04-09 15:38:49 +0200

    Deploy new contracts and re-enable stable deployment tests

--- contracts/vlayer/src/TestnetStableDeployment.sol
+++ contracts/vlayer/src/TestnetStableDeployment.sol
@@ -8,15 +8,15 @@ import {ProofVerifierRouter} from "./proof_verifier/ProofVerifierRouter.sol";
 
 library TestnetStableDeployment {
     function repository() internal pure returns (Repository) {
-        return Repository(address(0xc9708B07ae9906b92FF19281Fd660FB19206a8fA));
+        return Repository(address(0xc4E4dC291A5C4dEbe9Ff5a3372F3FdD2e42Bac86));
     }
 
     function verifiers() internal pure returns (FakeProofVerifier, Groth16ProofVerifier, ProofVerifierRouter) {
-        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x913649a62807b2B8144a1E4d41a0E1aEe10a3985));
+        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x1e93F0309c7e8173C7ABf80d64044214b4E25019));
         Groth16ProofVerifier groth16ProofVerifier =
-            Groth16ProofVerifier(address(0x41f78ef6625E119f328EC8a88D54f9da1c227E6A));
+            Groth16ProofVerifier(address(0x7E231CfC3e3B549633D5AD61C30f07Dd4d408ad3));
         ProofVerifierRouter proofVerifierRouter =
-            ProofVerifierRouter(address(0x17C9793523c11eb0b6Da1DaeD53839146277AB4e));
+            ProofVerifierRouter(address(0x1ADc13111ea64918639809d5cA90f042483f59F6));
 
         return (fakeProofVerifier, groth16ProofVerifier, proofVerifierRouter);
     }
