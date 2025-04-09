9e7aa33e9d63e6da83996062eda8d778d3a9028b
Leonid Logvinov <logvinov.leon@gmail.com>
2025-04-09 11:04:21 +0200

    Fix last versions

--- bash/test-release-local-prover.sh
+++ bash/test-release-local-prover.sh
@@ -25,7 +25,7 @@ curl -L https://risczero.com/install | bash
 export PATH="$PATH:~/.risc0/bin"
 export PATH="$PATH:~/.cargo/bin"
 rzup install r0vm 2.0.1
-rzup install rust 1.81.0
+rzup install rust 1.85.0
 rzup show
 echo "::endgroup::"
 
