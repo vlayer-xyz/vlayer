87537c27e36efafd265488ca6135f8bb5436b8d2
Anna Jabłonowska <ania@vlayer.xyz>
2025-04-10 16:31:38 +0200

    Use UI

--- examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
+++ examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
@@ -1,18 +1,26 @@
 import { FallbackProps } from "react-error-boundary";
 import { AppError } from "../../errors";
+import styles from "./ErrorBoundary.module.css";
 
 export const ErrorBoundaryComponent = ({ error }: FallbackProps) => {
   const errorMsg =
     error instanceof AppError ? error.message : "Something went wrong";
 
   return (
-    <div
-      data-testid="Error display"
-      role="alert"
-      className="alert alert-error m-2"
-      style={{ width: "300px" }}
-    >
-      <span>{errorMsg}</span>
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <img src="../../../error-illustration.png" />
+      </div>
+      <div className={styles.errorMsg}>{errorMsg}</div>
+      <div className={styles.additionalText}>
+        Click the button below to refresh
+      </div>
+      <button
+        className={styles.button}
+        onClick={() => window.location.reload()}
+      >
+        Refresh
+      </button>
     </div>
   );
 };
