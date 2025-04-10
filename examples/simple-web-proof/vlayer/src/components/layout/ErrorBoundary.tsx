bc59dcd4309781efe8caabfa024884b0a4bb6d0e
Anna Jabłonowska <ania@vlayer.xyz>
2025-04-10 17:29:46 +0200

    Add AppErrorBoundaryComponent

--- examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
+++ examples/simple-web-proof/vlayer/src/components/layout/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
 import { FallbackProps } from "react-error-boundary";
 import { AppError } from "../../errors";
 import styles from "./ErrorBoundary.module.css";
+import { useNavigate } from "react-router";
 
 export const StepErrorBoundaryComponent = ({ error }: FallbackProps) => {
   const errorMsg =
@@ -13,7 +14,7 @@ export const StepErrorBoundaryComponent = ({ error }: FallbackProps) => {
       </div>
       <div className={styles.errorMsg}>{errorMsg}</div>
       <div className={styles.additionalText}>
-        Click the button below to refresh
+        Click the button below to refresh.
       </div>
       <button
         className={styles.button}
@@ -21,6 +22,39 @@ export const StepErrorBoundaryComponent = ({ error }: FallbackProps) => {
       >
         Refresh
       </button>
+    </div>
+  );
+};
+
+export const AppErrorBoundaryComponent = ({ error }: FallbackProps) => {
+  console.error(error.message);
+
+  const errorMsg =
+    error instanceof AppError ? error.message : "Something went wrong";
+
+  const navigate = useNavigate();
+
+  const handleStartAgain = () => {
+    navigate("/");
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.background}>
+      <div className="modal-box bg-white rounded-2xl items-center justify-center">
+        <div className={styles.errorScreen}>
+          <div className={styles.image}>
+            <img src="../../../error-illustration.png" />
+          </div>
+          <div className={styles.errorMsg}>{errorMsg}</div>
+          <div className={styles.additionalText}>
+            Click the button below or try again later.
+          </div>
+          <button className={styles.button} onClick={handleStartAgain}>
+            Start again
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
