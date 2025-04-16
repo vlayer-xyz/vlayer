7a86ce8545d0a32dcadf6c065b369eec9c067e30
Adam Król <adam.dawid.krol@gmail.com>
2025-04-16 13:46:15 +0200

    Update Precompiles comments

--- contracts/vlayer/src/TestnetStableDeployment.sol
+++ contracts/vlayer/src/TestnetStableDeployment.sol
@@ -12,11 +12,11 @@ library TestnetStableDeployment {
     }
 
     function verifiers() internal pure returns (FakeProofVerifier, Groth16ProofVerifier, ProofVerifierRouter) {
-        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x1A1649F5F5F8C439c43455C16375e84A44E3Cb70));
+        FakeProofVerifier fakeProofVerifier = FakeProofVerifier(address(0x2a1e28c0B20fd17004c0BB773E445C3016bdc18b));
         Groth16ProofVerifier groth16ProofVerifier =
             Groth16ProofVerifier(address(0x7E231CfC3e3B549633D5AD61C30f07Dd4d408ad3));
         ProofVerifierRouter proofVerifierRouter =
-            ProofVerifierRouter(address(0x8d3ca8D1953C3E377F7A731cd563a2B30B0BD359));
+            ProofVerifierRouter(address(0x88E86dfC00ACcDbD80BDDa272cd13a9dC137Dcd6));
 
         return (fakeProofVerifier, groth16ProofVerifier, proofVerifierRouter);
     }
