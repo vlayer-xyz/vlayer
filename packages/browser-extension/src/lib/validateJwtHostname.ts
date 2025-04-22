a2ebdd2bb23cc3da9938cf806c63e5ec7d784d20
Jakub Konka <jakub@vlayer.xyz>
2025-04-22 16:28:42 +0200

    browser-extension: fix validating hostname in JWT

--- packages/browser-extension/src/lib/validateJwtHostname.ts
+++ packages/browser-extension/src/lib/validateJwtHostname.ts
@@ -1,9 +1,14 @@
 import { type Claims } from "./types/jwt";
-import { JwtInvalidHostname } from "./errors";
+import { JwtInvalidHostname, JwtMissingHostname } from "./errors";
 
 export function validateJwtHostname(claims: Claims, expected: string): string {
+  if (claims.host === undefined) {
+    throw new JwtMissingHostname(expected);
+  }
+
   if (claims.host !== expected) {
     throw new JwtInvalidHostname(claims.host, expected);
   }
+
   return claims.host;
 }
