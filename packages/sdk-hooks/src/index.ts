83668effe881d1832cbecb7ec1b75bb47c7d0d48
Anna Jabłonowska <ania@vlayer.xyz>
2025-04-28 14:04:05 +0200

    Use useChain in app

--- packages/sdk-hooks/src/index.ts
+++ packages/sdk-hooks/src/index.ts
@@ -1,7 +1,14 @@
 import { useWebProof } from "./useWebproof/useWebProof";
 import { ProofProvider } from "./context";
 import { useCallProver } from "./useCallProver/useCallProver";
+import { useChain } from "./useChain/useChain";
 import { useWaitForProvingResult } from "./useWaitForProvingResult/useWaitForProvingResult";
 
-export { ProofProvider, useWebProof, useCallProver, useWaitForProvingResult };
+export {
+  ProofProvider,
+  useWebProof,
+  useCallProver,
+  useWaitForProvingResult,
+  useChain,
+};
 export * from "./types";
