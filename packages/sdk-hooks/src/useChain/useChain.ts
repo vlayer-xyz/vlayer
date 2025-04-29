96fae8746806c3f342f1c664bddd1e6099bfbba8
Arkadiusz Konior <akonior@gmail.com>
2025-04-29 09:27:52 +0200

    feat: introduce useChain hook (#2204)
    Co-authored-by: Anna Jabłonowska <ania@vlayer.xyz>

---
+++ packages/sdk-hooks/src/useChain/useChain.ts
@@ -0,0 +1,53 @@
+import { getChainSpecs } from "@vlayer/sdk/config";
+import { useEffect, useMemo, useState } from "react";
+import type { Chain } from "viem";
+import { useChainId, useChains } from "wagmi";
+
+export const useChain = (
+  configChain: string | undefined,
+): { chain: Chain | null; error: string | null } => {
+  const [chain, setChain] = useState<Chain | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const wagmiChainId = useChainId();
+  const wagmiChains = useChains();
+
+  const wagmiChain = useMemo(() => {
+    return wagmiChains
+      .find((chain) => chain.id === wagmiChainId)
+      ?.name.toLowerCase();
+  }, [wagmiChainId, wagmiChains]);
+
+  useEffect(() => {
+    if (!configChain) {
+      setChain(null);
+      setError(`Env chain ${configChain} not found`);
+      return;
+    }
+
+    try {
+      const chain = getChainSpecs(configChain);
+
+      if (!chain) {
+        setChain(null);
+        setError(`Chain ${configChain} is not supported`);
+        return;
+      }
+
+      if (wagmiChain === configChain) {
+        setChain(chain);
+        setError(null);
+      } else {
+        setChain(null);
+        setError(
+          `Chains mismatched. Wallet chain: ${wagmiChain} is not equal to env chain: ${configChain}`,
+        );
+      }
+    } catch {
+      setChain(null);
+      setError(`Chain ${configChain} is not supported`);
+    }
+  }, [wagmiChain, configChain]);
+
+  return { chain, error };
+};
