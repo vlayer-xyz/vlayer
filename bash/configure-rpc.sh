aecb24d915bf228b06ed0a6fcfccce4debb06ac2
Adam Król <adam.dawid.krol@gmail.com>
2025-04-30 14:09:22 +0200

    Rename `configure_rpc` to `configure-rpc`

--- bash/configure-rpc.sh
+++ bash/configure-rpc.sh
@@ -15,23 +15,23 @@ echo "::add-mask::$QK_KEY"
 case "$CHAIN_NAME" in
   optimismSepolia)
     FRAGMENT="optimism-sepolia"
-    ;;  
+    ;;
   sepolia)
     FRAGMENT="ethereum-sepolia"
     # override with the known Sepolia API key
     QK_KEY="7c1391dd1fa23de1a735fa4fae2565cc0cc36639"
-    ;;  
+    ;;
   *)
     # Fallback: convert camelCase -> kebab-case
     FRAGMENT=$(echo "$CHAIN_NAME" \
-      | sed -E 's/([a-z0-9])([A-Z])/[\\1-\\L\\2]/g' \
+      | sed -E 's/([a-z0-9])([A-Z])/\1-\L\2/g' \
       | tr '[:upper:]' '[:lower:]')
     ;;
 esac
 
-# Export variables for subsequent GitHub Actions steps
-# GITHUB_ENV is automatically provided in GitHub Actions
-
-echo "QUICKNODE_ENDPOINT=dry-alpha-tab" >> "$GITHUB_ENV"
-echo "QUICKNODE_API_KEY=$QK_KEY"    >> "$GITHUB_ENV"
-echo "JSON_RPC_URL=https://dry-alpha-tab.${FRAGMENT}.quiknode.pro/$QK_KEY" >> "$GITHUB_ENV"
+# Export variables for subsequent GitHub Actions steps in one grouped write
+{
+  echo "QUICKNODE_ENDPOINT=dry-alpha-tab"
+  echo "QUICKNODE_API_KEY=$QK_KEY"
+  echo "JSON_RPC_URL=https://dry-alpha-tab.${FRAGMENT}.quiknode.pro/$QK_KEY"
+} >> "$GITHUB_ENV"
