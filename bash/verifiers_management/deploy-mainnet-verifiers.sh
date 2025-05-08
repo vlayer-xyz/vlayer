25ed6231c1a74977c9f7e50a9d08bd645c6c7d88
rzadp <przemek@vlayer.xyz>
2025-05-08 14:40:42 +0200

    discard duplicate test

--- bash/verifiers_management/deploy-mainnet-verifiers.sh
+++ bash/verifiers_management/deploy-mainnet-verifiers.sh
@@ -16,26 +16,8 @@ function deploy_contracts(){
   done
 }
 
-# function verify_deployment_is_stable_across_networks(){
-#   local stable_deployment=$(cat deployed_contracts.json | jq -r)
-#   for network_txs in $(find "${CONTRACTS_DIR}/broadcast/${DEPLOYER_SCRIPT}" -type f -name "run-latest.json") ; do
-#     result=$(jq <"${network_txs}" -r '{contracts: .transactions | map({contractName: .contractName, contractAddress: .contractAddress})}')
-#     if [[ "${stable_deployment}" != "${result}" ]] ; then
-#       echo "Deployments are not consistent ❌" >&2
-#       echo "--------------------------" >&2
-#       echo "${stable_deployment}" | jq  >&2
-#       echo "--------------------------" >&2
-#       echo "${result}" | jq >&2
-#       echo "--------------------------" >&2
-#       exit 1
-#     fi
-#   done
-#   echo "Deployments has been verified ✅"
-# }
-
 cd "${CONTRACTS_DIR}"
 
 cleanup
 build_contracts
 deploy_contracts
-# verify_deployment_is_stable_across_networks
