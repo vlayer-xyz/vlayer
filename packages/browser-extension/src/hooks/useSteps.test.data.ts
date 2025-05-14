35ff6b38d1d1833869c6509df74470ac8fd32bbe
Piotr Żelazko <piotr@vlayer.xyz>
2025-05-14 15:29:13 +0200

    test: add redirect step state tests

--- packages/browser-extension/src/hooks/useSteps.test.data.ts
+++ packages/browser-extension/src/hooks/useSteps.test.data.ts
@@ -9,6 +9,11 @@ export const steps = [
     step: "startPage",
   },
   {
+    url: "https://example.com/redirect",
+    label: "Redirect Page",
+    step: "redirect",
+  },
+  {
     url: "https://example.com/*/expect",
     label: "Expect URL",
     step: "expectUrl",
@@ -27,7 +32,12 @@ export const testData = [
       history: [] as BrowsingHistoryItem[],
       id: "Empty history",
     },
-    output: [StepStatus.Current, StepStatus.Further, StepStatus.Further],
+    output: [
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
@@ -35,29 +45,60 @@ export const testData = [
       history: [
         { url: "https://example.com/start", ready: true },
       ] as BrowsingHistoryItem[],
-      id: "Start page visited ",
+      id: "Start page visited",
     },
-    output: [StepStatus.Completed, StepStatus.Current, StepStatus.Further],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
       isZkProvingDone: false,
       history: [
-        { url: "https://example.com/expect", ready: true },
+        { url: "https://example.com/start", ready: true },
+        { url: "https://example.com/redirect", ready: true },
       ] as BrowsingHistoryItem[],
-      id: "Expect page visited without visiting start page ",
+      id: "Redirect page visited and start page visited",
     },
-    output: [StepStatus.Current, StepStatus.Further, StepStatus.Further],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Current,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
-      isZkProvingDone: true,
+      isZkProvingDone: false,
       history: [
-        { url: "https://example.com/notarize", ready: true },
+        { url: "https://example.com/path/expect", ready: true },
       ] as BrowsingHistoryItem[],
-      id: "Notarize page visited and proof is in place but without visiting start page ",
+      id: "Expect page visited without visiting start page",
     },
-    output: [StepStatus.Current, StepStatus.Further, StepStatus.Further],
+    output: [
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
+  },
+  {
+    input: {
+      isZkProvingDone: false,
+      history: [
+        { url: "https://example.com/redirect", ready: true },
+      ] as BrowsingHistoryItem[],
+      id: "Redirect page visited without visiting start page",
+    },
+    output: [
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
@@ -66,43 +107,98 @@ export const testData = [
         { url: "https://example.com/start", ready: true },
         { url: "https://example.com/path/expect", ready: true },
       ] as BrowsingHistoryItem[],
-      id: "Expect page visited and start page visited ",
+      id: "Expect page visited without visiting redirect page",
     },
-    output: [StepStatus.Completed, StepStatus.Completed, StepStatus.Further],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
+  },
+  {
+    input: {
+      isZkProvingDone: true,
+      history: [
+        { url: "https://example.com/notarize", ready: true },
+      ] as BrowsingHistoryItem[],
+      id: "Notarize page visited and proof is in place but without visiting start page",
+    },
+    output: [
+      StepStatus.Current,
+      StepStatus.Further,
+      StepStatus.Further,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
       isZkProvingDone: false,
       history: [
         { url: "https://example.com/start", ready: true },
+        { url: "https://example.com/redirect", ready: true },
+        { url: "https://example.com/path/expect", ready: true },
+      ] as BrowsingHistoryItem[],
+      id: "Expect page visited, redirect page visited and start page visited",
+    },
+    output: [
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Further,
+    ],
+  },
+  {
+    input: {
+      isZkProvingDone: false,
+      history: [
+        { url: "https://example.com/start", ready: true },
+        { url: "https://example.com/redirect", ready: true },
         { url: "https://example.com/path/expect", ready: false },
       ] as BrowsingHistoryItem[],
-      id: "Expect page visited ( no cookies) and start page visited ",
+      id: "Expect page visited ( no cookies), redirect page visited and start page visited",
     },
-    output: [StepStatus.Completed, StepStatus.Completed, StepStatus.Further],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Further,
+    ],
   },
   {
     input: {
       isZkProvingDone: false,
       history: [
         { url: "https://example.com/start", ready: true },
+        { url: "https://example.com/redirect", ready: true },
         { url: "https://example.com/htap/expect", ready: true },
         { url: "https://example.com/notarize", ready: true },
       ] as BrowsingHistoryItem[],
       id: "All pages visited but no proof",
     },
-    output: [StepStatus.Completed, StepStatus.Completed, StepStatus.Current],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Current,
+    ],
   },
   {
     input: {
       isZkProvingDone: true,
       history: [
         { url: "https://example.com/start", ready: true },
+        { url: "https://example.com/redirect", ready: true },
         { url: "https://example.com/htap/expect", ready: true },
         { url: "http://example.com/notarize", ready: true },
       ] as BrowsingHistoryItem[],
       id: "All data in place",
     },
-    output: [StepStatus.Completed, StepStatus.Completed, StepStatus.Completed],
+    output: [
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Completed,
+      StepStatus.Completed,
+    ],
   },
 ];
