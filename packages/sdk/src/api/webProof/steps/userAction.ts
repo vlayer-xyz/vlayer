23a35e826123a0b87e94df3eef8dc72393f8c1d1
vanruch <ivanruch@gmail.com>
2025-05-19 10:41:21 +0200

    add action to userAction

--- packages/sdk/src/api/webProof/steps/userAction.ts
+++ packages/sdk/src/api/webProof/steps/userAction.ts
@@ -7,12 +7,17 @@ export const userAction = (
   url: string,
   label: string,
   text: string,
+  action: {
+    selector: string;
+    expected: string | boolean;
+  },
   image?: string,
 ) => {
   return {
     url,
     label,
     text,
+    action,
     image,
     step: EXTENSION_STEP.userAction,
   } as WebProofStepUserAction;
