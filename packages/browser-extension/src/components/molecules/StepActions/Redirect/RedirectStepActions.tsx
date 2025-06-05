827a5a7baeee87470fbf2ee11b9b089e24ca9763
Anna Jabłonowska <ania@vlayer.xyz>
2025-06-05 15:54:05 +0200

    Fix redirect step

--- packages/browser-extension/src/components/molecules/StepActions/Redirect/RedirectStepActions.tsx
+++ packages/browser-extension/src/components/molecules/StepActions/Redirect/RedirectStepActions.tsx
@@ -1,11 +1,20 @@
 import React, { FC, useEffect } from "react";
 import { StepStatus } from "constants/step";
+import browser from "webextension-polyfill";
 
 type RedirectStepActionProps = {
   isVisited: boolean;
   link: string;
   status: StepStatus;
   buttonText: string;
+};
+
+const redirect = async (link: string): Promise<void> => {
+  const tabs = await browser.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  await browser.tabs.update(tabs[0].id, { url: link });
 };
 
 export const RedirectStepActions: FC<RedirectStepActionProps> = ({
@@ -15,7 +24,9 @@ export const RedirectStepActions: FC<RedirectStepActionProps> = ({
 }) => {
   useEffect(() => {
     if (!isVisited && status == StepStatus.Current) {
-      window.location.href = link;
+      redirect(link).catch((error) => {
+        console.error("Error during redirecting:", error);
+      });
     }
   }, [isVisited, link, status]);
 
