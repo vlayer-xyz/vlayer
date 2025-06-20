66b1bd291e31ca4e6cd784f2413bc56a0fa5a27f
Anna Jabłonowska <ania@vlayer.xyz>
2025-06-20 12:42:06 +0200

    Update after packages removal

---
+++ bash/download-vouch-extension.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+VLAYER_HOME=$(git rev-parse --show-toplevel)
+
+source "$(dirname "${BASH_SOURCE[0]}")/lib/colors.sh"
+
+echo_color BLUE "Reading VOUCH_EXTENSION_DOWNLOAD_PAT from 1Password"
+VOUCH_EXTENSION_DOWNLOAD_PAT=$(op read op://Vouch-Engineering/GITHUB_VOUCH_PAT/credential)
+
+echo_color BLUE "Creating directory for browser extension"
+cd "${VLAYER_HOME}/packages"
+mkdir -p browser-extension
+cd browser-extension
+
+echo_color BLUE "Downloading latest browser extension release from GitHub"
+RELEASE_JSON=$(curl -s -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${VOUCH_EXTENSION_DOWNLOAD_PAT}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/vlayer-xyz/vouch/releases/tags/browser-extension-latest")
+
+ASSET_URL=$(echo "$RELEASE_JSON" | jq -r '.assets[] | select(.name == "browser-extension.tar.gz") | .url')
+echo_color YELLOW "Asset URL: $ASSET_URL"
+
+curl -L \
+    -H "Accept: application/octet-stream" \
+    -H "Authorization: Bearer ${VOUCH_EXTENSION_DOWNLOAD_PAT}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "$ASSET_URL" \
+    --output browser-extension.tar.gz
+
+echo_color BLUE "Extracting browser extension files to dist directory"
+mkdir -p dist
+tar -xzf browser-extension.tar.gz -C dist --strip-components=4
+rm browser-extension.tar.gz
+
+echo_color GREEN "Browser extension downloaded successfully to ${VLAYER_HOME}/packages/browser-extension/dist"
---
+++ packages/browser-extension/README.md
@@ -0,0 +1,9 @@
+
+# Browser-extension package was moved to Vouch
+
+To download the latest extension release from vouch GitHub:
+
+1. [Setup your 1Password CLI](https://developer.1password.com/docs/cli/get-started/#step-1-install-1password-cli)  
+    You need to have access in your 1Password to Personal Access Token for downloading release from vouch GitHub repository.
+2. Run `bash/download-vouch-extension.sh` script.  
+    It will use the PAT from your 1Password account to download latest release of vouch extension and extract it into dist directory here.
