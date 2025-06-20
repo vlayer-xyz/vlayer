eed1db281a75311487cf415cfeee8950af53784e
Anna Jabłonowska <ania@vlayer.xyz>
2025-06-20 16:13:33 +0200

    Update script

--- bash/download-vouch-extension.sh
+++ bash/download-vouch-extension.sh
@@ -6,6 +6,42 @@ VLAYER_HOME=$(git rev-parse --show-toplevel)
 
 source "$(dirname "${BASH_SOURCE[0]}")/lib/colors.sh"
 
+usage() {
+    echo_color YELLOW "Usage: $0 [OPTIONS]"
+    echo_color YELLOW "Options:"
+    echo_color YELLOW " --help      Display this help message"
+    echo_color YELLOW " --nightly   Download the browser extension version from vouch repo (default)"
+    echo_color YELLOW " --latest    Download the browser extension version from the latest release to Chrome web store"
+}
+
+handle_options() {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            --help)
+                usage
+                exit 0
+                ;;
+            --latest)
+                version="latest"
+                echo_color RED "Downloading the latest version from Chrome web store is not supported yet."
+                exit 1
+                ;;
+            --nightly)
+                version="nightly"
+                ;;
+            *)
+                echo_color RED "Invalid option: $1" >&2
+                usage
+                exit 1
+                ;;
+        esac
+        shift
+    done
+}
+
+version="nightly"
+handle_options "$@"
+
 echo_color BLUE "Reading VOUCH_EXTENSION_DOWNLOAD_PAT from 1Password"
 VOUCH_EXTENSION_DOWNLOAD_PAT=$(op read op://Vouch-Engineering/GITHUB_VOUCH_PAT/credential)
 
@@ -14,12 +50,12 @@ cd "${VLAYER_HOME}/packages"
 mkdir -p browser-extension
 cd browser-extension
 
-echo_color BLUE "Downloading latest browser extension release from GitHub"
+echo_color BLUE "Downloading browser extension release from GitHub (${version})"
 RELEASE_JSON=$(curl -s -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${VOUCH_EXTENSION_DOWNLOAD_PAT}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/vlayer-xyz/vouch/releases/tags/browser-extension-nightly")
+    "https://api.github.com/repos/vlayer-xyz/vouch/releases/tags/browser-extension-${version}")
 
 ASSET_URL=$(echo "$RELEASE_JSON" | jq -r '.assets[] | select(.name == "browser-extension.tar.gz") | .url')
 echo_color YELLOW "Asset URL: $ASSET_URL"
