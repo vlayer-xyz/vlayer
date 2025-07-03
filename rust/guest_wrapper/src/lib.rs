da4e66bc6b92c8f80f49d5548158b7eee078f7b5
Adam Król <adam.dawid.krol@gmail.com>
2025-07-03 12:18:16 +0200

    Make `to_u32_array` function generic with input and output size and test it

--- rust/guest_wrapper/src/lib.rs
+++ rust/guest_wrapper/src/lib.rs
@@ -21,15 +21,20 @@ pub static CHAIN_GUEST_ELF_WITH_CANONICAL_ID: GuestElf =
     GuestElf::new(to_u32_array(CHAIN_GUEST_IDS[0]), private::RISC0_CHAIN_GUEST_ELF);
 
 #[allow(unused)]
-const fn to_u32_array(bytes: [u8; 32]) -> [u32; 8] {
-    let mut arr = [0_u32; 8];
+const fn to_u32_array<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize>(
+    bytes: [u8; INPUT_SIZE],
+) -> [u32; OUTPUT_SIZE] {
+    const STRIDE: usize = u32::BITS as usize / 8; // 4
+    assert!(INPUT_SIZE % STRIDE == 0, "INPUT_SIZE has to be a multiple of 4 (u32::BITS/8)");
+    assert!(INPUT_SIZE / STRIDE == OUTPUT_SIZE, "INPUT_SIZE / 4 must equal OUTPUT_SIZE");
+    let mut arr = [0_u32; OUTPUT_SIZE];
     let mut i = 0;
-    while i < 8 {
+    while i < OUTPUT_SIZE {
         arr[i] = u32::from_le_bytes([
-            bytes[i * 4],
-            bytes[i * 4 + 1],
-            bytes[i * 4 + 2],
-            bytes[i * 4 + 3],
+            bytes[i * STRIDE],
+            bytes[i * STRIDE + 1],
+            bytes[i * STRIDE + 2],
+            bytes[i * STRIDE + 3],
         ]);
         i += 1;
     }
@@ -44,3 +49,28 @@ pub static CHAIN_GUEST_ELF: GuestElf = GuestElf::default();
 
 #[cfg(clippy)]
 pub static CHAIN_GUEST_ELF_WITH_CANONICAL_ID: GuestElf = GuestElf::default();
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_u32_array() {
+        let input = [1, 0, 0, 0, 2, 0, 0, 0];
+        assert_eq!(to_u32_array(input), [1, 2]);
+    }
+
+    #[test]
+    #[should_panic(expected = "INPUT_SIZE has to be a multiple of 4 (u32::BITS/8)")]
+    const fn test_to_u32_array_invalid_length() {
+        let input = [0];
+        to_u32_array::<1, 1>(input);
+    }
+
+    #[test]
+    #[should_panic(expected = "INPUT_SIZE / 4 must equal OUTPUT_SIZE")]
+    const fn test_to_u32_array_invalid_length_2() {
+        let input = [0, 0, 0, 0];
+        to_u32_array::<4, 2>(input);
+    }
+}
