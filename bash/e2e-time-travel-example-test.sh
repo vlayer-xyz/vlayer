ceac9bd5d0f9c2f9d7cc63f0f35f832cc05ba1f1
rzadp <przemek@vlayer.xyz>
2025-08-05 09:55:55 +0200

    Remove unnecessary stuff in scripts run only against vercel

--- bash/e2e-time-travel-example-test.sh
+++ bash/e2e-time-travel-example-test.sh
@@ -3,27 +3,6 @@
 set -ueo pipefail
 
 VLAYER_HOME=$(git rev-parse --show-toplevel)
-source "$(dirname "${BASH_SOURCE[0]}")/lib/proving_mode.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/lib/utils.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/lib/e2e.sh"
 
-set_proving_mode
-
-generate_ts_bindings
-
-if [[ -z "${WEB_SERVER_URL:-}" ]]; then
-  echo '::group::Running services'
-  source ${VLAYER_HOME}/bash/run-services.sh
-  echo '::endgroup::'
-else
-  echo "Using remote web server at: ${WEB_SERVER_URL}"
-  echo "Skipping local service setup"
-fi
-
 run_web_tests simple-time-travel
-
-if [[ -z "${WEB_SERVER_URL:-}" ]]; then
-  echo '::group::Cleanup'
-  cleanup
-  echo '::endgroup::'
-fi
