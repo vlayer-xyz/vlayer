24d3a8f9b740348dc1be276d2e52f0132af16b10
wgromniak2 <wiktor@vlayer.xyz>
2025-08-13 13:38:15 +0200

    chore: upgrade tlsn to alpha.12 (#2595)

--- packages/sdk/src/api/webProof/providers/extension.ts
+++ packages/sdk/src/api/webProof/providers/extension.ts
@@ -157,7 +157,7 @@ export const validateWebProofRequest = (
 };
 
 export const createExtensionWebProofProvider = ({
-  notaryUrl = "https://test-notary.vlayer.xyz/v0.1.0-alpha.11",
+  notaryUrl = "https://test-notary.vlayer.xyz/v0.1.0-alpha.12",
   wsProxyUrl = "wss://test-wsproxy.vlayer.xyz",
   token,
 }: WebProofProviderSetup = {}): WebProofProvider => {
